4273e51382bac5509bd96b082c691ea7ea7c2928
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-09-06 15:42:06 +0200

    Fix configuration error

--- myfreemp3api/myfreemp3_scrapper/configuration.py
+++ myfreemp3api/myfreemp3_scrapper/configuration.py
@@ -2,6 +2,8 @@
 
 configuration = {
     "postUrl" : 'https://myfreemp3juices.cc/api/search.php?callback=jQuery21307552220673040206_1662375436837' + 'search.json?page={}&page_size={}&search_term=a',
+
+    "dataFieldName" : "response",
     "titleFieldName" : "title",
     "artistFieldName" : "artist",
     "durationFieldName" : "duration"
