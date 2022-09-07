b7f586f18e664969d0d38956cab2c996a885d155
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-09-07 13:31:45 +0200

    Modify json response to be an array

--- myfreemp3api/myfreemp3_scrapper/scrapper.py
+++ myfreemp3api/myfreemp3_scrapper/scrapper.py
@@ -35,12 +35,12 @@ def getSongsFromMyfreemp3Json (json):
     return songs
 
 def getJsonTextFromSongs (songs):
-    songsJsonText = "{\"" + api_cfg.configuration["dataFieldName"] + "\": ["
+    songsJsonText = "["
     firstSong = True
     for song in songs:
         if firstSong: firstSong = False
         else: songsJsonText += ", "
         songsJsonText += json.dumps(song.__dict__)
-    songsJsonText += "]}"
+    songsJsonText += "]"
     return songsJsonText
     
