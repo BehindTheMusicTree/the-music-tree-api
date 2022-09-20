d29ee65d50e3acdd03d76220f7f7a5a3986819eb
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-09-20 11:57:00 +0200

    Return token when logged in successfully

--- myfreemp3api/api/views.py
+++ myfreemp3api/api/views.py
@@ -1,19 +1,32 @@
 from django.contrib.auth.models import User, Group
-from myfreemp3api.api.models import *
-from myfreemp3api.api.controller import user_creation_controller
-from rest_framework import viewsets
-from .serializers import *
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
-from myfreemp3api.myfreemp3_scrapper import scrapper
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from .serializers import *
+
+from myfreemp3api.api.models import *
+from myfreemp3api.api.controller import user_creation_controller
 import myfreemp3api.api.configuration as api_cfg
+from myfreemp3api.myfreemp3_scrapper import scrapper
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 @csrf_exempt
+@api_view(['POST'])
 def UserCreationView(request):
     if (request.method == "POST"):
         name = request.POST['name']
@@ -21,11 +34,10 @@ def UserCreationView(request):
         password = request.POST['password']
         userId = user_creation_controller.CreateUser(name, email, password)
         return HttpResponseRedirect(str(userId))
-    
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
 
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def SongView(request):
     if (request.method == "GET"):
         queryParameterName = api_cfg.configuration["query"]
--- myfreemp3api/urls.py
+++ myfreemp3api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import include, path
+
 from rest_framework import routers
+from rest_framework.authtoken import views as authtoken_views
+
 from myfreemp3api.api import views
+
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -8,7 +12,9 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('users/login', authtoken_views.obtain_auth_token),
+    path('users', include('django.contrib.auth.urls')),
     path('users/create', views.UserCreationView),
-    path('songs/', views.SongView),
+
+    path('songs/', views.SongView)
 ]
