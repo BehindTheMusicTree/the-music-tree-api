295c67f3ac503b9551eb93e827a73a9ed7b07eee
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-09-21 17:34:56 +0200

    Make download URL more Restful

--- myfreemp3api/urls.py
+++ myfreemp3api/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from myfreemp3api.api.views import views
+from myfreemp3api.api import views
 
 
 router = routers.DefaultRouter()
@@ -19,5 +19,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/create/', views.UserCreationView),
-    path('song_download/search/', views.SongDownloadView.as_view()),
+    path('songs/external/', views.SongsExternalView.as_view()),
 ]
