6a23cf2af3955025bed875594739482387a28967
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-09-24 10:09:22 +0200

    Add log but doesnt know if works

--- myfreemp3api/api/configuration.py
+++ myfreemp3api/api/configuration.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 configuration = {    
+    "logFolderBase" : "/var/log/447bpm/",
+    "logFolderMyfreemp3" : "myfreemp3/",
+
     "dataFieldName" : "data",
     "titleFieldName" : "title",
     "artistFieldName" : "artist",
--- myfreemp3api/myfreemp3_scrapper/scrapper.py
+++ myfreemp3api/myfreemp3_scrapper/scrapper.py
@@ -1,21 +1,39 @@
-from pickle import FALSE, TRUE
+from datetime import datetime
+import os
+import os.path
+from os import path
 import requests
+import logging
 import json
+
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+import http.client as http_client
+
 import myfreemp3api.myfreemp3_scrapper.configuration as myfreemp3_cfg
 import myfreemp3api.api.configuration as api_cfg
 
 from myfreemp3api.modele.song import Song
 
 def scrap (search):
+
+    http_client.HTTPConnection.debuglevel = 1
+    
+    # You must initialize logging, otherwise you'll not see debug output.
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
   
-    # data to be sent to api
-    data = {
+    dataToSend = {
         'q': search,
         'page':'0'
     }
     
-    # sending post request and saving response as response object
-    responseText = requests.post(url = myfreemp3_cfg.configuration["postUrl"], data = data).text
+    responseText = requests.post(url = myfreemp3_cfg.configuration["postUrl"], data = dataToSend).text
+    logResponseText(responseText)
 
     myfreemp3songsJsonText = "{" + responseText.split("{",2)[2]
     myfreemp3songsJsonText = myfreemp3songsJsonText[:len(myfreemp3songsJsonText) - 4]
@@ -36,6 +54,17 @@ def getSongsFromMyfreemp3Json (dataDict):
                 , songJson[myfreemp3_cfg.configuration["dateFieldName"]]
                 , songJson[myfreemp3_cfg.configuration["urlFieldName"]]))
     return songs
+
+def logResponseText (responseText):
+    myfreemp3LogFolderPath = api_cfg.configuration["logFolderBase"] + api_cfg.configuration["logFolderMyfreemp3"]
+    
+    if not path.exists(myfreemp3LogFolderPath):
+        os.makedirs(myfreemp3LogFolderPath)
+
+    f = open(myfreemp3LogFolderPath + datetime.now().strftime("%y-%m-%d %H%M%S") + ".txt", "x")
+    f.write(responseText)
+    f.close()
+
 
 def getJsonTextFromSongs (songs):
     songsJsonText = "["
