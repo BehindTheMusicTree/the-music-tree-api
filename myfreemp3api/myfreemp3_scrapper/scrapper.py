ed3d185c8b67221bbcfe2cbf493306906a747547
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-10-09 09:29:14 +0200

    Improve scrapper logging

--- myfreemp3api/myfreemp3_scrapper/scrapper.py
+++ myfreemp3api/myfreemp3_scrapper/scrapper.py
@@ -3,30 +3,16 @@ import os
 import os.path
 from os import path
 import requests
-import logging
 import json
-
-# These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# The only thing missing will be the response.body which is not logged.
-import http.client as http_client
 
 import myfreemp3api.myfreemp3_scrapper.configuration as myfreemp3_cfg
 import myfreemp3api.api.configuration as api_cfg
+import myfreemp3api.settings as settings
 
 from myfreemp3api.modele.song import Song
 
 def scrap (search):
 
-    http_client.HTTPConnection.debuglevel = 1
-    
-    # You must initialize logging, otherwise you'll not see debug output.
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
-  
     dataToSend = {
         'q': search,
         'page':'0'
@@ -56,15 +42,14 @@ def getSongsFromMyfreemp3Json (dataDict):
     return songs
 
 def logResponseText (responseText):
-    myfreemp3LogFolderPath = api_cfg.configuration["logFolderBase"] + api_cfg.configuration["logFolderMyfreemp3"]
+    myfreemp3ScrapperLogFolderPath = settings.MYFREEMP3_SCRAPPER_LOG_FOLDER_PATH
     
-    if not path.exists(myfreemp3LogFolderPath):
-        os.makedirs(myfreemp3LogFolderPath)
+    if not path.exists(myfreemp3ScrapperLogFolderPath):
+        os.makedirs(myfreemp3ScrapperLogFolderPath)
 
-    f = open(myfreemp3LogFolderPath + datetime.now().strftime("%y-%m-%d %H%M%S") + ".txt", "x")
+    f = open(myfreemp3ScrapperLogFolderPath + datetime.now().strftime("%y-%m-%d %H%M%S") + ".txt", "x")
     f.write(responseText)
     f.close()
-
 
 def getJsonTextFromSongs (songs):
     songsJsonText = "["
