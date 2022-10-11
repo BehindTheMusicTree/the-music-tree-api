234b3b4d6a784cd465915719dfd79230e719b9a4
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-10-11 18:53:00 +0200

    Add tags to songdb

--- myfreemp3api/models.py
+++ myfreemp3api/models.py
@@ -4,4 +4,10 @@ from django.contrib.auth.models import User
 class SongDB(models.Model):
     path = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, default=None, blank=True, null=True)
+    artist = models.CharField(max_length=200, default=None, blank=True, null=True)
+    album = models.CharField(max_length=200, default=None, blank=True, null=True)
+    genre = models.CharField(max_length=200, default=None, blank=True, null=True)
+    duration = models.CharField(max_length=200, default=None, blank=True, null=True)
+    rating = models.IntegerField (default=None, blank=True, null=True)
     added_on = models.DateTimeField(auto_now_add=True)
