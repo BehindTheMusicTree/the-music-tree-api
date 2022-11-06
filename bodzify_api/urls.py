054aa9513e1b954fa816b0754b9935d5f5799183
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-11-06 11:44:46 +0100

    Put API version in all paths

--- bodzify_api/urls.py
+++ bodzify_api/urls.py
@@ -18,24 +18,22 @@ router.register(r'groups', views.GroupViewSet)
 router.register(r'tracks', LibraryTrackViewSet)
 router.register(r'genres', GenreViewSet)
 
-logger = logging.getLogger('info')
-logger.info('DOIHDOIUHDODIUHPDOIHUDPOIDHPDIUHDPIUHPIUDHDPIHUDPIDUHPDIUH')
 base = 'api/' + r'^(?P<version>(v1))/'
 
 urlpatterns = [
-    path(base, include(router.urls)),
+    re_path(base, include(router.urls)),
     
-    path('admin/', admin.site.urls),
+    re_path(base + 'admin/', admin.site.urls),
 
-    path('auth/', include('django.contrib.auth.urls')),
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(base + 'auth/', include('django.contrib.auth.urls')),
+    re_path(base + 'auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    re_path(base + 'auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('users/create/', views.user_create, name='user-create'),
+    re_path(base + 'users/create/', views.user_create, name='user-create'),
 
-    path('mine/tracks/download/', views.mine_track_download, name='mine-track-download'),
+    re_path(base + 'mine/tracks/download/', views.mine_track_download, name='mine-track-download'),
 
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(base + 'api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', 
         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
