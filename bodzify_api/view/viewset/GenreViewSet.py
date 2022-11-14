43f053a33aad378e4043fe50ebe47bfbcd162a42
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-11-14 15:42:19 +0100

    Add track count to get genres

--- bodzify_api/view/viewset/GenreViewSet.py
+++ bodzify_api/view/viewset/GenreViewSet.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
-from django.db.models import Count
+from django.http import JsonResponse
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 
 from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiTypes
 
-from bodzify_api.serializer.GenreSerializer import GenreSerializer
+from bodzify_api.view.viewset.MultiSerializerViewSet import MultiSerializerViewSet
+from bodzify_api.serializer.GenreSerializer import GenreRequestSerializer, GenreResponseSerializer
 from bodzify_api.models import Genre
 
 NAME_FIELD = "name"
 PARENT_FIELD = "parent"
 
-class GenreViewSet(viewsets.ModelViewSet):
-    serializer_class = GenreSerializer
+class GenreViewSet(MultiSerializerViewSet):
     queryset = Genre.objects.all()
+    serializers = {
+        'default': GenreRequestSerializer,
+        'list':  GenreResponseSerializer,
+        'retrieve':  GenreResponseSerializer,
+    }
 
     def get_queryset(self):
         queryset = Genre.objects.all()
@@ -26,16 +31,27 @@ class GenreViewSet(viewsets.ModelViewSet):
 
         if name is None: queryset = queryset.filter(parent=parent)
         else: queryset = queryset.filter(name__contains=name, parent=parent)
-
-        queryset = queryset.annotate(track_number=Count('librarytrack'))
         
         return queryset
 
     @extend_schema(
-        parameters=[
-          OpenApiParameter(NAME_FIELD, OpenApiTypes.STR, OpenApiParameter.PATH),
-          OpenApiParameter(PARENT_FIELD, OpenApiTypes.STR, OpenApiParameter.PATH)
-        ]
+      request=GenreRequestSerializer,
+      responses=GenreResponseSerializer
+    )
+    def create(self, request, *args, **kwargs):
+        requestSerializer = self.get_serializer(data=request.data)
+        requestSerializer.is_valid(raise_exception=True)
+        genre = requestSerializer.save(user=self.request.user)
+        responseSerializer = GenreResponseSerializer(genre)
+        headers = self.get_success_headers(responseSerializer.data)
+        return JsonResponse(responseSerializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @extend_schema(
+      parameters=[
+        OpenApiParameter(NAME_FIELD, OpenApiTypes.STR, OpenApiParameter.PATH),
+        OpenApiParameter(PARENT_FIELD, OpenApiTypes.STR, OpenApiParameter.PATH)
+      ],
+      responses=GenreResponseSerializer
     )
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
