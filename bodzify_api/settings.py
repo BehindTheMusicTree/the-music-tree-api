482adf2e22945cd72bb51fff0c5bee2ae04b4517
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-11-30 11:47:10 +0100

    Put database information in env variables

--- bodzify_api/settings.py
+++ bodzify_api/settings.py
@@ -74,16 +74,30 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bodzify_api.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bodzify_api',
-        'USER': 'django',
-        'PASSWORD': 'T6q)32hr6fr',
-        'HOST': 'localhost',
-        'PORT': '',
+DB_USERNAME=os.environ.get("DB_USERNAME")
+DB_PASSWORD=os.environ.get("DB_PASSWORD")
+DB_HOST=os.environ.get("DB_HOST")
+DB_PORT=os.environ.get("DB_PORT")
+DB_DATABASE=os.environ.get("DB_DATABASE")
+DB_IS_AVAIL = all([
+        DB_USERNAME, 
+        DB_PASSWORD, 
+        DB_HOST,
+        DB_PORT,
+        DB_DATABASE
+])
+
+if DB_IS_AVAIL:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': DB_DATABASE,
+            'USER': DB_USERNAME,
+            'PASSWORD': DB_PASSWORD,
+            'HOST': DB_HOST,
+            'PORT': DB_PORT,
+        }
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
