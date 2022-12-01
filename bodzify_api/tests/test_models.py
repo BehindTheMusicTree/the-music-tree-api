9ed7711acd702bed3539a3dd12af39c2a42c741c
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-12-01 16:49:40 +0100

    Add first test on criteria

---
+++ bodzify_api/tests/test_models.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from bodzify_api.model.criteria.Criteria import Criteria
+from bodzify_api.model.criteria.CriteriaType import CriteriaType
+
+class CriteriaTestCase(TestCase):
+    fixtures = ['initial_data']
+
+    def createCriteria(self, name, type, allGenreCriteria, user):
+        return Criteria.objects.create(
+            name=name,
+            type=type,
+            parent=allGenreCriteria,
+            user=user)
+
+    def test_criterias(self):
+        user = User.objects.get(username="admin")
+        name = "Rap"
+        type = CriteriaType.objects.get(pk=1)
+        allGenreCriteria = Criteria.objects.get(type=type, parent=None, user=user)
+
+        criteria = self.createCriteria(
+            name=name, 
+            type=type, 
+            allGenreCriteria=allGenreCriteria, 
+            user=user)
+        self.assertTrue(isinstance(criteria, Criteria))
+        self.assertIn(
+            name + " " + allGenreCriteria.uuid + " " + allGenreCriteria.name + " None", 
+            str(criteria))
