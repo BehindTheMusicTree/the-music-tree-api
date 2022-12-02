dd0e8184188c8bf9a8ca870baabfe3bca470e15b
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-12-02 18:46:57 +0100

    Test genre criteria creation and str function

--- bodzify_api/tests/test_models.py
+++ bodzify_api/tests/test_models.py
@@ -1,13 +1,29 @@
+import pytest
+
 from django.test import TestCase
 from django.contrib.auth.models import User
 
 from bodzify_api.model.criteria.Criteria import Criteria
-from bodzify_api.model.criteria.CriteriaType import CriteriaType
+from bodzify_api.model.criteria.CriteriaType import CriteriaType, CriteriaTypesIds
 
+USER_TEST_PK = 2
+
+@pytest.mark.django_db(transaction=True)
 class CriteriaTestCase(TestCase):
-    fixtures = ['initial_data']
 
-    def createCriteria(self, name, type, allGenreCriteria, user):
+    fixtures = ['initial_data', 'test_data']
+    
+    def createTestUser(self):
+        User.objects.create_user(
+            pk=USER_TEST_PK, 
+            username="test", 
+            password="test", 
+            email="test@test.com"
+        )
+
+    def createGenreCriteria(self, name, user):
+        type = CriteriaType.objects.get(pk=CriteriaTypesIds.GENRE)
+        allGenreCriteria = Criteria.objects.get(type=type, parent=None, user=user)
         return Criteria.objects.create(
             name=name,
             type=type,
@@ -15,15 +31,8 @@ class CriteriaTestCase(TestCase):
             user=user)
 
     def test_criterias(self):
-        user = User.objects.get(username="admin")
+        userTest = User.objects.get(pk=USER_TEST_PK)
         name = "Rap"
-        type = CriteriaType.objects.get(pk=1)
-        allGenreCriteria = Criteria.objects.get(type=type, parent=None, user=user)
-
-        criteria = self.createCriteria(
-            name=name, 
-            type=type, 
-            allGenreCriteria=allGenreCriteria, 
-            user=user)
-        self.assertTrue(isinstance(criteria, Criteria))
-        self.assertIn(criteria.uuid + " " + criteria.name, str(criteria))
+        criteria = self.createGenreCriteria(name=name, user=userTest)
+        assert isinstance(criteria, Criteria) is True
+        assert criteria.uuid + " " + criteria.name, str(criteria) is True
