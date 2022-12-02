756dde316fd7f64251b8b175010f84861d56a24f
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-12-02 18:46:23 +0100

    Set database settings in project setting and not in env variables

--- bodzify_api/settings.py
+++ bodzify_api/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'coverage',
-    'bodzify_api'
+    'bodzify_api',
 ]
 
 MIDDLEWARE = [
@@ -75,30 +75,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bodzify_api.wsgi.application'
 
-DB_USERNAME=os.environ.get("DB_USERNAME")
-DB_PASSWORD=os.environ.get("DB_PASSWORD")
-DB_HOST=os.environ.get("DB_HOST")
-DB_PORT=os.environ.get("DB_PORT")
-DB_DATABASE=os.environ.get("DB_DATABASE")
-DB_IS_AVAILABLE = all([
-        DB_USERNAME, 
-        DB_PASSWORD, 
-        DB_HOST,
-        DB_PORT,
-        DB_DATABASE
-])
-
-if DB_IS_AVAILABLE:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': DB_DATABASE,
-            'USER': DB_USERNAME,
-            'PASSWORD': DB_PASSWORD,
-            'HOST': DB_HOST,
-            'PORT': DB_PORT,
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bodzify_api',
+        'USER': 'django',
+        'PASSWORD': 'T6q)32hr6fr',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
