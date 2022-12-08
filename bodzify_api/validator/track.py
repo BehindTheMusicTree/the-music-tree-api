7eb45511ff77df58add5e6ba5f096f066e20699b
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-12-08 01:16:31 +0100

    Add track size validator

---
+++ bodzify_api/validator/track.py
@@ -0,0 +1,13 @@
+from django.utils.deconstruct import deconstructible
+from django.template.defaultfilters import filesizeformat
+from django.core.exceptions import ValidationError
+
+import bodzify_api.settings as settings
+
+
+def track_size(value): # add this to some file where you can import it from
+    limit = settings.TRACK_SIZE_LIMIT_IN_MO * 1000000
+    if value.size > limit:
+        trackSizeErrorTooLargeMessage = 'File too large. Size should not exceed {size:.3f} Mo.'
+        raise ValidationError(trackSizeErrorTooLargeMessage.format(
+            size = settings.TRACK_SIZE_LIMIT_IN_MO))
