2cdde28bd6607335e96758075f8621bfd9d86ad8
Andréas <garcia.andreas.1991@gmail.com>
2022-12-13 16:03:51 +0100

    Remove test sample files folder from global settings

--- bodzify_api/settings.py
+++ bodzify_api/settings.py
@@ -206,9 +206,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_TEMP = os.path.join(MEDIA_ROOT, "temp/")
 LIBRARIES_FOLDER_NAME = "libraries"
 USER_LIBRARY_FOLDER_NAME_PREFIXE = "user_"
-
-TEST_SAMPLE_PATH = os.path.join(APP_ROOT, "test/sample/")
-
 TRACK_SIZE_LIMIT_IN_MO = 500
 
 if os.getenv('DJANGO_DEV') == 'true':
