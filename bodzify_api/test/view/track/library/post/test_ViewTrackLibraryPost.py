4fc8fd58dadd9e8682fe3128d6b71b7819c2abcb
Andréas <garcia.andreas.1991@gmail.com>
2022-12-15 18:57:49 +0100

    Clean code

--- bodzify_api/test/view/track/library/post/test_ViewTrackLibraryPost.py
+++ bodzify_api/test/view/track/library/post/test_ViewTrackLibraryPost.py
@@ -9,27 +9,34 @@ from bodzify_api.model.playlist.Playlist import PlaylistSpecialNames
 
 class LibraryTrackPostViewTestCase(LibraryTrackViewTestCase):
 
+    fixtures = ['initial_data', 'TestUserData', 'TestViewTrackLibraryPostData']
+
     def setUp(self) -> None:
         return super().setUp(sampleRelativePath="test/view/track/library/post/sample/")
 
     def test_libraryTrackPost(self):
         self.login(self.testUser)
 
-        response = self.postSampleTrack("image.jpeg")
+        response = self.postSampleTrack("post_image.jpeg")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
         response = self.postSampleTrack(
-            "Big_File 1-01 - Shine On You Crazy Diamond, Parts I–V.flac")
+            "post_Big_File 1-01 - Shine On You Crazy Diamond, Parts I–V.flac")
         assert response.status_code == status.HTTP_201_CREATED
 
-        response = self.postSampleTrack("bad_extension.mp4")
+        response = self.postSampleTrack("post_sample_without_rating.flac")
+        assert response.status_code == status.HTTP_201_CREATED
+        track = LibraryTrack.objects.get(title="Je suis sympa", user=self.testUser)
+        assert track.rating == 0
+
+        response = self.postSampleTrack("post_bad_extension.mp4")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-        response = self.postSampleTrack("genre_foo_non_existing.mp3")
+        response = self.postSampleTrack("post_genre_foo_non_existing.mp3")
         assert response.status_code == status.HTTP_201_CREATED
         assert Criteria.objects.filter(user=self.testUser, name="Foo").exists()
 
-        response = self.postSampleTrack("1-08 - Luz De Luna.flac")
+        response = self.postSampleTrack("post_1-08 - Luz De Luna.flac")
         assert response.status_code == status.HTTP_201_CREATED
         track = LibraryTrack.objects.get(title="Luz De Luna", user=self.testUser)
         assert track.artist == "PNL"
@@ -40,7 +47,7 @@ class LibraryTrackPostViewTestCase(LibraryTrackViewTestCase):
         assert track.playlists.filter(name=PlaylistSpecialNames.GENRE_ALL).exists()
         assert track.playlists.filter(name="French cloud rap").exists()
 
-        response = self.postSampleTrack("sample.wav")
+        response = self.postSampleTrack("post_sample.wav")
         assert response.status_code == 201
         track = LibraryTrack.objects.get(title="La zumba", user=self.testUser)
         assert track.artist == "Joni"
@@ -53,10 +60,10 @@ class LibraryTrackPostViewTestCase(LibraryTrackViewTestCase):
 
         assert track.playlists.filter(name="j\"\"\"\"j").exists()
 
-        response = self.postSampleTrack("with_all_tags.mp3")
+        response = self.postSampleTrack("post_with_all_tags.mp3")
         assert response.status_code == status.HTTP_201_CREATED
 
-        response = self.postSampleTrack("Eminem_Without_Me_sans_genre.mp3")
+        response = self.postSampleTrack("post_Eminem_Without_Me_sans_genre.mp3")
         assert response.status_code == status.HTTP_201_CREATED
 
         track = LibraryTrack.objects.get(title="Without Me", user=self.testUser)
@@ -64,16 +71,5 @@ class LibraryTrackPostViewTestCase(LibraryTrackViewTestCase):
         assert track.album == "The Eminem Show (Expanded Edition)"
         assert track.genre.name == "Genreless"
         assert track.fileExtension == ".mp3"
-
-        print([Playlist.objects.get(
-            user=self.testUser,
-            name=PlaylistSpecialNames.GENRE_ALL
-        ) for val in track.playlists.all() if val in track.playlists.all()])
-        print([Playlist.objects.get(
-            user=self.testUser,
-            name=PlaylistSpecialNames.GENRE_GENRELESS
-        ) for val in track.playlists.all() if val in track.playlists.all()])
-        print(track.playlists.all().count())
-
         assert track.playlists.filter(name=PlaylistSpecialNames.GENRE_GENRELESS).exists()
         assert track.playlists.filter(name=PlaylistSpecialNames.GENRE_ALL).exists()
