ab2488a3c7f898d0b4934c5d69e19e3a0c879433
Andréas <garcia.andreas.1991@gmail.com>
2022-12-21 18:00:44 +0100

    Try new reinit script in prod

--- reinit_database_script_prod.sh
+++ reinit_database_script_prod.sh
@@ -1,12 +1,12 @@
 sudo -u postgres dropdb -e bodzify_api -f
 sudo -u postgres -H -- psql -c "create database bodzify_api with owner django;"
-sudo rm -r /var/www/html/bodzify-api/bodzify_api/migrations
 sudo rm -r /var/www/html/bodzify-api/media/libraries/*
-python3 /var/www/html/bodzify-api/manage.py migrate
-python3 /var/www/html/bodzify-api/manage.py migrate --fake
-python3 /var/www/html/bodzify-api/manage.py makemigrations 
-python3 /var/www/html/bodzify-api/manage.py migrate --fake-initial
-python3 /var/www/html/bodzify-api/manage.py migrate
+python3 /var/www/html/bodzify-api//manage.py clear_cache
+python3 /var/www/html/bodzify-api//manage.py clean_pyc
+python3 /var/www/html/bodzify-api//manage.py reset_schema
+python3 /var/www/html/bodzify-api//manage.py reset_db
+sudo rm -r /var/www/html/bodzify-api/bodzify_api/migrations
+python3 /var/www/html/bodzify-api/manage.py makemigrations
 python3 /var/www/html/bodzify-api/manage.py makemigrations bodzify_api
-python3 /var/www/html/bodzify-api/manage.py migrate --fake
+python3 /var/www/html/bodzify-api/manage.py migrate
 python3 /var/www/html/bodzify-api/manage.py runserver
