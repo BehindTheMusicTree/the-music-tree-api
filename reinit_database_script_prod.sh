4848f46ecee5dcfefcbee3833a0df66a726a96a1
Andréas <garcia.andreas.1991@gmail.com>
2022-12-21 18:09:04 +0100

    Try new reinit script

--- reinit_database_script_prod.sh
+++ reinit_database_script_prod.sh
@@ -3,10 +3,8 @@ sudo -u postgres -H -- psql -c "create database bodzify_api with owner django;"
 sudo rm -r /var/www/html/bodzify-api/media/libraries/*
 python3 /var/www/html/bodzify-api//manage.py clear_cache
 python3 /var/www/html/bodzify-api//manage.py clean_pyc
-python3 /var/www/html/bodzify-api//manage.py reset_schema
-python3 /var/www/html/bodzify-api//manage.py reset_db
 sudo rm -r /var/www/html/bodzify-api/bodzify_api/migrations
 python3 /var/www/html/bodzify-api/manage.py makemigrations
 python3 /var/www/html/bodzify-api/manage.py makemigrations bodzify_api
-python3 /var/www/html/bodzify-api/manage.py migrate
+python3 /var/www/html/bodzify-api/manage.py migrate --run-syncdb
 python3 /var/www/html/bodzify-api/manage.py runserver
