311a6f9bdb20a327e32fc974a53d3f133c5ed4c4
Andréas <garcia.andreas.1991@gmail.com>
2022-12-21 19:03:01 +0100

    Add init data

--- reinit_database_script_prod.sh
+++ reinit_database_script_prod.sh
@@ -7,4 +7,5 @@ sudo rm -r /var/www/html/bodzify-api/bodzify_api/migrations
 python3 /var/www/html/bodzify-api/manage.py makemigrations
 python3 /var/www/html/bodzify-api/manage.py makemigrations bodzify_api
 python3 /var/www/html/bodzify-api/manage.py migrate
+python3 /var/www/html/bodzify-api/manage.py loaddata initial_data
 python3 /var/www/html/bodzify-api/manage.py runserver
