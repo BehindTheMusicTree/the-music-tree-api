34f1b1f1495515f28140582b0335e1f96d611b3d
Andréas <garcia.andreas.1991@gmail.com>
2022-12-21 18:18:41 +0100

    New try

--- reinit_database_script_prod.sh
+++ reinit_database_script_prod.sh
@@ -4,7 +4,7 @@ sudo rm -r /var/www/html/bodzify-api/media/libraries/*
 python3 /var/www/html/bodzify-api//manage.py clear_cache
 python3 /var/www/html/bodzify-api//manage.py clean_pyc
 sudo rm -r /var/www/html/bodzify-api/bodzify_api/migrations
-python3 /var/www/html/bodzify-api/manage.py migrate --run-syncdb
 python3 /var/www/html/bodzify-api/manage.py makemigrations
 python3 /var/www/html/bodzify-api/manage.py makemigrations bodzify_api
+python3 /var/www/html/bodzify-api/manage.py migrate
 python3 /var/www/html/bodzify-api/manage.py runserver
