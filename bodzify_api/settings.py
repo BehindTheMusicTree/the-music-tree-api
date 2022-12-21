639bcce18b273d501a87631791944f5a29c2f172
Andréas <garcia.andreas.1991@gmail.com>
2022-12-21 16:11:09 +0100

    Fix secret key

--- bodzify_api/settings.py
+++ bodzify_api/settings.py
@@ -23,7 +23,7 @@ ATOMIC_REQUESTS = True
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
