295a726dae498dc6a78757e41aa38cad0df00885
Andréas <garcia.andreas.1991@gmail.com>
2022-12-22 01:52:26 +0100

    Set db settings as env variables

--- bodzify_api/settings_dev.py
+++ bodzify_api/settings_dev.py
@@ -3,15 +3,3 @@ import os
 ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_DATABASE'),
-        'USER': os.getenv('DB_USERNAME'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'DISABLE_SERVER_SIDE_CURSORS': True
-    }
-}
