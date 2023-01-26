2baa3b7910148818da882c72d6f16e6e792a8275
mignot <garcia.andreas.1991@gmail.com>
2023-01-27 00:52:05 +0100

    Add log folder setup

--- setup_filesystem.sh
+++ setup_filesystem.sh
@@ -3,3 +3,9 @@ mediaDir=/var/lib/bodzify-api/media/
 sudo mkdir -p $mediaDir
 sudo chown $USER $mediaDir
 sudo chmod 775 $mediaDir
+
+logDir=/var/log/bodzify-api/
+
+sudo mkdir -p $logDir
+sudo chown $USER $logDir
+sudo chmod 775 $logDir
