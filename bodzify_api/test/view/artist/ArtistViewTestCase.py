bb71aaf147f1e197b3c9e97481ba8976d99d6a75
mignot <garcia.andreas.1991@gmail.com>
2023-02-04 14:07:12 +0100

    Clean code

--- bodzify_api/test/view/artist/ArtistViewTestCase.py
+++ bodzify_api/test/view/artist/ArtistViewTestCase.py
@@ -8,5 +8,4 @@ from bodzify_api.test.view.ViewTestCase import ViewTestCase
 class ArtistViewTestCase(ViewTestCase):
 
     def delete(self, artistUuid):
-        return self.apiClient.post(
-            path=reverse('artist-detail'), kwargs={'pk': artistUuid})
+        return self.apiClient.delete(path=reverse('artist-detail', kwargs={'pk': artistUuid}))
