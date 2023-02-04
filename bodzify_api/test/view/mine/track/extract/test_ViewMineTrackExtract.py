a8842059659431cc91555b04f231b6eb416c6357
mignot <garcia.andreas.1991@gmail.com>
2023-02-04 10:36:07 +0100

    Add test to track extraction

--- bodzify_api/test/view/mine/track/extract/test_ViewMineTrackExtract.py
+++ bodzify_api/test/view/mine/track/extract/test_ViewMineTrackExtract.py
@@ -12,7 +12,7 @@ import bodzify_api.settings as settings
 
 class MineTrackExtractViewTestCase(ViewTestCase):
 
-    fixtures = ['initial_data', 'TestUserData']
+    fixtures = ['initial_data', 'TestUserData', 'TestViewMineTrackExtractData']
 
     def extract(self, data):
         return self.apiClient.post(
@@ -42,4 +42,28 @@ class MineTrackExtractViewTestCase(ViewTestCase):
         assert track.genre.name == CriteriaSpecialNames.GENRE_GENRELESS
         assert track.rating == 0
         assert track.file.name == self.testUserLibraryRelativePath + "Jul_-_du_rap.mp3"
+        assert os.path.exists(settings.MEDIA_ROOT + track.file.name)
+
+        # With existing artist
+        trackUrl = ("https://cs9-15v4.vkuseraudio.net/s/v1/acmp/qCKkBk5i-Rl-QBdJM2m2lGbeRX6gB2ji" +
+                    "zqo-ZXY7dSsA7VYaDDbb7nHloh42XVdi1gZ-U0BtWIa1I5qZJ3RspFGJbomdr4P-LwffbPvwWnZ" +
+                    "_hyJ2dSP4WIET6pg2tz6yUtco3HKAodQaY85KeQocwpIiOzKLUb1hDAf5a7xQ9_NrLESvCw.mp3")
+        data = {
+            "url": trackUrl,
+            "title": "Summer Moved On",
+            "artist": "a-ha",
+            # No album field returned by myfreemp3
+            "duration": 1.2233,
+            "releasedOn": 1290292
+        }
+
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+
+        track = LibraryTrack.objects.get(title="Summer Moved On")
+        assert track.artist.name == "a-ha"
+        assert track.album == ""
+        assert track.genre.name == CriteriaSpecialNames.GENRE_GENRELESS
+        assert track.rating == 0
+        assert track.file.name == self.testUserLibraryRelativePath + "a-ha_-_Summer_Moved_On.mp3"
         assert os.path.exists(settings.MEDIA_ROOT + track.file.name)
