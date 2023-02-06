885c16ba36ba0a9a1fab63a794ba6a3030768a0f
mignot <garcia.andreas.1991@gmail.com>
2023-02-06 15:21:27 +0100

    Fix reinit db script for prod

--- reinit_database_script_prod.sh
+++ reinit_database_script_prod.sh
@@ -4,12 +4,12 @@ sudo rm -r /var/lib/bodzify-api/*
 sudo rm -r /var/log/bodzify-api/*
 
 homeDir="/home/app/webapp"
-python3 $homeDir/bodzify-api/manage.py migrate
-python3 $homeDir/bodzify-api/manage.py migrate --fake
-python3 $homeDir/bodzify-api/manage.py makemigrations 
-python3 $homeDir/bodzify-api/manage.py migrate --fake-initial
-python3 $homeDir/bodzify-api/manage.py migrate
-python3 $homeDir/bodzify-api/manage.py makemigrations bodzify_api
-python3 $homeDir/bodzify-api/manage.py migrate
-python3 $homeDir/bodzify-api/manage.py loaddata initial_data
-python3 $homeDir/bodzify-api/manage.py runserver
+python3 $homeDir/manage.py migrate
+python3 $homeDir/manage.py migrate --fake
+python3 $homeDir/manage.py makemigrations 
+python3 $homeDir/manage.py migrate --fake-initial
+python3 $homeDir/manage.py migrate
+python3 $homeDir/manage.py makemigrations bodzify_api
+python3 $homeDir/manage.py migrate
+python3 $homeDir/manage.py loaddata initial_data
+python3 $homeDir/manage.py runserver
