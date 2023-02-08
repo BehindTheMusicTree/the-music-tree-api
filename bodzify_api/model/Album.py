f07f0e592fcb7886a332724c9bb7266349d6e490
mignot <garcia.andreas.1991@gmail.com>
2023-02-08 14:52:58 +0100

    Add model

---
+++ bodzify_api/model/Album.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import shortuuid
+
+from django.db import models
+from django.contrib.auth.models import User
+
+from bodzify_api.model.Artist import Artist
+
+
+class Album(models.Model):
+    # Django's UUIDField won't validate a shortuuid
+    uuid = models.CharField(
+        primary_key=True,
+        default=shortuuid.uuid,
+        max_length=22,
+        editable=False
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, default=None, blank=True, null=True)
+    year = models.CharField(max_length=4, default=None, blank=True, null=True)
+    albumArtists = models.ManyToManyField(Artist, default=None, null=True)
