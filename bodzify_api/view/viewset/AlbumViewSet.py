89339a1fcdf7b0be76238892a8241441eba2e67e
mignot <garcia.andreas.1991@gmail.com>
2023-02-08 15:02:16 +0100

    Add viewset

---
+++ bodzify_api/view/viewset/AlbumViewSet.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from bodzify_api.view.viewset.MultiSerializerViewSet import MultiSerializerViewSet
+
+
+from bodzify_api.model.Album import Album
+from bodzify_api.serializer.AlbumSerializer import AlbumSerializer
+
+class AlbumViewSet(MultiSerializerViewSet):
+
+    queryset = Album.objects.all()
+    serializers = {
+        'default': AlbumSerializer,
+        'list':  AlbumSerializer,
+        'retrieve':  AlbumSerializer,
+        'update':  AlbumSerializer,
+    }
+
+    def get_queryset(self):
+        return Album.objects.filter(user=self.request.user)
+
+    def list(self, request, *args, **kwargs):
+            return super().list(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+         return super().destroy(request, *args, **kwargs)
