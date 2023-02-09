05ea826141fec31897baeec3b2066b2e89df22aa
mignot <garcia.andreas.1991@gmail.com>
2023-02-09 01:20:54 +0100

    Fix reinit db dev script

--- reinit_database_script_dev.sh
+++ reinit_database_script_dev.sh
@@ -1,13 +1,16 @@
+projectDir=~/Git/bodzify-api
+managePath=~/Git/bodzify-api/manage.py
+echo $managePath
 sudo -u postgres dropdb -e bodzify_api -f
 sudo -u postgres -H -- psql -c "create database bodzify_api with owner django;"
-sudo rm -r ~/Git/bodzify-api/bodzify_api/migrations/*
-sudo rm -r ~/Git/bodzify-api/media/libraries/*
-python3 ~/Git/bodzify-api/manage.py migrate
-python3 ~/Git/bodzify-api/manage.py migrate --fake
-python3 ~/Git/bodzify-api/manage.py makemigrations 
-python3 ~/Git/bodzify-api/manage.py migrate --fake-initial
-python3 ~/Git/bodzify-api/manage.py migrate
-python3 ~/Git/bodzify-api/manage.py makemigrations bodzify_api
-python3 ~/Git/bodzify-api/manage.py migrate
-python3 ~/Git/bodzify-api/manage.py loaddata initial_data
-python3 ~/Git/bodzify-api/manage.py runserver
+sudo rm -r $projectDir/bodzify_api/migrations/*
+sudo rm -r /var/lib/bodzify-api/*
+python3 $managePath migrate
+python3 $managePath migrate --fake
+python3 $managePath makemigrations 
+python3 $managePath --fake-initial
+python3 $managePath migrate
+python3 $managePath makemigrations bodzify_api
+python3 $managePath migrate
+python3 $managePath loaddata initial_data
+python3 $managePath runserver
