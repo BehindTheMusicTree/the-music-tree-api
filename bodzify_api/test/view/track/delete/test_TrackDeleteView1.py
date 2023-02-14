ec4b1724d4b7ace42cdb8c211d9fa03a2c31a06b
mignot <garcia.andreas.1991@gmail.com>
2023-02-15 00:52:17 +0100

    Fix tests

--- bodzify_api/test/view/track/delete/test_TrackDeleteView1.py
+++ bodzify_api/test/view/track/delete/test_TrackDeleteView1.py
@@ -16,10 +16,10 @@ class TrackDeleteViewTestCase1(TrackViewTestCase):
 
         """
         Deleting the track '1-03 - We're All To Blame' must delete the associated file 
-        'delete_1-03 - We're All To Blame.mp3'.
+        '1-03 - We're All To Blame.mp3'.
         """
         response = self.deleteTrack(trackUuid="36nS4LVDssLh4BvTARbJEK")
         assert response.status_code == status.HTTP_204_NO_CONTENT
         assert LibraryTrack.objects.filter(title="1-03 - We're All To Blame").exists() == False
         assert os.path.isfile(
-                self.testUserLibraryAbsolutePath + "delete_1-03 - We're All To Blame.mp3") == False
+                self.testUserLibraryAbsolutePath + "1-03 - We're All To Blame.mp3") == False
