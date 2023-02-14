51789a56f2f3ac1be9c00a61d5b2586ec70fdaf2
mignot <garcia.andreas.1991@gmail.com>
2023-02-14 03:09:57 +0100

    Move track update in model's save function

--- bodzify_api/model/track/LibraryTrack.py
+++ bodzify_api/model/track/LibraryTrack.py
@@ -5,9 +5,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.exceptions import ObjectDoesNotExist
 from upload_validator import FileTypeValidator
 from bodzify_api.validator.LibraryTrackSizeValidator import trackSize
-from bodzify_api.model.track.LibraryTrackQuerySet import LibraryTrackQuerySet
 from bodzify_api.model.criteria.Criteria import Criteria
 from bodzify_api.model.playlist.Playlist import Playlist
 from bodzify_api.model.playlist.PlaylistType import PlaylistType
@@ -15,7 +15,7 @@ from bodzify_api.model.playlist.PlaylistType import PlaylistTypeIds
 import bodzify_api.settings as settings
 
 
-def userDirectoryPath(instance, filename):
+def _userDirectoryPath(instance, filename):
     return '{0}{1}/{2}'.format(
             settings.LIBRARIES_FOLDER_NAME + '/' + settings.USER_LIBRARY_FOLDER_NAME_PREFIXE,
             instance.user.id, 
@@ -33,15 +33,13 @@ class LibraryTrack(models.Model):
     ATTRIBUTE_DURATION_LABEL = "duration"
     ATTRIBUTE_RATING_LABEL = "rating"
     ATTRIBUTE_LANGUAGE_LABEL = "language"
-
-    objects = LibraryTrackQuerySet.as_manager()
     
     # Django's UUIDField won't validate a shortuuid
     uuid = models.CharField(
             primary_key=True, default=shortuuid.uuid, max_length=22, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.FileField(
-            upload_to=userDirectoryPath, 
+            upload_to=_userDirectoryPath, 
             help_text="Only audio formats accepted.", 
             validators=[
                     FileExtensionValidator(['flac', 'wav', 'mp3']), 
@@ -118,3 +116,21 @@ class LibraryTrack(models.Model):
                 criteria=oldGenreTreeItem))
             oldGenreTreeItem = oldGenreTreeItem.parent
         self.save()
+
+    def save(self, *args, **kwargs):
+        try:
+            oldTrack = LibraryTrack.objects.get(uuid=self.uuid)
+            oldGenre = oldTrack.genre
+            oldArtist = oldTrack.artist
+            oldAlbum = self.album
+            super().save(*args, **kwargs)
+
+            if oldGenre != self.genre:
+                self.updatePlaylists(oldGenre=oldGenre)
+            if oldAlbum != self.album and oldAlbum != None:
+                oldAlbum.deleteIfNoTrackLinked()
+
+            if oldArtist != self.artist and oldArtist != None:
+                oldArtist.deleteIfNothingLinked()
+        except ObjectDoesNotExist:
+            super().save(*args, **kwargs)
