e56ece0fcb9077a41ef11f01dbb184277b23f523
mignot <garcia.andreas.1991@gmail.com>
2023-02-27 11:17:33 +0100

    Fix tests

--- bodzify_api/test/view/track/put/test_TrackPutViewWav.py
+++ bodzify_api/test/view/track/put/test_TrackPutViewWav.py
@@ -35,3 +35,43 @@ class TrackPutViewTestCaseWav(TrackViewTestCase):
         assert track.rating == 8
         assert track.language == "French"
         assert Artist.objects.filter(name="Joni").count() == 0
+        
+    
+    """
+    Update:
+        - on a wav file entitled "La Zumba";
+     - the former artist "Joni", not having any track related left, must be deleted;
+        - the new artist specified is empty so no artist;
+        - same album's name "American Idiot" as an existing one but with an album 
+        artist named "Queen". Thus a new album has to be created;
+        - the old album "American Idiot" (with no album artists) still has a track linked to it. 
+        It must therefore not be 
+        deleted. We should then have two albums "American Idiot" (one with no album artist, one 
+        with album artist "Queen")
+        - rating isn't specified so unchanged (255);
+        - the previous track's album "BOOM" hasn't anythink linked to it anymore. It must then be 
+    deleted;
+        - new existing genre "Nu metal".
+    """
+    def test_trackPutWavWithTags(self):
+
+        data = {
+            "title": "Bohemian Raphsody",
+            "artistName": "",
+            "albumName": "American Idiot",
+            "albumArtistsName": "Queen",
+            "genreName": "Nu metal",
+            "language": "French"
+        }
+        response = self.putSampleTrack(trackUuid="dyFYZTP3anyaUBcLYKKKDJ", data=data)
+        assert response.status_code == status.HTTP_200_OK
+        track = LibraryTrack.objects.get(title="Bohemian Raphsody")
+        assert track.artist_id == None
+        assert track.album.name == "American Idiot"
+        assert track.album.albumArtists.filter(name="Queen").exists()
+        assert Album.objects.filter(user=self.testUser, name="American Idiot").count() == 2
+        assert Album.objects.filter(user=self.testUser, name="BOOM").exists() == False
+        assert track.genre.name == "Nu metal"
+        assert track.rating == 255
+        assert track.language == "French"
+        assert Artist.objects.filter(name="Joni").count() == 0
