4141441d35b8e98f4fef5e52cc75004d669b8de0
mignot <garcia.andreas.1991@gmail.com>
2023-03-07 10:03:33 +0100

    Add rating source to app's models

--- bodzify_api/admin.py
+++ bodzify_api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .model.track.LibraryTrack import LibraryTrack
+from .model.track.RatingSource import RatingSource
 from .model.criteria.Criteria import Criteria
 from .model.criteria.CriteriaType import CriteriaType
 from .model.playlist.Playlist import Playlist
@@ -12,5 +13,6 @@ admin.site.register(CriteriaType)
 admin.site.register(Playlist)
 admin.site.register(PlaylistType)
 admin.site.register(LibraryTrack)
+admin.site.register(RatingSource)
 admin.site.register(Album)
 admin.site.register(Artist)
