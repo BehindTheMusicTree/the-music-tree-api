92333844674b9bd87ad7ebfee25983d56d7f6b9a
mignot <garcia.andreas.1991@gmail.com>
2023-03-08 17:30:57 +0100

    Reorganize tests

--- bodzify_api/test/view/track/post/extraField/test_TrackPostViewExtraField.py
+++ bodzify_api/test/view/track/post/extraField/test_TrackPostViewExtraField.py
@@ -2,16 +2,13 @@
 import pytest
 from rest_framework import status
 from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
-from bodzify_api.model.track.LibraryTrack import LibraryTrack
-from bodzify_api.model.playlist.PlaylistType import PlaylistTypeIds
-from bodzify_api.model.playlist.Playlist import Playlist
-from bodzify_api.model.criteria.Criteria import CriteriaSpecialNames
 
 
 @pytest.mark.django_db
 class TrackPostViewTestCaseExtraField(TrackViewTestCase):
 
     fixtures = ['initial_data', 'TestUserData']
+    sampleDirectoryRelativePath = "test/view/track/post/extraField/sample/"
 
     """
      Trying to post a track with extra fields should fail with a 400 error code.
@@ -20,4 +17,4 @@ class TrackPostViewTestCaseExtraField(TrackViewTestCase):
         self.login(self.testUser)
         response = self.postSampleTrack(
                 "1-08 - Luz De Luna.flac", {"nonExistingField": "qofkqspofk"})
-        assert response == status.HTTP_400_BAD_REQUEST
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
