94cb80ba08b1bfd2fb3307e48eca121afc90e7f7
mignot <garcia.andreas.1991@gmail.com>
2023-03-08 09:58:46 +0100

    Remove rating source

--- bodzify_api/admin.py
+++ bodzify_api/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .model.track.LibraryTrack import LibraryTrack
-from .model.track.RatingSource import RatingSource
 from .model.criteria.Criteria import Criteria
 from .model.criteria.CriteriaType import CriteriaType
 from .model.playlist.Playlist import Playlist
@@ -13,6 +12,5 @@ admin.site.register(CriteriaType)
 admin.site.register(Playlist)
 admin.site.register(PlaylistType)
 admin.site.register(LibraryTrack)
-admin.site.register(RatingSource)
 admin.site.register(Album)
 admin.site.register(Artist)
