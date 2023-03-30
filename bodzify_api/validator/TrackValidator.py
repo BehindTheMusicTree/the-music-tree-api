a84ff4a6ab17282728e4e841d04c3862aaabbb08
mignot <garcia.andreas.1991@gmail.com>
2023-03-30 10:54:19 +0200

    Clean code

--- bodzify_api/validator/TrackValidator.py
+++ bodzify_api/validator/TrackValidator.py
@@ -4,8 +4,8 @@ import bodzify_api.settings as settings
 
 
 def validateTrackSize(file):
-    limit = settings.TRACK_SIZE_LIMIT_IN_MO * 1000000
+    limit = settings.TRACK_FILE_SIZE_LIMIT_IN_MO * 1000000
     if file.size > limit:
         trackSizeErrorTooLargeMessage = 'File too large. Size should not exceed {size:.3f} Mo.'
         raise ValidationError(trackSizeErrorTooLargeMessage.format(
-            size=settings.TRACK_SIZE_LIMIT_IN_MO))
+            size=settings.TRACK_FILE_SIZE_LIMIT_IN_MO))
