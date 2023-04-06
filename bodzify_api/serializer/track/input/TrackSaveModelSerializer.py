664955d5eaa663a3fc9d9012091113799a0bb9e5
mignot <garcia.andreas.1991@gmail.com>
2023-04-06 10:18:22 +0200

    Refactor tests

--- bodzify_api/serializer/track/input/TrackSaveModelSerializer.py
+++ bodzify_api/serializer/track/input/TrackSaveModelSerializer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from bodzify_api.model.track.LibraryTrack import LibraryTrack
+from bodzify_api.model.track.LibraryTrack import ATTRIBUTES_LABEL
 from bodzify_api.serializer.InputModelSerializer import InputModelSerializer
 
 
@@ -7,15 +8,12 @@ class TrackSaveModelSerializer(InputModelSerializer):
 
     class Meta:
         model = LibraryTrack
-        fields = [
-            LibraryTrack.ATTRIBUTE_USER_LABEL,
-            LibraryTrack.ATTRIBUTE_FILE_LABEL,
-            LibraryTrack.ATTRIBUTE_TITLE_LABEL, 
-            LibraryTrack.ATTRIBUTE_ARTIST_LABEL,
-            LibraryTrack.ATTRIBUTE_ALBUM_LABEL, 
-            LibraryTrack.ATTRIBUTE_GENRE_LABEL,
-            LibraryTrack.ATTRIBUTE_DURATION_LABEL,
-            LibraryTrack.ATTRIBUTE_RATING_LABEL, 
-            LibraryTrack.ATTRIBUTE_LANGUAGE_LABEL,
-        ]
-        
+        fields = [ATTRIBUTES_LABEL.USER,
+                  ATTRIBUTES_LABEL.FILE,
+                  ATTRIBUTES_LABEL.TITLE,
+                  ATTRIBUTES_LABEL.ARTIST,
+                  ATTRIBUTES_LABEL.ALBUM,
+                  ATTRIBUTES_LABEL.GENRE,
+                  ATTRIBUTES_LABEL.DURATION,
+                  ATTRIBUTES_LABEL.RATING,
+                  ATTRIBUTES_LABEL.LANGUAGE]
