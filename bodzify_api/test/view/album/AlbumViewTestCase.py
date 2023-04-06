8ad6517fb3279ad5a2bd3a5c4397e7a284dddfb6
mignot <garcia.andreas.1991@gmail.com>
2023-04-06 10:56:21 +0200

    Refactor tests

--- bodzify_api/test/view/album/AlbumViewTestCase.py
+++ bodzify_api/test/view/album/AlbumViewTestCase.py
@@ -5,6 +5,5 @@ from bodzify_api.test.view.ViewTestCase import ViewTestCase
 
 class AlbumViewTestCase(ViewTestCase):
 
-    def _loginAndDelete(self, albumUuid: str):
-        self._login(self.testUser)
+    def delete(self, albumUuid: str):
         return self.apiClient.delete(path=reverse('album-detail', kwargs={'pk': albumUuid}))
