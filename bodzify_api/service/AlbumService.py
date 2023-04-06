63ae401af4872d83ae4c12919c7dc52a120a294d
mignot <garcia.andreas.1991@gmail.com>
2023-04-06 10:33:18 +0200

    Refactor tests

--- bodzify_api/service/AlbumService.py
+++ bodzify_api/service/AlbumService.py
@@ -5,17 +5,20 @@ import bodzify_api.service.ArtistService as ArtistService
 from bodzify_api.model.Album import Album
 
 
-def GetAlbumFromNameAndAlbumArtistsNameAfterEventualCreations(
+def GetAlbumFromNameAndAlbumArtistsNameListAfterEventualCreations(
         user: User, albumName: str, albumArtistsNameList: list) -> Album:
     
     if albumName is None or albumName == "":
         return None
     else:
         if albumArtistsNameList is not None:
-            albumArtists = [ArtistService.GetArtistFromNameAfterEventualCreation(
-                 user=user, artistName=artistName) for artistName in albumArtistsNameList]
+            if len(albumArtistsNameList) > 0:
+                albumArtists = [ArtistService.GetArtistFromNameAfterEventualCreation(
+                    user=user, artistName=artistName) for artistName in albumArtistsNameList]
+            else:
+                albumArtists = []
         else:
-            albumArtists = None
+            albumArtists = []
                 
         return _getAlbumFromNameAndArtistsListAfterHavingEventuallyCreatedTheAlbum(
                 user=user, albumName=albumName, artists=albumArtists)
@@ -25,7 +28,7 @@ def _getAlbumFromNameAndArtistsListAfterHavingEventuallyCreatedTheAlbum(
         user: User, albumName: str, artists: list):
     
     albumQueryset = Album.objects.filter(user=user, name=albumName)
-    if artists is not None:
+    if len(artists) > 0:
         for albumArtist in artists:
             albumQueryset = albumQueryset.filter(albumArtists__in=[albumArtist])
     else:
