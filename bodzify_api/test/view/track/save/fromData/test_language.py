0d364b790c99c4bcd49ced5a446d394469307d32
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 10:10:18 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/fromData/test_language.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from rest_framework import status
+from ddf import G
+from bodzify_api import settings
+from bodzify_api.model.Artist import Artist
+from bodzify_api.model.track.LibraryTrack import LibraryTrack
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+import bodzify_api.service.AudioMetadataService as AudioMetadataService
+
+
+class LanguageTestCase(TrackViewTestCase):
+
+    def test_notProvided(self):
+        language = "French"
+        track = G(LibraryTrack,
+                  user=self.testUser,
+                  title="Love",
+                  genre=self.testUserGenrelessGenre,
+                  language=language,
+                  duration=0)
+        data = {}
+        response = self.putSampleTrack(track.uuid, data=data)
+        assert response.status_code == status.HTTP_200_OK
+        assert self.savedTrack.language == language
+
+    def test_null(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "language" : None
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.language == ""
+
+    def test_empty(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "language" : ""
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.language == ""
+
+    def test_longest(self):
+        language = "a" * settings.TRACK_LANGUAGE_MAX_CHAR
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "language": language
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.language == language
---
+++ bodzify_api/test/view/track/save/fromData/test_playlist.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+import pytest
+from rest_framework import status
+from ddf import G
+from bodzify_api.model.playlist.Playlist import Playlist
+from bodzify_api.model.playlist.PlaylistType import PlaylistTypesId
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+from bodzify_api.model.criteria.Criteria import Criteria, CriteriaSpecialNames
+
+
+@pytest.mark.django_db
+class PlaylistTestCase(TrackViewTestCase):
+
+    def test_noGenreThenInTheAllAndGenrelessPlaylists(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        trackPlaylists = self.savedTrack.playlists.all()
+        assert len(trackPlaylists) == 2
+        assert trackPlaylists.filter(
+            name=CriteriaSpecialNames.GENRE_ALL).exists()
+        assert trackPlaylists.filter(
+            name=CriteriaSpecialNames.GENRE_GENRELESS).exists()
+
+    def test_newGenreThenInNewGenrePlaylistAndAllPlaylist(self):
+        genreName = "Rock"
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "genreName": genreName,
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        trackPlaylists = self.savedTrack.playlists.all()
+        assert len(trackPlaylists) == 2
+        assert trackPlaylists.filter(name=genreName).exists()
+        assert trackPlaylists.filter(
+            name=CriteriaSpecialNames.GENRE_ALL).exists()
+
+    def test_existingGenreWithParentAllThenTrackInExistingPlaylistAndAllPlaylist(self):
+        genreName = "Rock"
+        G(Criteria, user=self.testUser, name=genreName)
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "genreName": genreName,
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        trackPlaylists = self.savedTrack.playlists.all()
+        assert len(trackPlaylists) == 2
+        assert trackPlaylists.filter(name=genreName).exists()
+        assert trackPlaylists.filter(
+            name=CriteriaSpecialNames.GENRE_ALL).exists()
+
+        genrePlaylist = trackPlaylists.get(name=genreName)
+        assert genrePlaylist.parent.name == CriteriaSpecialNames.GENRE_ALL
+
+    def test_existingGenreWith2ParentsThenTrackIn3ExistingPlaylists(self):
+        genreName = "Hard rock"
+        
+        rockGenre = G(Criteria, user=self.testUser, name="Rock")
+        hardrockGenre = G(Criteria, user=self.testUser,
+                          name=genreName, parent=rockGenre)
+
+        rockPlaylist = G(Playlist, user=self.testUser,
+                         type=PlaylistTypesId.GENRE, criteria=rockGenre)
+        hardrockPlaylist = G(Playlist, user=self.testUser,
+                         type=PlaylistTypesId.GENRE, criteria=hardrockGenre)
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "genreName": genreName,
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        trackPlaylists = self.savedTrack.playlists.all()
+        assert len(trackPlaylists) == 3
+        assert trackPlaylists.filter(name=hardrockPlaylist.name).exists()
+        assert trackPlaylists.filter(name=rockPlaylist.name).exists()
+        assert trackPlaylists.filter(
+            name=CriteriaSpecialNames.GENRE_ALL).exists()
