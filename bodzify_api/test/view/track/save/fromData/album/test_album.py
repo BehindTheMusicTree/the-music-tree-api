71cac17cd14d154ad0bf2d978a728ba127de8fa8
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 09:51:11 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/fromData/album/test_album.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+from rest_framework import status
+from ddf import G
+from bodzify_api import settings
+from bodzify_api.model.Album import Album
+from bodzify_api.model.Artist import Artist
+from bodzify_api.model.track.LibraryTrack import LibraryTrack
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+import bodzify_api.service.AudioMetadataService as AudioMetadataService
+
+
+class ArtistTestCase(TrackViewTestCase):
+
+    def test_notProvided(self):
+        album = G(Artist, user=self.testUser, name="Jojo")
+        track = G(LibraryTrack,
+                  user=self.testUser,
+                  title="Love",
+                  album=album,
+                  genre=self.testUserGenrelessGenre,
+                  duration=0)
+        data = {}
+        response = self.putSampleTrack(track.uuid, data=data)
+        assert response.status_code == status.HTTP_200_OK
+        assert self.savedTrack.album.uuid == album.uuid
+
+    def test_null(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "albumName" : None
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.album == None
+
+    def test_empty(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "albumName" : ""
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.album == None
+
+    def test_longest(self):
+        albumName = "a" * settings.ALBUM_NAME_MAX_CHAR
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "albumName": albumName
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.album.name == albumName
+
+    def test_existing(self):
+        albumName = "Kopoe"
+        G(Album, user=self.testUser, name=albumName)
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "albumName": albumName,
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.album.name == albumName
+
+    def test_notExisting(self):
+        albumName = "hoho"
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "albumName": albumName,
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.album.name == albumName
