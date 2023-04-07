1b686cfb264b0e952bb7e766180803918c15e136
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 09:38:13 +0200

    Refactor tests

--- bodzify_api/test/view/track/save/create/file/test_size.py
+++ bodzify_api/test/view/track/save/create/file/test_size.py
@@ -6,9 +6,6 @@ from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
 @pytest.mark.django_db
 class TrackPostViewTestCase9(TrackViewTestCase):
 
-    fixtures = ['initial_data', 'TestUserData']
-    #sampleDirectoryRelativePath = "test/view/track/post/sample/9/"
-
     """
     As the file is too big to be uploaded on Github, the pytest won't work during Github's
     actions. Therefore we have to comment this test before any dev push (as it triggers 
@@ -18,5 +15,5 @@ class TrackPostViewTestCase9(TrackViewTestCase):
         "post_Big_File 1-01 - Shine On You Crazy Diamond, Parts I–V.flac")
     assert response.status_code == status.HTTP_201_CREATED
     """
-    def test_libraryTrackPost9(self):
+    def test_errorWhenTooBig(self):
         ""
