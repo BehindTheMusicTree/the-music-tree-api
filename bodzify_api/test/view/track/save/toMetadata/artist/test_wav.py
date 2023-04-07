a4a1174de490fe84266db3c06c47786553a60749
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 17:27:42 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/toMetadata/artist/test_wav.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from rest_framework import status
+from bodzify_api import settings
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+import bodzify_api.service.AudioMetadataService as AudioMetadataService
+
+
+class AlbumArtistsTestCase(TrackViewTestCase):
+
+    def test_longest(self):
+        artistName = "a" * settings.ARTIST_NAME_MAX_CHAR
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "artistName": artistName
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        artistNameKey = AudioMetadataService.METADATA_DICT_KEYS.ARTIST_NAME
+        assert self.savedTrackMetadata[artistNameKey] == artistName
+
+    def test_null(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "artistName": None,
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        artistNameKey = AudioMetadataService.METADATA_DICT_KEYS.ARTIST_NAME
+        assert self.savedTrackMetadata[artistNameKey] in ["", None]
