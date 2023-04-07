eede1e2e91ccef393fb527bc70d9175cf427f276
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 17:14:46 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/toMetadata/albumArtists/test_formatting.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from rest_framework import status
+from ddf import G
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+import bodzify_api.service.AudioMetadataService as AudioMetadataService
+
+
+class AlbumArtistsTestCase(TrackViewTestCase):
+
+    def test_withCorrectSpacing(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "albumName": "Chuck",
+            "albumArtistsName": "Chuck Berry,  The Beatles,The Rolling Stones "
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        albumArtistsKey = AudioMetadataService.METADATA_DICT_KEYS.ALBUM_ARTISTS_NAMES
+        assert self.savedTrackMetadata[albumArtistsKey] == "Chuck Berry,The Beatles,The Rolling Stones"
