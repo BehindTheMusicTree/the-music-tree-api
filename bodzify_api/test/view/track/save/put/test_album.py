4cfa6374935565c8cacc5b124e31b41244a05d61
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 18:07:30 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/put/test_album.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from rest_framework import status
+from ddf import G
+from bodzify_api.model.Album import Album
+from bodzify_api.model.track.LibraryTrack import LibraryTrack
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+
+
+class AlbumTestCase(TrackViewTestCase):
+
+    def test_deleteOldOneBecauseNothingLinkedToIt(self):
+        albumName = "Le Noir"
+        album = G(Album, user=self.testUser, name=albumName)
+        track = G(LibraryTrack,
+                  user=self.testUser,
+                  title="Foire",
+                  album=album,
+                  genre=self.testUserGenrelessGenre,
+                  duration=0)
+        data = {
+            "albumName": "Paul",
+        }
+        response = self.putSampleTrack(trackUuid=track.uuid, data=data)
+        assert response.status_code == status.HTTP_200_OK
+        assert Album.objects.filter(
+            user=self.testUser, name=albumName).count() == 0
+
+    def test_notDeleteOldOneBecauseATrackLinkedToIt(self):
+        albumName = "La Saucisse"
+        album = G(Album, user=self.testUser, name=albumName)
+        track = G(LibraryTrack,
+                  user=self.testUser,
+                  title="Foire",
+                  album=album,
+                  genre=self.testUserGenrelessGenre,
+                  duration=0)
+        G(LibraryTrack,
+          user=self.testUser,
+          title="Josie",
+          album=album,
+          genre=self.testUserGenrelessGenre,
+          duration=0)
+        data = {
+            "albumName": "Paul",
+        }
+        response = self.putSampleTrack(trackUuid=track.uuid, data=data)
+        assert response.status_code == status.HTTP_200_OK
+        assert Album.objects.filter(
+            user=self.testUser, name=albumName).count() == 1
