87b2d1c640235fea234d27e50c7e16b74a23fa28
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 18:04:16 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/put/test_artist.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+from rest_framework import status
+from ddf import G
+from bodzify_api.model.Album import Album
+from bodzify_api.model.Artist import Artist
+from bodzify_api.model.track.LibraryTrack import LibraryTrack
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+
+
+class ArtistTestCase(TrackViewTestCase):
+
+    def test_deleteOldOneBecauseNothingLinkedToIt(self):
+        artistName = "a-ha"
+        artist = G(Artist, user=self.testUser, name=artistName)
+        track = G(LibraryTrack,
+                  user=self.testUser,
+                  title="Foire",
+                  artist=artist,
+                  genre=self.testUserGenrelessGenre,
+                  duration=0)
+        data = {
+            "artistName": "Paul",
+        }
+        response = self.putSampleTrack(trackUuid=track.uuid, data=data)
+        assert response.status_code == status.HTTP_200_OK
+        assert Artist.objects.filter(
+            user=self.testUser, name=artistName).count() == 0
+
+    def test_notDeleteOldOneBecauseATrackLinkedToIt(self):
+        artistName = "a-ha"
+        artist = G(Artist, user=self.testUser, name=artistName)
+        track = G(LibraryTrack,
+                  user=self.testUser,
+                  title="Foire",
+                  artist=artist,
+                  genre=self.testUserGenrelessGenre,
+                  duration=0)
+        G(LibraryTrack,
+          user=self.testUser,
+          title="Josie",
+          artist=artist,
+          genre=self.testUserGenrelessGenre,
+          duration=0)
+        data = {
+            "artistName": "Paul",
+        }
+        response = self.putSampleTrack(trackUuid=track.uuid, data=data)
+        assert response.status_code == status.HTTP_200_OK
+        assert Artist.objects.filter(
+            user=self.testUser, name=artistName).count() == 1
+
+    def test_notDeleteOldOneBecauseAnAlbumLinkedToIt(self):
+        artistName = "a-ha"
+        artist = G(Artist, user=self.testUser, name=artistName)
+        track = G(LibraryTrack,
+                  user=self.testUser,
+                  title="Foire",
+                  artist=artist,
+                  genre=self.testUserGenrelessGenre,
+                  duration=0)
+        album = G(Album, user=self.testUser, name="Hunting High and Low", albumArtists=[artist])
+        G(LibraryTrack,
+          user=self.testUser,
+          title="Josie",
+          album=album,
+          genre=self.testUserGenrelessGenre,
+          duration=0)
+        data = {
+            "artistName": "Paul",
+        }
+        response = self.putSampleTrack(trackUuid=track.uuid, data=data)
+        assert response.status_code == status.HTTP_200_OK
+        assert Artist.objects.filter(
+            user=self.testUser, name=artistName).count() == 1
