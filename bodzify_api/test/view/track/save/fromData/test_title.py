ebd71fb032c26c2dfb156adfb63782012967a662
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 10:22:18 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/fromData/test_title.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from rest_framework import status
+from ddf import G
+from bodzify_api import settings
+from bodzify_api.model.track.LibraryTrack import LibraryTrack
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+import bodzify_api.service.AudioMetadataService as AudioMetadataService
+
+
+class TitleTestCase(TrackViewTestCase):
+
+    def test_notProvided(self):
+        title = "Mon Amour"
+        track = G(LibraryTrack,
+                  user=self.testUser,
+                  title=title,
+                  genre=self.testUserGenrelessGenre,
+                  duration=0)
+        data = {}
+        response = self.putSampleTrack(track.uuid, data=data)
+        assert response.status_code == status.HTTP_200_OK
+        assert self.savedTrack.title == title
+
+    def test_errorWhenNull(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "title" : None
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.title == ""
+
+    def test_empty(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "title" : ""
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.title == ""
+
+    def test_longest(self):
+        title = "a" * settings.TRACK_LANGUAGE_MAX_CHAR
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "title": title
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.title == title
