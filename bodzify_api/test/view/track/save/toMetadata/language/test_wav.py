89e187a66b1bf9dfbcd85359e04b17863aed8127
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 17:34:54 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/toMetadata/language/test_wav.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from rest_framework import status
+from bodzify_api import settings
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+import bodzify_api.service.AudioMetadataService as AudioMetadataService
+
+
+class WavTestCase(TrackViewTestCase):
+
+    def test_longest(self):
+        language = "a" * settings.TRACK_LANGUAGE_MAX_CHAR
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "language": language
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        languageKey = AudioMetadataService.METADATA_DICT_KEYS.LANGUAGE
+        assert self.savedTrackMetadata[languageKey] == language
+
+    def test_null(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/wav/0001.wav",
+            "language": None,
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        languageKey = AudioMetadataService.METADATA_DICT_KEYS.LANGUAGE
+        assert self.savedTrackMetadata[languageKey] in ["", None]
