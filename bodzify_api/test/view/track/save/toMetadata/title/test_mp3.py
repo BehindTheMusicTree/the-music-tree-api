8b663f795c7f82e3c30df2d07f4a467505234da3
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 17:46:52 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/toMetadata/title/test_mp3.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from rest_framework import status
+from bodzify_api import settings
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+import bodzify_api.service.AudioMetadataService as AudioMetadataService
+
+
+class Mp3TestCase(TrackViewTestCase):
+
+    def test_longest(self):
+        title = "a" * settings.TRACK_TITLE_MAX_CHAR
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/mp3/0037.mp3",
+            "title": title
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        titleKey = AudioMetadataService.METADATA_DICT_KEYS.TITLE
+        assert self.savedTrackMetadata[titleKey] == title
+
+    def test_null(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/mp3/0037.mp3",
+            "title": None
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        titleKey = AudioMetadataService.METADATA_DICT_KEYS.TITLE
+        assert self.savedTrackMetadata[titleKey] in ["", None]
