3ce25b37a6492bdf6820885a017378e4b8401a82
mignot <garcia.andreas.1991@gmail.com>
2023-04-07 17:31:19 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/toMetadata/genre/test_flac.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from rest_framework import status
+from bodzify_api import settings
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+import bodzify_api.service.AudioMetadataService as AudioMetadataService
+
+
+class FlacTestCase(TrackViewTestCase):
+
+    def test_longest(self):
+        genreName = "a" * settings.CRITERIA_NAME_MAX_CHAR
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/flac/0127.flac",
+            "genreName": genreName,
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        genreNameKey = AudioMetadataService.METADATA_DICT_KEYS.GENRE_NAME
+        assert self.savedTrackMetadata[genreNameKey] == genreName
+
+    def test_null(self):
+        data = {
+            "url": "https://lasonotheque.org/UPLOAD/flac/0127.flac",
+            "genreName": None,
+        }
+        response = self.extract(data=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        genreNameKey = AudioMetadataService.METADATA_DICT_KEYS.GENRE_NAME
+        assert self.savedTrackMetadata[genreNameKey] in ["", None]
