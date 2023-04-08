2a07dc08e2fcf216cb4c3234134debd3170204b7
mignot <garcia.andreas.1991@gmail.com>
2023-04-08 15:08:52 +0200

    Clean code

--- bodzify_api/test/view/track/save/create/file/test_wrongExtension.py
+++ bodzify_api/test/view/track/save/create/file/test_wrongExtension.py
@@ -7,10 +7,10 @@ from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
 @pytest.mark.django_db
 class ExtensionTestCase(TrackViewTestCase):
 
-    def test_errorWhenBadExtensionJpeg(self):
+    def test_jpeg(self):
         response = self.postSampleTrack("image.jpeg")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-    def test_errorWhenBadExtensionMp4(self):
+    def test_mp4(self):
         response = self.postSampleTrack("bad_extension.mp4")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
