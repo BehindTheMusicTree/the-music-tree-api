34ba66e8798db3acd6392d035297e4bc614d252c
mignot <garcia.andreas.1991@gmail.com>
2023-04-08 15:10:41 +0200

    Clean code

--- bodzify_api/test/view/track/save/fromMetadata/rating/kid3/flac/test_TrackPostViewRatingKid3Flac.py
+++ bodzify_api/test/view/track/save/fromMetadata/rating/kid3/flac/test_TrackPostViewRatingKid3Flac.py
@@ -7,76 +7,37 @@ from bodzify_api.model.track.LibraryTrack import LibraryTrack
 @pytest.mark.django_db
 class TrackPostViewTestCaseRatingKid3Flac(TrackViewTestCase):
 
-    fixtures = ['initial_data', 'TestUserData']
-    sampleDirectoryRelativePath = "test/view/track/post/rating/kid3/flac/sample/"
-
-
-    """
-    The uploaded flac track has a no rating from Kid3. The corresponding 
-    value in the app must then be None.
-    """
-    def test_trackPostRatingKid3FlacNoRating(self):
-        self._login(self.testUser)
+    def test_None(self):
         response = self.postSampleTrack("no rating.flac")
         trackUuid = response.json()[LibraryTrack.ATTRIBUTE_UUID_LABEL]
         track = LibraryTrack.objects.get(uuid=trackUuid)
         assert track.rating == None
 
-
-    """
-    The uploaded flac track has a 1 star rating from Kid3. The corresponding 
-    value in the app must then be 2.
-    """
-    def test_trackPostRatingKid3Flac1Star(self):
-        self._login(self.testUser)
+    def test_1Then2(self):
         response = self.postSampleTrack("1 star.flac")
         trackUuid = response.json()[LibraryTrack.ATTRIBUTE_UUID_LABEL]
         track = LibraryTrack.objects.get(uuid=trackUuid)
         assert track.rating == 2
     
-
-    """
-    The uploaded flac track has a 2 stars rating set from Kid3. The corresponding 
-    value in the app must then be 4.
-    """
-    def test_trackPostRatingKid3Flac2Stars(self):
-        self._login(self.testUser)
+    def test_2Then4(self):
         response = self.postSampleTrack("2 stars.flac")
         trackUuid = response.json()[LibraryTrack.ATTRIBUTE_UUID_LABEL]
         track = LibraryTrack.objects.get(uuid=trackUuid)
         assert track.rating == 4
     
-
-    """
-    The uploaded flac track has a 3 stars rating set from Kid3. The corresponding 
-    value in the app must then be 6.
-    """
-    def test_trackPostRatingKid3Flac3Stars(self):
-        self._login(self.testUser)
+    def test_3Then6(self):
         response = self.postSampleTrack("3 stars.flac")
         trackUuid = response.json()[LibraryTrack.ATTRIBUTE_UUID_LABEL]
         track = LibraryTrack.objects.get(uuid=trackUuid)
         assert track.rating == 6
 
-
-    """
-    The uploaded flac track has a 4 stars rating set from Kid3. The corresponding 
-    value in the app must then be 8.
-    """
-    def test_trackPostRatingKid3Flac4Stars(self):
-        self._login(self.testUser)
+    def test_4Then8(self):
         response = self.postSampleTrack("4 stars.flac")
         trackUuid = response.json()[LibraryTrack.ATTRIBUTE_UUID_LABEL]
         track = LibraryTrack.objects.get(uuid=trackUuid)
         assert track.rating == 8
 
-
-    """
-    The uploaded flac track has a 5 stars rating set from Kid3. The corresponding 
-    value in the app must then be 10.
-    """
-    def test_trackPostRatingKid3Flac5Stars(self):
-        self._login(self.testUser)
+    def test_5Then10(self):
         response = self.postSampleTrack("5 stars.flac")
         trackUuid = response.json()[LibraryTrack.ATTRIBUTE_UUID_LABEL]
         track = LibraryTrack.objects.get(uuid=trackUuid)
