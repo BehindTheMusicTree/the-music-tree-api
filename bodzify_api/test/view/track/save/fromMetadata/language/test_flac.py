51fc89137eedc3d245d9e3ce50195cd14fbbae04
mignot <garcia.andreas.1991@gmail.com>
2023-04-08 12:36:04 +0200

    Test language metadata reading

---
+++ bodzify_api/test/view/track/save/fromMetadata/language/test_flac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import pytest
+from rest_framework import status
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+
+
+@pytest.mark.django_db
+class FlacTestCase(TrackViewTestCase):
+    
+    def test_noneThenNone(self):
+        response = self.postSampleTrack(sampleFilename="noneThenNone.flac")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.language == None
+    
+    def test_longest(self):
+        response = self.postSampleTrack(sampleFilename="100CharLanguage.flac")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.language == "4bTyH6zRq7Psk7Y9Pydmb4gTYs9VCVvehPANcaZHbviunfxtl5Kwj" + \
+            "gJQdUyvX9WKnsv0KAtwAiWmi739Fqt2KsGZi7F3Fn9AXPI3"
+	
