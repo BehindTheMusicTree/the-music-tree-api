05ec991f8677b6588b10bc33cdb1d792a66b8773
mignot <garcia.andreas.1991@gmail.com>
2023-04-08 16:24:56 +0200

    Refactor tests

---
+++ bodzify_api/test/view/track/save/fromMetadata/rating/winamp/mp3/test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import pytest
+from rest_framework import status
+from bodzify_api.test.view.track.TrackViewTestCase import TrackViewTestCase
+
+
+@pytest.mark.django_db
+class TestCase(TrackViewTestCase):
+
+    def test_None(self):
+        response = self.postSampleTrack("no rating.mp3")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.rating == None
+
+    def test_1Then2(self):
+        response = self.postSampleTrack("1 star.mp3")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.rating == 2
+    
+    def test_2Then4(self):
+        response = self.postSampleTrack("2 stars.mp3")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.rating == 4
+    
+    def test_3Then6(self):
+        response = self.postSampleTrack("3 stars.mp3")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.rating == 6
+
+    def test_4Then8(self):
+        response = self.postSampleTrack("4 stars.mp3")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.rating == 8
+
+    def test_5Then10(self):
+        response = self.postSampleTrack("5 stars.mp3")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.rating == 10
