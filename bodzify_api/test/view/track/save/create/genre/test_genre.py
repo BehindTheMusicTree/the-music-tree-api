61e2a6f0886a602fbefa41580046cc5e9c0e1f3e
mignot <garcia.andreas.1991@gmail.com>
2023-04-11 09:56:37 +0200

    Test genre when not provided in new created track

---
+++ bodzify_api/test/view/track/save/create/genre/test_genre.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from rest_framework import status
+from ddf import G
+from bodzify_api.model.criteria.Criteria import CriteriaSpecialNames
+from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
+
+
+class TestCase(ApiViewTestCase):
+
+    def test_notProvidedThenGenreless(self):
+        response = self.postSampleTrack(
+            sampleFilename="notProvidedThenGenreless.mp3", dataJson={})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedTrack.genre.name == CriteriaSpecialNames.GENRE_GENRELESS
