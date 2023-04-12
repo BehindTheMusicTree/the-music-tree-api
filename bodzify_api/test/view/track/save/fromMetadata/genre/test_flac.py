29d6f213f0a44f984aac02e606d1b3a8aecf5cc3
mignot <garcia.andreas.1991@gmail.com>
2023-04-12 19:57:51 +0200

    Test genre reading from metadata

--- bodzify_api/test/view/track/save/fromMetadata/genre/test_flac.py
+++ bodzify_api/test/view/track/save/fromMetadata/genre/test_flac.py
@@ -5,16 +5,16 @@ from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
 
 
 @pytest.mark.django_db
-class FlacTestCase(ApiViewTestCase):
+class TestCase(ApiViewTestCase):
     
     def test_noneThenNone(self):
         response = self.postSampleTrack(sampleFilename="noneThenNone.flac")
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.savedTrack.artist == None
+        assert self.savedTrack.genre == None
     
     def test_longest(self):
-        response = self.postSampleTrack(sampleFilename="100CharArtistName.flac")
+        response = self.postSampleTrack(sampleFilename="100CharGenreName.flac")
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.savedTrack.artist.name == "4bTyH6zRq7Psk7Y9Pydmb4gTYs9VCVvehPANcaZHbviunfxtl5Kwj" + \
+        assert self.savedTrack.genre.name == "4bTyH6zRq7Psk7Y9Pydmb4gTYs9VCVvehPANcaZHbviunfxtl5Kwj" + \
             "gJQdUyvX9WKnsv0KAtwAiWmi739Fqt2KsGZi7F3Fn9AXPI3"
 	
--- bodzify_api/test/view/track/save/fromMetadata/genre/test_wav.py
+++ bodzify_api/test/view/track/save/fromMetadata/genre/test_wav.py
@@ -5,7 +5,7 @@ from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
 
 
 @pytest.mark.django_db
-class WavTestCase(ApiViewTestCase):
+class TestCase(ApiViewTestCase):
     
     def test_noneThenNone(self):
         response = self.postSampleTrack(sampleFilename="noneThenNone.wav")
@@ -13,9 +13,9 @@ class WavTestCase(ApiViewTestCase):
         assert self.savedTrack.artist == None
     
     def test_longest(self):
-        response = self.postSampleTrack(sampleFilename="100CharArtistName.wav")
+        response = self.postSampleTrack(sampleFilename="100CharGenreName.wav")
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.savedTrack.artist.name == "4bTyH6zRq7Psk7Y9Pydmb4gTYs9VCVvehPANcaZHbviunfxtl5Kwj" + \
+        assert self.savedTrack.genre.name == "4bTyH6zRq7Psk7Y9Pydmb4gTYs9VCVvehPANcaZHbviunfxtl5Kwj" + \
             "gJQdUyvX9WKnsv0KAtwAiWmi739Fqt2KsGZi7F3Fn9AXPI3"
 	
 	
