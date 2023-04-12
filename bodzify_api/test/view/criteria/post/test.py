557a5a00b0abae3721fb24e992a130716d15ffef
mignot <garcia.andreas.1991@gmail.com>
2023-04-12 18:31:21 +0200

    Test criteria creation

---
+++ bodzify_api/test/view/criteria/post/test.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+from rest_framework import status
+from ddf import G
+from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
+from bodzify_api.model.criteria.Criteria import CriteriaSpecialNames, \
+    ATTRIBUTES_LABEL as CRITERIA_ATTRIBUTES_LABEL
+    
+from bodzify_api import settings
+
+
+class TestCase(ApiViewTestCase):
+    
+    def test_longest(self):
+        genreName = "a" * settings.CRITERIA_NAME_MAX_CHAR
+        data = {
+            CRITERIA_ATTRIBUTES_LABEL.NAME: genreName
+        }
+        response = self.postGenre(dataJson=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedGenre.name == genreName
+    
+    def test_errorWhenTooLong(self):
+        data = {
+            CRITERIA_ATTRIBUTES_LABEL.NAME: "a" * (settings.CRITERIA_NAME_MAX_CHAR + 1)
+        }
+        response = self.postGenre(dataJson=data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+    
+    def test_errorWhenAllNameUsed(self):
+        data = {
+            CRITERIA_ATTRIBUTES_LABEL.NAME: CriteriaSpecialNames.GENRE_ALL
+        }
+        response = self.postGenre(dataJson=data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+    
+    def test_errorWhenGenrelessNameUsed(self):
+        data = {
+            CRITERIA_ATTRIBUTES_LABEL.NAME: CriteriaSpecialNames.GENRE_GENRELESS
+        }
+        response = self.postGenre(dataJson=data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+    
+    def test_parentAllWhenNoParentProvided(self):
+        data = {
+            CRITERIA_ATTRIBUTES_LABEL.NAME: "Rock"
+        }
+        response = self.postGenre(dataJson=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedGenre.parent.name == CriteriaSpecialNames.GENRE_ALL
+    
+    def test_withExistingParent(self):
+        data = {
+            CRITERIA_ATTRIBUTES_LABEL.NAME: "Rock"
+        }
+        self.postGenre(dataJson=data)
+        rockGenre = self.savedGenre
+        data = {
+            CRITERIA_ATTRIBUTES_LABEL.NAME: "Hard rock",
+            CRITERIA_ATTRIBUTES_LABEL.PARENT: rockGenre.uuid
+        }
+        response = self.postGenre(dataJson=data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.savedGenre.parent.uuid == rockGenre.uuid
+    
+    def test_errorWhenNotExistingParent(self):
+        data = {
+            CRITERIA_ATTRIBUTES_LABEL.NAME: "Hard rock",
+            CRITERIA_ATTRIBUTES_LABEL.PARENT: "notexisting"
+        }
+        response = self.postGenre(dataJson=data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+    
+    def test_errorWhenExtraField(self):
+        data = {
+            "notExistingField": "Koko"
+        }
+        response = self.postGenre(dataJson=data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
