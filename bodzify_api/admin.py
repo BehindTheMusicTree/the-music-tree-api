adf17e538c46abe28a487fea06f7269cf56c2575
mignot <garcia.andreas.1991@gmail.com>
2023-04-13 11:31:02 +0200

    Add criteria and simple playlist models to admin

--- bodzify_api/admin.py
+++ bodzify_api/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .model.playlist.CriteriaPlaylist import CriteriaPlaylist
+from .model.playlist.SimplePlaylist import SimplePlaylist
 from .model.track.LibraryTrack import LibraryTrack
 from .model.criteria.Criteria import Criteria
 from .model.criteria.CriteriaType import CriteriaType
@@ -10,6 +12,8 @@ from .model.Artist import Artist
 admin.site.register(Criteria)
 admin.site.register(CriteriaType)
 admin.site.register(Playlist)
+admin.site.register(SimplePlaylist)
+admin.site.register(CriteriaPlaylist)
 admin.site.register(PlaylistType)
 admin.site.register(LibraryTrack)
 admin.site.register(Album)
