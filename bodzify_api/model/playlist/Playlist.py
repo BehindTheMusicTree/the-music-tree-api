4aad26b3e93546dc921d80c800430943109394ef
mignot <garcia.andreas.1991@gmail.com>
2023-05-31 15:15:34 +0200

    Remove name property

--- bodzify_api/model/playlist/Playlist.py
+++ bodzify_api/model/playlist/Playlist.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import shortuuid
 from django.db import models
 from django.contrib.auth.models import User
@@ -22,8 +21,3 @@ class Playlist(PolymorphicModel):
         primary_key=True, default=shortuuid.uuid, max_length=22, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     addedOn = models.DateTimeField(auto_now_add=True, editable=False)
-
-    
-    @property
-    def name(self) -> str:
-        return None
