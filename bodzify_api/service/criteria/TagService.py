a8ddafc6369c8d20acefc2854a750bd760a5f1fc
mignot <garcia.andreas.1991@gmail.com>
2023-05-31 11:36:53 +0200

    Add tag service

---
+++ bodzify_api/service/criteria/TagService.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from django.contrib.auth.models import User
+from bodzify_api.model.criteria.Criteria import Criteria
+from bodzify_api.model.playlist.criteria.TagPlaylist import TagPlaylist
+
+
+class TagService:
+    def createLinkedPlaylist(self, user: User, criteria: Criteria):
+        TagPlaylist(user=user, criteria=criteria).save()
