5ba119d872186eaf701c150142721dec92a24ec6
mignot <garcia.andreas.1991@gmail.com>
2023-05-31 11:18:53 +0200

    Add tag playlist model

---
+++ bodzify_api/model/playlist/criteria/TagPlaylist.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from bodzify_api.model.playlist.criteria.CriteriaPlaylist import CriteriaPlaylist
+
+
+class SPECIAL_NAMES:
+    TAGLESS = "Tagless"
+    
+
+class TagPlaylist(CriteriaPlaylist):
+
+    TYPE_LABEL = 'tag'
+
+    @property
+    def noCriteriaName(self) -> str:
+        return SPECIAL_NAMES.TAGLESS
