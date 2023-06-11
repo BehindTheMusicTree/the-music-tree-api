dcbfeebf1a28ff4bc3f32711bb2b7375a6526840
mignot <garcia.andreas.1991@gmail.com>
2023-06-11 14:30:59 +0200

    Clean code

--- bodzify_api/test/view/playlist/save/post/simple/test.py
+++ bodzify_api/test/view/playlist/save/post/simple/test.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-
+from rest_framework import status
 from bodzify_api import settings
 from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
-from bodzify_api.model.playlist.SimplePlaylist import \
-    ATTRIBUTES_LABEL as SIMPLE_PLAYLIST_ATTRIBUTES_LABEL
+from bodzify_api.model.playlist.Playlist import \
+    ATTRIBUTES_LABEL as PLAYLIST_ATTRIBUTES_LABEL
 
 
 class TestCase(ApiViewTestCase):
     
     def test_longestCustomName(self):
         data = {
-            SIMPLE_PLAYLIST_ATTRIBUTES_LABEL.CUSTOM_NAME: "a" * settings.CRITERIA_NAME_MAX_CHAR
+            PLAYLIST_ATTRIBUTES_LABEL.NAME: "a" * settings.CRITERIA_NAME_MAX_CHAR
         }
-        response = self.post
+        response = self.postSimplePlaylist(data)
+        assert response.status_code == status.HTTP_201_CREATED
