878eaa3500506a228781d06aa05cebb744061ee6
mignot <garcia.andreas.1991@gmail.com>
2023-06-12 17:39:12 +0200

    Clean code

--- bodzify_api/view/viewset/playlist/GenrePlaylistViewSet.py
+++ bodzify_api/view/viewset/playlist/GenrePlaylistViewSet.py
@@ -2,8 +2,6 @@
 from bodzify_api.model.playlist.criteria.GenrePlaylist import GenrePlaylist
 from bodzify_api.serializer.playlist.output.GenrePlaylistWithTrackSerializer import \
     GenrePlaylistWithTracksSerializer
-from bodzify_api.serializer.playlist.output.PlaylistWithTrackSerializer import \
-    PlaylistWithTracksSerializer
 from bodzify_api.view.viewset.MultiSerializerViewSet import MultiSerializerViewSet
 from bodzify_api.model.playlist.Playlist import ATTRIBUTES_LABEL as PLAYLIST_ATTRIBUTES_LABEL
 from bodzify_api.model.playlist.criteria.CriteriaPlaylist import \
