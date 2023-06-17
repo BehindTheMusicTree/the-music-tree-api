5e46968c1fcdb0b8f8da50ba788a58ffaf1b9b2d
mignot <garcia.andreas.1991@gmail.com>
2023-06-17 11:14:57 +0200

    Change user to bodzify when setting up the filesystem

--- scripts/setup_filesystem.sh
+++ scripts/setup_filesystem.sh
@@ -1,11 +1,11 @@
 mediaDir=/var/lib/bodzify-api/media/
 
 sudo mkdir -p $mediaDir
-sudo chown $USER $mediaDir
+sudo chown bodzify $mediaDir
 sudo chmod 775 $mediaDir
 
 logDir=/var/log/bodzify-api/
 
 sudo mkdir -p $logDir
-sudo chown $USER $logDir
+sudo chown bodzify $logDir
 sudo chmod 775 $logDir
