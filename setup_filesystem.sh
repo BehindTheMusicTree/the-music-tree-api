deeb2585a899bc147b67b3dae1f31997218d8ced
mignot <andreas.garcia@bodzify.com>
2023-10-09 15:26:24 +0200

    Add webhook and webhook scripts creations to set up script

--- setup_filesystem.sh
+++ setup_filesystem.sh
@@ -1,11 +1,23 @@
 mediaDir=/var/lib/bodzify-api/media/
 
 sudo mkdir -p $mediaDir
-sudo chown $USER $mediaDir
+sudo chown bodzify $mediaDir
 sudo chmod 775 $mediaDir
 
 logDir=/var/log/bodzify-api/
 
 sudo mkdir -p $logDir
-sudo chown $USER $logDir
+sudo chown bodzify $logDir
 sudo chmod 775 $logDir
+
+webhookDir=/var/webhooks/
+
+sudo mkdir -p $webhookDir
+sudo chown bodzify $webhookDir
+sudo chmod 775 $webhookDir
+
+webhookScriptsDir=${webhookDir}scripts/
+
+sudo mkdir -p $webhookScriptsDir
+sudo chown bodzify $webhookScriptsDir
+sudo chmod 775 $webhookScriptsDir
