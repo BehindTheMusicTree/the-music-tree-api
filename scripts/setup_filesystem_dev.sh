9e01e1c7467059f16d74e0145c4b785f715a2c49
mignot <andreas.garcia@bodzify.com>
2023-10-17 19:18:15 +0200

    Adapt filesystem setup to dev config

---
+++ scripts/setup_filesystem_dev.sh
@@ -0,0 +1,13 @@
+mediaDir=/var/lib/bodzify-api/media/
+
+sudo mkdir -p $mediaDir
+sudo chmod 775 $mediaDir
+
+logDir=/var/log/bodzify-api/
+
+sudo mkdir -p $logDir
+sudo touch ${logDir}access.log
+sudo touch ${logDir}general.log
+sudo touch ${logDir}info.log
+sudo chmod -R 775 $logDir
+
