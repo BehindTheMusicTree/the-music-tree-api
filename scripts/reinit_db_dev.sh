b5850a8955ecb21deb734cec9cb8fdc691041bd1
mignot <andreas.garcia@bodzify.com>
2023-10-18 12:15:09 +0200

    Use command lines to reinitialise db

--- scripts/reinit_db_dev.sh
+++ scripts/reinit_db_dev.sh
@@ -1,7 +1,8 @@
 projectDir=~/Git/bodzify-api
 managePath=~/Git/bodzify-api/manage.py
-sudo -u postgres dropdb -e bodzify_api -f
-sudo -u postgres -H -- psql -c "create database bodzify_api with owner django;"
+dropdb -e bodzify_api -f
+createdb bodzify_api
+createdb -O django bodzify_api
 sudo rm -r $projectDir/bodzify_api/migrations/*
 sudo rm -r /var/lib/bodzify-api/*
 python3 $managePath migrate
