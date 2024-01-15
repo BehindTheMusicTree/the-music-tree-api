470af3eacb4d35222a4f0ae72f14254ccde6b807
mignot <andreas.garcia@bodzify.com>
2024-01-15 14:42:03 +0100

    Try fix CSRF_TRUSTED_ORIGINS

--- bodzify_api/settings_test.py
+++ bodzify_api/settings_test.py
@@ -2,9 +2,10 @@
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = ['https://bodzify.com']
+
 
 ALLOWED_HOSTS = [
     'bodzify.com',
     'www.bodzify.com',
-    'https://bodzify.com'
 ]
