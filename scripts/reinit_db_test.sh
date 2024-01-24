e81a9b7881c2078e2dd7aa6d69dec04f9f66e522
mignot <andreas.garcia@bodzify.com>
2024-01-24 16:58:11 +0100

    Fix fixtures

--- scripts/reinit_db_test.sh
+++ scripts/reinit_db_test.sh
@@ -7,7 +7,7 @@ sudo rm -r /var/log/bodzify-api/*
 
 docker exec BODZIFY_API python manage.py migrate
 docker exec BODZIFY_API python manage.py migrate --fake
-docker exec BODZIFY_API python manage.py migmakemigrationsrate
+docker exec BODZIFY_API python manage.py makemigrations
 docker exec BODZIFY_API python manage.py migrate --fake-initial
 docker exec BODZIFY_API python manage.py makemigrations bodzify_api
 docker exec BODZIFY_API python manage.py migrate
@@ -15,3 +15,4 @@ docker exec BODZIFY_API python manage.py loaddata app_initial_data
 docker exec BODZIFY_API python manage.py loaddata admin_user_initial_data_test
 docker exec BODZIFY_API python manage.py loaddata mobile_test_user_initial_data
 docker exec BODZIFY_API python manage.py loaddata postman_test_user_initial_data
+docker exec BODZIFY_API python manage.py loaddata ultimate_music_guide_user_initial_data_test
