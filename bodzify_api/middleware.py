bcd9079b310321181afc72036c0b619137b3831a
mignot <andreas.garcia@bodzify.com>
2024-01-25 13:42:11 +0100

    Add header info to request log

--- bodzify_api/middleware.py
+++ bodzify_api/middleware.py
@@ -1,15 +1,23 @@
 import logging
 
+def _generate_log_about_headers(request):
+    logMessage = "Headers: "
+    for header in request.headers:
+        logMessage += f"{header}: {request.headers[header]} "
+    return logMessage
+
 class RequestLoggingMiddleware:
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        logMessage = f"Incoming Request: {request.method} {request.path}"
+        logMessage = f"Incoming Request: {request.method} {request.path} {request.META['REMOTE_ADDR']} "
         requestLogger = logging.getLogger('request')
         requestLogger.info(logMessage)
         requestDebugLogger = logging.getLogger('django.request')
         requestDebugLogger.info(logMessage)
+        requestDebugLogger.info(_generate_log_about_headers(request))
 
         response = self.get_response(request)
         return response
