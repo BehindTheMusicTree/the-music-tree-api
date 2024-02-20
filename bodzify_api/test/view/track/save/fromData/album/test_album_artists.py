73c612907ff5403c7fe3e28e590871dc29a2db6d
mignot <andreas.garcia@bodzify.com>
2024-02-20 18:50:05 +0100

    Clean code

--- bodzify_api/test/view/track/save/fromData/album/test_album_artists.py
+++ bodzify_api/test/view/track/save/fromData/album/test_album_artists.py
@@ -1,81 +1,83 @@
 #!/usr/bin/env python
-from rest_framework import status
+
 from ddf import G
+from rest_framework import status
+
 from bodzify_api import settings
 from bodzify_api.model.Album import Album
 from bodzify_api.model.Artist import Artist
 from bodzify_api.model.track.LibraryTrack import LibraryTrack
-from bodzify_api.serializer.track.input.schema.TrackSaveSchemaSerializer import \
-    ATTRIBUTES_LABEL as TRACK_SCHEMA_ATTRIBUTES_LABEL
 from bodzify_api.serializer.track.input.schema.TrackExtractSchemaSerializer import \
     ATTRIBUTES_LABEL as TRACK_SCHEMA_EXTRACT_ATTRIBUTES_LABEL
+from bodzify_api.serializer.track.input.schema.TrackSaveSchemaSerializer import \
+    ATTRIBUTES_LABEL as TRACK_SCHEMA_ATTRIBUTES_LABEL
 from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
 
 
 class TestCase(ApiViewTestCase):
 
     def test_longest(self):
-        album_artistsName = "a" * settings.ALBUM_ARTISTS_FIELD_LENGTH_MAX
+        album_artists_name = "a" * settings.ALBUM_ARTISTS_FIELD_LENGTH_MAX
         track = G(LibraryTrack,
                   user=self.test_user,
                   title="Love",
                   duration=0)
         data = {
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: "Chuck",
-            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: album_artistsName
+            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: album_artists_name
         }
         response = self.put_sample_track(track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
 
         assert list(self.saved_track.album.album_artists.all())[
-            0].name == album_artistsName
+            0].name == album_artists_name
 
-    def test_whenalbum_artistsExist(self):
-        album_artistsName = "Muse"
-        artist = G(Artist, user=self.test_user, name=album_artistsName)
+    def test_when_album_artists_exist(self):
+        album_artists_name = "Muse"
+        artist = G(Artist, user=self.test_user, name=album_artists_name)
         track = G(LibraryTrack,
                   user=self.test_user,
                   title="Love",
                   duration=0)
         data = {
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: "Chuck",
-            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: album_artistsName
+            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: album_artists_name
         }
         response = self.put_sample_track(track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
 
-        album_artistsList = list(self.saved_track.album.album_artists.all())
-        assert len(album_artistsList) == 1
-        assert album_artistsList[0].uuid == artist.uuid
+        album_artists_list = list(self.saved_track.album.album_artists.all())
+        assert len(album_artists_list) == 1
+        assert album_artists_list[0].uuid == artist.uuid
 
-    def test_whenOneOutOfTwoalbum_artistsExist(self):
-        museartist_name = "Muse"
-        museArtist = G(Artist, user=self.test_user, name=museartist_name)
-        billartist_name = "Bill"
+    def test_when_one_out_of_two_album_artists_exist(self):
+        muse_artist_name = "Muse"
+        muse_artist = G(Artist, user=self.test_user, name=muse_artist_name)
+        bill_artist_name = "Bill"
         track = G(LibraryTrack,
                   user=self.test_user,
                   title="Love",
                   duration=0)
-        
+
         data = {
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: "Starlight",
-            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: \
-                museartist_name + ", " + billartist_name
+            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING:
+            muse_artist_name + ", " + bill_artist_name
         }
         response = self.put_sample_track(track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
 
-        museAlbumArtist = self.saved_track.album.album_artists.get(
-            name=museartist_name)
-        assert museAlbumArtist.uuid == museArtist.uuid
+        muse_album_artist = self.saved_track.album.album_artists.get(
+            name=muse_artist_name)
+        assert muse_album_artist.uuid == muse_artist.uuid
         assert self.saved_track.album.album_artists.filter(
-            name=billartist_name).exists()
+            name=bill_artist_name).exists()
         album_artists = list(self.saved_track.album.album_artists.all())
         assert len(album_artists) == 2
 
-    def test_whenExistingAlbumWithSamealbum_artists(self):
-        album_artistsName = "Muse"
-        artist = G(Artist, user=self.test_user, name=album_artistsName)
+    def test_when_existing_album_with_same_album_artists(self):
+        album_artists_name = "Muse"
+        artist = G(Artist, user=self.test_user, name=album_artists_name)
         album_name = "Absolution"
         album = G(Album, user=self.test_user,
                   name=album_name, album_artists=[artist])
@@ -83,32 +85,32 @@ class TestCase(ApiViewTestCase):
                   user=self.test_user,
                   title="Love",
                   duration=0)
-        
+
         data = {
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: album_name,
-            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: album_artistsName
+            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: album_artists_name
         }
         response = self.put_sample_track(track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
-        
-        assert self.saved_track.album.uuid == album.uuid
-        album_artistsList = list(self.saved_track.album.album_artists.all())
-        assert len(album_artistsList) == 1
-        assert album_artistsList[0].uuid == artist.uuid
 
-    def test_null_then_none(self):        
+        assert self.saved_track.album.uuid == album.uuid
+        album_artists_list = list(self.saved_track.album.album_artists.all())
+        assert len(album_artists_list) == 1
+        assert album_artists_list[0].uuid == artist.uuid
+
+    def test_null_then_none(self):
         track = G(LibraryTrack,
                   user=self.test_user,
                   title="Love",
                   duration=0)
-        
+
         data = {
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: "Chuck",
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: None
         }
         response = self.put_sample_track(track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
-        
+
         assert len(list(self.saved_track.album.album_artists.all())) == 0
 
     def test_empty_then_none(self):
@@ -116,17 +118,17 @@ class TestCase(ApiViewTestCase):
                   user=self.test_user,
                   title="Love",
                   duration=0)
-        
+
         data = {
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: "Chuck",
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: ""
         }
         response = self.put_sample_track(track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
-        
+
         assert len(list(self.saved_track.album.album_artists.all())) == 0
 
-    def test_errorWhenAlbumMissing(self):
+    def test_error_when_album_missing(self):
         track_url = "https://lasonotheque.org/UPLOAD/wav/0001.wav"
         data = {
             TRACK_SCHEMA_EXTRACT_ATTRIBUTES_LABEL.URL: track_url,
@@ -135,7 +137,7 @@ class TestCase(ApiViewTestCase):
         response = self.extract(data=data)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-    def test_errorWhenAlbumNull(self):
+    def test_error_when_album_null(self):
         track_url = "https://lasonotheque.org/UPLOAD/wav/0001.wav"
         data = {
             TRACK_SCHEMA_EXTRACT_ATTRIBUTES_LABEL.URL: track_url,
@@ -145,95 +147,95 @@ class TestCase(ApiViewTestCase):
         response = self.extract(data=data)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-    def test_newAlbumWhenalbum_artistsNotProvided(self):
-        sumArtist = G(Artist, user=self.test_user, name="Sum 41")
+    def test_new_album_when_album_artists_not_provided(self):
+        sum_artist = G(Artist, user=self.test_user, name="Sum 41")
         album_name = "Chuck"
-        chuckAlbum = G(Album, user=self.test_user,
-                       name=album_name, album_artists=[sumArtist])
-        
+        chuck_album = G(Album, user=self.test_user,
+                        name=album_name, album_artists=[sum_artist])
+
         track = G(LibraryTrack,
                   user=self.test_user,
                   title="Love",
                   duration=0)
-        
+
         data = {
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: album_name
         }
         response = self.put_sample_track(track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
-        
-        assert self.saved_track.album_id != chuckAlbum.uuid
+
+        assert self.saved_track.album_id != chuck_album.uuid
         assert len(list(self.saved_track.album.album_artists.all())) == 0
 
-    def test_sentTwiceButShouldBeCreatedOnce(self):
+    def test_sent_twice_but_should_be_created_once(self):
         artist_name = "Sum"
         album_name = "Chuck"
-        
+
         track = G(LibraryTrack,
                   user=self.test_user,
                   title="Love",
                   duration=0)
-        
+
         data = {
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: album_name,
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: artist_name + "," + artist_name
         }
         response = self.put_sample_track(track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
-        
+
         assert Album.objects.get(
             user=self.test_user, name=album_name).album_artists.count() == 1
         assert Artist.objects.filter(
             user=self.test_user, name=artist_name).count() == 1
 
-    def test_existingAlbumWithSameNameButDifferentAlbumArtist(self):
-        kendalartist_name = "Kendal"
-        kendalArtist = G(Artist, user=self.test_user, name=kendalartist_name)
+    def test_existing_album_with_same_name_but_different_album_artist(self):
+        kendal_artist_name = "Kendal"
+        kendal_artist = G(Artist, user=self.test_user, name=kendal_artist_name)
         album_name = "Hello"
-        hello1Album = G(Album, user=self.test_user,
-                        name=album_name, album_artists=[kendalArtist])
+        hello1_album = G(Album, user=self.test_user,
+                         name=album_name, album_artists=[kendal_artist])
         track = G(LibraryTrack,
                   user=self.test_user,
                   title="Joie",
-                  album=hello1Album,
+                  album=hello1_album,
                   duration=0)
-        robertdeniroArtist = G(
+        robertdeniro_artist = G(
             Artist, user=self.test_user, name="Robert De Niro")
-        hello2Album = G(Album, user=self.test_user, name=album_name,
-                        album_artists=[robertdeniroArtist])
-        
+        hello2_album = G(Album, user=self.test_user, name=album_name,
+                         album_artists=[robertdeniro_artist])
+
         data = {
-            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: hello2Album.name,
-            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: robertdeniroArtist.name,
+            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: hello2_album.name,
+            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: robertdeniro_artist.name,
         }
         response = self.put_sample_track(track_uuid=track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
-        
-        assert self.saved_track.album.uuid == hello2Album.uuid
+
+        assert self.saved_track.album.uuid == hello2_album.uuid
         assert Album.objects.filter(
             user=self.test_user, name=album_name).count() == 1
         assert Artist.objects.filter(
-            user=self.test_user, name=kendalartist_name).exists() == False
+            user=self.test_user, name=kendal_artist_name).exists() == False
 
-    def test_oneExistingAlbumArtistAndOneNot(self):
-        pnlArtist = G(Artist, user=self.test_user, name="PNL")
-        tristeartist_name = "Triste"
+    def test_one_existing_album_artist_and_one_not(self):
+        pnl_artist = G(Artist, user=self.test_user, name="PNL")
+        triste_artist_name = "Triste"
         track = G(LibraryTrack,
                   user=self.test_user,
                   title="Joie",
                   duration=0)
-        
+
         data = {
             TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_NAME: "VOLART",
-            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING: \
-                pnlArtist.name + "," + tristeartist_name
+            TRACK_SCHEMA_ATTRIBUTES_LABEL.ALBUM_ARTISTS_NAMES_STRING:
+            pnl_artist.name + "," + triste_artist_name
         }
         response = self.put_sample_track(track.uuid, data_json=data)
         assert response.status_code == status.HTTP_200_OK
-        
-        album_artistsList = list(self.saved_track.album.album_artists.all())
-        assert len(album_artistsList) == 2
-        assert pnlArtist in album_artistsList
-        tristeArtist = Artist.objects.get(
-            user=self.test_user, name=tristeartist_name)
-        assert tristeArtist in album_artistsList
+
+        album_artists_list = list(self.saved_track.album.album_artists.all())
+        assert len(album_artists_list) == 2
+        assert pnl_artist in album_artists_list
+        triste_artist = Artist.objects.get(
+            user=self.test_user, name=triste_artist_name)
+        assert triste_artist in album_artists_list
