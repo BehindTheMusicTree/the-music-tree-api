e21712ecfe91ebb7f267c873b00fa58bf33991ea
mignot <andreas.garcia@bodzify.com>
2024-02-22 13:42:41 +0100

    Add SimplePlaylistWithTracksSerializer

---
+++ bodzify_api/serializer/playlist/simple/output/SimplePlaylistWithTracksSerializer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from bodzify_api.model.playlist.Playlist import ATTRIBUTES_LABEL as PLAYLIST_ATTRIBUTES_LABEL, \
+    FOREIGN_MODEL_RELATIONS_STR as PLAYLIST_FOREIGN_MODEL_RELATIONS_STR
+from bodzify_api.model.playlist.SimplePlaylist import SimplePlaylist, \
+    ATTRIBUTES_LABELS as SIMPLE_PLAYLIST_ATTRIBUTES_LABEL
+from bodzify_api.serializer.playlist.simple.output.SimplePlaylistWithoutTrackSerializer import \
+    SimplePlaylistWithoutTrackSerializer
+from bodzify_api.serializer.track.output.TrackWithoutAlbumAndPlaylistSerializer import \
+    TrackWithoutAlbumAndPlaylistSerializer
+
+
+class SimplePlaylistWithTracksSerializer(SimplePlaylistWithoutTrackSerializer):
+    library_tracks = TrackWithoutAlbumAndPlaylistSerializer(
+        source=PLAYLIST_FOREIGN_MODEL_RELATIONS_STR.LIBRARY_TRACKS, many=True)
+
+    class Meta:
+        model = SimplePlaylist
+        fields = [PLAYLIST_ATTRIBUTES_LABEL.UUID,
+                  SIMPLE_PLAYLIST_ATTRIBUTES_LABEL.NAME,
+                  PLAYLIST_ATTRIBUTES_LABEL.ADDED_ON,
+                  PLAYLIST_ATTRIBUTES_LABEL.LIBRARY_TRACKS_COUNT,
+                  PLAYLIST_ATTRIBUTES_LABEL.LIBRARY_TRACKS]
