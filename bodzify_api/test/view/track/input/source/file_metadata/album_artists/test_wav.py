359284c3aff2975f81a0be4001cb9203fd844039
mignot <andreas.garcia@bodzify.com>
2024-02-23 02:46:02 +0100

    Reorganise track tests

--- bodzify_api/test/view/track/input/source/file_metadata/album_artists/test_wav.py
+++ bodzify_api/test/view/track/input/source/file_metadata/album_artists/test_wav.py
@@ -11,12 +11,12 @@ from bodzify_api.test.view.track.input.source.file_metadata.AttributeFromFileMet
 class TestCase(AttributeFromFileMetadataTestCase):
 
     def test_none_then_none(self):
-        response = self.post_sample_library_track(sample_filename="none.mp3")
+        response = self.post_sample_library_track(sample_filename="none.wav")
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_library_track.album == None
+        assert self.saved_library_track.album.album_artists.count() == 0
 
     def test_longest(self):
-        response = self.post_sample_library_track(sample_filename="100_char_album_name.mp3")
+        response = self.post_sample_library_track(sample_filename="100_char_album_artists_name.wav")
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_library_track.album.name == "4bTyH6zRq7Psk7Y9Pydmb4gTYs9VCVvehPANcaZHbviunfxtl5Kwj" + \
-            "gJQdUyvX9WKnsv0KAtwAiWmi739Fqt2KsGZi7F3Fn9AXPI3"
+        assert self.saved_library_track.album.album_artists.all().first().name == "4bTyH6zRq7Psk7Y9Pydmb4g" \
+            + "TYs9VCVvehPANcaZHbviunfxtl5KwjgJQdUyvX9WKnsv0KAtwAiWmi739Fqt2KsGZi7F3Fn9AXPI3"
