3886b637152e3e7272d21d7a3b2f023818617d7f
mignot <andreas.garcia@bodzify.com>
2024-02-23 13:53:34 +0100

    Reorganise track tests

---
+++ bodzify_api/test/view/track/input/source/file_metadata/rating/LibraryTrackFromFileMetadataRatingWithHalfAttributeTestCase.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from abc import abstractmethod
+import pytest
+
+from bodzify_api.test.view.track.input.source.file_metadata.rating.LibraryTrackFromFileMetadataRatingAttributeTestCase \
+    import LibTrackFromFileMetadataRatingAttributeTestCase
+
+
+@pytest.mark.django_db
+class LibraryTrackFromFileMetadataRatingWithHalfAttributeTestCase(LibTrackFromFileMetadataRatingAttributeTestCase):
+
+    @abstractmethod
+    def test_0_then_0(self):
+        pass
+
+    @abstractmethod
+    def test_0_and_half_then_1(self):
+        pass
+
+    @abstractmethod
+    def test_1_and_half_then_3(self):
+        pass
+
+    @abstractmethod
+    def test_2_and_half_then_5(self):
+        pass
+
+    @abstractmethod
+    def test_3_and_half_then_7(self):
+        pass
+
+    @abstractmethod
+    def test_4_and_half_then_9(self):
+        pass
