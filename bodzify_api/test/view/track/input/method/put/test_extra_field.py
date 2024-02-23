0b3b0233cde6ae87af9a36d5696aa128d1651ee6
mignot <andreas.garcia@bodzify.com>
2024-02-23 02:31:43 +0100

    Reorganize track tests

--- bodzify_api/test/view/track/input/method/put/test_extra_field.py
+++ bodzify_api/test/view/track/input/method/put/test_extra_field.py
@@ -5,15 +5,15 @@ from bodzify_api.model.track.LibraryTrack import LibraryTrack
 from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
 
 
-class ExtraFieldTestCase(ApiViewTestCase):
+class TestCase(ApiViewTestCase):
 
     def test_error(self):
-        track = G(LibraryTrack, 
+        track = G(LibraryTrack,
                   user=self.test_user,
                   title="Foire",
                   duration=0)
         data = {
             "nonExistingField": "oifjqoif",
         }
-        response = self.put_sample_track(track_uuid=track.uuid, data_json=data)
+        response = self.put_library_track(library_track_uuid=track.uuid, data_json=data)
         assert response.status_code == status.HTTP_400_BAD_REQUEST
