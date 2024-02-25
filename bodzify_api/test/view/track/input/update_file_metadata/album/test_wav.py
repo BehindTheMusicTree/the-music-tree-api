69ec9e2ffb13845c2ef4eeaabffabf3f8f633a18
mignot <andreas.garcia@bodzify.com>
2024-02-25 15:13:44 +0100

    Reorganise track tests

---
+++ bodzify_api/test/view/track/input/update_file_metadata/album/test_wav.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from bodzify_api import settings
+import bodzify_api.AudioMetadataManager as AudioMetadataManager
+from bodzify_api.serializer.track.input.schema.LibTrackSchemaSaveSerializer import FIELDS as SAVE_FIELDS
+from bodzify_api.test.view.track.input.update_file_metadata.UpdateFileMetadataStringTestCase import \
+    UpdateFileMetadataStringTestCase
+
+
+class TestCase(UpdateFileMetadataStringTestCase):
+
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(save_field=SAVE_FIELDS.ALBUM_NAME,
+                         metadata_dict_key=AudioMetadataManager.METADATA_DICT_KEYS.ALBUM_NAME,
+                         file_extension='wav',
+                         length_max=settings.ALBUM_NAME_LENGTH_MAX,
+                         methodName=methodName)
