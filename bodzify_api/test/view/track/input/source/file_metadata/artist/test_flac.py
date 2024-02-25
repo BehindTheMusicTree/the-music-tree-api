3160a48400f8c6e255d34f0f2b06cccc438125fc
mignot <andreas.garcia@bodzify.com>
2024-02-25 15:10:11 +0100

    Reorganise track tests

---
+++ bodzify_api/test/view/track/input/source/file_metadata/artist/test_flac.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import pytest
+from rest_framework import status
+
+from bodzify_api.test.view.track.input.source.file_metadata.AttributeFromFileMetadataTestCase import \
+    AttributeFromFileMetadataTestCase
+
+
+@pytest.mark.django_db
+class TestCase(AttributeFromFileMetadataTestCase):
+
+    def test_none_then_none(self):
+        response = self.post_lib_track_with_generic_sample_no_tags(generic_sample_extension="flac")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_lib_track.artist == None
+
+    def test_longest(self):
+        response = self.post_lib_track_with_generic_sample_tags_max_length_of_a(generic_sample_extension="flac")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_lib_track.artist.name == "4bTyH6zRq7Psk7Y9Pydmb4gTYs9VCVvehPANcaZHbviunfxtl5Kwj" + \
+            "gJQdUyvX9WKnsv0KAtwAiWmi739Fqt2KsGZi7F3Fn9AXPI3"
