029e846cd799a3e0f305d5ee51ff4c0927ed6566
mignot <andreas.garcia@bodzify.com>
2024-02-25 15:10:13 +0100

    Reorganise track tests

---
+++ bodzify_api/test/view/track/input/update_file_metadata/test_without_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from rest_framework import status
+from ddf import G
+from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
+from bodzify_api.model.track.LibraryTrack import LibraryTrack
+from bodzify_api.serializer.track.input.schema.LibTrackSchemaPostSerializer import FIELDS as POST_FIELDS
+
+
+class TestCase(ApiViewTestCase):
+
+    def test_without_a_file_and_a_title_then_ok(self):
+        track = G(LibraryTrack,
+                  user=self.test_user,
+                  title="Foire",
+                  duration=0)
+        data = {
+            POST_FIELDS.TITLE: "Jobo"
+        }
+        response = self.put_lib_track(lib_track_uuid=track.uuid, data_json=data)
+        assert response.status_code == status.HTTP_200_OK
