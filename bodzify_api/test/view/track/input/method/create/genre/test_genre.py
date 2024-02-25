208d66408a7d7137f01883406331d5f546d77d5d
mignot <andreas.garcia@bodzify.com>
2024-02-25 14:19:39 +0100

    Reorganise track tests

--- bodzify_api/test/view/track/input/method/create/genre/test_genre.py
+++ bodzify_api/test/view/track/input/method/create/genre/test_genre.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 from rest_framework import status
 from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
 
@@ -6,6 +7,6 @@ from bodzify_api.test.view.ApiViewTestCase import ApiViewTestCase
 class TestCase(ApiViewTestCase):
 
     def test_not_povided_then_none(self):
-        response = self.post_sample_track(sample_filename="notProvided.mp3", data_json={})
+        response = self.post_lib_track_with_specific_sample(specific_sample_filename="not_provided.mp3", data_json={})
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_track.genre == None
+        assert self.saved_lib_track.genre == None
