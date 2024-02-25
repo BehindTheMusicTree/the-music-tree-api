65e356cc33abad6d306e32ea91b8b8724907073c
mignot <andreas.garcia@bodzify.com>
2024-02-25 15:13:42 +0100

    Reorganise track tests

--- bodzify_api/test/view/track/input/source/file_metadata/genre/test_mp3.py
+++ bodzify_api/test/view/track/input/source/file_metadata/genre/test_mp3.py
@@ -11,12 +11,11 @@ from bodzify_api.test.view.track.input.source.file_metadata.AttributeFromFileMet
 class TestCase(AttributeFromFileMetadataTestCase):
 
     def test_none_then_none(self):
-        response = self.post_sample_lib_track(sample_filename="none.mp3")
+        response = self.post_lib_track_with_generic_sample_no_tags(generic_sample_extension="mp3")
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_lib_track.genre == None
 
     def test_longest(self):
-        response = self.post_sample_lib_track(
-            sample_filename="50_char_genre_name.mp3")
+        response = self.post_lib_track_with_generic_sample_tags_max_length_of_a(generic_sample_extension="mp3")
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_lib_track.genre.name == "4bTyH6zRq7Psk7Y9Pydmb4gTYs9VCVvehPANcaZHbviunfxtl5"
