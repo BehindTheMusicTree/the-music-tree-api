31164d4edf6087ef902a2d03e832a8fa93300669
mignot <andreas.garcia@bodzify.com>
2024-03-13 13:40:22 +0100

    Parse incoming camelcase into snake case

--- bodzify_api/view/viewset/model/AppModelViewSet.py
+++ bodzify_api/view/viewset/model/AppModelViewSet.py
@@ -2,6 +2,7 @@
 
 from abc import abstractmethod
 import logging
+import re
 from django.db import IntegrityError
 from rest_framework.response import Response
 from rest_framework import status
@@ -17,13 +18,23 @@ logger = logging.getLogger('bodyzify_api')
 class AppModelViewSet(MultiSerializerViewSet):
     permission_classes = [IsAuthenticated]
 
+    @staticmethod
+    def camel_to_snake(name):
+        name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+    @staticmethod
+    def get_dict_with_snake_case_keys(dictionary: dict):
+        return {AppModelViewSet.camel_to_snake(key): value for key, value in dictionary.items()}
+
     def __init__(self, service, **kwargs):
         super().__init__(**kwargs)
         self.service = service
 
     def _create(self, request, *args, **kwargs):
+        request_data_snake_case = self.get_dict_with_snake_case_keys(request.data)
         try:
-            instance = self.service.create(user=request.user, post_schema_data=request.data)
+            instance = self.service.create(user=request.user, post_schema_data=request_data_snake_case)
         except IntegrityError as e:
             logger.exception(e)
             return utility.get_json_response_when_bad_request(exception=e)
@@ -34,9 +45,10 @@ class AppModelViewSet(MultiSerializerViewSet):
         return Response(data=response_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def _update(self, request, *args, **kwargs):
+        request_data_snake_case = self.get_dict_with_snake_case_keys(request.data)
         updated_instance = self.service.update(
             user=request.user,
-            put_schema_data=request.data,
+            put_schema_data=request_data_snake_case,
             old_instance=self.get_object())
         response_serializer_data = self._get_detailed_serializer(updated_instance).data
         headers = self.get_success_headers(response_serializer_data)
