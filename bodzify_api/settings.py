04a1974bc785b08fda19c8c0db7d2be4856b1321
mignot <andreas.garcia@bodzify.com>
2024-03-13 15:29:30 +0100

    Set token access lifetime to 1 minute for umg tests ing

--- bodzify_api/settings.py
+++ bodzify_api/settings.py
@@ -179,7 +179,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=100),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
