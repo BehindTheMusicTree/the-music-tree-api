34a0ce5d98210d4fd9934ec03aa968f55d7fd27e
mignot <andreas.garcia@bodzify.com>
2024-03-28 14:07:07 +0100

    Check multiple values for post criteria name

--- bodzify_api/test/view/criteria/save/fields/test_parent.py
+++ bodzify_api/test/view/criteria/save/fields/test_parent.py
@@ -3,7 +3,7 @@
 from rest_framework import status
 from ddf import G
 from bodzify_api.model.criteria.Criteria import Criteria
-from bodzify_api.serializer.criteria.input.schema.CriteriaSaveSchemaSerializer import FIELDS as POST_FIELD
+from bodzify_api.serializer.criteria.input.schema.CriteriaSaveSchemaSerializer import FIELDS as POST_FIELDS
 from bodzify_api.model.criteria.CriteriaType import CRITERIA_TYPES_ID
 from bodzify_api.test.view.criteria.CriteriaTestCase import CriteriaTestCase
 
@@ -12,28 +12,28 @@ class TestCase(CriteriaTestCase):
 
     def test_empty_then_none(self):
         data = {
-            POST_FIELD.NAME: "Punk",
-            POST_FIELD.PARENT: ""
+            POST_FIELDS.NAME: "Punk",
+            POST_FIELDS.PARENT: ""
         }
         response = self.post_genre(data_dict=data)
-        assert response.status_code == status.HTTP_201_CREATED # type: ignore
+        assert response.status_code == status.HTTP_201_CREATED  # type: ignore
         assert self.saved_genre.parent == None
 
     def test_existing(self):
         rock_genre = G(Criteria, name="Rock", user=self.test_user, type=CRITERIA_TYPES_ID.GENRE)
         data = {
-            POST_FIELD.NAME: "Punk",
-            POST_FIELD.PARENT: rock_genre.uuid # type: ignore
+            POST_FIELDS.NAME: "Punk",
+            POST_FIELDS.PARENT: rock_genre.uuid  # type: ignore
         }
         response = self.post_genre(data_dict=data)
-        assert response.status_code == status.HTTP_201_CREATED # type: ignore
+        assert response.status_code == status.HTTP_201_CREATED  # type: ignore
         assert self.saved_genre.parent == rock_genre
 
     def test_error_when_not_existing(self):
         G(Criteria, name="Rock", user=self.test_user, type=CRITERIA_TYPES_ID.GENRE)
         data = {
-            POST_FIELD.NAME: "Punk",
-            POST_FIELD.PARENT: "not existing"
+            POST_FIELDS.NAME: "Punk",
+            POST_FIELDS.PARENT: "not existing"
         }
         response = self.post_genre(data_dict=data)
-        assert response.status_code == status.HTTP_400_BAD_REQUEST # type: ignore
+        assert response.status_code == status.HTTP_400_BAD_REQUEST  # type: ignore
--- bodzify_api/test/view/criteria/save/fields/test_root.py
+++ bodzify_api/test/view/criteria/save/fields/test_root.py
@@ -5,13 +5,13 @@ from ddf import G
 from bodzify_api.model.criteria.CriteriaType import CRITERIA_TYPES_ID
 from bodzify_api.model.criteria.Criteria import Criteria
 from bodzify_api.test.view.criteria.CriteriaTestCase import CriteriaTestCase
-from bodzify_api.serializer.criteria.input.schema.CriteriaSaveSchemaSerializer import FIELDS as POST_FIELD
+from bodzify_api.serializer.criteria.input.schema.CriteriaSaveSchemaSerializer import FIELDS as POST_FIELDS
 
 
 class TestCase(CriteriaTestCase):
 
     def test_parent_none_then_root_itself(self):
-        data = {POST_FIELD.NAME: "Rock"}
+        data = {POST_FIELDS.NAME: "Rock"}
         response = self.post_genre(data_dict=data)
         assert response.status_code == status.HTTP_201_CREATED  # type: ignore
         assert self.saved_genre.root == self.saved_genre
@@ -19,8 +19,8 @@ class TestCase(CriteriaTestCase):
     def test_one_acendant_then_root_is_parent(self):
         rock = G(Criteria, name="Rock", user=self.test_user, type=CRITERIA_TYPES_ID.GENRE)
         data = {
-            POST_FIELD.NAME: "Punk",
-            POST_FIELD.PARENT: rock.uuid  # type: ignore
+            POST_FIELDS.NAME: "Punk",
+            POST_FIELDS.PARENT: rock.uuid  # type: ignore
         }
         response = self.post_genre(data_dict=data)
         assert response.status_code == status.HTTP_201_CREATED  # type: ignore
@@ -30,8 +30,8 @@ class TestCase(CriteriaTestCase):
         rockGenre = G(Criteria, name="Rock", user=self.test_user, type=CRITERIA_TYPES_ID.GENRE)
         punk_genre = G(Criteria, name="Punk", user=self.test_user, type=CRITERIA_TYPES_ID.GENRE, parent=rockGenre)
         data = {
-            POST_FIELD.NAME: "Punk hardcore",
-            POST_FIELD.PARENT: punk_genre.uuid  # type: ignore
+            POST_FIELDS.NAME: "Punk hardcore",
+            POST_FIELDS.PARENT: punk_genre.uuid  # type: ignore
         }
         response = self.post_genre(data_dict=data)
         assert response.status_code == status.HTTP_201_CREATED  # type: ignore
