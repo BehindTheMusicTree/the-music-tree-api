c3e6ef734148604b60fe01261da5b8e2b1f1e808
mignot <andreas.garcia@bodzify.com>
2024-04-02 18:14:46 +0200

    Fix logs on dev

--- bodzify_api/settings_dev.py
+++ bodzify_api/settings_dev.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from pathlib import Path
+
 import bodzify_api.settings as settings
 
 ALLOWED_HOSTS = ['127.0.0.1']
@@ -8,4 +10,4 @@ STATIC_ROOT = settings.BASE_DIR / 'staticfiles'
 
 MEDIA_ROOT = settings.BASE_DIR / 'media'
 
-LOG_PATH = '/var/log/django/'
+LOG_PATH = Path('/var/log/django/')
