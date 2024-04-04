b3805afd90004434b0f6f24c019923605e77f09d
mignot <andreas.garcia@bodzify.com>
2024-04-04 16:26:37 +0200

    Test track position in existing criteria playlist

--- bodzify_api/test/view/track/input/save/test_playlist_position.py
+++ bodzify_api/test/view/track/input/save/test_playlist_position.py
@@ -12,6 +12,7 @@ from bodzify_api.model.playlist.children.SimplePlaylist import SimplePlaylist
 from bodzify_api.model.track.LibraryTrack import LibraryTrack
 from bodzify_api.serializer.track.input.schema.endpoint.LibTrackPostSerializer import FIELDS as POST_FIELDS
 from bodzify_api.test.view import playlist
+from bodzify_api.test.view.playlist.children import genre
 from bodzify_api.test.view.track.TrackTestCase import TrackTestCase
 
 
@@ -27,11 +28,19 @@ class TestCase(TrackTestCase):
         assert PlaylistLibTrackRelation.objects.get(playlist=genre_playlist,
                                                     library_track=self.saved_lib_track).position == 1
 
-    def test_existing_genre_then_first_position(self):
+    def test_existing_genre_then_first_position_and_other_tracks_after(self):
         genre_name = "Rock"
         genre = G(Criteria, name=genre_name, user=self.test_user, type=CRITERIA_TYPES_ID.GENRE)
+        lib_track1 = G(LibraryTrack, user=self.test_user, title="We're All To Blame", genre=genre)
+        lib_track2 = G(LibraryTrack, user=self.test_user, title="We're All To Blame", genre=genre)
         data = {POST_FIELDS.GENRE_NAME: genre_name}
         response = self.post_lib_track_with_generic_sample_no_tags(data_dict=data)
+        assert response.status_code == status.HTTP_201_CREATED  # type: ignore
+        genre_playlist = CriteriaPlaylist.objects.get(criteria__name=genre_name).playlist
+        assert PlaylistLibTrackRelation.objects.get(playlist=genre_playlist,
+                                                    library_track=self.saved_lib_track).position == 1
+        assert PlaylistLibTrackRelation.objects.get(playlist=genre_playlist, library_track=lib_track1).position == 3
+        assert PlaylistLibTrackRelation.objects.get(playlist=genre_playlist, library_track=lib_track2).position == 2
 
     # def test_new_criteria_then_not_in_old_criteria_playlist_anymore(self):
     #     old_genre = G(Criteria, name="Metal", user=self.test_user, type=CRITERIA_TYPES_ID.GENRE)
