3a965cc163496a98dbe780cb8bb7fee415975519
mignot <andreas.garcia@bodzify.com>
2024-04-25 15:30:51 +0200

    Fix test file in library when uploading

--- bodzify_api/test/view/track/input/method/create/attributes/file/test_location.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/test_location.py
@@ -8,11 +8,11 @@ from bodzify_api.test.view.track.TrackTestCase import TrackTestCase
 class TestCase(TrackTestCase):
 
     def test_in_library(self):
-        filename = self.LIB_TRACK_GENERIC_SAMPLES_FILENAMES_WITHOUT_EXTENSION.TAGS_NONE
+        filename_without_extension = self.LIB_TRACK_GENERIC_SAMPLES_FILENAMES_WITHOUT_EXTENSION.TAGS_NONE
         file_extension = 'wav'
-        filename_with_underscores_and_extension = filename.replace(' ', '_') + '.' + file_extension
+        filename_with_extension = f'{filename_without_extension}.{file_extension}'
         response = self.post_lib_track_with_generic_sample_no_tags(extension=file_extension)
         assert response.status_code == status.HTTP_201_CREATED  # type: ignore
         assert Path(self.saved_lib_track.file_obj.file.name) == \
-            self.test_user_lib_path_relative_to_media_dir / filename_with_underscores_and_extension
-        assert self._does_track_filename_exist_in_test_user_lib(filename_with_underscores_and_extension)
+            self.test_user_lib_path_relative_to_media_dir / filename_with_extension
+        assert self._does_track_filename_exist_in_test_user_lib(filename_with_extension)
