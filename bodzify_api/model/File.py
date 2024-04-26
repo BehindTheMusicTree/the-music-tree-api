629a32b277555a966a4045f77fa2832b4a72cbc2
mignot <andreas.garcia@bodzify.com>
2024-04-26 14:17:22 +0200

    Correct md5 if failed

--- bodzify_api/model/File.py
+++ bodzify_api/model/File.py
@@ -9,6 +9,7 @@ from django.db import models
 from django.db.models import F
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
 
 from bodzify_api import settings
 from bodzify_api.validator.track_file_validator \
@@ -20,7 +21,7 @@ class ATTRIBUTES_LABEL:
     FILE = 'file'
     FILENAME = 'filename'
     EXTENSION = 'extension'
-    ORIGINAL_FLAC_FILE_MD5_CHECK_IS_VALID = 'original_flac_file_md5_check_is_valid'
+    HAD_FLAC_MD5_BEEN_CORRECTED = 'had_flac_md5_been_corrected'
     SIZE_IN_BYTES = 'size_in_bytes'
     SIZE_IN_KO = 'size_in_ko'
     SIZE_IN_MO = 'size_in_mo'
@@ -49,7 +50,7 @@ class File(models.Model):
                             null=True)
     filename = models.CharField(max_length=settings.LIB_TRACK_FILENAME_LENGTH_MAX, blank=True)
     extension = models.CharField(max_length=5, blank=True)
-    original_flac_file_md5_check_is_valid = models.BooleanField(null=True, default=None, blank=True)
+    had_flac_md5_been_corrected = models.BooleanField(null=True, default=None, blank=True)
     size_in_bytes = models.FloatField(null=True, blank=True)
     size_in_ko = models.GeneratedField(expression=F(ATTRIBUTES_LABEL.SIZE_IN_BYTES) / 1024,
                                        output_field=models.FloatField(),
@@ -61,8 +62,15 @@ class File(models.Model):
     @staticmethod
     def is_flac_file_md5_valid(file_path):
         result = subprocess.run(['flac', '-t', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stderr = result.stderr.decode()
         return 'ok' in result.stderr.decode()
+
+    @staticmethod
+    def replace_flac_file_with_corrected_md5(file_path):
+        result = subprocess.run(['flac', '-f', '--best', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stderr = result.stderr.decode()
+        if 'ok' not in stderr:
+            raise ValidationError("The Flac file md5 check failed and could not be corrected. The file is probably " +
+                                  "corrupted.")
 
     def __str__(self) -> str:
         if self.file and self.file.name:
@@ -78,5 +86,9 @@ class File(models.Model):
         super().save(*args, **kwargs)  # So that the file is saved before the eventual md5 check
 
         if self.file and self.extension == '.flac':
-            self.original_flac_file_md5_check_is_valid = self.is_flac_file_md5_valid(self.file.path)
-            super().save(update_fields=[ATTRIBUTES_LABEL.ORIGINAL_FLAC_FILE_MD5_CHECK_IS_VALID])
+            if not self.is_flac_file_md5_valid(self.file.path):
+                self.replace_flac_file_with_corrected_md5(self.file.path)
+                self.had_flac_md5_been_corrected = True
+            else:
+                self.had_flac_md5_been_corrected = False
+            super().save(update_fields=[ATTRIBUTES_LABEL.HAD_FLAC_MD5_BEEN_CORRECTED])
