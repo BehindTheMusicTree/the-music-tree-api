437d848c1aebbae3ee63d156e90f55934f73c92c
mignot <andreas.garcia@bodzify.com>
2024-04-26 11:49:21 +0200

    Check if flac md5 valid

--- bodzify_api/model/File.py
+++ bodzify_api/model/File.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python
+
 import os
+import subprocess
 
 from django.core.files.storage import FileSystemStorage
 from django.db import models
@@ -15,6 +18,7 @@ class ATTRIBUTES_LABEL:
     FILE = 'file'
     FILENAME = 'filename'
     EXTENSION = 'extension'
+    ORIGINAL_FLAC_FILE_MD5_CHECK_IS_VALID = 'original_flac_file_md5_check_is_valid'
     SIZE_IN_BYTES = 'size_in_bytes'
     SIZE_IN_KO = 'size_in_ko'
     SIZE_IN_MO = 'size_in_mo'
@@ -43,6 +47,7 @@ class File(models.Model):
                             null=True)
     filename = models.CharField(max_length=settings.LIB_TRACK_FILENAME_LENGTH_MAX, blank=True)
     extension = models.CharField(max_length=5, blank=True)
+    original_flac_file_md5_check_is_valid = models.BooleanField(null=True, default=None, blank=True)
     size_in_bytes = models.FloatField(null=True, blank=True)
     size_in_ko = models.GeneratedField(expression=F(ATTRIBUTES_LABEL.SIZE_IN_BYTES) / 1024,
                                        output_field=models.FloatField(),
@@ -50,6 +55,11 @@ class File(models.Model):
     size_in_mo = models.GeneratedField(expression=F(ATTRIBUTES_LABEL.SIZE_IN_BYTES) / (1024 * 1024),
                                        output_field=models.FloatField(),
                                        db_persist=True)
+
+    @staticmethod
+    def is_flac_file_md5_valid(file_path):
+        result = subprocess.run(['flac', '-t', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return 'ok' in result.stderr.decode()
 
     def __str__(self) -> str:
         if self.file and self.file.name:
@@ -61,4 +71,8 @@ class File(models.Model):
             self.filename = os.path.basename(self.file.name)
             self.extension = os.path.splitext(self.file.name)[1]
             self.size_in_bytes = self.file.size
+
+            if self.extension == '.flac':
+                self.original_flac_file_md5_check_is_valid = self.is_flac_file_md5_valid(self.file.path)
+
         super().save(*args, **kwargs)
--- bodzify_api/serializer/file/FileDetailedSerializer.py
+++ bodzify_api/serializer/file/FileDetailedSerializer.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python
+
 from rest_framework import serializers
+
 from bodzify_api.model.File import ATTRIBUTES_LABEL as ATTRIBUTES_LABEL, File
 
 
@@ -6,6 +9,7 @@ class FIELDS:
     FILE = ATTRIBUTES_LABEL.FILE
     FILENAME = ATTRIBUTES_LABEL.FILENAME
     EXTENSION = ATTRIBUTES_LABEL.EXTENSION
+    ORIGINAL_FLAC_FILE_MD5_CHECK_IS_VALID = ATTRIBUTES_LABEL.ORIGINAL_FLAC_FILE_MD5_CHECK_IS_VALID
     SIZE_IN_BYTES = ATTRIBUTES_LABEL.SIZE_IN_BYTES
     SIZE_IN_KO = ATTRIBUTES_LABEL.SIZE_IN_KO
     SIZE_IN_MO = ATTRIBUTES_LABEL.SIZE_IN_MO
@@ -18,6 +22,7 @@ class FileDetailedSerializer(serializers.ModelSerializer):
         fields = [FIELDS.FILE,
                   FIELDS.FILENAME,
                   FIELDS.EXTENSION,
+                  FIELDS.ORIGINAL_FLAC_FILE_MD5_CHECK_IS_VALID,
                   FIELDS.SIZE_IN_BYTES,
                   FIELDS.SIZE_IN_KO,
                   FIELDS.SIZE_IN_MO]
