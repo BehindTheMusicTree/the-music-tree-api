288d44688a0799ea259fe1480303c55968271582
mignot <andreas.garcia@bodzify.com>
2024-05-06 10:10:53 +0200

    Fix

--- bodzify_api/test/view/track/input/attributes_source/data/genre/uuid/GenreUuidTestCase.py
+++ bodzify_api/test/view/track/input/attributes_source/data/genre/uuid/GenreUuidTestCase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-from bodzify_api.model.criteria.Criteria import Criteria
-from bodzify_api.model.criteria.CriteriaType import CRITERIA_TYPES_ID
-from bodzify_api.serializer.track.input.endpoint.LibTrackPostSerializer import FIELDS as POST_FIELDS
 from rest_framework import status
 
+from bodzify_api import settings
+from bodzify_api.serializer.track.input.endpoint.LibTrackPostSerializer import FIELDS as POST_FIELDS
 from bodzify_api.test.view.track.input.attributes_source.data.FieldFromDataTestCase import NullableUuidFieldFromDataTestCase
 
 
