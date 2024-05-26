e0e76b166ce93fd2f74b37b4bda4597ef0f61158
mignot <andreas.garcia@bodzify.com>
2024-05-26 15:36:24 +0200

    Add a more readable duration in MineTrackSerializer

--- bodzify_api/serializer/mine/track/MineTrackSerializer.py
+++ bodzify_api/serializer/mine/track/MineTrackSerializer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import datetime
 from rest_framework import serializers
 from bodzify_api import settings
 from bodzify_api.model.track.MineTrack import ATTRIBUTES_LABEL
@@ -9,6 +10,7 @@ class FIELDS:
     TITLE = ATTRIBUTES_LABEL.TITLE
     ARTIST_NAME = ATTRIBUTES_LABEL.ARTIST_NAME
     DURATION = ATTRIBUTES_LABEL.DURATION
+    DURATION_STR = ATTRIBUTES_LABEL.DURATION + "_str"
     RELEASED_ON = ATTRIBUTES_LABEL.RELEASED_ON
     URL = ATTRIBUTES_LABEL.URL
 
@@ -17,6 +19,7 @@ class MineTrackSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=settings.MINE_TRACK_TITLE_LEN_MAX)
     artist_name = serializers.CharField(max_length=settings.ARTIST_NAME_LEN_MAX)
     duration = serializers.IntegerField()
+    duration_str = serializers.SerializerMethodField()
     released_on = serializers.CharField(max_length=settings.MINE_TRACK_RELEASED_ON_LEN_MAX)
     url = serializers.CharField(max_length=settings.MINE_TRACK_URL_LEN_MAX)
 
@@ -24,5 +27,10 @@ class MineTrackSerializer(serializers.Serializer):
         fields = [FIELDS.TITLE,
                   FIELDS.ARTIST_NAME,
                   FIELDS.DURATION,
+                  FIELDS.DURATION_STR,
                   FIELDS.RELEASED_ON,
                   FIELDS.URL]
+
+    def get_duration_str(self, obj):
+        duration = datetime.timedelta(seconds=obj.duration)
+        return str(duration)
