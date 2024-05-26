afea6e297ccf8c38f993bd31fe4f820b7b8d2496
mignot <andreas.garcia@bodzify.com>
2024-05-26 18:34:35 +0200

    Translate in english

--- scripts/git_push_progressively.sh
+++ scripts/git_push_progressively.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Vérifiez si le message de commit est passé en argument
+# Check if the commit message is passed as an argument
 if [ $# -eq 0 ]; then
-    echo "Veuillez fournir un message de commit en argument."
+    echo "Please provide a commit message as an argument."
     exit 1
 fi
 
 commit_message=$1
 
 while true; do
-    # Obtenez une liste des fichiers modifiés
+    # Get a list of modified files
     IFS=$'\n'
     files=($(git status --porcelain | awk 'BEGIN{srand()} {print rand() "\t" $0}' | sort -n | cut -f2-))
 
-    # Vérifiez si la liste de fichiers est vide
+    # Check if the file list is empty
     if [ ${#files[@]} -eq 0 ]; then
-        echo "Aucun fichier à ajouter. Fin du script."
+        echo "No files to add. Ending script."
         break
     fi
 
-    # Ajoutez les fichiers à l'index de git
+    # Add the files to the git index
     for file in "${files[@]}"; do
         status=$(echo "$file" | awk '{print $1}')
         file_path=$(echo "$file" | awk '{print substr($0, index($0,$2))}')
@@ -38,7 +38,7 @@ while true; do
         fi
     done
 
-    # Commit et push
+    # Commit and push
     git commit -m "$commit_message"
     git push
 done
