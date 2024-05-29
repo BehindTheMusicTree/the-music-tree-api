9ec721783683a583f01ca8ba28881957afed02d9
mignot <andreas.garcia@bodzify.com>
2024-05-29 09:04:31 +0200

    Add created_on and updated_on to fixture models creation

--- bodzify_api/test/ModelFixtureFactory.py
+++ bodzify_api/test/ModelFixtureFactory.py
@@ -1,5 +1,7 @@
+import datetime
 from pathlib import Path
 from typing import List, Optional
+import uuid
 from ddf import G
 
 from bodzify_api.model.Album import Album
@@ -7,6 +9,8 @@ from bodzify_api.model.Artist import Artist
 from bodzify_api.model.TrackFile import TrackFile as TrackFile
 from bodzify_api.model.criteria.Criteria import Criteria
 from bodzify_api.model.criteria.CriteriaType import CRITERIA_TYPES_ID
+from bodzify_api.model.musicbrainz.MusicbrainzArtist import MusicbrainzArtist
+from bodzify_api.model.musicbrainz.MusicbrainzRecording import MusicbrainzRecording
 from bodzify_api.model.playlist.children.SimplePlaylist import SimplePlaylist
 from bodzify_api.model.track.LibraryTrack import LibraryTrack
 from bodzify_api.test.TestUser import TestUser
@@ -19,20 +23,46 @@ class ModelFixtureFactory:
     def __init__(self, test_user: TestUser) -> None:
         self.test_user = test_user
 
-    def __create_criteria(self, name: str, type: int, parent: Optional[Criteria] = None) -> Criteria:
-        return G(Criteria, user=self.test_user.django_user, name=name, type=type, parent=parent)  # type: ignore
+    def __create_criteria(self,
+                          name: str,
+                          type: int,
+                          parent: Optional[Criteria] = None,
+                          created_on: Optional[datetime.date] = datetime.date.today(),
+                          updated_on: Optional[datetime.date] = datetime.date.today()) -> Criteria:
+        return G(Criteria,
+                 user=self.test_user.django_user,
+                 name=name, type=type,
+                 parent=parent,
+                 created_on=created_on,
+                 updated_on=updated_on)  # type: ignore
 
-    def create_artist(self, name: str) -> Artist:
-        return G(Artist, user=self.test_user.django_user, name=name)  # type: ignore
+    def create_artist(self,
+                      name: str,
+                      created_on: Optional[datetime.date] = datetime.date.today(),
+                      updated_on: Optional[datetime.date] = datetime.date.today()) -> Artist:
+        return G(Artist, user=self.test_user.django_user, name=name, created_on=created_on, updated_on=updated_on)  # type: ignore
 
-    def create_album(self, name: str, album_artists: List[Artist] = [], year: Optional[int] = None) -> Album:
-        return G(Album, user=self.test_user.django_user, name=name, album_artists=album_artists, year=year)  # type: ignore
+    def create_album(self,
+                     name: str,
+                     album_artists: List[Artist] = [],
+                     year: Optional[int] = None,
+                     created_on: Optional[datetime.date] = datetime.date.today(),
+                     updated_on: Optional[datetime.date] = datetime.date.today()) -> Album:
+        return G(Album,
+                 user=self.test_user.django_user,
+                 name=name,
+                 album_artists=album_artists,
+                 year=year,
+                 created_on=created_on,
+                 updated_on=updated_on)  # type: ignore
 
-    def create_file(self, filename: str) -> TrackFile:
+    def create_file(self, filename: str, created_on: Optional[datetime.date] = datetime.date.today()) -> TrackFile:
         return G(TrackFile,
                  user=self.test_user.django_user,
                  file=str(Path(self.test_user.lib_abs_path) / filename),
-                 size_in_ko=None, size_in_mo=None)  # type: ignore
+                 size_in_ko=None,
+                 size_in_mo=None,
+                 created_on=created_on)  # type: ignore
 
     def create_lib_track(self,
                          title: str,
@@ -42,7 +72,9 @@ class ModelFixtureFactory:
                          genre: Optional[Criteria] = None,
                          rating: Optional[int] = None,
                          language: Optional[str] = None,
-                         play_count: Optional[int] = 0) -> LibraryTrack:
+                         play_count: Optional[int] = 0,
+                         created_on: Optional[datetime.date] = datetime.date.today(),
+                         updated_on: Optional[datetime.date] = datetime.date.today()) -> LibraryTrack:
         if track_file is None:
             track_file = self.create_file(filename=self.test_user.lib_track_default_filename)
         return G(LibraryTrack,
@@ -54,13 +86,47 @@ class ModelFixtureFactory:
                  genre=genre,
                  rating=rating,
                  language=language,
-                 play_count=play_count)  # type: ignore
+                 play_count=play_count,
+                 created_on=created_on,
+                 updated_on=updated_on)  # type: ignore
 
-    def create_genre(self, name: str, parent: Optional[Criteria] = None) -> Criteria:
-        return self.__create_criteria(name=name, type=CRITERIA_TYPES_ID.GENRE, parent=parent)
+    def create_musicbrainz_recording(self, uuid: uuid.UUID, title: str, duration: int,
+                                     musicbrainz_artists: Optional[list[MusicbrainzArtist]] = None,
+                                     release_date: Optional[datetime.date] = None,
+                                     created_on: Optional[datetime.date] = datetime.date.today(),
+                                     updated_on: Optional[datetime.date] = datetime.date.today()) -> MusicbrainzRecording:
+        return G(MusicbrainzRecording,
+                 uuid=uuid, title=title,
+                 duration=duration,
+                 musicbrainz_artists=musicbrainz_artists,
+                 release_date=release_date,
+                 created_on=created_on,
+                 updated_on=updated_on)  # type: ignore
 
-    def create_tag(self, name: str, parent: Optional[Criteria] = None) -> Criteria:
-        return self.__create_criteria(name=name, type=CRITERIA_TYPES_ID.TAG, parent=parent)
+    def create_genre(self,
+                     name: str,
+                     parent: Optional[Criteria] = None,
+                     created_on: Optional[datetime.date] = datetime.date.today(),
+                     updated_on: Optional[datetime.date] = datetime.date.today()) -> Criteria:
+        return self.__create_criteria(
+            name=name, type=CRITERIA_TYPES_ID.GENRE, parent=parent, created_on=created_on, updated_on=updated_on)
 
-    def create_simple_playlist(self, name, play_count: Optional[int] = 0) -> SimplePlaylist:
-        return G(SimplePlaylist, playlist__user=self.test_user.django_user, name=name, playlist__play_count=play_count)  # type: ignore
+    def create_tag(self,
+                   name: str,
+                   parent: Optional[Criteria] = None,
+                   created_on: Optional[datetime.date] = datetime.date.today(),
+                   updated_on: Optional[datetime.date] = datetime.date.today()) -> Criteria:
+        return self.__create_criteria(
+            name=name, type=CRITERIA_TYPES_ID.TAG, parent=parent, created_on=created_on, updated_on=updated_on)
+
+    def create_simple_playlist(self,
+                               name,
+                               play_count: Optional[int] = 0,
+                               created_on: Optional[datetime.date] = datetime.date.today(),
+                               updated_on: Optional[datetime.date] = datetime.date.today()) -> SimplePlaylist:
+        return G(SimplePlaylist,
+                 playlist__user=self.test_user.django_user,
+                 name=name,
+                 playlist__play_count=play_count,
+                 playlist__created_on=created_on,
+                 updated_on=updated_on)  # type: ignore
