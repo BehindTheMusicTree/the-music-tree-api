1c909932162245e669e9c0d0801c44a8de07c56e
mignot <andreas.garcia@bodzify.com>
2024-05-29 12:31:17 +0200

    Add musicbrainz links to models

--- bodzify_api/model/musicbrainz/MusicbrainzArtist.py
+++ bodzify_api/model/musicbrainz/MusicbrainzArtist.py
@@ -2,6 +2,9 @@
 
 from django.utils import timezone
 from django.db import models
+from django.db.models import Value, F
+
+from bodzify_api import settings
 
 
 class ATTRIBUTES_LABEL:
@@ -11,9 +14,20 @@ class ATTRIBUTES_LABEL:
     UPDATED_ON = 'updated_on'
 
 
+class ConcatOp(models.Func):
+    arg_joiner = " || "
+    function = None
+    output_field = models.TextField()
+    template = "%(expressions)s"
+
+
 class MusicbrainzArtist(models.Model):
     uuid = models.UUIDField(primary_key=True, editable=False)
-    name = models.CharField(max_length=255, default=None)
+    name = models.CharField(max_length=settings.MUSICBRAINZ_ARTIST_NAME_LEN_MAX, default=None)
+    musicbrainz_link = models.GeneratedField(  # type: ignore
+        expression=ConcatOp(Value(settings.MUSICBRAINZ_ARTIST_URL), F(ATTRIBUTES_LABEL.UUID)),
+        output_field=models.CharField(max_length=len(settings.MUSICBRAINZ_ARTIST_URL) + 36),
+        db_persist=True)
     created_on = models.DateTimeField(default=timezone.now, editable=False)
     updated_on = models.DateTimeField(auto_now=True, editable=True)
 
--- bodzify_api/model/musicbrainz/MusicbrainzRecording.py
+++ bodzify_api/model/musicbrainz/MusicbrainzRecording.py
@@ -2,7 +2,9 @@
 
 from django.utils import timezone
 from django.db import models
+from django.db.models import F, Value
 
+from bodzify_api import settings
 from bodzify_api.model.musicbrainz.MusicbrainzArtist import MusicbrainzArtist
 
 
@@ -16,12 +18,23 @@ class ATTRIBUTES_LABEL:
     UPDATED_ON = 'updated_on'
 
 
+class ConcatOp(models.Func):
+    arg_joiner = " || "
+    function = None
+    output_field = models.TextField()
+    template = "%(expressions)s"
+
+
 class MusicbrainzRecording(models.Model):
     uuid = models.UUIDField(primary_key=True, editable=False)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=settings.MUSICBRAINZ_RECORDING_TITLE_LEN_MAX)
     duration = models.IntegerField()
     release_date = models.DateField(null=True, blank=True)
     musicbrainz_artists = models.ManyToManyField(MusicbrainzArtist)
+    musicbrainz_link = models.GeneratedField(  # type: ignore
+        expression=ConcatOp(Value(settings.MUSICBRAINZ_RECORDING_URL), F(ATTRIBUTES_LABEL.UUID)),
+        output_field=models.CharField(max_length=len(settings.MUSICBRAINZ_RECORDING_URL) + 36),
+        db_persist=True)
     created_on = models.DateTimeField(default=timezone.now, editable=False)
     updated_on = models.DateTimeField(auto_now=True, editable=True)
 
