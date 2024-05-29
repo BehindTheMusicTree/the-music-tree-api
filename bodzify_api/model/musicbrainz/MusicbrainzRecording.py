3f547cbf116f01d513e175528a0ad0a6ffc8346a
mignot <andreas.garcia@bodzify.com>
2024-05-29 14:25:40 +0200

    Clean warnings

--- bodzify_api/model/musicbrainz/MusicbrainzRecording.py
+++ bodzify_api/model/musicbrainz/MusicbrainzRecording.py
@@ -21,8 +21,8 @@ class ATTRIBUTES_LABEL:
 
 class ConcatOp(models.Func):
     arg_joiner = " || "
-    function = None
-    output_field = models.TextField()
+    function = None  # type: ignore
+    output_field = models.TextField()  # type: ignore
     template = "%(expressions)s"
 
 
