de6a92e02959afb7a046d812882cb538da96d133
mignot <andreas.garcia@bodzify.com>
2024-05-29 19:55:19 +0200

    Implement MB recording score tests

---
+++ bodzify_api/test/view/track/input/method/create/attributes/musicbrainz_recording/test_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from rest_framework import status
+
+from bodzify_api.test.view.track.TrackTestCase import TrackTestCase
+
+
+class TestCase(TrackTestCase):
+
+    def test_totaleclipe_5m35_with_three_scores_then_highest(self):
+        response = self.post_lib_track_with_specific_sample("three_scores.flac")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert float(self.saved_lib_track.musicbrainz_recording.score) == 0.98117745  # type: ignore
