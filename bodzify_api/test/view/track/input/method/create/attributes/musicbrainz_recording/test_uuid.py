dd9c85deb89b534e892791d1a8384c995e5b5288
mignot <andreas.garcia@bodzify.com>
2024-05-29 14:34:45 +0200

    Implement first musicbrainz recording id test

--- bodzify_api/test/view/track/input/method/create/attributes/musicbrainz_recording/test_uuid.py
+++ bodzify_api/test/view/track/input/method/create/attributes/musicbrainz_recording/test_uuid.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import uuid
+
 from rest_framework import status
 
 from bodzify_api.test.view.track.TrackTestCase import TrackTestCase
@@ -11,4 +13,5 @@ class TestCase(TrackTestCase):
     def test_drown_7m21(self):
         response = self.post_lib_track_with_specific_sample("oostil - drown (massano remix) - 7m21.mp3")
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_lib_track.musicbrainz_recording.uuid == "d2fe3873-d123-4bea-a5ee-4340d865777c"  # type: ignore
+        assert self.saved_lib_track.musicbrainz_recording.uuid == uuid.UUID(
+            "4a45b00b-273d-40ed-9ecd-42f387f59c22")  # type: ignore
