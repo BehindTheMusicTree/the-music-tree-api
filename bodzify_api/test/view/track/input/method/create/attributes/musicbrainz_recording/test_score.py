89bb24103362ab67802405a7c765b208bc24c8a4
mignot <andreas.garcia@bodzify.com>
2024-05-30 09:48:29 +0200

    Rename score test file

--- bodzify_api/test/view/track/input/method/create/attributes/musicbrainz_recording/test_score.py
+++ bodzify_api/test/view/track/input/method/create/attributes/musicbrainz_recording/test_score.py
@@ -8,6 +8,6 @@ from bodzify_api.test.view.track.TrackTestCase import TrackTestCase
 class TestCase(TrackTestCase):
 
     def test_totaleclipe_5m35_with_three_scores_then_highest(self):
-        response = self.post_lib_track_with_specific_sample("three_scores.flac")
+        response = self.post_lib_track_with_specific_sample("total_eclipse_three_scores.flac")
         assert response.status_code == status.HTTP_201_CREATED
         assert float(self.saved_lib_track.musicbrainz_recording.score) == 0.98117745  # type: ignore
