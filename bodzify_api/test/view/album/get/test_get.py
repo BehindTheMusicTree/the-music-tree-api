3b6f6ef256340b3aaa07c49e00ccdb07f98a352d
mignot <andreas.garcia@bodzify.com>
2024-05-30 13:21:38 +0200

    Fix album get test

--- bodzify_api/test/view/album/get/test_get.py
+++ bodzify_api/test/view/album/get/test_get.py
@@ -10,11 +10,12 @@ class TestCase(AlbumViewTestCase):
 
     def test_get(self):
         sum41_artist = self.model_fixture_factory.create_artist(name="Sum 41")
-        self.model_fixture_factory.create_album(name="All Killer No Filler", year=2001, album_artists=[sum41_artist],)
+        allkiller_album = self.model_fixture_factory.create_album(
+            name="All Killer No Filler", year=2001, album_artists=[sum41_artist],)
         chuck_album = self.model_fixture_factory.create_album(name="Chuck", year=2004, album_artists=[sum41_artist])
         self.model_fixture_factory.create_lib_track(title="In Too Deep", artist=sum41_artist, album=chuck_album)
         self.model_fixture_factory.create_lib_track(title="We're All To Blame", artist=sum41_artist, album=chuck_album)
-        self.model_fixture_factory.create_lib_track(title="Pieces", artist=sum41_artist, album=chuck_album)
+        self.model_fixture_factory.create_lib_track(title="Pieces", artist=sum41_artist, album=allkiller_album)
         response = self.get_albums()
         assert response.status_code == status.HTTP_200_OK
         assert self.overall_total == 2
