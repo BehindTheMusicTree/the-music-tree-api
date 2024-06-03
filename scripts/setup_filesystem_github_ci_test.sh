d462cefebecf8c49703ec9d66fdea4de5bf0b1c6
mignot <andreas.garcia@bodzify.com>
2024-06-03 18:41:04 +0200

    Rename github workflow

--- scripts/setup_filesystem_github_ci_test.sh
+++ scripts/setup_filesystem_github_ci_test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 mediaDir=/var/lib/bodzify-api/media/
-
 sudo mkdir -p $mediaDir
 sudo chown $USER $mediaDir
 sudo chmod 775 $mediaDir
 
-logDir=/var/log/django/
+sharedDjangoTempUploadedFilesDir=/var/django_temp_uploaded_files/
+sudo mkdir $sharedDjangoTempUploadedFilesDir
+sudo chmod 775 $sharedDjangoTempUploadedFilesDir
 
+logDir=/var/log/django/
 sudo mkdir -p $logDir
 sudo chown $USER $logDir
 sudo chmod 775 $logDir
