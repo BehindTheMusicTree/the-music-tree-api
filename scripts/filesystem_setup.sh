fcb8ca77fbe26d015aa8758bc438bc4fc799758f
mignot <andreas.garcia@bodzify.com>
2024-06-26 19:22:33 +0200

    Fix CI tests log creation

--- scripts/filesystem_setup.sh
+++ scripts/filesystem_setup.sh
@@ -15,24 +15,20 @@ sudo mkdir -p $tempUploadedFilesDir
 sudo chmod 775 $tempUploadedFilesDir
 sudo chown -R $USER $tempUploadedFilesDir
 
-if [ -z "$LOG_DIR" ]
-then
-    djangoLogDir=$SCRIPTS_DIR../
-else
+if [ -n "$LOG_DIR" ] ; then
     djangoLogDir=$LOG_DIR
+    sudo mkdir -p $djangoLogDir
+    sudo touch ${djangoLogDir}requests.log
+    sudo touch ${djangoLogDir}requests.debug.log
+    sudo touch ${djangoLogDir}exceptions.log
+    sudo touch ${djangoLogDir}general.log
+    sudo touch ${djangoLogDir}info.log
+    sudo touch ${djangoLogDir}django.log
+    sudo touch ${djangoLogDir}bodzify-api.log
+
+    sudo chmod -R 775 $djangoLogDir
+    sudo chown -R $USER $djangoLogDir
 fi
-
-sudo mkdir -p $djangoLogDir
-sudo touch ${djangoLogDir}requests.log
-sudo touch ${djangoLogDir}requests.debug.log
-sudo touch ${djangoLogDir}exceptions.log
-sudo touch ${djangoLogDir}general.log
-sudo touch ${djangoLogDir}info.log
-sudo touch ${djangoLogDir}django.log
-sudo touch ${djangoLogDir}bodzify-api.log
-
-sudo chmod -R 775 $djangoLogDir
-sudo chown -R $USER $djangoLogDir
 
 staticDir=/var/www/bodzify-api/static/
 sudo mkdir -p $staticDir
