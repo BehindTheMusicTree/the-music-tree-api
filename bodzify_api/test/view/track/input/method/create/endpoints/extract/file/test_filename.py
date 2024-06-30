75b3ac215bdea6d8c3025f9866924a9c371cf366
mignot <andreas.garcia@bodzify.com>
2024-06-30 15:04:41 +0200

    Clean code

--- bodzify_api/test/view/track/input/method/create/endpoints/extract/file/test_filename.py
+++ bodzify_api/test/view/track/input/method/create/endpoints/extract/file/test_filename.py
@@ -47,8 +47,8 @@ class FilenameTestCase(TrackTestCase):
         data_dict = {EXTRACT_FIELDS.TITLE: title}
         response = self.extract_default_mine_track(data_dict=data_dict)
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_lib_track.track_file.filename == f"{title}.{
-            TrackTestCase.SAMPLE_MINE_TRACK_DEFAULT_EXTENSION} "
+        assert self.saved_lib_track.track_file.filename == \
+            f"{title}.{TrackTestCase.SAMPLE_MINE_TRACK_DEFAULT_EXTENSION} "
 
     def test_not_providing_title_nor_artist_and_original_filename_too_long_then_generate_filename(self):
         track_url = (
