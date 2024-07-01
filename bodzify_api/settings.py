5a277382fb5463c21c6c6931a1c9bd7d5fbaff0b
mignot <andreas.garcia@bodzify.com>
2024-07-01 23:20:59 +0200

    Fix settings if no enf file

--- bodzify_api/settings.py
+++ bodzify_api/settings.py
@@ -13,12 +13,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 APP_ENV_FILE_RELATIVE_PATH = os.getenv('ENV_FILE', 'env/.env')
 
-
 APP_ENV_FILE = None
 if APP_ENV_FILE_RELATIVE_PATH:
     APP_ENV_FILE = BASE_DIR / APP_ENV_FILE_RELATIVE_PATH
     if not APP_ENV_FILE.exists():
-        raise EnvironmentError("The env file {APP_ENV_FILE} does not exist")
+        print("No env file at {APP_ENV_FILE}")
 
     dotenv.load_dotenv(APP_ENV_FILE)
 else:
