2fdb46f0e059d2edefb649e24821db6e30f1be34
mignot <andreas.garcia@bodzify.com>
2024-07-03 23:21:01 +0200

    Improve audiofingerprinter connection test

--- bodzify_api/test/critical/test_audio_fingerprinter_connection_ok.py
+++ bodzify_api/test/critical/test_audio_fingerprinter_connection_ok.py
@@ -13,8 +13,9 @@ class TestCase(TrackTestCase):
     @pytest.mark.critical
     def test_drown_7m21_mp3_then_ok(self):
         response = self.post_lib_track_with_specific_sample("oostil - drown (massano remix) - 7m21.mp3")
-        if response.status_code != status.HTTP_201_CREATED:
-            print(response.data)
+        assert response.status_code == status.HTTP_201_CREATED
+        if self.saved_lib_track.track_file.fingerprinting_error_code is not None:
+            print(self.saved_lib_track.track_file.fingerprinting_error_code)
             assert False
         else:
             assert self.saved_lib_track.musicbrainz_recording.uuid == uuid.UUID(  # type: ignore
