229018fd0a3b0c8b442953f165fbd60a804cc9b7
mignot <andreas.garcia@bodzify.com>
2024-07-04 12:37:35 +0200

    Improve reinit script

--- scripts/data/reinit_django_data.sh
+++ scripts/data/reinit_django_data.sh
@@ -2,11 +2,19 @@
 
 echo "Initializing Django data"
 
+# Get the directory of the script even when it's called from another script
+SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)/
+SCRIPTS_DIR=$(realpath "$SCRIPT_DIR../")/
+PROJECT_DIR=$(realpath "$SCRIPTS_DIR../")/
+
 APP_ENV_FILE=$1
 if [ -z "$APP_ENV_FILE" ]; then
-  echo "No env file provided"
+  echo "No env file provided as arg. Checking env/.env file"
+  APP_ENV_FILE="${PROJECT_DIR}env/.env"
+fi
+if [ ! -f "$APP_ENV_FILE" ]; then
+    echo "env file $APP_ENV_FILE does not exist" >&2
 else
-  echo "Env file provided $APP_ENV_FILE"
   if [ -f "$APP_ENV_FILE" ]; then
       echo "Loading environment variables from ${APP_ENV_FILE}"
       while IFS='=' read -r key value
@@ -20,11 +28,26 @@ else
   fi
 fi
 
+CALCULATED_PATHS_ENV_FILE=${PROJECT_DIR}env/calculated_paths/.env
+bash "${SCRIPTS_DIR}generate_calculated_paths_env_file.sh" "$PROJECT_DIR" "$CALCULATED_PATHS_ENV_FILE"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to generate calculated paths env file"
+    exit 1
+fi
+
+echo "Loading calculated paths from ${CALCULATED_PATHS_ENV_FILE}"
+while IFS='=' read -r key value
+do
+    export "$key=$value"
+done < "$CALCULATED_PATHS_ENV_FILE"
+
 required_vars=(
   APP_NAME
   DB_CONTAINER_NAME
   DB_BODZIFY_API_DB_NAME
   DB_SUPERUSER_NAME
+  LIBRARIES_DIR
 )
 for var in "${required_vars[@]}"; do
   if [ -z "${!var}" ]; then
@@ -32,6 +55,24 @@ for var in "${required_vars[@]}"; do
     exit 1
   fi
 done
+
+ACTIVE_CONNECTIONS=$(docker exec -i $DB_CONTAINER_NAME \
+psql -U $DB_SUPERUSER_NAME \
+-tAc "SELECT COUNT(*) FROM pg_stat_activity WHERE datname='${DB_BODZIFY_API_DB_NAME}' AND pid <> pg_backend_pid();")
+
+if [ "$ACTIVE_CONNECTIONS" -gt 0 ]; then
+    echo "ERROR: Database ${DB_BODZIFY_API_DB_NAME} is being accessed by other users. Aborting."
+    exit 1
+else
+    echo "Database is not being accessed by other users. Proceeding."
+fi
+
+echo "Empty library directory"
+users_subfolders_count=$(find "$LIBRARIES_DIR" -mindepth 1 -maxdepth 1 -type d | wc -l)
+total_track_files_count=$(find "$LIBRARIES_DIR" -mindepth 2 -type f | wc -l)
+rm -rf "$LIBRARIES_DIR"*
+echo "$users_subfolders_count user subfolders were deleted."
+echo "$total_track_files_count track files were deleted."
 
 echo "Check if database exists"
 DB_EXISTS=$(docker exec -i $DB_CONTAINER_NAME \
@@ -54,12 +95,6 @@ if [ "$USER_EXISTS" = "1" ]; then
 else 
     echo "User does not exist."
 fi
-
-# Get the directory of the script even when it's called from another script
-SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)/
-SCRIPTS_DIR=$(realpath "$SCRIPT_DIR../")/
-PROJECT_DIR=$(realpath "$SCRIPTS_DIR../")/
-APP_ENV_FILE="${PROJECT_DIR}env/.env"
 
 Echo "Deleting migrations."
 MIGRATIONS_DIR="${PROJECT_DIR}${APP_NAME}/migrations/"
