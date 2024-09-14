9dec8762f1b2d6370f46371ec7f03d99bd339ddd
mignot <andreas.garcia@bodzify.com>
2024-09-14 02:53:07 +0200

    Fix dockerfile

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,19 +3,19 @@
 set -e
 
 REQUIRES_VARS=(
-    INIT_IF NECESSARY_DB_AND_ROLE_SCRIPT
+    INIT_IF_NECESSARY_DB_AND_ROLE_SCRIPT
     APP_PORT
     GUNICORN_LOG_DIR
     GUNICORN_LOG_ERROR_FILENAME
     GUNICORN_LOG_ACCESS_FILENAME
 )
 
-if [ -z "INIT_IF NECESSARY_DB_AND_ROLE_SCRIPT" ]; then
-    echo "INIT_IF NECESSARY_DB_AND_ROLE_SCRIPT must be set." >&2
+if [ -z "INIT_IF_NECESSARY_DB_AND_ROLE_SCRIPT" ]; then
+    echo "INIT_IF_NECESSARY_DB_AND_ROLE_SCRIPT must be set." >&2
     exit 1
 fi
 
-OUTPUT=$(bash ${INIT_IF NECESSARY_DB_AND_ROLE_SCRIPT})
+OUTPUT=$(bash ${INIT_IF_NECESSARY_DB_AND_ROLE_SCRIPT})
 if [ $? -ne 0 ]; then
     echo "Failed to initialize the database and roles: $OUTPUT" >&2
     exit 1
