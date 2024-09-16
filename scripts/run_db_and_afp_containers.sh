2b16d4d78d624bfb60ec7b10952a6d9618a10515
mignot <andreas.garcia@bodzify.com>
2024-09-16 11:34:19 +0200

    Fix typo

--- scripts/run_db_and_afp_containers.sh
+++ scripts/run_db_and_afp_containers.sh
@@ -35,8 +35,8 @@ load_env_vars () {
         "AUDIO_META_ANALYSE_IS_NEEDED"
     )
     check_bool_vars_are_set ${REQUIRED_BOOL_VARS[@]}
-    export_value_removing_surrounding_quotes "DB_SUPERUSER_PASSWORD"
-    export_value_removing_surrounding_quotes "DB_BODZIFY_API_USER_PASSWORD"
+    export_value_removing_eventual_surrounding_quotes "DB_SUPERUSER_PASSWORD"
+    export_value_removing_eventual_surrounding_quotes "DB_BODZIFY_API_USER_PASSWORD"
     echo "Environment variables loaded successfully."
 }
 
