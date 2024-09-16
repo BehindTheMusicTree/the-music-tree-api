e5633cd4e96ce50044ae26c190249d27ad952049
mignot <andreas.garcia@bodzify.com>
2024-09-16 23:46:16 +0200

    Debug pytest db init

--- scripts/check_db.sh
+++ scripts/check_db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Source the utils.sh file to include the check_if_db_empty_or_exit function
-source ./utils.sh
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/
+source ${SCRIPTS_DIR}utils.sh
 
 # Call the function
 check_if_db_empty_or_exit
