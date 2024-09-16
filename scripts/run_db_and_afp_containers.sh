6530e8e4bc915fd0033e0a33a47b8f674244df07
mignot <andreas.garcia@bodzify.com>
2024-09-16 22:36:15 +0200

    Refactor code

--- scripts/run_db_and_afp_containers.sh
+++ scripts/run_db_and_afp_containers.sh
@@ -37,64 +37,76 @@ load_env_vars () {
     echo "Environment variables loaded successfully."
 }
 
-echo "Running the database and audio fingerprinter containers."
+main() {
+    echo "Running the database and audio fingerprinter containers..."
 
-# Get the directory of the script even when it's called from another script
-SCRIPTS_DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)/
-APP_DIR=$(realpath $(dirname "$SCRIPTS_DIR"))/
-APP_ENV_FILE="${APP_DIR}env/.env"
-source "${SCRIPTS_DIR}utils.sh"
+    SCRIPTS_DIR=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)/
+    APP_DIR=$(realpath $(dirname "$SCRIPTS_DIR"))/
+    APP_ENV_FILE="${APP_DIR}env/.env"
+    source "${SCRIPTS_DIR}utils.sh"
 
-load_env_vars
+    load_env_vars
 
-echo "Pulling the database and audio fingerprinter images..."
-docker pull $DOCKERHUB_USERNAME/$DB_IMAGE_REPO:$DB_VERSION
-docker pull $DOCKERHUB_USERNAME/$AFP_IMAGE_REPO:$AFP_VERSION
-echo "Images pulled successfully."
+    echo "Pulling the database and audio fingerprinter images..."
+    echo $DOCKERHUB_USERNAME/$DB_IMAGE_REPO:$DB_VERSION
+    docker pull $DOCKERHUB_USERNAME/$DB_IMAGE_REPO:$DB_VERSION
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to pull the database image." >&2
+        exit 1
+    fi
+    docker pull $DOCKERHUB_USERNAME/$AFP_IMAGE_REPO:$AFP_VERSION
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to pull the audio fingerprinter image." >&2
+        exit 1
+    fi
+    echo "Images pulled successfully."
 
-CONTAINER_IDS=$(docker ps -a -q)
-if [ -n "$CONTAINER_IDS" ]; then
-    echo "Removing existing containers..."
-    docker rm -f $CONTAINER_IDS
-    echo "Containers removed successfully."
-else
-    echo "No container to remove."
-fi
+    CONTAINER_IDS=$(docker ps -a -q)
+    if [ -n "$CONTAINER_IDS" ]; then
+        echo "Removing existing containers..."
+        docker rm -f $CONTAINER_IDS
+        echo "Containers removed successfully."
+    else
+        echo "No container to remove."
+    fi
 
-echo "Running the database container..."
-if [ "$DB_DATA_MUST_PERSIST" = true ]; then
+    echo "Running the database container..."
+    if [ "$DB_DATA_MUST_PERSIST" = true ]; then
+        docker run \
+            --name=$DB_CONTAINER_NAME \
+            --volume=db-data:$DB_DATA_DIR \
+            -p $DB_PORT:$DB_PORT \
+            -e ENV=$ENV \
+            -e POSTGRES_DB=$DB_BODZIFY_API_DB_NAME \
+            -e POSTGRES_USER=$DB_SUPERUSER_NAME \
+            -e POSTGRES_PASSWORD=$DB_SUPERUSER_PASSWORD \
+            -e POSTGRES_PORT=$DB_PORT \
+            -d $DOCKERHUB_USERNAME/$DB_IMAGE_REPO:$DB_VERSION
+    else
+        docker run \
+            --name=$DB_CONTAINER_NAME \
+            -p $DB_PORT:$DB_PORT \
+            -e ENV=$ENV \
+            -e POSTGRES_DB=$DB_BODZIFY_API_DB_NAME \
+            -e POSTGRES_USER=$DB_SUPERUSER_NAME \
+            -e POSTGRES_PASSWORD=$DB_SUPERUSER_PASSWORD \
+            -e POSTGRES_PORT=$DB_PORT \
+            -d $DOCKERHUB_USERNAME/$DB_IMAGE_REPO:$DB_VERSION
+    fi
+    echo "Database container running successfully."
+
+    echo "Running the audio fingerprinter container..."
     docker run \
-        --name=$DB_CONTAINER_NAME \
-        --volume=db-data:$DB_DATA_DIR \
-        -p $DB_PORT:$DB_PORT \
+        --name=$AFP_CONTAINER_NAME \
+        --volume=$TMP_UPLOADED_FILES:$AFP_POOL_DIR_EXTERNAL \
+        -p $AFP_PORT:$AFP_PORT \
         -e ENV=$ENV \
-        -e POSTGRES_DB=$DB_BODZIFY_API_DB_NAME \
-        -e POSTGRES_USER=$DB_SUPERUSER_NAME \
-        -e POSTGRES_PASSWORD=$DB_SUPERUSER_PASSWORD \
-        -e POSTGRES_PORT=$DB_PORT \
-        -d $DOCKERHUB_USERNAME/$DB_IMAGE_REPO:$DB_VERSION
-else
-    docker run \
-        --name=$DB_CONTAINER_NAME \
-        -p $DB_PORT:$DB_PORT \
-        -e ENV=$ENV \
-        -e POSTGRES_DB=$DB_BODZIFY_API_DB_NAME \
-        -e POSTGRES_USER=$DB_SUPERUSER_NAME \
-        -e POSTGRES_PASSWORD=$DB_SUPERUSER_PASSWORD \
-        -e POSTGRES_PORT=$DB_PORT \
-        -d $DOCKERHUB_USERNAME/$DB_IMAGE_REPO:$DB_VERSION
-fi
-echo "Database container running successfully."
+        -e DEBUG=$DEBUG \
+        -e APP_PORT=$AFP_PORT \
+        -d $DOCKERHUB_USERNAME/$AFP_IMAGE_REPO:$AFP_VERSION
+    echo "Audio fingerprinter container running successfully."
 
-echo "Running the audio fingerprinter container..."
-docker run \
-    --name=$AFP_CONTAINER_NAME \
-    --volume=$TMP_UPLOADED_FILES:$AFP_POOL_DIR_EXTERNAL \
-    -p $AFP_PORT:$AFP_PORT \
-    -e ENV=$ENV \
-    -e DEBUG=$DEBUG \
-    -e APP_PORT=$AFP_PORT \
-    -d $DOCKERHUB_USERNAME/$AFP_IMAGE_REPO:$AFP_VERSION
-echo "Audio fingerprinter container running successfully."
+    echo "Containers running successfully."
+}
 
-echo "Containers running successfully."
+main "$@"
