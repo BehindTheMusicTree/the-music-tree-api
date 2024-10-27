4d278441279780734dc8212500bc3ca48dfa28bd
mignot <andreas.garcia@bodzify.com>
2024-10-27 14:59:16 +0100

    Refactor using manager

--- bodzify_api/model/playlist/children/criteria/CriteriaPlaylist.py
+++ bodzify_api/model/playlist/children/criteria/CriteriaPlaylist.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
-from django.db import models
+from django.db import models, transaction
+from typing import Dict, Any, Optional, TYPE_CHECKING
 
 from bodzify_api.model.criteria.Criteria import Criteria, Fields as ModelFields
 from bodzify_api.model.criteria.CriteriaType import CriteriaType, CriteriaTypesId
 from bodzify_api.model.playlist.BasePlaylist import BasePlaylist, Fields as BasePlaylistFields
-from bodzify_api.model.playlist.children.ChildPlaylist import ChildPlaylist, Fields as ChildFields
+from bodzify_api.model.playlist.children.ChildPlaylist import ChildPlaylist
+from bodzify_api.model.playlist.children.Fields import Fields as ChildFields
 from bodzify_api.model.playlist.children.criteria.CriteriaPlaylistManager import CriteriaPlaylistManager
+from bodzify_api.utils.model import SaveContext, ensure_update_field
+
+if TYPE_CHECKING:
+    from django.db.models.manager import Manager
 
 
 class SpecialNames:
@@ -41,27 +47,36 @@ class Fields:
 
 
 class CriteriaPlaylist(ChildPlaylist):
-    base_playlist = models.OneToOneField(BasePlaylist,
-                                         on_delete=models.CASCADE,
-                                         primary_key=True,
-                                         related_name=BasePlaylistFields.CRITERIA_CHILD_PLAYLIST)
-    criteria = models.OneToOneField(Criteria,
-                                    on_delete=models.CASCADE,
-                                    blank=True,
-                                    null=True,
-                                    related_name=ModelFields.CRITERIA_PLAYLIST)
-    type = models.ForeignKey(CriteriaType,
-                             on_delete=models.CASCADE,
-                             blank=True,
-                             null=False)
-
-    parent = models.ForeignKey('self',
-                               on_delete=models.CASCADE,
-                               null=True,
-                               related_name='child_playlist')
-    root = models.ForeignKey('self',
-                             on_delete=models.CASCADE,
-                             related_name='descendant_playlist')
+    base_playlist = models.OneToOneField(
+        BasePlaylist,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name=BasePlaylistFields.CRITERIA_CHILD_PLAYLIST
+    )
+    criteria = models.OneToOneField(
+        Criteria,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name=ModelFields.CRITERIA_PLAYLIST
+    )
+    type = models.ForeignKey(
+        CriteriaType,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=False
+    )
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='child_playlist'
+    )
+    root = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='descendant_playlist'
+    )
 
     objects: CriteriaPlaylistManager = CriteriaPlaylistManager()
 
@@ -70,9 +85,16 @@ class CriteriaPlaylist(ChildPlaylist):
         verbose_name = 'Criteria Playlist'
         verbose_name_plural = 'Criteria Playlists'
         indexes = [
-            models.Index(fields=[Fields.BASE_PLAYLIST, Fields.CRITERIA],
-                         name='criteria_playlist_idx'),
+            models.Index(
+                fields=[Fields.BASE_PLAYLIST, Fields.CRITERIA],
+                name='criteria_playlist_idx'
+            ),
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._original_parent = getattr(self, f"{Fields.PARENT}_id", None)
+        self._original_root = getattr(self, f"{Fields.ROOT}_id", None)
 
     @property
     def name(self):
@@ -91,10 +113,80 @@ class CriteriaPlaylist(ChildPlaylist):
     def __str__(self) -> str:
         return f'{self.base_playlist.uuid} | {self.name}'
 
-    def save(self, *args, **kwargs):
-        CriteriaPlaylist.objects.create(
-            user=self.user,
-            type=self.type,
-            criteria=self.criteria,
-            base_playlist=self.base_playlist
+    def _prepare_save(self, **kwargs) -> Dict[str, Any]:
+        ctx = SaveContext(
+            kwargs=kwargs,
+            modified_fields=[],
+            update_fields=kwargs.get('update_fields')
         )
+
+        if self._set_parent():
+            ctx.add_modified_field(Fields.PARENT)
+
+        if self._set_root():
+            ctx.add_modified_field(Fields.ROOT)
+
+        if ctx.modified_fields and not ctx.should_track_fields:
+            ctx.kwargs['update_fields'] = ctx.modified_fields
+
+        return ctx.kwargs
+
+    def _set_parent(self) -> bool:
+        current_parent_id = getattr(self, f"{Fields.PARENT}_id", None)
+
+        if self.criteria and self.criteria.parent:
+            try:
+                parent = CriteriaPlaylist.objects.get(criteria=self.criteria.parent)
+                if current_parent_id != parent.id:
+                    self.parent = parent
+                    return True
+            except CriteriaPlaylist.DoesNotExist:
+                pass
+        elif current_parent_id is not None:
+            self.parent = None
+            return True
+        return False
+
+    def _set_root(self) -> bool:
+        current_root_id = getattr(self, f"{Fields.ROOT}_id", None)
+
+        if self.criteria and self.criteria.root:
+            try:
+                root = CriteriaPlaylist.objects.get(criteria=self.criteria.root)
+                if current_root_id != root.id:
+                    self.root = root
+                    return True
+            except CriteriaPlaylist.DoesNotExist:
+                pass
+        return False
+
+    def _is_creating(self) -> bool:
+        return getattr(self, f"{Fields.ROOT}_id", None) is None
+
+    def _post_save(self, created: bool):
+        if created:
+            self._post_create()
+        else:
+            self._post_update()
+
+    def _post_create(self):
+        pass
+
+    def _post_update(self):
+        current_root_id = getattr(self, f"{Fields.ROOT}_id", None)
+        if self._original_root != current_root_id:
+            self._update_children_root()
+
+    def _update_children_root(self):
+        for child in self.children:
+            child.root = self.root
+            child.save(update_fields=[Fields.ROOT])
+
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        created = self._is_creating()
+        if created:
+            self.root = self
+
+        kwargs = self._prepare_save(**kwargs)
+        super().save(force_insert=force_insert, force_update=force_update, *args, **kwargs)
+        self._post_save(created=created)
