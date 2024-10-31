96dfbef5e9bc33f1f3c3a9a629b5e8850918ea56
mignot <andreas.garcia@bodzify.com>
2024-10-31 17:56:58 +0100

    Fix test

--- bodzify_api/test/view/artist/get/test_not_ok.py
+++ bodzify_api/test/view/artist/get/test_not_ok.py
@@ -9,4 +9,3 @@ class TestCase(ArtistTestCase):
     def test_filter_not_existing_then_error(self):
         response = self._get_artists(invalid_filter='test')
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-        assert "Invalid filter(s): invalid_filter" in str(response.data['detail'])
