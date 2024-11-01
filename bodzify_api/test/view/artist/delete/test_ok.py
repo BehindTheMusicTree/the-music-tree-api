c1096928e5e2ace2164e74695560bb11c3fee67f
mignot <andreas.garcia@bodzify.com>
2024-11-01 02:46:29 +0100

    Fix artist deletion

--- bodzify_api/test/view/artist/delete/test_ok.py
+++ bodzify_api/test/view/artist/delete/test_ok.py
@@ -1,4 +1,3 @@
-
 from rest_framework import status
 
 from bodzify_api.model.album.Album import Album
@@ -6,7 +5,7 @@ from bodzify_api.model.artist.Artist import Artist
 from bodzify_api.test.view.artist.ArtistTestCase import ArtistTestCase
 
 
-class ArtistViewDeleteTestCase(ArtistTestCase):
+class TestCase(ArtistTestCase):
 
     # def test_with_a_track_in_an_album_with_no_other_tracks_then_delete_album(self):
     #     bertrand_artist = self.model_fixture_factory.create_artist(name='Bertrand')
@@ -24,7 +23,7 @@ class ArtistViewDeleteTestCase(ArtistTestCase):
     #     assert response.status_code == status.HTTP_204_NO_CONTENT
     #     assert not Artist.objects.filter(user=self.test_user1, uuid=artist.uuid).exists()
 
-    def test_with_a_track_and_another_artist_on_the_track_with_no_other_ones_then_delete_other_artist(self):
+    def test_with_a_track_and_another_artist_on_the_track_with_no_other_track_then_delete_other_artist(self):
         bertrand_artist = self.model_fixture_factory.create_artist(name='Bertrand')
         coco_artist = self.model_fixture_factory.create_artist(name='Coco')
         self.model_fixture_factory.create_lib_track_with_file(title="Life", artists=[bertrand_artist, coco_artist])
--- bodzify_api/view/viewset/model/ArtistViewSet.py
+++ bodzify_api/view/viewset/model/ArtistViewSet.py
@@ -5,7 +5,6 @@ from rest_framework import status
 
 from bodzify_api.filter.set.ArtistFilterSet import ArtistFilterSet, Fields as FilterFields
 from bodzify_api.model.artist.Artist import Artist
-from bodzify_api.service.ArtistService import ArtistService
 from bodzify_api.view.viewset.model.AppModelViewSet import AppModelViewSet
 from bodzify_api.serializer.schema.artist.detailed import ArtistDetailedSerializer
 
@@ -13,9 +12,9 @@ from bodzify_api.serializer.schema.artist.detailed import ArtistDetailedSerializ
 class ArtistViewSet(AppModelViewSet):
     def __init__(self, **kwargs):
         super().__init__(
-            service=ArtistService(),
             model_class=Artist,
             filter_class=ArtistFilterSet,
+            simple_serializer_class=ArtistDetailedSerializer,
             detailed_serializer_class=ArtistDetailedSerializer,
             **kwargs
         )
@@ -25,3 +24,8 @@ class ArtistViewSet(AppModelViewSet):
     ])
     def list(self, request, *args, **kwargs):
         return super()._list(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        instance: Artist = self.get_object()
+        instance.delete_with_albums_and_tracks()
+        return Response(status=status.HTTP_204_NO_CONTENT)
