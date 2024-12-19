39e5a25cd68eecf6094cd93eeff13bc7ebe65a9f
mignot <andreas.garcia@bodzify.com>
2024-12-19 10:07:31 +0100

    Encode json in tests only when not multipart

--- bodzify_api/test/AppApiClient.py
+++ bodzify_api/test/AppApiClient.py
@@ -15,21 +15,22 @@ class AppApiClient(APIClient):
             data_url_encoded = data_transformer.replace_none_with_empty_string(**data)
         return super().get(path, data_url_encoded, follow, **extra)
 
-    def post(self, path, data: Optional[dict] = None, content_type=None, follow=False, **extra
+    def post(self, path, data: Optional[dict] = None, content_type=None, follow=False, format=None, **extra
              ) -> HttpResponse:
         data_url_encoded = None
         if data:
             data_url_encoded = data_transformer.replace_none_with_empty_string(**data)
-            data_url_encoded = json.dumps(data_url_encoded)
-        return super().post(path, data_url_encoded, content_type=content_type, follow=follow, **extra)
+            if format != 'multipart':
+                data_url_encoded = json.dumps(data_url_encoded)
+        return super().post(path, data_url_encoded, content_type=content_type, follow=follow, format=format, **extra)
 
     def put(self, path, data: Optional[dict] = None, format=None, content_type=None, follow=False, **extra
             ) -> HttpResponse:
         data_url_encoded = None
         if data:
             data_url_encoded = data_transformer.replace_none_with_empty_string(**data)
-            data_url_encoded = json.dumps(data_url_encoded)
-            print(data_url_encoded)
+            if format != 'multipart':
+                data_url_encoded = json.dumps(data_url_encoded)
         return super().put(path, data_url_encoded, format, content_type, follow, **extra)
 
     def delete(self, path, data: Optional[dict] = None, format=None, content_type=None, follow=False, **extra
@@ -37,5 +38,6 @@ class AppApiClient(APIClient):
         data_url_encoded = None
         if data:
             data_url_encoded = data_transformer.replace_none_with_empty_string(**data)
-            data_url_encoded = json.dumps(data_url_encoded)
+            if format != 'multipart':
+                data_url_encoded = json.dumps(data_url_encoded)
         return super().delete(path, data_url_encoded, format, content_type, follow, **extra)
