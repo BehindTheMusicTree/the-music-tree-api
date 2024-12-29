cdc2b6adbd9aa3251008e82c8d7c2379b17666f3
mignot <andreas.garcia@bodzify.com>
2024-12-29 09:42:37 +0100

    Make criteria filterset inherit PrivateUniqueResourceFilterSet

--- bodzify_api/filtering/set/criteria/CriteriaFilterSet.py
+++ bodzify_api/filtering/set/criteria/CriteriaFilterSet.py
@@ -1,11 +1,11 @@
 from bodzify_api.filtering.filter.ForeignKeyFilter import ForeignKeyFilter
 from bodzify_api.filtering.filter.char.NonEmptiableCharFilter import NonEmptiableCharFilter
-from bodzify_api.filtering.set.AppFilterSet import AppFilterSet
+from bodzify_api.filtering.set.private_unique_resource.PrivateUniqueResourceFilterSet import PrivateUniqueResourceFilterSet
 from bodzify_api.model.criteria.Criteria import Criteria
 from .Fields import Fields
 
 
-class CriteriaFilterSet(AppFilterSet):
+class CriteriaFilterSet(PrivateUniqueResourceFilterSet):
     name = NonEmptiableCharFilter(field_name=Fields.NAME_INTERNAL,
                                   lookup_expr='icontains',
                                   field_name_user_friendly=Fields.NAME)
