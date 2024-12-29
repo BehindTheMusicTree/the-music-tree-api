2e5cb5b25a51f3c001a9db3238a27e135fe15676
mignot <andreas.garcia@bodzify.com>
2024-12-29 08:41:16 +0100

    Improve tests

---
+++ bodzify_api/test/get_filters/date/DateFilterTestCase.py
@@ -0,0 +1,10 @@
+from bodzify_api.test.get_filters.FilterTestCase import FilterTestCase
+
+
+class DateFilterTestCase(FilterTestCase):
+
+    def setUp(self, methods_names_to_implement=None):
+        class_methods_to_implement = ['test_format_is_wrong_then_error']
+        if methods_names_to_implement:
+            class_methods_to_implement += methods_names_to_implement
+        return super().setUp(methods_names_to_implement=methods_names_to_implement, allow_empty_value=False)
