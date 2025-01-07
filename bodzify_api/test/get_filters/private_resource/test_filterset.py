d8b62f8ab8c60f39e53e7cba6d0aa755b57026ee
mignot <andreas.garcia@bodzify.com>
2025-01-07 17:23:25 +0100

    Improve tests

--- bodzify_api/test/get_filters/private_resource/test_filterset.py
+++ bodzify_api/test/get_filters/private_resource/test_filterset.py
@@ -1,10 +1,13 @@
 
 from datetime import timedelta
+
 from django.utils import timezone
+
 from bodzify_api.model.criteria.Criteria import Criteria
 from bodzify_api.filtering.set.private_unique_resource.PrivateUniqueResourceFilterSet \
     import PrivateUniqueResourceFilterSet
 from bodzify_api.filtering.set.private_unique_resource.Fields import Fields as PrivateUniqueResourceFields
+from bodzify_api.model.criteria.children.genre.Genre import Genre
 from bodzify_api.test.ApiTestCase import ApiTestCase
 
 
@@ -15,78 +18,90 @@ class TestPrivateUniqueResourceFilterSet(ApiTestCase):
         self.past = self.now - timedelta(days=5)
         self.future = self.now + timedelta(days=5)
 
-        self.past_record = self.model_fixture_factory.create_genre(name='rock',
+        self.past_record = self.model_fixture_factory.create_genre(name='past_genre',
                                                                    created_on=self.past,
                                                                    updated_on=self.past)
 
-        self.present_record = self.model_fixture_factory.create_genre(name='pop',
+        self.present_record = self.model_fixture_factory.create_genre(name='present_genre',
                                                                       created_on=self.now,
                                                                       updated_on=self.now)
-        self.future_record = self.model_fixture_factory.create_genre(name='jazz',
+        self.future_record = self.model_fixture_factory.create_genre(name='future_genre',
                                                                      created_on=self.future,
                                                                      updated_on=self.future)
 
     def test_created_on_exact_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.CREATED_ON: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert list(filterset.qs) == [self.present_record]
 
     def test_created_on_gt_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.CREATED_ON_GT: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert list(filterset.qs) == [self.future_record]
 
     def test_created_on_lt_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.CREATED_ON_LT: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert list(filterset.qs) == [self.past_record]
 
     def test_created_on_gte_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.CREATED_ON_GTE: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert set(filterset.qs) == {self.present_record, self.future_record}
 
     def test_created_on_lte_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.CREATED_ON_LTE: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert set(filterset.qs) == {self.past_record, self.present_record}
 
     def test_updated_on_exact_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.UPDATED_ON: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert list(filterset.qs) == [self.present_record]
 
     def test_updated_on_gt_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.UPDATED_ON_GT: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert list(filterset.qs) == [self.future_record]
 
     def test_updated_on_lt_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.CREATED_ON_LT: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert list(filterset.qs) == [self.past_record]
 
     def test_updated_on_gte_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.UPDATED_ON_GTE: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert set(filterset.qs) == {self.present_record, self.future_record}
 
     def test_updated_on_lte_filter(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.UPDATED_ON_LTE: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert set(filterset.qs) == {self.past_record, self.present_record}
 
     def test_invalid_datetime_format(self):
         filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.CREATED_ON: 'invalid-datetime'},
-                                                   queryset=Criteria.objects.all())
+                                                   queryset=Genre.objects.all())
         assert not filterset.is_valid()
 
     def test_combined_filters(self):
-        filterset = PrivateUniqueResourceFilterSet({PrivateUniqueResourceFields.CREATED_ON_GTE: self.past.isoformat(),
-                                                    PrivateUniqueResourceFields.CREATED_ON_LTE: self.now.isoformat(),
-                                                    PrivateUniqueResourceFields.UPDATED_ON_GTE: self.past.isoformat(),
-                                                    PrivateUniqueResourceFields.UPDATED_ON_LTE: self.now.isoformat()},
-                                                   queryset=Criteria.objects.all())
-        print('HOOOOOOOO')
-        print(Criteria.objects.all())
+        filter_params = {
+            PrivateUniqueResourceFields.CREATED_ON_GTE: self.past.isoformat(),
+            PrivateUniqueResourceFields.CREATED_ON_LTE: self.now.isoformat(),
+            PrivateUniqueResourceFields.UPDATED_ON_GTE: self.past.isoformat(),
+            PrivateUniqueResourceFields.UPDATED_ON_LTE: self.now.isoformat()
+        }
+        print("Filter params:", filter_params)
+        print("Past:", self.past.isoformat())
+        print("Now:", self.now.isoformat())
+        print("Past record created_on:", self.past_record.created_on)
+        print("Past record updated_on:", self.past_record.updated_on)
+        print("Present record created_on:", self.present_record.created_on)
+        print("Present record updated_on:", self.present_record.updated_on)
+
+        filterset = PrivateUniqueResourceFilterSet(filter_params, queryset=Genre.objects.all())
+        print("Is bound:", filterset.is_bound)
+        print("Form is valid:", filterset.is_valid())
+        print("Form errors:", filterset.errors)
+        print("Form cleaned data:", filterset.form.cleaned_data if filterset.is_valid() else None)
         assert set(filterset.qs) == {self.past_record, self.present_record}
--- bodzify_api/test/get_filters/private_resource/test_viewset_inheritance.py
+++ bodzify_api/test/get_filters/private_resource/test_viewset_inheritance.py
@@ -1,8 +1,8 @@
 
-from django.utils import timezone
-from django_filters import FilterSet
 from datetime import timedelta
 
+from django.utils import timezone
+from django_filters import FilterSet
 
 from bodzify_api.filtering.set.private_unique_resource.Fields import Fields as PrivateUniqueResourceFields
 from bodzify_api.filtering.set.album.AlbumFilterSet import AlbumFilterSet
