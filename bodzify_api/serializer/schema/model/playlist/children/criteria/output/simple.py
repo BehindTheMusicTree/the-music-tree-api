d05d8917c80099e32f8a53befe2960681d94ec0c
mignot <andreas.garcia@bodzify.com>
2025-01-07 16:41:59 +0100

    Clean code

--- bodzify_api/serializer/schema/model/playlist/children/criteria/output/simple.py
+++ bodzify_api/serializer/schema/model/playlist/children/criteria/output/simple.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 
 from bodzify_api.model.playlist.children.criteria.CriteriaPlaylist import CriteriaPlaylist
 from bodzify_api.serializer.schema.model.playlist.children.criteria.output.Fields import Fields as AvailableFields
-from bodzify_api.serializer.schema.model.playlist.children.criteria.output.minumum import CriteriaPlaylistMinimumSerializer
+from bodzify_api.serializer.schema.model.playlist.children.criteria.output.minumum \
+    import CriteriaPlaylistMinimumSerializer
 from bodzify_api.serializer.schema.model.criteria.output.simple import CriteriaSimpleSerializer
 
 
@@ -24,7 +25,8 @@ class CriteriaPlaylistSimpleSerializer(serializers.ModelSerializer):
     root = CriteriaPlaylistMinimumSerializer()  # type: ignore
 
     def to_representation(self, instance):
-        assert isinstance(instance, CriteriaPlaylist), f"Expected a CriteriaPlaylist, got {type(instance)}"
+        if not isinstance(instance, CriteriaPlaylist):
+            raise serializers.ValidationError("Invalid instance type")
         return super().to_representation(instance)
 
     class Meta:
