b2da33a1557eb5770f9022eab63fd2f1258cd294
mignot <andreas.garcia@bodzify.com>
2025-01-07 17:14:39 +0100

    Fix dates filters

--- bodzify_api/filtering/set/private_unique_resource/Fields.py
+++ bodzify_api/filtering/set/private_unique_resource/Fields.py
@@ -4,12 +4,12 @@ from bodzify_api.model.private_unique_resource.Fields import Fields as ModelFiel
 
 class Fields:
     CREATED_ON = ModelFields.CREATED_ON
-    CREATED_ON_GT = ModelFields.CREATED_ON + '_Gt'
-    CREATED_ON_LT = ModelFields.CREATED_ON + '_Lt'
-    CREATED_ON_GTE = ModelFields.CREATED_ON + '_Gte'
-    CREATED_ON_LTE = ModelFields.CREATED_ON + '_Lte'
+    CREATED_ON_GT = ModelFields.CREATED_ON + '_gt'
+    CREATED_ON_LT = ModelFields.CREATED_ON + '_lt'
+    CREATED_ON_GTE = ModelFields.CREATED_ON + '_gte'
+    CREATED_ON_LTE = ModelFields.CREATED_ON + '_lte'
     UPDATED_ON = ModelFields.UPDATED_ON
-    UPDATED_ON_GT = ModelFields.UPDATED_ON + '_Gt'
-    UPDATED_ON_LT = ModelFields.UPDATED_ON + '_Lt'
-    UPDATED_ON_GTE = ModelFields.UPDATED_ON + '_Gte'
-    UPDATED_ON_LTE = ModelFields.UPDATED_ON + '_Lte'
+    UPDATED_ON_GT = ModelFields.UPDATED_ON + '_gt'
+    UPDATED_ON_LT = ModelFields.UPDATED_ON + '_lt'
+    UPDATED_ON_GTE = ModelFields.UPDATED_ON + '_gte'
+    UPDATED_ON_LTE = ModelFields.UPDATED_ON + '_lte'
