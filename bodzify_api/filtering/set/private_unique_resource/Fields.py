ea7b701f6073661f84687954d0053a398ef739e6
mignot <andreas.garcia@bodzify.com>
2025-01-07 16:46:59 +0100

    Add update on and created on filters

--- bodzify_api/filtering/set/private_unique_resource/Fields.py
+++ bodzify_api/filtering/set/private_unique_resource/Fields.py
@@ -1,6 +1,15 @@
+
 from bodzify_api.model.private_unique_resource.Fields import Fields as ModelFields
 
 
 class Fields:
     CREATED_ON = ModelFields.CREATED_ON
+    CREATED_ON_GT = ModelFields.CREATED_ON + '_Gt'
+    CREATED_ON_LT = ModelFields.CREATED_ON + '_Lt'
+    CREATED_ON_GTE = ModelFields.CREATED_ON + '_Gte'
+    CREATED_ON_LTE = ModelFields.CREATED_ON + '_Lte'
     UPDATED_ON = ModelFields.UPDATED_ON
+    UPDATED_ON_GT = ModelFields.UPDATED_ON + '_Gt'
+    UPDATED_ON_LT = ModelFields.UPDATED_ON + '_Lt'
+    UPDATED_ON_GTE = ModelFields.UPDATED_ON + '_Gte'
+    UPDATED_ON_LTE = ModelFields.UPDATED_ON + '_Lte'
