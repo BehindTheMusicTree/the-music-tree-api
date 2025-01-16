6bc69aea7ca496df27036d0607881a2545a171e3
mignot <andreas.garcia@bodzify.com>
2025-01-16 07:48:45 +0100

    Fix album artist filter when empty

---
+++ bodzify_api/filtering/filter/char/PrimaryFieldCharFilter.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models import Q, QuerySet
+
+from bodzify_api.filtering.filter.char.EmptiableCharFilter import EmptiableCharFilter
+
+
+class PrimaryFieldCharFilter(EmptiableCharFilter):
+    """
+    A filter that handles filtering by a primary field of a related instance.
+    When the filter value is empty, it returns results with no related instance.
+    """
+
+    def __init__(self, primary_field: str, lookup_expr: str = 'iexact', *args, **kwargs):
+        if not primary_field:
+            raise ImproperlyConfigured(
+                f'{self.__class__.__name__} requires a primary_field argument.'
+            )
+
+        self.primary_field = primary_field
+        # Pass lookup_expr through kwargs instead of setting it directly as self.lookup_expr
+        # to ensure proper field-level configuration in CharFilter parent class
+        kwargs['lookup_expr'] = lookup_expr
+        super().__init__(*args, **kwargs)
+
+    def filter(self, qs: QuerySet, value: Optional[str]) -> QuerySet:
+        if not self.field_name:
+            raise ImproperlyConfigured(
+                f'{self.__class__.__name__} requires a field_name to be set.'
+            )
+
+        if not value:
+            return qs.filter(**{f"{self.field_name}__isnull": True})
+
+        lookup = f"{self.field_name}__{self.primary_field}__{self.lookup_expr}"
+        return qs.filter(Q(**{lookup: value}))
--- bodzify_api/filtering/set/album/AlbumFilterSet.py
+++ bodzify_api/filtering/set/album/AlbumFilterSet.py
@@ -1,10 +1,11 @@
-
-from bodzify_api.filtering.filter.char.EmptiableCharFilter import EmptiableCharFilter
+from attr import field
 from bodzify_api.filtering.filter.char.NonEmptiableCharFilter import NonEmptiableCharFilter
+from bodzify_api.filtering.filter.char.PrimaryFieldCharFilter import PrimaryFieldCharFilter
 from bodzify_api.filtering.set.private_unique_resource.PrivateUniqueResourceFilterSet \
     import PrivateUniqueResourceFilterSet
 from bodzify_api.model.album.Album import Album
 from bodzify_api.model.album.Fields import Fields as ModelFields
+from bodzify_api.model.artist.Fields import Fields as ArtistModelFields
 from .Fields import Fields
 
 
@@ -12,7 +13,12 @@ class AlbumFilterSet(PrivateUniqueResourceFilterSet):
     name = NonEmptiableCharFilter(field_name=ModelFields.NAME_INTERNAL,
                                   field_name_user_friendly=ModelFields.NAME,
                                   lookup_expr='icontains')
-    album_artist_name = EmptiableCharFilter(lookup_expr='icontains')
+    album_artist_name = PrimaryFieldCharFilter(
+        primary_field=ArtistModelFields.NAME_INTERNAL,
+        field_name=ModelFields.ALBUM_ARTISTS,
+        field_name_user_friendly=Fields.ALBUM_ARTIST_NAME,
+        lookup_expr='icontains'
+    )
 
     class Meta:
         model = Album
