98f3a0a9f67c98b880decee696498fc0b8f12b47
mignot <andreas.garcia@bodzify.com>
2025-01-17 14:09:01 +0100

    Clean code

--- bodzify_api/model/album/Fields.py
+++ bodzify_api/model/album/Fields.py
@@ -1,19 +1,19 @@
-from bodzify_api.model.lib_track_mixin.Fields import Fields as LibTrackMixinFields
+from bodzify_api.model.lib_track_mixin.Fields import Fields as Fields
 
 
 class Fields:
-    CREATED_ON = LibTrackMixinFields.CREATED_ON
-    UPDATED_ON = LibTrackMixinFields.UPDATED_ON
-    UUID = LibTrackMixinFields.UUID
-    USER = LibTrackMixinFields.USER
-    NAME = LibTrackMixinFields.NAME
-    NAME_INTERNAL = LibTrackMixinFields.NAME_INTERNAL
-    LIB_TRACKS = LibTrackMixinFields.LIB_TRACKS
-    LIB_TRACKS_NOT_ARCHIVED = LibTrackMixinFields.LIB_TRACKS_NOT_ARCHIVED
-    LIB_TRACKS_COUNT = LibTrackMixinFields.LIB_TRACKS_COUNT
-    LIB_TRACKS_ARCHIVED_COUNT = LibTrackMixinFields.LIB_TRACKS_ARCHIVED_COUNT
-    DURATION_IN_SEC = LibTrackMixinFields.DURATION_IN_SEC
-    DURATION_STR_IN_HOUR_MIN_SEC = LibTrackMixinFields.DURATION_STR_IN_HOUR_MIN_SEC
+    CREATED_ON = Fields.CREATED_ON
+    UPDATED_ON = Fields.UPDATED_ON
+    UUID = Fields.UUID
+    USER = Fields.USER
+    NAME = Fields.NAME
+    NAME_INTERNAL = Fields.NAME_INTERNAL
+    LIB_TRACKS = Fields.LIB_TRACKS
+    LIB_TRACKS_NOT_ARCHIVED = Fields.LIB_TRACKS_NOT_ARCHIVED
+    LIB_TRACKS_COUNT = Fields.LIB_TRACKS_COUNT
+    LIB_TRACKS_ARCHIVED_COUNT = Fields.LIB_TRACKS_ARCHIVED_COUNT
+    DURATION_IN_SEC = Fields.DURATION_IN_SEC
+    DURATION_STR_IN_HOUR_MIN_SEC = Fields.DURATION_STR_IN_HOUR_MIN_SEC
     LIB_TRACKS_RELATED_NAME = 'lib_tracks_of_album'
     YEAR = 'year'
     ALBUM_ARTISTS = 'album_artists'
--- bodzify_api/model/all_lib_tracks_mixin/Fields.py
+++ bodzify_api/model/all_lib_tracks_mixin/Fields.py
@@ -1,15 +1,15 @@
-from bodzify_api.model.lib_track_mixin.Fields import Fields as LibTrackMixinFields
+from bodzify_api.model.lib_track_mixin.Fields import Fields as Fields
 
 
 class Fields:
-    UUID = LibTrackMixinFields.UUID
-    USER = LibTrackMixinFields.USER
-    NAME = LibTrackMixinFields.NAME
-    CREATED_ON = LibTrackMixinFields.CREATED_ON
-    UPDATED_ON = LibTrackMixinFields.UPDATED_ON
-    LIB_TRACKS = LibTrackMixinFields.LIB_TRACKS
-    LIB_TRACKS_NOT_ARCHIVED = LibTrackMixinFields.LIB_TRACKS_NOT_ARCHIVED
-    LIB_TRACKS_COUNT = LibTrackMixinFields.LIB_TRACKS_COUNT
-    LIB_TRACKS_ARCHIVED_COUNT = LibTrackMixinFields.LIB_TRACKS_ARCHIVED_COUNT
-    DURATION_IN_SEC = LibTrackMixinFields.DURATION_IN_SEC
-    DURATION_STR_IN_HOUR_MIN_SEC = LibTrackMixinFields.DURATION_STR_IN_HOUR_MIN_SEC
+    UUID = Fields.UUID
+    USER = Fields.USER
+    NAME = Fields.NAME
+    CREATED_ON = Fields.CREATED_ON
+    UPDATED_ON = Fields.UPDATED_ON
+    LIB_TRACKS = Fields.LIB_TRACKS
+    LIB_TRACKS_NOT_ARCHIVED = Fields.LIB_TRACKS_NOT_ARCHIVED
+    LIB_TRACKS_COUNT = Fields.LIB_TRACKS_COUNT
+    LIB_TRACKS_ARCHIVED_COUNT = Fields.LIB_TRACKS_ARCHIVED_COUNT
+    DURATION_IN_SEC = Fields.DURATION_IN_SEC
+    DURATION_STR_IN_HOUR_MIN_SEC = Fields.DURATION_STR_IN_HOUR_MIN_SEC
