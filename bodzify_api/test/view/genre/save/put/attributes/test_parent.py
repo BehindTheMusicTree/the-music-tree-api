17de2c33075c7e393faa9462dcde8f51ff94a916
mignot <andreas.garcia@bodzify.com>
2025-01-21 16:33:26 +0100

    Fix tests

--- bodzify_api/test/view/genre/save/put/attributes/test_parent.py
+++ bodzify_api/test/view/genre/save/put/attributes/test_parent.py
@@ -9,7 +9,7 @@ class TestCase(GenreTestCase):
     def test_not_provided_then_unchanged(self):
         rock_genre = self.model_fixture_factory.create_genre(name="Rock")
         punk_genre = self.model_fixture_factory.create_genre(name="Punk", parent=rock_genre)
-        response = self._put_genre(uuid=punk_genre.uuid, **{})
+        response = self._put_genre(uuid=punk_genre.uuid, **{PutFields.NAME: "New Punk"})
         assert response.status_code == status.HTTP_200_OK
         assert self.saved_genre.parent == rock_genre
 
