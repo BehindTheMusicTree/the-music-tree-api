08fb69d88adad31fa81f771174146d9adb4fbb7f
mignot <andreas.garcia@bodzify.com>
2025-01-23 16:55:47 +0100

    Fix tests

--- bodzify_api/test/view/genre/save/put/attributes/playlist/test_positions.py
+++ bodzify_api/test/view/genre/save/put/attributes/playlist/test_positions.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from bodzify_api.model.lib_track_playlist_rel.LibTrackPlaylistRel import LibTrackPlaylistRel
 from bodzify_api.model.playlist.children.criteria.CriteriaPlaylist import CriteriaPlaylist
 from bodzify_api.serializer.schema.model.criteria.input.put import Fields as PutFields
+from bodzify_api.serializer.schema.model.lib_track.input.post import Fields as LibTrackPostFields
 from bodzify_api.test.view.genre.GenreTestCase import GenreTestCase
 
 
@@ -42,47 +43,66 @@ class TestCase(GenreTestCase):
 
     def test_new_parent_then_tracks_in_same_order_and_added_at_the_beginning_of_parent_of_parent(self) -> None:
         genre_guitare = self.model_fixture_factory.create_genre(name="Guitare")
-        playlist_guitare = CriteriaPlaylist.objects.get(user=self.test_user1, criteria=genre_guitare)
         lib_track_previously_second_in_guitare = self.model_fixture_factory.create_lib_track_with_file(
-            genre=genre_guitare, title="guitare2")
+            **{LibTrackPostFields.GENRE_UUID: genre_guitare.uuid, LibTrackPostFields.TITLE: "guitare2"},
+            use_manager_for_genre_playlist_adding=True)
         lib_track_previously_first_in_guitare = self.model_fixture_factory.create_lib_track_with_file(
-            genre=genre_guitare, title="guitare1")
+            **{LibTrackPostFields.GENRE_UUID: genre_guitare.uuid, LibTrackPostFields.TITLE: "guitare1"},
+            use_manager_for_genre_playlist_adding=True)
+
+        assert genre_guitare.criteria_playlist.library_tracks.count() == 2
+        first_track_in_guitare_playlist = genre_guitare.criteria_playlist.library_tracks.first()
+        assert first_track_in_guitare_playlist
+        assert first_track_in_guitare_playlist == lib_track_previously_first_in_guitare
+        second_track_in_guitare_playlist = genre_guitare.criteria_playlist.library_tracks.last()
+        assert second_track_in_guitare_playlist
+        assert second_track_in_guitare_playlist == lib_track_previously_second_in_guitare
 
         genre_rock = self.model_fixture_factory.create_genre(name="Rock", parent=genre_guitare)
-        lib_track_previously_second_in_rock = self.model_fixture_factory.create_lib_track_with_file(genre=genre_rock,
-                                                                                                    title="rock2")
-        lib_track_previously_first_in_rock = self.model_fixture_factory.create_lib_track_with_file(genre=genre_rock,
-                                                                                                   title="rock1")
+
+        lib_track_previously_second_in_rock = self.model_fixture_factory.create_lib_track_with_file(
+            **{LibTrackPostFields.GENRE_UUID: genre_rock.uuid, LibTrackPostFields.TITLE: "rock2"},
+            use_manager_for_genre_playlist_adding=True)
+        lib_track_previously_first_in_rock = self.model_fixture_factory.create_lib_track_with_file(
+            **{LibTrackPostFields.GENRE_UUID: genre_rock.uuid, LibTrackPostFields.TITLE: "rock1"},
+            use_manager_for_genre_playlist_adding=True)
 
         genre_punk = self.model_fixture_factory.create_genre(name="Punk")
-        lib_track_previously_second_in_punk = self.model_fixture_factory.create_lib_track_with_file(genre=genre_punk,
-                                                                                                    title="punk2")
-        lib_track_previously_first_in_punk = self.model_fixture_factory.create_lib_track_with_file(genre=genre_punk,
-                                                                                                   title="punk1")
+        lib_track_previously_second_in_punk = self.model_fixture_factory.create_lib_track_with_file(
+            **{LibTrackPostFields.GENRE_UUID: genre_punk.uuid, LibTrackPostFields.TITLE: "punk2"},
+            use_manager_for_genre_playlist_adding=True)
+        lib_track_previously_first_in_punk = self.model_fixture_factory.create_lib_track_with_file(
+            **{LibTrackPostFields.GENRE_UUID: genre_punk.uuid, LibTrackPostFields.TITLE: "punk1"},
+            use_manager_for_genre_playlist_adding=True)
 
         response = self._put_genre(uuid=genre_punk.uuid, **{PutFields.PARENT: genre_rock.uuid})
-
         assert response.status_code == status.HTTP_200_OK
-
         lib_track_playlist_rels: list[LibTrackPlaylistRel] = \
-            list(LibTrackPlaylistRel.objects.filter(user=self.test_user1, playlist=playlist_guitare))
-        tracks_positions = {relation.library_track.uuid: relation.position for relation in lib_track_playlist_rels}
-        assert tracks_positions[lib_track_previously_first_in_punk.uuid] == 1
-        assert tracks_positions[lib_track_previously_second_in_punk.uuid] == 2
-        assert tracks_positions[lib_track_previously_first_in_rock.uuid] == 3
-        assert tracks_positions[lib_track_previously_second_in_rock.uuid] == 4
-        assert tracks_positions[lib_track_previously_first_in_guitare.uuid] == 5
-        assert tracks_positions[lib_track_previously_second_in_guitare.uuid] == 6
+            list(LibTrackPlaylistRel.objects.filter(user=self.test_user1, playlist=genre_guitare.criteria_playlist))
+        tracks_uuids_positions = {
+            relation.library_track.uuid: relation.position for relation in lib_track_playlist_rels}
+        assert tracks_uuids_positions[lib_track_previously_first_in_punk.uuid] == 1
+        assert tracks_uuids_positions[lib_track_previously_second_in_punk.uuid] == 2
+        assert tracks_uuids_positions[lib_track_previously_first_in_rock.uuid] == 3
+        assert tracks_uuids_positions[lib_track_previously_second_in_rock.uuid] == 4
+        assert tracks_uuids_positions[lib_track_previously_first_in_guitare.uuid] == 5
+        assert tracks_uuids_positions[lib_track_previously_second_in_guitare.uuid] == 6
 
     def test_new_parent_not_acendant_of_old_parent_then_update_positions_in_old_parent(self) -> None:
         genre_rock = self.model_fixture_factory.create_genre(name="Rock")
         genre_punk = self.model_fixture_factory.create_genre(name="Punk", parent=genre_rock)
-        track_fourth_in_rock = self.model_fixture_factory.create_lib_track_with_file(genre=genre_rock,
-                                                                                     title="Rock song 2")
-        self.model_fixture_factory.create_lib_track_with_file(genre=genre_punk, title="Punk song 2")
-        track_second_in_rock = self.model_fixture_factory.create_lib_track_with_file(genre=genre_rock,
-                                                                                     title="Rock song")
-        self.model_fixture_factory.create_lib_track_with_file(genre=genre_punk, title="Punk song")
+        track_fourth_in_rock = self.model_fixture_factory.create_lib_track_with_file(
+            **{LibTrackPostFields.GENRE_UUID: genre_rock.uuid, LibTrackPostFields.TITLE: "Rock song 2"},
+            use_manager_for_genre_playlist_adding=True)
+        self.model_fixture_factory.create_lib_track_with_file(
+            **{LibTrackPostFields.GENRE_UUID: genre_rock.uuid, LibTrackPostFields.TITLE: "Punk song 2"},
+            use_manager_for_genre_playlist_adding=True)
+        track_second_in_rock = self.model_fixture_factory.create_lib_track_with_file(
+            **{LibTrackPostFields.GENRE_UUID: genre_rock.uuid, LibTrackPostFields.TITLE: "Rock song 1"},
+            use_manager_for_genre_playlist_adding=True)
+        self.model_fixture_factory.create_lib_track_with_file(
+            **{LibTrackPostFields.GENRE_UUID: genre_punk.uuid, LibTrackPostFields.TITLE: "Punk song 1"},
+            use_manager_for_genre_playlist_adding=True)
 
         response = self._put_genre(uuid=genre_punk.uuid, **{PutFields.PARENT: ''})
 
