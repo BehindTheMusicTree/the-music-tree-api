9d4992fe25c8a9dba354c8e1cd543a5d13fbbe3f
mignot <andreas.garcia@bodzify.com>
2025-01-28 11:32:49 +0100

    Fix manual playlist post

--- bodzify_api/serializer/schema/model/playlist/children/manual/input.py
+++ bodzify_api/serializer/schema/model/playlist/children/manual/input.py
@@ -1,22 +1,22 @@
 
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from bodzify_api import settings
 from bodzify_api.model.playlist.children.manual.ManualPlaylist import ManualPlaylist
 from bodzify_api.model.playlist.children.manual.Fields import Fields as ModelFields
 from bodzify_api.serializer.AppValidationSerializer import AppValidationSerializer
 
 
 class ManualPlaylistInputSerializer(AppValidationSerializer, serializers.ModelSerializer):
+    name = serializers.CharField(max_length=settings.MANUAL_PLAYLIST_NAME_LEN_MAX, allow_blank=False)
+
     class Meta:
         model = ManualPlaylist
         fields = [ModelFields.NAME_PUBLIC]
 
     def validate(self, data):
         user = self.context['request'].user
-        name = data.get(ModelFields.NAME_PUBLIC)  # Try to get from validated data first
-        if name is None:  # If not in validated data
-            name = self.initial_data.get('name')  # Get from initial data
-            data[ModelFields.NAME_PUBLIC] = name  # Add to data dictionary
+        name = data.get(ModelFields.NAME_PUBLIC)
         if ManualPlaylist.objects.filter(user=user, name=name).exists():
             raise ValidationError({ModelFields.NAME_PUBLIC: "already exists"})
         return super().validate(data)
