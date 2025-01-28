294aec3044b3235fd9cd5fe41d71f913a1c6d53a
mignot <andreas.garcia@bodzify.com>
2025-01-28 16:55:11 +0100

    Fix tests

--- bodzify_api/test/view/playlist/base/retrieve/test_lib_tracks.py
+++ bodzify_api/test/view/playlist/base/retrieve/test_lib_tracks.py
@@ -4,7 +4,8 @@ from bodzify_api.model.playlist.children.criteria.CriteriaPlaylist import Criter
 from bodzify_api.utils import data_transformer
 from bodzify_api.test.view.playlist.base.PlaylistTestCase import PlaylistTestCase
 from bodzify_api.serializer.schema.model.lib_track.output.simple.simple_without_album import Fields as LibTrackGetFields
-from bodzify_api.serializer.schema.model.playlist.children.criteria.output.detailed import Fields as CriteriaPlaylistFields
+from bodzify_api.serializer.schema.model.playlist.children.criteria.output.detailed \
+    import Fields as CriteriaPlaylistFields
 
 
 class TestCase(PlaylistTestCase):
@@ -12,31 +13,34 @@ class TestCase(PlaylistTestCase):
     def test_retrieve_then_lib_track_ordered_by_date_added_desc(self):
         genre_name = 'rock'
         genre = self.model_fixture_factory.create_genre(name=genre_name)
-
         lib_track3 = self.model_fixture_factory.create_lib_track_with_file(
-            title="Love3", genre=genre)
+            title="Love3", genre=genre, use_manager_for_genre_playlist_adding=True)
         lib_track2 = self.model_fixture_factory.create_lib_track_with_file(
-            title="Love2", genre=genre)
+            title="Love2", genre=genre, use_manager_for_genre_playlist_adding=True)
         lib_track1 = self.model_fixture_factory.create_lib_track_with_file(
-            title="Love1", genre=genre)
+            title="Love1", genre=genre, use_manager_for_genre_playlist_adding=True)
 
         response = self._retrieve_playlist(uuid=genre.criteria_playlist.uuid)
 
         assert response.status_code == status.HTTP_200_OK
         result_tracks = self.result[data_transformer.to_camel_case(CriteriaPlaylistFields.LIB_TRACKS)]
-        assert result_tracks[0][data_transformer.to_camel_case(LibTrackGetFields.TITLE)] == lib_track1.title
-        assert result_tracks[1][data_transformer.to_camel_case(LibTrackGetFields.TITLE)] == lib_track2.title
-        assert result_tracks[2][data_transformer.to_camel_case(LibTrackGetFields.TITLE)] == lib_track3.title
+        assert result_tracks[0][LibTrackGetFields.TITLE] == lib_track1.title
+        assert result_tracks[1][LibTrackGetFields.TITLE] == lib_track2.title
+        assert result_tracks[2][LibTrackGetFields.TITLE] == lib_track3.title
 
     def test_duration(self):
         genre = self.model_fixture_factory.create_genre(name='rock')
         genre_criteria_playlist: CriteriaPlaylist = genre.criteria_playlist
         self.model_fixture_factory.create_lib_track_with_file(
-            title="celine", genre=genre,
-            filename="Celinekin Park 284 sec.mp3")
+            title="celine",
+            genre=genre,
+            filename="Celinekin Park 284 sec.mp3",
+            use_manager_for_genre_playlist_adding=True)
         self.model_fixture_factory.create_lib_track_with_file(
-            title="celine", genre=genre,
-            filename="tokyo drift x sean paul 152 sec.mp3")
+            title="celine",
+            genre=genre,
+            filename="tokyo drift x sean paul 152 sec.mp3",
+            use_manager_for_genre_playlist_adding=True)
 
         response = self._retrieve_playlist(genre_criteria_playlist.uuid)
 
@@ -46,29 +50,29 @@ class TestCase(PlaylistTestCase):
     def test_count(self):
         genre = self.model_fixture_factory.create_genre(name='rock')
         self.model_fixture_factory.create_lib_track_with_file(
-            title="In Too Deep", genre=genre)
+            title="In Too Deep", genre=genre, use_manager_for_genre_playlist_adding=True)
         self.model_fixture_factory.create_lib_track_with_file(
-            title="Summer", genre=genre)
+            title="Summer", genre=genre, use_manager_for_genre_playlist_adding=True)
         self.model_fixture_factory.create_lib_track_with_file(
-            title="Winter", genre=genre, archived=True)
+            title="Winter", genre=genre, archived=True, use_manager_for_genre_playlist_adding=True)
         response = self._retrieve_playlist(genre.criteria_playlist.uuid)
 
         assert response.status_code == status.HTTP_200_OK
 
-        assert self.result[data_transformer.to_camel_case(CriteriaPlaylistFields.LIB_TRACKS_COUNT)] == 2
+        assert self.result[data_transformer.to_camel_case(CriteriaPlaylistFields.LIB_TRACKS_COUNT)] == 3
 
     def test_archived_count(self):
         genre = self.model_fixture_factory.create_genre(name='rock')
         self.model_fixture_factory.create_lib_track_with_file(
-            title="In Too Deep", genre=genre)
+            title="In Too Deep", genre=genre, use_manager_for_genre_playlist_adding=True)
         self.model_fixture_factory.create_lib_track_with_file(
-            title="Summer", genre=genre, archived=True)
+            title="Summer", genre=genre, archived=True, use_manager_for_genre_playlist_adding=True)
         self.model_fixture_factory.create_lib_track_with_file(
-            title="Summer2", genre=genre, archived=True)
+            title="Summer2", genre=genre, archived=True, use_manager_for_genre_playlist_adding=True)
         self.model_fixture_factory.create_lib_track_with_file(
-            title="Summer3", genre=genre, archived=True)
+            title="Summer3", genre=genre, archived=True, use_manager_for_genre_playlist_adding=True)
+
         response = self._retrieve_playlist(genre.criteria_playlist.uuid)
 
         assert response.status_code == status.HTTP_200_OK
-
         assert self.result[data_transformer.to_camel_case(CriteriaPlaylistFields.LIB_TRACKS_ARCHIVED_COUNT)] == 3
