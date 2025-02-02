d92cefd7d2fa3967fcb08413f4ac001822f2e2d9
mignot <andreas.garcia@bodzify.com>
2025-02-02 19:10:34 +0100

    Clean code

--- bodzify_api/filtering/filter/char/EmptiableCharFilter.py
+++ bodzify_api/filtering/filter/char/EmptiableCharFilter.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 from django_filters import CharFilter
 
 from bodzify_api.filtering.filter.AppFilter import AppFilter
+from bodzify_api.model.base.BaseQuerySet import BaseQuerySet
 
 if TYPE_CHECKING:
     from bodzify_api.filtering.set.AppFilterSet import AppFilterSet
@@ -13,7 +14,7 @@ class EmptiableCharFilter(CharFilter, AppFilter):
         self.method_name = kwargs.pop('method', None)
         super().__init__(*args, **kwargs)
 
-    def filter(self, qs, value):
+    def filter(self, qs: BaseQuerySet, value):
         if not self.method_name:
             result = super().filter(qs, value)
             return result
