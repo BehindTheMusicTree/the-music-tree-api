5abe8b6db64350e111bd21b11f77a953663809fb
mignot <andreas.garcia@bodzify.com>
2025-02-02 23:19:34 +0100

    Improve tests

---
+++ bodzify_api/test/view/genre/save/put/test_extra_field.py
@@ -0,0 +1,12 @@
+from rest_framework import status
+
+from bodzify_api.serializer.schema.model.criteria.input.post import Fields as PostFields
+from bodzify_api.test.view.genre.GenreTestCase import GenreTestCase
+
+
+class TestCase(GenreTestCase):
+
+    def test_extra_field_then_error(self):
+        genre = self.model_fixture_factory.create_genre(name="Rock")
+        response = self._put_genre(uuid=genre.uuid, **{PostFields.NAME_PUBLIC: "Rock", "extra_field": "extra_value"})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
