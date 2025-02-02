aa1f8f88c371315fd1ff50b936eb1f4d986b7958
mignot <andreas.garcia@bodzify.com>
2025-02-02 18:44:41 +0100

    Improve tests

---
+++ bodzify_api/test/test_internal_error_then_500_error_code_with_detail_error_code.py
@@ -0,0 +1,21 @@
+
+from unittest.mock import patch
+
+from rest_framework import status
+from django.db import IntegrityError
+
+from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
+from bodzify_api.view.error.ErrorCode import ErrorCode
+
+
+class TestCase(LibTrackTestCase):
+
+    def test_internal_error_then_500_with_response_error_code(self):
+        with patch('bodzify_api.model.track.lib.LibraryTrack.LibraryTrack.save') as mock:
+            exception_message = "DB Integrity Error"
+            mock.side_effect = IntegrityError(exception_message)
+
+            results = self._post_lib_track_with_generic_sample_no_tags(title='test')
+            assert results.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+            json = results.json()
+            assert json['code'] == ErrorCode.SYSTEM_INTERNAL_ERROR.value
