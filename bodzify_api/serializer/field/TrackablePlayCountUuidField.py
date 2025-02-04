1dff74ccbd465f24e1b5ddf6ce66153d14cab90a
mignot <andreas.garcia@bodzify.com>
2025-02-04 15:23:14 +0100

    Add TrackablePlayCountUuidField and use it

---
+++ bodzify_api/serializer/field/TrackablePlayCountUuidField.py
@@ -0,0 +1,61 @@
+from typing import Union, Type, Optional
+from uuid import UUID
+
+from django.contrib.auth.models import User, AnonymousUser
+from django.db.models import Model, QuerySet
+from rest_framework.exceptions import ValidationError
+from rest_framework.request import Request
+
+from bodzify_api.model.playlist.Playlist import Playlist
+from bodzify_api.model.track.lib.LibraryTrack import LibraryTrack
+from bodzify_api.serializer.field.PrivateUuidField import PrivateUuidField
+from bodzify_api.utils.validation_error_utils import raise_validation_error
+from bodzify_api.view.error.FieldValidationErrorCode import FieldValidationErrorCode
+
+
+class TrackablePlayCountUuidField(PrivateUuidField):
+    """
+    A field that validates a UUID refers to a trackable play count object (Playlist or LibraryTrack)
+    owned by the current user.
+    """
+
+    def __init__(self, **kwargs):
+        # Initialize with a dummy queryset, we'll handle the actual validation in to_internal_value
+        super().__init__(queryset=Playlist.objects.none(), **kwargs)
+
+    def _get_object_by_uuid(self, uuid_value: UUID, user: Union[User, AnonymousUser], model_class: Type[Model]) -> Optional[Model]:
+        """Helper method to get an object by UUID and user."""
+        if isinstance(user, AnonymousUser):
+            return None
+            
+        try:
+            return model_class.objects.get(uuid=uuid_value, user=user)
+        except model_class.DoesNotExist:
+            return None
+
+    def to_internal_value(self, data):
+        if data in [None, ''] and self.allow_null:
+            return None
+
+        uuid_value = super(PrivateUuidField, self).to_internal_value(data)
+        request = self.context.get('request')
+
+        if not isinstance(request, Request):
+            raise ValueError("TrackablePlayCountUuidField requires request in the context")
+
+        user = request.user
+        
+        # Try to find the object in either Playlist or LibraryTrack
+        content_object = (
+            self._get_object_by_uuid(uuid_value, user, Playlist) or
+            self._get_object_by_uuid(uuid_value, user, LibraryTrack)
+        )
+
+        if not content_object:
+            raise_validation_error(
+                message='Object with this ID does not exist or does not belong to the user',
+                field_validation_error_code=FieldValidationErrorCode.FIELD_RESOURCE_NOT_OWNED,
+                field=self.field_name or 'content_object_uuid'
+            )
+
+        return uuid_value
--- bodzify_api/serializer/schema/model/play/input/schema/schema.py
+++ bodzify_api/serializer/schema/model/play/input/schema/schema.py
@@ -1,10 +1,7 @@
 from rest_framework import serializers
 
 from bodzify_api.model.play.Play import Play, Fields as ModelFields
-from bodzify_api.model.playlist.Playlist import Playlist
-from bodzify_api.model.track.lib.LibraryTrack import LibraryTrack
-from bodzify_api.utils.validation_error_utils import raise_validation_error
-from bodzify_api.view.error.FieldValidationErrorCode import FieldValidationErrorCode
+from bodzify_api.serializer.field.TrackablePlayCountUuidField import TrackablePlayCountUuidField
 
 
 class Fields:
@@ -12,19 +9,8 @@ class Fields:
 
 
 class PlaySchemaSerializer(serializers.ModelSerializer):
-    content_object_uuid = serializers.UUIDField()
+    content_object_uuid = TrackablePlayCountUuidField()
 
     class Meta:
         model = Play
         fields = [Fields.CONTENT_OBJECT_UUID]
-
-    def validate_content_object_uuid(self, object_pk):
-        user = self.context['request'].user
-        if not Playlist.objects.filter(user=user, uuid=object_pk).exists() \
-                and not LibraryTrack.objects.filter(user=user, uuid=object_pk).exists():
-            raise_validation_error(
-                message='Object with this ID does not exist or does not belong to the user',
-                field_validation_error_code=FieldValidationErrorCode.FIELD_RESOURCE_NOT_OWNED,
-                field=Fields.CONTENT_OBJECT_UUID
-            )
-        return object_pk
