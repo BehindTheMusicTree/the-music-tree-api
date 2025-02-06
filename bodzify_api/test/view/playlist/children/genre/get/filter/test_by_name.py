4c301106d26cf6016748fc57cb258cf96d601170
mignot <andreas.garcia@bodzify.com>
2025-02-06 15:11:51 +0100

    Fix tests

--- bodzify_api/test/view/playlist/children/genre/get/filter/test_by_name.py
+++ bodzify_api/test/view/playlist/children/genre/get/filter/test_by_name.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 
 from bodzify_api.serializer.schema.model.playlist.children.criteria.output.detailed import Fields as GetResultFields
+from bodzify_api.filtering.set.playlist.Fields import Fields as FilterFields
 from bodzify_api.test.field.filter.char.NotNullableFreeCharFilterTestCase import NotNullableFreeCharFilterTestCase
 from bodzify_api.test.view.playlist.children.genre.GenrePlaylistTestCase import GenrePlaylistTestCase
 
@@ -11,7 +12,7 @@ class TestCase(GenrePlaylistTestCase, NotNullableFreeCharFilterTestCase):
         tag = self.model_fixture_factory.create_tag(name="foot")
         genre = self.model_fixture_factory.create_genre(name="foot rock")
 
-        response = self._get_genre_playlists(name='foot')
+        response = self._get_genre_playlists(**{FilterFields.NAME: 'foot'})
 
         assert response.status_code == status.HTTP_200_OK
         assert self.results_overall_total == 1
@@ -22,7 +23,7 @@ class TestCase(GenrePlaylistTestCase, NotNullableFreeCharFilterTestCase):
     def test_empty_then_error(self):
         self.model_fixture_factory.create_genre(name="Rock")
 
-        response = self._get_genre_playlists(name='')
+        response = self._get_genre_playlists(**{FilterFields.NAME: ''})
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
@@ -31,7 +32,7 @@ class TestCase(GenrePlaylistTestCase, NotNullableFreeCharFilterTestCase):
         criteria2 = self.model_fixture_factory.create_genre(name="Rockabilly")
         self.model_fixture_factory.create_genre(name="Punk")
 
-        response = self._get_genre_playlists(name='RO')
+        response = self._get_genre_playlists(**{FilterFields.NAME: 'RO'})
 
         assert response.status_code == status.HTTP_200_OK
         assert self.results_overall_total == 2
@@ -47,7 +48,7 @@ class TestCase(GenrePlaylistTestCase, NotNullableFreeCharFilterTestCase):
         response = self._get_genre_playlists()
 
         assert response.status_code == status.HTTP_200_OK
-        assert self.results_overall_total == 3
+        assert self.results_overall_total == 4
         result_names = [result[GetResultFields.NAME] for result in self.results]
         assert criteria1.name in result_names
         assert criteria2.name in result_names
