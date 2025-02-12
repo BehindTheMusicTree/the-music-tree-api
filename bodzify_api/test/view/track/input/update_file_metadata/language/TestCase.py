d5ee69427ff5733880c426b78e66e9302e4d5399
mignot <andreas.garcia@bodzify.com>
2025-02-12 14:04:34 +0100

    Clean tests

--- bodzify_api/test/view/track/input/update_file_metadata/language/TestCase.py
+++ bodzify_api/test/view/track/input/update_file_metadata/language/TestCase.py
@@ -1,12 +1,12 @@
 from bodzify_api import settings
 from bodzify_api.utils.audio_metadata.NormalizedMetadataKeys import NormalizedMetadataKeys
-from bodzify_api.serializer.schema.model.lib_track.input.Fields import InputFields as InputFields
+from bodzify_api.serializer.schema.model.lib_track.input.Fields import Fields as Fields
 from bodzify_api.test.view.track.input.update_file_metadata.UpdateFileMetadataStrTestCase import \
     UpdateFileMetadataStrTestCase
 
 
 class TestCase(UpdateFileMetadataStrTestCase):
-    save_field = InputFields.LANGUAGE
+    save_field = Fields.LANGUAGE
     lib_track_normalized_metadata_key = NormalizedMetadataKeys.LANGUAGE
     length_max = settings.LIB_TRACK_LANGUAGE_LEN_MAX
 
