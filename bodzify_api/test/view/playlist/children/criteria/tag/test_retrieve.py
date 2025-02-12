66c93cc87f9ad745296c30b212cb38676a06abbc
mignot <andreas.garcia@bodzify.com>
2025-02-12 12:43:44 +0100

    Fix tests

--- bodzify_api/test/view/playlist/children/criteria/tag/test_retrieve.py
+++ bodzify_api/test/view/playlist/children/criteria/tag/test_retrieve.py
@@ -8,9 +8,9 @@ class TestCase(TagPlaylistTestCase):
 
     def test_retrieve_then_ok(self):
         tag_fiesta_name = "Fiesta"
-        tag_fiesta_playlist = self.model_fixture_factory.create_tag(name=tag_fiesta_name)
+        tag_fiesta = self.model_fixture_factory.create_tag(name=tag_fiesta_name)
 
-        response = self._retrieve_tag_playlist(uuid=tag_fiesta_playlist.uuid)
+        response = self._retrieve_tag_playlist(uuid=tag_fiesta.criteria_playlist.uuid)
 
         assert response.status_code == status.HTTP_200_OK
         assert self.result[RietrieveFields.NAME] == tag_fiesta_name
