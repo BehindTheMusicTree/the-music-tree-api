58ed48139c3ce8c24eb765494d1ec9579c197603
mignot <andreas.garcia@bodzify.com>
2025-02-18 14:15:00 +0100

    Fix tests

--- bodzify_api/test/view/playlist/children/manual/ManualPlaylistTestCase.py
+++ bodzify_api/test/view/playlist/children/manual/ManualPlaylistTestCase.py
@@ -8,34 +8,28 @@ from bodzify_api.test.ApiTestCase import ApiTestCase
 
 
 class ManualPlaylistTestCase(ApiTestCase):
-    saved_manual_playlist: ManualPlaylist
-
-    def _set_saved_manual_playlist_attribute(self, response):
-        uuid = response.json()[ManualPlaylistGetFields.UUID]
-        self.saved_manual_playlist = ManualPlaylist.objects.get(uuid=uuid)
+    saved_object: ManualPlaylist
+    model_class = ManualPlaylist
 
     def _post_manual_playlist(self, **kwargs):
         return self.api_client.post(
             path=reverse('manual-playlist-list'),
             data=kwargs,
             content_type='application/json',
-            on_success=self._set_saved_manual_playlist_attribute,
-            on_bad_request=self._set_bad_request_result
+            handle_response=self._set_results
         )
 
     def _retrieve_manual_playlist(self, uuid):
         return self.api_client.get(
             path=reverse('manual-playlist-detail', kwargs={'pk': uuid}),
-            on_success=self._set_saved_manual_playlist_attribute,
-            on_bad_request=self._set_bad_request_result
+            handle_response=self._set_results
         )
 
     def _get_manual_playlists(self, **kwargs):
         return self.api_client.get(
             path=reverse('manual-playlist-list'),
             data=kwargs,
-            on_success=self._set_results_attributes,
-            on_bad_request=self._set_bad_request_result
+            handle_response=self._set_results
         )
 
     def _put_manual_playlist(self, uuid: UUID, **kwargs):
@@ -43,8 +37,7 @@ class ManualPlaylistTestCase(ApiTestCase):
             path=reverse('manual-playlist-detail', kwargs={'pk': uuid}),
             data=kwargs,
             content_type='application/json',
-            on_success=self._set_saved_manual_playlist_attribute,
-            on_bad_request=self._set_bad_request_result
+            handle_response=self._set_results
         )
 
     def _delete_manual_playlist(self, uuid):
