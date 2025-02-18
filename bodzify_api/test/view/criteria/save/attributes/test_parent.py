d9220b087e1a38f4bdc03daf3c0223f8a18e30d5
mignot <andreas.garcia@bodzify.com>
2025-02-18 10:38:23 +0100

    Fix tests

--- bodzify_api/test/view/criteria/save/attributes/test_parent.py
+++ bodzify_api/test/view/criteria/save/attributes/test_parent.py
@@ -40,6 +40,5 @@ class TestCase(GenreTestCase, NullableBodyDataTestCase):
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
-        print('error', error)
         assert error['field'] == Fields.PARENT
-        assert error['code'] == FieldValidationErrorCode.RESOURCE_NOT_OWNED.value
+        assert error['code'] == FieldValidationErrorCode.INVALID_REFERENCE.value
--- bodzify_api/test/view/criteria/save/post/attributes/test_name.py
+++ bodzify_api/test/view/criteria/save/post/attributes/test_name.py
@@ -9,22 +9,25 @@ class TestCase(GenreTestCase):
 
     def test_not_provided_then_error(self):
         response = self._post_genre(**{})
+
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
         assert error['field'] == PostFields.NAME_PUBLIC
-        assert error['code'] == FieldValidationErrorCode.REQUIRED
+        assert error['code'] == FieldValidationErrorCode.REQUIRED.value
 
     def test_empty_then_error(self):
         response = self._post_genre(**{PostFields.PARENT: ""})
+
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
         assert error['field'] == PostFields.PARENT
-        assert error['code'] == FieldValidationErrorCode.BLANK
+        assert error['code'] == FieldValidationErrorCode.BLANK.value
 
     def test_value_then_ok(self):
         name = "rock"
         response = self._post_genre(**{PostFields.NAME_PUBLIC: name})
+
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.name == name
