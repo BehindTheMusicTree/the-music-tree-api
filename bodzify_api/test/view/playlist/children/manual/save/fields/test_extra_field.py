06727d727b654452d0b967ab60a844a9a752c10c
mignot <andreas.garcia@bodzify.com>
2025-02-18 14:16:10 +0100

    Fix tests

--- bodzify_api/test/view/playlist/children/manual/save/fields/test_extra_field.py
+++ bodzify_api/test/view/playlist/children/manual/save/fields/test_extra_field.py
@@ -3,14 +3,16 @@ from rest_framework import status
 from bodzify_api.serializer.schema.model.criteria.input.post import Fields as PostFields
 from bodzify_api.test.view.playlist.children.manual.ManualPlaylistTestCase import ManualPlaylistTestCase
 from bodzify_api.validator.FieldValidationErrorCode import FieldValidationErrorCode
+from bodzify_api.view.error.ErrorResponseFields import ErrorResponseFields
 
 
 class TestCase(ManualPlaylistTestCase):
 
     def test_extra_field_then_error(self):
-        response = self._post_manual_playlist(**{PostFields.NAME_PUBLIC: "Rock", "extra_field": "extra_value"})
+        extra_field = "extraField"
+        response = self._post_manual_playlist(**{PostFields.NAME_PUBLIC: "Rock", extra_field: "extra_value"})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
-        assert error[ErrorResponseFields.FIELD] == 'extra_field'
-        assert error[ErrorResponseFields.CODE] == FieldValidationErrorCode.INVALID_REFERENCE
+        assert error[ErrorResponseFields.FIELD] == extra_field
+        assert error[ErrorResponseFields.CODE] == FieldValidationErrorCode.UNKNOWN_FIELD
