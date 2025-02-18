ed24fffc98694e1a9408af98fed7b7fb7d26a41f
mignot <andreas.garcia@bodzify.com>
2025-02-18 20:10:34 +0100

    Fix tests

--- bodzify_api/test/view/track/delete/test_old_linked_album_and_artists.py
+++ bodzify_api/test/view/track/delete/test_old_linked_album_and_artists.py
@@ -8,7 +8,7 @@ from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
 @pytest.mark.django_db
-class TrackDeleteViewTestCase(LibTrackTestCase):
+class TestCase(LibTrackTestCase):
 
     def test_linked_album_then_delete_it_as_nothing_linked_to_it_anymore(self):
         album_name = "Chuck"
--- bodzify_api/test/view/track/input/save/album/test_album.py
+++ bodzify_api/test/view/track/input/save/album/test_album.py
@@ -4,6 +4,7 @@ from bodzify_api import settings
 from bodzify_api.serializer.schema.model.lib_track.input.extract import Fields as ExtractFields
 from bodzify_api.test.field.body_data.type.NullableBodyDataTestCase import NullableBodyDataTestCase
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
+from bodzify_api.utils.data_transformer import to_camel_case
 from bodzify_api.validator.FieldValidationErrorCode import FieldValidationErrorCode
 from bodzify_api.view.error.ErrorResponseFields import ErrorResponseFields
 
@@ -25,7 +26,7 @@ class TestCase(LibTrackTestCase, NullableBodyDataTestCase):
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
-        assert error[ErrorResponseFields.FIELD] == ExtractFields.ALBUM_NAME
+        assert error[ErrorResponseFields.FIELD] == to_camel_case(ExtractFields.ALBUM_NAME)
         assert error[ErrorResponseFields.CODE] == FieldValidationErrorCode.STRING_TOO_LONG.value
 
     def test_empty_then_none(self):
