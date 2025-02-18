f0d689596051cc6a40af9fec202107d1ba149798
mignot <andreas.garcia@bodzify.com>
2025-02-18 11:10:37 +0100

    Fix tests

--- bodzify_api/test/view/criteria/save/put/test_extra_field.py
+++ bodzify_api/test/view/criteria/save/put/test_extra_field.py
@@ -11,10 +11,11 @@ class TestCase(GenreTestCase):
     def test_extra_field_then_error(self):
         genre = self.model_fixture_factory.create_genre(name="Rock")
 
-        response = self._put_genre(uuid=genre.uuid, **{PostFields.NAME_PUBLIC: "Rock", "extra_field": "extra_value"})
+        extra_field = "extraField"
+        response = self._put_genre(uuid=genre.uuid, **{PostFields.NAME_PUBLIC: "Rock", extra_field: "extra_value"})
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
-        assert error['field'] == 'extra_field'
-        assert error[ErrorResponseFields.CODE] == FieldValidationErrorCode.INVALID_REFERENCE.value
+        assert error['field'] == extra_field
+        assert error[ErrorResponseFields.CODE] == FieldValidationErrorCode.UNKNOWN_FIELD.value
