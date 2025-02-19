1cf4f279f38374674203d378618831b52aac78fb
mignot <andreas.garcia@bodzify.com>
2025-02-19 12:13:54 +0100

    Add manual playlist input Fields

--- bodzify_api/serializer/schema/model/playlist/children/manual/input/post.py
+++ bodzify_api/serializer/schema/model/playlist/children/manual/input/post.py
@@ -1,16 +1,16 @@
 
 from bodzify_api import settings
 from bodzify_api.model.playlist.children.manual.ManualPlaylist import ManualPlaylist
-from bodzify_api.model.playlist.children.manual.Fields import Fields as ModelFields
 from bodzify_api.serializer.field.UniquePerUserNameField import UniquePerUserNameField
 from bodzify_api.serializer.schema.model.playlist.children.manual.input.input import ManualPlaylistInputSerializer
+from .Fields import Fields
 
 
 class ManualPlaylistPostSerializer(ManualPlaylistInputSerializer):
     name = UniquePerUserNameField(max_length=settings.MANUAL_PLAYLIST_NAME_LEN_MAX,
-                                  source=ModelFields.NAME_INTERNAL,
+                                  source=Fields.NAME_INTERNAL,
                                   model=ManualPlaylist)
 
     class Meta:
         model = ManualPlaylist
-        fields = [ModelFields.NAME_PUBLIC]
+        fields = [Fields.NAME_PUBLIC]
