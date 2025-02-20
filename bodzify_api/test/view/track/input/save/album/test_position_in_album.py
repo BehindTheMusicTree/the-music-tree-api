16d11f0db4f3ce3938a7b1830cdb90a36bdffcf2
mignot <andreas.garcia@bodzify.com>
2025-02-20 17:13:11 +0100

    Fix tests

--- bodzify_api/test/view/track/input/save/album/test_position_in_album.py
+++ bodzify_api/test/view/track/input/save/album/test_position_in_album.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from bodzify_api import settings
 from bodzify_api.serializer.schema.model.lib_track.input.post.Fields import Fields as PostFields
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
+from bodzify_api.utils.data_transformer import to_camel_case
 from bodzify_api.validator.FieldValidationErrorCode import FieldValidationErrorCode
 from bodzify_api.view.error.ErrorResponseFields import ErrorResponseFields
 
@@ -27,7 +28,7 @@ class TestCase(LibTrackTestCase):
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
-        assert error[ErrorResponseFields.FieldErrors.FIELD] == PostFields.POSITION_IN_ALBUM
+        assert error[ErrorResponseFields.FieldErrors.FIELD] == to_camel_case(PostFields.POSITION_IN_ALBUM)
         assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.POSITION_IN_ALBUM_TOO_SMALL.value
 
     def test_one_then_ok(self):
@@ -54,7 +55,7 @@ class TestCase(LibTrackTestCase):
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
-        assert error[ErrorResponseFields.FieldErrors.FIELD] == PostFields.POSITION_IN_ALBUM
+        assert error[ErrorResponseFields.FieldErrors.FIELD] == to_camel_case(PostFields.POSITION_IN_ALBUM)
         assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.POSITION_IN_ALBUM_TOO_LARGE.value
 
     def test_negative_one_then_error(self):
@@ -63,7 +64,7 @@ class TestCase(LibTrackTestCase):
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
-        assert error[ErrorResponseFields.FieldErrors.FIELD] == PostFields.POSITION_IN_ALBUM
+        assert error[ErrorResponseFields.FieldErrors.FIELD] == to_camel_case(PostFields.POSITION_IN_ALBUM)
         assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.POSITION_IN_ALBUM_TOO_SMALL.value
 
     def test_not_integer_then_error(self):
@@ -72,5 +73,5 @@ class TestCase(LibTrackTestCase):
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
-        assert error[ErrorResponseFields.FieldErrors.FIELD] == PostFields.POSITION_IN_ALBUM
+        assert error[ErrorResponseFields.FieldErrors.FIELD] == to_camel_case(PostFields.POSITION_IN_ALBUM)
         assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.INVALID_FORMAT.value
