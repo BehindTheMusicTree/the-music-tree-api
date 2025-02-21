588052d30dc42e8a4df3d88befeda980401a5848
mignot <andreas.garcia@bodzify.com>
2025-02-21 14:57:13 +0100

    Fix tests

--- bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_musicbrainz_recording.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_musicbrainz_recording.py
@@ -1,10 +1,12 @@
 import uuid
 
+import pytest
 from rest_framework import status
 
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
+@pytest.mark.usefixtures("enable_audio_metadata_analysis")
 class TestCase(LibTrackTestCase):
 
     def test_no_matching_recording_then_none(self):
@@ -17,14 +19,14 @@ class TestCase(LibTrackTestCase):
         response = self._post_lib_track_with_specific_sample("total_eclipse_9_matches_but_one_with_duration.flac")
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
-        assert self.saved_object.track_file.musicbrainz_recording.uuid == uuid.UUID(
+        assert self.saved_object.track_file.musicbrainz_recording.musicbrainz_id == uuid.UUID(
             '9f3c3b61-41a6-4bb9-a49c-33606f536784')
 
     def test_with_2_matches_then_the_one_with_closest_duration(self):
         response = self._post_lib_track_with_specific_sample("lorie_2_matches_but_one_with_closest_duration.mp3")
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
-        assert self.saved_object.track_file.musicbrainz_recording.uuid == uuid.UUID(
+        assert self.saved_object.track_file.musicbrainz_recording.musicbrainz_id == uuid.UUID(
             '76e1d5e6-9713-4c6b-8238-9d7983fd4497')
 
     def test_with_2_matches_with_same_duration_and_same_number_of_fields_then_the_one_with_the_most_release_groups(self):
@@ -32,7 +34,7 @@ class TestCase(LibTrackTestCase):
             "allumerlefeu_2_matches_but_one_with_more_release_groups.mp3")
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
-        assert self.saved_object.track_file.musicbrainz_recording.uuid == uuid.UUID(
+        assert self.saved_object.track_file.musicbrainz_recording.musicbrainz_id == uuid.UUID(
             '82b4c5fe-0980-4495-95b0-bd5e124486d8')
 
     def test_with_25_matches_then_select_the_one_with_best_duration_and_most_fields_and_most_release_groups(self):
@@ -40,5 +42,5 @@ class TestCase(LibTrackTestCase):
             "queen_25_matches_but_one_with_best_duration_and_most_fields_and_most_release_groups.mp3")
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
-        assert self.saved_object.track_file.musicbrainz_recording.uuid == uuid.UUID(
+        assert self.saved_object.track_file.musicbrainz_recording.musicbrainz_id == uuid.UUID(
             '3604eb06-4bc2-4416-9b31-ceadae51bc70')
--- bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_title.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_title.py
@@ -1,18 +1,22 @@
+import pytest
 from rest_framework import status
 
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
+@pytest.mark.usefixtures("enable_audio_metadata_analysis")
 class TestCase(LibTrackTestCase):
 
     def test_drown_7m21_mp3_then_ok(self):
         response = self._post_lib_track_with_specific_sample("oostil - drown (massano remix) - 7m21.mp3")
+
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
         assert self.saved_object.track_file.musicbrainz_recording.title == "Drown (Massano remix)"
 
     def test_totaleclipe_5m35_flac_then_ok(self):
         response = self._post_lib_track_with_specific_sample("Bonnie Tyler - Total Eclipse of the Heart - 5m35.flac")
+
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
         assert self.saved_object.track_file.musicbrainz_recording.title == "Total Eclipse of the Heart"
