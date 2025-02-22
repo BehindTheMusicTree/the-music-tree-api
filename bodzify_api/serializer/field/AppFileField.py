de689443da6f63d78f098daecb4739c7274586ea
mignot <andreas.garcia@bodzify.com>
2025-02-22 11:10:14 +0100

    Implement and use AppFileField

---
+++ bodzify_api/serializer/field/AppFileField.py
@@ -0,0 +1,15 @@
+
+from rest_framework import serializers
+from django.utils.translation import gettext as _
+
+from bodzify_api.serializer.field.AppField import AppField
+
+
+class AppFileField(AppField, serializers.FileField):
+
+    def __init__(self, *args, **kwargs):
+        self.field_name = kwargs.pop('field_name', None)
+        super().__init__(*args, **kwargs)
+
+    def to_internal_value(self, data):
+        return serializers.FileField.to_internal_value(self, data)
--- bodzify_api/serializer/model/lib_track/input/post/post.py
+++ bodzify_api/serializer/model/lib_track/input/post/post.py
@@ -1,8 +1,12 @@
-
-from rest_framework import serializers
-
 from bodzify_api.serializer.model.lib_track.input.input import LibTrackInputSerializer
+from bodzify_api.validator.track_file_validator import FileExtensionValidator
+from bodzify_api.serializer.field.AppFileField import AppFileField
+from .Fields import Fields
 
 
 class LibTrackPostSerializer(LibTrackInputSerializer):
-    file = serializers.FileField(required=True)
+    file = AppFileField(
+        required=True,
+        field_name=Fields.TRACK_FILE_PUBLIC,
+        validators=[
+            FileExtensionValidator(allowed_extensions=['wav', 'mp3', 'flac'])])
