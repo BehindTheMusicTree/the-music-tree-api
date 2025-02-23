cfc0108e8a23395cb5733684cccca230267cecff
mignot <andreas.garcia@bodzify.com>
2025-02-23 16:55:22 +0100

    Refactor riff and id3v2 genre map

--- bodzify_api/utils/audio_metadata/id3/Id3v1Manager.py
+++ bodzify_api/utils/audio_metadata/id3/Id3v1Manager.py
@@ -4,6 +4,9 @@ import struct
 from bodzify_api.utils.audio_metadata.MetadataManager import MetadataManager, NormalizedMetadataKeys
 
 
+from ..constants import ID3V2_AND_RIFF_GENRE_MAP
+
+
 class Id3v1Manager(MetadataManager):
     """
     Manages ID3v1 metadata for audio files.
@@ -21,33 +24,6 @@ class Id3v1Manager(MetadataManager):
     Note: If byte 125 is null and byte 126 is non-null in the comment field,
     then byte 126 is the track number (ID3v1.1 extension).
     """
-
-    # Standard ID3v1 genre codes mapping
-    GENRE_MAP = {
-        0: "Blues", 1: "Classic Rock", 2: "Country", 3: "Dance",
-        4: "Disco", 5: "Funk", 6: "Grunge", 7: "Hip-Hop",
-        8: "Jazz", 9: "Metal", 10: "New Age", 11: "Oldies",
-        12: "Other", 13: "Pop", 14: "R&B", 15: "Rap",
-        16: "Reggae", 17: "Rock", 18: "Techno", 19: "Industrial",
-        20: "Alternative", 21: "Ska", 22: "Death Metal", 23: "Pranks",
-        24: "Soundtrack", 25: "Euro-Techno", 26: "Ambient", 27: "Trip-Hop",
-        28: "Vocal", 29: "Jazz+Funk", 30: "Fusion", 31: "Trance",
-        32: "Classical", 33: "Instrumental", 34: "Acid", 35: "House",
-        36: "Game", 37: "Sound Clip", 38: "Gospel", 39: "Noise",
-        40: "Alternative Rock", 41: "Bass", 42: "Soul", 43: "Punk",
-        44: "Space", 45: "Meditative", 46: "Instrumental Pop",
-        47: "Instrumental Rock", 48: "Ethnic", 49: "Gothic",
-        50: "Darkwave", 51: "Techno-Industrial", 52: "Electronic",
-        53: "Pop-Folk", 54: "Eurodance", 55: "Dream",
-        56: "Southern Rock", 57: "Comedy", 58: "Cult",
-        59: "Gangsta", 60: "Top 40", 61: "Christian Rap",
-        62: "Pop/Funk", 63: "Jungle", 64: "Native US",
-        65: "Cabaret", 66: "New Wave", 67: "Psychadelic",
-        68: "Rave", 69: "Showtunes", 70: "Trailer",
-        71: "Lo-Fi", 72: "Tribal", 73: "Acid Punk",
-        74: "Acid Jazz", 75: "Polka", 76: "Retro",
-        77: "Musical", 78: "Rock & Roll", 79: "Hard Rock",
-    }
 
     def get_raw_metadata(self) -> dict:
         """Read ID3v1 tag from the end of the file."""
@@ -80,7 +56,7 @@ class Id3v1Manager(MetadataManager):
             'album': [album] if album else [],
             'year': [year] if year else [],
             'comment': [comment] if comment else [],
-            'genre': [genre] if genre < len(self.GENRE_MAP) else [],
+            'genre': [genre] if genre < len(ID3V2_AND_RIFF_GENRE_MAP) else [],
             'track': [track] if track is not None else []
         }
 
@@ -105,7 +81,7 @@ class Id3v1Manager(MetadataManager):
         if 'genre' in self.file_raw_metadata:
             try:
                 genre_code = self.file_raw_metadata['genre'][0]
-                return self.GENRE_MAP.get(genre_code, "Other")
+                return ID3V2_AND_RIFF_GENRE_MAP.get(genre_code, "Other")
             except (IndexError, KeyError):
                 return None
         return None
@@ -132,7 +108,7 @@ class Id3v1Manager(MetadataManager):
         return 0
 
     def get_eventually_normalized_rating_value(self,
-                                             normalized_rating_max_value: Optional[int] = None) -> Optional[int]:
+                                               normalized_rating_max_value: Optional[int] = None) -> Optional[int]:
         """ID3v1 doesn't support ratings."""
         return None
 
@@ -142,7 +118,7 @@ class Id3v1Manager(MetadataManager):
             normalized_metadata_key: str,
             normalized_rating_max_value: Optional[int] = None):
         """Update ID3v1 tag field.
-        
+
         Note: This implementation is read-only as modifying ID3v1 tags
         requires careful handling of the fixed-length fields and file seeking.
         """
