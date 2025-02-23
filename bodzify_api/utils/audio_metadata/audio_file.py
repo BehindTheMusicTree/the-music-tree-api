511360a009f7ce9d5c5134d198e222618fedf5ab
mignot <andreas.garcia@bodzify.com>
2025-02-23 01:27:15 +0100

    Fix InMemoryFile IO operations

--- bodzify_api/utils/audio_metadata/audio_file.py
+++ bodzify_api/utils/audio_metadata/audio_file.py
@@ -18,14 +18,11 @@ class AudioFile:
         elif isinstance(file, FieldFile):
             self.file_path = file.path
         elif isinstance(file, InMemoryUploadedFile):
-            temp_file = tempfile.NamedTemporaryFile(delete=False)
-            temp_file.write(file.read())
-            temp_file.close()
-            self.file_path = temp_file.name
+            self.file_path = None
         else:
             self.file_path = file
 
-        if not os.path.exists(self.file_path):
+        if self.file_path is not None and not os.path.exists(self.file_path):
             print('file does not exist')
             raise FileNotFoundError(f"File {self.file_path} does not exist")
 
@@ -39,16 +36,29 @@ class AudioFile:
         return
 
     def read(self, size: int = -1) -> bytes:
-        with open(self.file_path, 'rb') as f:
-            return f.read(size)
+        if isinstance(self.file, InMemoryUploadedFile):
+            return self.file.read(size)
+        elif self.file_path is not None:
+            with open(self.file_path, 'rb') as f:
+                return f.read(size)
+        else:
+            raise NotImplementedError(f"Reading is not supported for file type: {type(self.file)}")
 
     def write(self, data: bytes) -> int:
-        with open(self.file_path, 'wb') as f:
-            return f.write(data)
+        if isinstance(self.file, InMemoryUploadedFile):
+            return self.file.write(data)
+        elif self.file_path is not None:
+            with open(self.file_path, 'wb') as f:
+                return f.write(data)
+        else:
+            raise NotImplementedError(f"Writing is not supported for file type: {type(self.file)}")
 
     def seek(self, offset: int, whence: int = 0) -> int:
-        with open(self.file_path, 'rb') as f:
-            return f.seek(offset, whence)
+        if self.file_path is not None:
+            with open(self.file_path, 'rb') as f:
+                return f.seek(offset, whence)
+        else:
+            raise NotImplementedError(f"Seeking is not supported for file type: {type(self.file)}")
 
     def close(self) -> None:
         if isinstance(self.file, (TemporaryUploadedFile, FieldFile, InMemoryUploadedFile)):
