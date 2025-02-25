180560c996d67759c0f6c38abc66038ec22d38d6
mignot <andreas.garcia@bodzify.com>
2025-02-25 23:05:01 +0100

    Clean code

--- bodzify_api/utils/audio_fingerprinter/service.py
+++ bodzify_api/utils/audio_fingerprinter/service.py
@@ -9,7 +9,7 @@ from bodzify_api.model.track.file.fingerprinting.missing_cause.FingerprintMissin
 from bodzify_api.model.user.User import User
 
 from . import utils
-from .utils import exception as audio_fingerprinter_exception
+from .utils import exception as audio_fingerprinter_exc
 
 
 def _get_fingerprint_and_duration_from_file(user_id: str, file, title: str) -> tuple[bytes, int]:
@@ -34,27 +34,26 @@ def get_fingerprinting_result(user: User, track_file: DjangoFile, title: str) ->
         fingerprint, duration_in_sec = _get_fingerprint_and_duration_from_file(
             user_id=user.pk, file=track_file, title=title)
 
-    except audio_fingerprinter_exception.AudioFingerprinterException as e:
+    except audio_fingerprinter_exc.AudioFingerprinterException as e:
         fingerprint = None
         error_mapping = {
-            audio_fingerprinter_exception.WrongFileExtension: FingerprintMissingCauseCode.Codes.WRONG_FILE_EXTENSION,
-            audio_fingerprinter_exception.WrongFileType: FingerprintMissingCauseCode.Codes.WRONG_FILE_TYPE,
-            audio_fingerprinter_exception.FileNotInPool: FingerprintMissingCauseCode.Codes.FILE_NOT_FOUND_IN_POOL,
-            audio_fingerprinter_exception.BadRequestException: FingerprintMissingCauseCode.Codes.UNKNOWN_BAD_REQUEST,
-            audio_fingerprinter_exception.InternalServerException: FingerprintMissingCauseCode.Codes.INTERNAL_ERROR,
-            audio_fingerprinter_exception.TimeoutException: FingerprintMissingCauseCode.Codes.TIMEOUT_ERROR,
-            audio_fingerprinter_exception.FpcalcStatusException: FingerprintMissingCauseCode.Codes.FPCALC_ERROR_WITH_STATUS_2,
-            audio_fingerprinter_exception.UnknownUnprocessableEntityException: FingerprintMissingCauseCode.Codes.UNKNOWN_UNPROCESSABLE_ENTITY_ERROR,
-            audio_fingerprinter_exception.ServiceNotFoundException: FingerprintMissingCauseCode.Codes.SERVICE_NOT_FOUND,
-            audio_fingerprinter_exception.ConnectionException: FingerprintMissingCauseCode.Codes.UNKNOWN_CONNEXION_ERROR
+            audio_fingerprinter_exc.WrongFileExtension: FingerprintMissingCauseCode.Codes.WRONG_FILE_EXTENSION,
+            audio_fingerprinter_exc.WrongFileType: FingerprintMissingCauseCode.Codes.WRONG_FILE_TYPE,
+            audio_fingerprinter_exc.FileNotInPool: FingerprintMissingCauseCode.Codes.FILE_NOT_FOUND_IN_POOL,
+            audio_fingerprinter_exc.BadRequestException: FingerprintMissingCauseCode.Codes.UNKNOWN_BAD_REQUEST,
+            audio_fingerprinter_exc.InternalServerException: FingerprintMissingCauseCode.Codes.INTERNAL_ERROR,
+            audio_fingerprinter_exc.TimeoutException: FingerprintMissingCauseCode.Codes.TIMEOUT_ERROR,
+            audio_fingerprinter_exc.FpcalcStatusException: FingerprintMissingCauseCode.Codes.FPCALC_ERROR_WITH_STATUS_2,
+            audio_fingerprinter_exc.UnknownUnprocessableEntityException:
+            FingerprintMissingCauseCode.Codes.UNKNOWN_UNPROCESSABLE_ENTITY_ERROR,
+            audio_fingerprinter_exc.ServiceNotFoundException: FingerprintMissingCauseCode.Codes.SERVICE_NOT_FOUND,
+            audio_fingerprinter_exc.ConnectionException: FingerprintMissingCauseCode.Codes.UNKNOWN_CONNEXION_ERROR
         }
 
         fingerprinting_missing_cause_code = error_mapping.get(e.__class__, None)
 
-        fingerprint_missing_cause = FingerprintMissingCause.objects.create(user=user,
-                                                                           code=fingerprinting_missing_cause_code,
-                                                                           message=str(e))
+        fingerprint_missing_cause = FingerprintMissingCause.objects.create(
+            user=user, code=fingerprinting_missing_cause_code, message=e.message)
 
-    return FingerprintingResult(fingerprint=fingerprint,
-                                duration_in_sec=duration_in_sec,
-                                error=fingerprint_missing_cause)
+    return FingerprintingResult(
+        fingerprint=fingerprint, duration_in_sec=duration_in_sec, error=fingerprint_missing_cause)
