9e0e806883eb5213796f305bd92bc29496bc5679
mignot <andreas.garcia@bodzify.com>
2025-02-25 19:45:08 +0100

    Make audio fingerprinter use AudioFile

--- bodzify_api/utils/audio_fingerprinter/service.py
+++ bodzify_api/utils/audio_fingerprinter/service.py
@@ -1,53 +1,27 @@
 import os
-import tempfile
-from typing import Dict
 
-from django.db.models.fields.files import FieldFile
 from django.core.files.base import File as DjangoFile
-from django.core.files.uploadedfile import InMemoryUploadedFile, TemporaryUploadedFile
 
-from bodzify_api import settings
 from bodzify_api.model.user.User import User
 from bodzify_api.model.track.file.fingerprinting.missing_cause.FingerprintMissingCause import FingerprintMissingCause
-from bodzify_api.model.track.file.fingerprinting.missing_cause.code.FingerprintMissingCauseCode import FingerprintMissingCauseCode
+from bodzify_api.model.track.file.fingerprinting.missing_cause.code.FingerprintMissingCauseCode \
+    import FingerprintMissingCauseCode
 from bodzify_api.model.track.file.fingerprinting.FingerprintingResult import FingerprintingResult
-from bodzify_api.utils.AppDjangoFIle import AppDjangoFile
 from .utils import exception as audio_fingerprinter_exception
 from . import utils
 
 
 def _get_fingerprint_and_duration_from_file(user_id: str, file, title: str) -> tuple[bytes, int]:
-    if isinstance(file, InMemoryUploadedFile):
-        with tempfile.NamedTemporaryFile(delete=False, dir=settings.FILE_UPLOAD_TEMP_DIR) as tmp_file:
-            for chunk in file.chunks():
-                tmp_file.write(chunk)
-            file_path = tmp_file.name
-            filename = os.path.basename(file_path)
-            fingerprint, duration_in_sec = utils.post_fingerprint_audio(user_id=user_id,
-                                                                        filename=filename,
-                                                                        title=title,)
-            os.remove(file_path)
-    elif isinstance(file, TemporaryUploadedFile):
-        file_path = file.file.name
-        filename = os.path.basename(file_path)
-        fingerprint, duration_in_sec = utils.post_fingerprint_audio(user_id=user_id,
-                                                                    filename=filename,
-                                                                    title=title)
-    elif isinstance(file, AppDjangoFile):
-        filename = os.path.basename(file.file_abs_path)
-        fingerprint, duration_in_sec = utils.post_fingerprint_audio(user_id=user_id,
-                                                                    filename=filename,
-                                                                    title=title)
-    elif isinstance(file, FieldFile):
-        file_path = file.path
-        filename = os.path.basename(file_path)
-        fingerprint, duration_in_sec = utils.post_fingerprint_audio(user_id=user_id,
-                                                                    filename=filename,
-                                                                    title=title)
-    else:
-        raise ValueError(f"Unsupported file type {type(file)}")
+    from bodzify_api.utils.AudioFile import AudioFile
 
-    return fingerprint, int(duration_in_sec)
+    with AudioFile(file) as audio_file:
+        filename = os.path.basename(audio_file.get_file_name())
+        fingerprint, duration_in_sec = utils.post_fingerprint_audio(
+            user_id=user_id,
+            filename=filename,
+            title=title
+        )
+        return fingerprint, int(duration_in_sec)
 
 
 def get_fingerprinting_result(user: User, track_file: DjangoFile, title: str) -> FingerprintingResult:
@@ -61,19 +35,17 @@ def get_fingerprinting_result(user: User, track_file: DjangoFile, title: str) ->
 
     except audio_fingerprinter_exception.AudioFingerprinterException as e:
         fingerprint = None
-        error_mapping: Dict[type, FingerprintMissingCauseCode.Codes] = {
+        error_mapping = {
             audio_fingerprinter_exception.WrongFileExtension: FingerprintMissingCauseCode.Codes.WRONG_FILE_EXTENSION,
             audio_fingerprinter_exception.WrongFileType: FingerprintMissingCauseCode.Codes.WRONG_FILE_TYPE,
             audio_fingerprinter_exception.FileNotInPool: FingerprintMissingCauseCode.Codes.FILE_NOT_FOUND_IN_POOL,
             audio_fingerprinter_exception.BadRequestException: FingerprintMissingCauseCode.Codes.UNKNOWN_BAD_REQUEST,
-            audio_fingerprinter_exception.InternalServerException]: FingerprintMissingCauseCode.Codes.INTERNAL_ERROR,
+            audio_fingerprinter_exception.InternalServerException: FingerprintMissingCauseCode.Codes.INTERNAL_ERROR,
             audio_fingerprinter_exception.TimeoutException: FingerprintMissingCauseCode.Codes.TIMEOUT_ERROR,
-            audio_fingerprinter_exception.FpcalcStatusException:
-            FingerprintMissingCauseCode.Codes.FPCALC_ERROR_WITH_STATUS_2,
-            audio_fingerprinter_exception.UnknownUnprocessableEntityException:
-            FingerprintMissingCauseCode.Codes.UNKNOWN_UNPROCESSABLE_ENTITY_ERROR,
+            audio_fingerprinter_exception.FpcalcStatusException: FingerprintMissingCauseCode.Codes.FPCALC_ERROR_WITH_STATUS_2,
+            audio_fingerprinter_exception.UnknownUnprocessableEntityException: FingerprintMissingCauseCode.Codes.UNKNOWN_UNPROCESSABLE_ENTITY_ERROR,
             audio_fingerprinter_exception.ServiceNotFoundException: FingerprintMissingCauseCode.Codes.SERVICE_NOT_FOUND,
-            audio_fingerprinter_exception.ConnectionException: FingerprintMissingCauseCode.Codes.UNKNOWN_CONNEXION_ERROR,
+            audio_fingerprinter_exception.ConnectionException: FingerprintMissingCauseCode.Codes.UNKNOWN_CONNEXION_ERROR
         }
 
         fingerprinting_missing_cause_code = error_mapping.get(e.__class__, None)
