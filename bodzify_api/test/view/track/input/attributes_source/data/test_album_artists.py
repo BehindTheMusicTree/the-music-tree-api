4b8a4278f4b7e0b9ca46436717148fcfd9e90724
mignot <andreas.garcia@bodzify.com>
2025-02-28 00:42:48 +0100

    Clean code

--- bodzify_api/test/view/track/input/attributes_source/data/test_album_artists.py
+++ bodzify_api/test/view/track/input/attributes_source/data/test_album_artists.py
@@ -4,11 +4,12 @@ from rest_framework import status
 
 from bodzify_api.model.artist.Artist import Artist
 from bodzify_api.serializer.model.lib_track.input.post.Fields import Fields as PostFields
+from bodzify_api.test.utils.field.body_data.type.NullableListBodyDataTestCase import NullablelistBodyDataTestCase
 from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
-class TestCase(LibTrackTestCase):
+class TestCase(LibTrackTestCase, NullablelistBodyDataTestCase):
 
     def test_value_then_ok(self):
         value = 'astititit'
