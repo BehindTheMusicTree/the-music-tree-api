6ccfdafd7ba78feac12d9aab58cdc19fac79d440
mignot <andreas.garcia@bodzify.com>
2025-02-28 00:42:42 +0100

    Clean code

--- bodzify_api/test/view/track/input/attributes_source/data/test_rating.py
+++ bodzify_api/test/view/track/input/attributes_source/data/test_rating.py
@@ -2,21 +2,24 @@ from rest_framework import status
 
 from bodzify_api.exception.validation.FieldValidationErrorCode import FieldValidationErrorCode
 from bodzify_api.serializer.model.lib_track.input.post.Fields import Fields as PostFields
+from bodzify_api.test.utils.field.body_data.type.NullablePositiveIntBodyDataTestCase import (
+    NullablePositiveIntBodyDataTestCase)
 from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
-from bodzify_api.test.utils.field.body_data.type.FieldIntFromDataTestCase import FieldIntFromDataTestCase
 from bodzify_api.view.error.ErrorResponseFields import ErrorResponseFields
 
 
-class RatingTestCase(FieldIntFromDataTestCase):
+class RatingTestCase(NullablePositiveIntBodyDataTestCase):
 
     def test_value_then_ok(self):
         value = 1
         response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3, **{PostFields.RATING: value})
+
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == value
 
     def test_empty_then_none(self):
         response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3, **{PostFields.RATING: ""})
+
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == None
 
