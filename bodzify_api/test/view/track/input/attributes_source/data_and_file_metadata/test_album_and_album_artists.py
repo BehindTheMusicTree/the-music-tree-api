11ddf5be0b3e3af52d6e05621e424ab61a08d69d
mignot <andreas.garcia@bodzify.com>
2025-02-28 00:42:31 +0100

    Clean code

--- bodzify_api/test/view/track/input/attributes_source/data_and_file_metadata/test_album_and_album_artists.py
+++ bodzify_api/test/view/track/input/attributes_source/data_and_file_metadata/test_album_and_album_artists.py
@@ -2,6 +2,7 @@ from rest_framework import status
 
 from bodzify_api import settings
 from bodzify_api.serializer.model.lib_track.input.post.Fields import Fields as PostFields
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
@@ -10,7 +11,7 @@ class TestCase(LibTrackTestCase):
     def test_album_in_both_then_take_from_data(self):
         data_album_name = "ko"
         data_dict = {PostFields.ALBUM_NAME: data_album_name}
-        response = self._post_lib_track_with_generic_sample_tags_max_length_of_a(**data_dict)
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3v2_MP3, **data_dict)
 
         assert response.status_code == status.HTTP_201_CREATED
         album = self.saved_object.album
@@ -20,7 +21,7 @@ class TestCase(LibTrackTestCase):
     def test_only_album_in_data_and_only_album_artists_in_metadata_then_take_both(self):
         data_album_name = "Best of"
         data_dict = {PostFields.ALBUM_NAME: data_album_name}
-        response = self._post_lib_track_with_generic_sample_tag_album_artists_koko_without_album(**data_dict)
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_KOKO_ID3V2_MP3, **data_dict)
 
         assert response.status_code == status.HTTP_201_CREATED
         album = self.saved_object.album
@@ -33,7 +34,7 @@ class TestCase(LibTrackTestCase):
     def test_only_album_in_data_and_album_and_album_artists_max_a_in_metadata_then_take_album_from_data_and_album_artists_from_metadata(self):
         data_album_name = "oiuhgoi"
         data_dict = {PostFields.ALBUM_NAME: data_album_name}
-        response = self._post_lib_track_with_generic_sample_tags_max_length_of_a(**data_dict)
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3v2_MP3, **data_dict)
 
         assert response.status_code == status.HTTP_201_CREATED
         album = self.saved_object.album
@@ -50,7 +51,7 @@ class TestCase(LibTrackTestCase):
             PostFields.ALBUM_NAME: data_album_name,
             PostFields.ALBUM_ARTISTS_NAMES_ARRAY: data_album_artists_str
         }
-        response = self._post_lib_track_with_generic_sample_tag_album_koko_without_album_artists(**data_dict)
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_KOKO_ID3V2_MP3, **data_dict)
 
         assert response.status_code == status.HTTP_201_CREATED
         album = self.saved_object.album
@@ -63,9 +64,11 @@ class TestCase(LibTrackTestCase):
     def test_album_and_album_artists_in_data_and_metadata_then_take_from_data(self):
         data_album_name = "non"
         data_album_artists_str = "oiuhgoi efe"
-        data_dict = {PostFields.ALBUM_NAME: data_album_name,
-                     PostFields.ALBUM_ARTISTS_NAMES_ARRAY: data_album_artists_str}
-        response = self._post_lib_track_with_generic_sample_tags_max_length_of_a(**data_dict)
+        data_dict = {
+            PostFields.ALBUM_NAME: data_album_name,
+            PostFields.ALBUM_ARTISTS_NAMES_ARRAY: data_album_artists_str
+        }
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3v2_MP3, **data_dict)
 
         assert response.status_code == status.HTTP_201_CREATED
         album = self.saved_object.album
