25d83cd9fd83777dbc3cbebc278f9114c0c2eae4
mignot <andreas.garcia@bodzify.com>
2025-02-28 02:16:25 +0100

    Clean code

--- bodzify_api/test/view/track/input/update_file_metadata/UpdateFileMetadataStrTestCase.py
+++ bodzify_api/test/view/track/input/update_file_metadata/UpdateFileMetadataStrTestCase.py
@@ -2,6 +2,7 @@
 
 from rest_framework import status
 
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
