425b8ccbe998fe22c2423f00776c269c41f0e888
mignot <andreas.garcia@bodzify.com>
2025-02-28 01:58:58 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/put/test_extra_field.py
+++ bodzify_api/test/view/track/input/method/put/test_extra_field.py
@@ -7,7 +7,7 @@ from bodzify_api.view.error.ErrorResponseFields import ErrorResponseFields
 
 class TestCase(LibTrackTestCase):
 
-    def test_extra_field_then_error(self):
+    def test_extra_field_then_400(self):
         extra_field = "extraField"
         track = self.model_fixture_factory.create_lib_track_with_file(title="Foire")
 
