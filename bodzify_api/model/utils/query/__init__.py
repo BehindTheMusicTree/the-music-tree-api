7fab9705917c23c6e969fc89783d4bec15b680bc
mignot <andreas.garcia@bodzify.com>
2025-02-28 20:16:50 +0100

    Remove useless utility

--- bodzify_api/model/utils/query/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Query utilities for model field transformations."""
-
-from .field_transform import transform_name_fields, uses_internal_name
-
-
-__all__ = ['transform_name_fields', 'uses_internal_name']
