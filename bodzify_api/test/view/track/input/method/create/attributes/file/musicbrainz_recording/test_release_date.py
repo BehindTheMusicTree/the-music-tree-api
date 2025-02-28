796a7c65a4ae604d2a3a0536f7475041c40b515b
mignot <andreas.garcia@bodzify.com>
2025-02-28 01:36:40 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_release_date.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_release_date.py
@@ -3,6 +3,7 @@ import datetime
 import pytest
 from rest_framework import status
 
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
@@ -10,7 +11,7 @@ from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 class TestCase(LibTrackTestCase):
 
     def test_multiple_release_dates_then_earliest(self):
-        response = self._post_lib_track("queen_multiple_release_dates.mp3")
+        response = self._post_lib_track(TestLibTrackFilename.RECORDING_QUEEN_MULTIPLE_RELEASE_DATES_MP3)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
         assert self.saved_object.track_file.musicbrainz_recording.release_date == datetime.date(1977, 10, 28)
