52bbd8e19ff94222e1e33df3d57d617e121f52ee
mignot <andreas.garcia@bodzify.com>
2025-02-28 13:10:16 +0100

    Fix tests

--- bodzify_api/test/utils/field/filter/char/NotNullableFreeCharFilterTestCase.py
+++ bodzify_api/test/utils/field/filter/char/NotNullableFreeCharFilterTestCase.py
@@ -8,7 +8,7 @@ class NotNullableFreeCharFilterTestCase(FilterTestCase):
 
     def setUp(self, methods_names_to_implement: list[str] | None = None) -> None:
         class_methods_names_to_implement = ['test_contains_in_another_case_then_results',
-                                            'test_not_provided_then_results'
+                                            'test_not_provided_then_results',
                                             'test_empty_then_400',]
         if methods_names_to_implement:
             class_methods_names_to_implement += methods_names_to_implement
