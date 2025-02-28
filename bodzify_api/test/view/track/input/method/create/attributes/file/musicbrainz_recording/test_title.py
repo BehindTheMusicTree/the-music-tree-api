ddd3ef97decbd44bd25bb413d3c4d512fcace0e5
mignot <andreas.garcia@bodzify.com>
2025-02-28 01:36:29 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_title.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_title.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework import status
 
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
@@ -8,14 +9,14 @@ from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 class TestCase(LibTrackTestCase):
 
     def test_drown_7m21_mp3_then_ok(self):
-        response = self._post_lib_track("oostil - drown (massano remix) - 7m21.mp3")
+        response = self._post_lib_track(TestLibTrackFilename.RECORDING_JUAN_HANSEN_OOSTIL_DROWN_MASSANO_REMIX_7M21_MP3)
 
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
         assert self.saved_object.track_file.musicbrainz_recording.title == "Drown (Massano remix)"
 
     def test_totaleclipe_5m35_flac_then_ok(self):
-        response = self._post_lib_track("Bonnie Tyler - Total Eclipse of the Heart - 5m35.flac")
+        response = self._post_lib_track(TestLibTrackFilename.RECORDING_TOTAL_ECLIPSE_5M35_FLAC)
 
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
