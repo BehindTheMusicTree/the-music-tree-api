df44e92ba7a68d8c65b22619badbd2be553d5d55
mignot <andreas.garcia@bodzify.com>
2025-02-28 11:08:22 +0100

    Clean code

---
+++ bodzify_api/test/utils/ApiTestCase.py
@@ -0,0 +1,164 @@
+from pathlib import Path
+from typing import Generic, Type, TypeVar, Union
+
+from django.core.management import call_command
+from django.db import models
+from django.http import HttpResponse, JsonResponse
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
+
+from bodzify_api.model.track.lib.LibraryTrack import LibraryTrack
+from bodzify_api.model.user.User import User
+from bodzify_api.model.uuid.Fields import Fields as UuidModelFields
+from bodzify_api.serializer.model.lib_track.input.post.Fields import Fields as LibTrackPostFields
+from bodzify_api.test.utils.AppApiClient import AppApiClient
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
+from bodzify_api.test.utils.ModelFixtureFactory import ModelFixtureFactory
+from bodzify_api.utils import audio_metadata, data_transformer
+from bodzify_api.view.error.ErrorResponseFields import ErrorResponseFields
+from bodzify_api.view.pagination.PaginatedResponseFields import PaginatedResponseFields
+
+
+T = TypeVar('T', bound=models.Model)
+
+
+class ApiTestCase(TestCase, Generic[T]):
+    model_class: Type[T]  # Must be defined in child classes
+    saved_object: T  # Must be defined in child classes
+
+    api_client: AppApiClient
+    saved_lib_track_metadata: dict
+
+    TEST_FILES_BASE_DIR = Path(__file__).parent.parent / 'utils' / 'lib_track' / 'files'
+
+    def _login_as_user(self, user: User):
+        self.api_client.force_authenticate(user=user)
+        AccessToken.for_user(user)
+        self.api_client.credentials(HTTP_AUTHORIZATION='Bearer {access}')
+
+    def _login_as_test_user1(self):
+        self._login_as_user(self.test_user1)
+
+    def _login_as_test_user2(self):
+        self._login_as_user(self.test_user2)
+
+    def _login_as_test_admin(self):
+        self._login_as_user(self.test_admin_user)
+
+    def _set_saved_object(self, response):
+        """Get model instance from response UUID.
+
+        Child classes must define model_class class attribute.
+        """
+        if not hasattr(self, 'model_class') or self.model_class is None:
+            raise NotImplementedError("Test case must define model_class")
+
+        uuid = response.json()[UuidModelFields.UUID]
+        # At this point model_class is guaranteed to be a Model class with objects manager
+        self.saved_object = self.model_class.objects.get(uuid=uuid)  # type: ignore
+        if isinstance(self.saved_object, LibraryTrack):
+            self._set_saved_lib_track_metadata(response)
+
+    def _set_single_result(self, response):
+        self.result = response.json()
+        if hasattr(self, 'model_class'):
+            self._set_saved_object(response)
+        else:
+            raise NotImplementedError("Test case must define model_class")
+
+    def _set_results(self, response):
+        if response.status_code == status.HTTP_400_BAD_REQUEST:
+            self._set_bad_request_result(response)
+        elif response.status_code in [status.HTTP_200_OK, status.HTTP_201_CREATED]:
+            # list endpoints return paginated results
+            if isinstance(response.json(), dict) and PaginatedResponseFields.RESULTS in response.json():
+                self._set_results_attributes(response)
+            else:
+                self._set_single_result(response)
+
+    def _set_bad_request_result(self, response):
+        """Store bad request result details with field-specific error information.
+
+        Handles responses with field errors in the format:
+        {
+            "code": 2001,
+            "message": "Bad Request",
+            "success": false,
+            "details": [{
+                "message": "One or more fields contain invalid data. Please check the error details for specific 
+                    validation requirements",
+                "fieldErrors": {
+                    "field1": {"message": "...", "code": "..."},
+                    "field2": {"message": "...", "code": "..."}
+                }
+            }]
+        }
+        """
+        self.bad_request_result = response.json()
+        bad_request_result_details = self.bad_request_result[ErrorResponseFields.DETAILS][0]
+        self.bad_request_result_field_errors_json = bad_request_result_details[ErrorResponseFields.FIELD_ERRORS]
+
+        # Convert field errors to a list format for easier testing
+        self.bad_request_result_field_errors = []
+        for field_name, error_list in self.bad_request_result_field_errors_json.items():
+            # error_list is a list of error dictionaries
+            for error in error_list:
+                self.bad_request_result_field_errors.append({
+                    ErrorResponseFields.FieldErrors.FIELD: field_name,
+                    ErrorResponseFields.FieldErrors.MESSAGE: error[ErrorResponseFields.MESSAGE],
+                    ErrorResponseFields.FieldErrors.CODE: error[ErrorResponseFields.FieldErrors.CODE]
+                })
+
+    def _set_results_attributes(self, response):
+        response_json = response.json()
+        self.results = response_json[PaginatedResponseFields.RESULTS]
+        self.results_overall_total = response_json[PaginatedResponseFields.OVERALL_TOTAL]
+
+    def _set_saved_lib_track_metadata(self, response):
+        saved_lib_track: LibraryTrack = self.saved_object  # type: ignore
+        self.saved_lib_track_metadata = \
+            audio_metadata.get_merged_app_metadata_dict(file=saved_lib_track.track_file.file)
+
+    # Defined here and not in LibTrackTestCase because other views needs sometimes to post a track for testing purposes
+    # (testing metadata updates for example)
+    def _post_lib_track(
+            self, test_lib_track_filename: TestLibTrackFilename = TestLibTrackFilename.DEFAULT_MP3, **kwargs
+    ) -> Union[JsonResponse, HttpResponse]:
+        file_abs_path = self.TEST_FILES_BASE_DIR / test_lib_track_filename
+
+        with open(file_abs_path, "rb") as sample_file:
+            file_field_dict = {LibTrackPostFields.TRACK_FILE_PUBLIC: sample_file}
+            if kwargs:
+                kwargs = data_transformer.merge_two_dicts(file_field_dict, kwargs)
+            else:
+                kwargs = file_field_dict
+
+            return self.api_client.post(
+                path=reverse('library-track-list'), data=kwargs, format='multipart', handle_response=self._set_results)
+
+    def setUp(self, methods_names_to_implement: list[str] | None = None) -> None:
+
+        call_command('loaddata', 'app')
+        self.test_admin_user = User.objects.create_superuser(
+            username='test_admin', password='test_admin', email='test_admin@example.com', is_test_user=True)
+
+        self.test_user1 = User.objects.create_instance(
+            username='pytest_user1', password='pytest_user1', email='pytest@user1.com', is_test_user=True)
+
+        self.test_user2 = User.objects.create_instance(
+            username='pytest_user2', password='pytest_user2', email='pytest@user2.com', is_test_user=True)
+
+        self.model_fixture_factory = ModelFixtureFactory(
+            default_test_user=self.test_user1, test_lib_track_dir=self.TEST_FILES_BASE_DIR,)
+
+        super().setUp()
+
+        if methods_names_to_implement:
+            for method_name in methods_names_to_implement:
+                if not hasattr(self, method_name) or not callable(getattr(self, method_name)):
+                    raise NotImplementedError(f"Subclasses must implement the '{method_name}' method")
+
+        self.api_client = AppApiClient(test_case=self)
+        self._login_as_test_user1()
