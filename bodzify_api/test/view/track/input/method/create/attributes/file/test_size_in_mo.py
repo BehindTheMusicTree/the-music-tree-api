c686bd00773d214820aad314d2abf4703ddb58cf
mignot <andreas.garcia@bodzify.com>
2025-02-28 01:53:29 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/create/attributes/file/test_size_in_mo.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/test_size_in_mo.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
@@ -18,7 +19,7 @@ class TestCase(LibTrackTestCase):
             self.LibTrackGenericSamplesTagsNoneSizeInMo.MP3)
 
     def test_flac(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3, )
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3)
         assert response.status_code == status.HTTP_201_CREATED
         assert str(round(self.saved_object.track_file.size_in_mo, 2)) == str(
             self.LibTrackGenericSamplesTagsNoneSizeInMo.FLAC)
