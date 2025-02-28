8797be5a9a90f93a24067c89318ae1ce5750ac79
mignot <andreas.garcia@bodzify.com>
2025-02-28 01:36:38 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_musicbrainz_recording.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_musicbrainz_recording.py
@@ -2,6 +2,7 @@
 import pytest
 from rest_framework import status
 
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
@@ -9,20 +10,24 @@ from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 class TestCase(LibTrackTestCase):
 
     def test_no_matching_recording_then_none(self):
-        response = self._post_lib_track(
-            "Tokyo Drift x Temperature - no musicbrainz recording.mp3")
+        response = self._post_lib_track(TestLibTrackFilename.RECORDING_TOKYO_DRIFT_NO_MUSICBRAINZ_RECORDING_MP3)
+
         assert response.status_code == status.HTTP_201_CREATED
         assert not self.saved_object.track_file.musicbrainz_recording
 
     def test_with_9_matches_then_the_one_with_duration_field(self):
-        response = self._post_lib_track("total_eclipse_9_matches_but_one_with_duration.flac")
+        response = self._post_lib_track(
+            TestLibTrackFilename.RECORDING_TOTAL_ECLIPSE_9_MATCHES_BUT_ONE_WITH_DURATION_FLAC)
+
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
         assert self.saved_object.track_file.musicbrainz_recording.musicbrainz_id == \
             '9f3c3b61-41a6-4bb9-a49c-33606f536784'
 
     def test_with_2_matches_then_the_one_with_closest_duration(self):
-        response = self._post_lib_track("lorie_2_matches_but_one_with_closest_duration.mp3")
+        response = self._post_lib_track(
+            TestLibTrackFilename.RECORDING_LORIE_2_MATCHES_BUT_ONE_WITH_CLOSEST_DURATION_MP3)
+
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
         assert self.saved_object.track_file.musicbrainz_recording.musicbrainz_id == \
@@ -30,7 +35,8 @@ class TestCase(LibTrackTestCase):
 
     def test_with_2_matches_with_same_duration_and_same_number_of_fields_then_the_one_with_the_most_release_groups(self):
         response = self._post_lib_track(
-            "allumerlefeu_2_matches_but_one_with_more_release_groups.mp3")
+            TestLibTrackFilename.RECORDING_ALLUMERLEFEU_2_MATCHES_ONE_WITH_MORE_RELEASE_GROUPS_MP3)
+
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
         assert self.saved_object.track_file.musicbrainz_recording.musicbrainz_id == \
@@ -38,7 +44,7 @@ class TestCase(LibTrackTestCase):
 
     def test_with_25_matches_then_select_the_one_with_best_duration_and_most_fields_and_most_release_groups(self):
         response = self._post_lib_track(
-            "queen_25_matches_but_one_with_best_duration_and_most_fields_and_most_release_groups.mp3")
+            TestLibTrackFilename.RECORDING_QUEEN_25_MATCHES_BUT_ONE_WITH_BEST_DURATION_AND_MOST_FIELDS_AND_MOST_RELEASE_GROUPS_MP3)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.track_file.musicbrainz_recording
         assert self.saved_object.track_file.musicbrainz_recording.musicbrainz_id == \
