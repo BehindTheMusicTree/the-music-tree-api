b380a9ea19246816cb0c48fa1d809307ac8c6c33
mignot <andreas.garcia@bodzify.com>
2025-02-28 01:53:25 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/create/attributes/file/test_type.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/test_type.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
@@ -14,5 +15,5 @@ class TestCase(LibTrackTestCase):
         assert response.status_code == status.HTTP_201_CREATED
 
     def test_flac(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3, )
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3)
         assert response.status_code == status.HTTP_201_CREATED
