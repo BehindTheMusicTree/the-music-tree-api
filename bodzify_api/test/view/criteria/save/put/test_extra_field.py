e8e58399873e6cc1ef580790abefb23e46c3f415
mignot <andreas.garcia@bodzify.com>
2025-02-28 01:58:59 +0100

    Clean code

--- bodzify_api/test/view/criteria/save/put/test_extra_field.py
+++ bodzify_api/test/view/criteria/save/put/test_extra_field.py
@@ -8,7 +8,7 @@ from bodzify_api.view.error.ErrorResponseFields import ErrorResponseFields
 
 class TestCase(GenreTestCase):
 
-    def test_extra_field_then_error(self):
+    def test_extra_field_then_400(self):
         genre = self.model_fixture_factory.create_genre(name="Rock")
 
         extra_field = "extraField"
