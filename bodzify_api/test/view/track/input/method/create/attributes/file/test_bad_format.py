55c3dcb9002acabbb5a5c20f588701dc087868b7
mignot <andreas.garcia@bodzify.com>
2025-02-28 01:31:16 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/create/attributes/file/test_bad_format.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/test_bad_format.py
@@ -3,6 +3,7 @@ from rest_framework import status
 
 from bodzify_api.exception.validation.FieldValidationErrorCode import FieldValidationErrorCode
 from bodzify_api.serializer.model.lib_track.input.post.Fields import Fields as LibTrackPostFields
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 from bodzify_api.view.error.ErrorResponseFields import ErrorResponseFields
 
@@ -11,7 +12,7 @@ from bodzify_api.view.error.ErrorResponseFields import ErrorResponseFields
 class TextCase(LibTrackTestCase):
 
     def test_bad_format_then_error(self):
-        response = self._post_lib_track("bad_format.wav")
+        response = self._post_lib_track(TestLibTrackFilename.FORMAT_BAD_CONTENT_MP3)
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert len(self.bad_request_result_field_errors) == 1
