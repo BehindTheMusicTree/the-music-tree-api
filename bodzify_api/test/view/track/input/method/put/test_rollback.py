e083743cd089e3639ed0d57b82e49827f1f7df6e
mignot <andreas.garcia@bodzify.com>
2025-02-28 02:07:01 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/put/test_rollback.py
+++ bodzify_api/test/view/track/input/method/put/test_rollback.py
@@ -8,9 +8,9 @@ class TestCase(LibTrackTestCase):
 
     def test_exception_then_rollback(self):
         original_genre = self.model_fixture_factory.create_genre(name="rock")
-        track = self.model_fixture_factory.create_lib_track_with_file(
-            title="joie", genre=original_genre)
+        track = self.model_fixture_factory.create_lib_track_with_file(title="joie", genre=original_genre)
         new_genre_name = "Rock"
+
         with patch('bodzify_api.model.track.lib.LibraryTrack.LibraryTrack.save') as mock:
             exception_message = "Save failed!"
             mock.side_effect = Exception(exception_message)
