4a5f466d1c9e7a262b185adc7db89a020a1cf299
mignot <andreas.garcia@bodzify.com>
2025-02-28 13:03:04 +0100

    Fix tests

--- bodzify_api/test/utils/field/filter/char/CharFilterTestCase.py
+++ bodzify_api/test/utils/field/filter/char/CharFilterTestCase.py
@@ -8,8 +8,7 @@ class CharFilterTestCase(FilterTestCase):
 
     def setUp(self, allow_empty_value: bool = False, methods_names_to_implement: list[str] | None = None) -> None:
         class_methods_names_to_implement = ['test_contains_in_another_case_then_results',
-                                            'test_empty_then_results',
-                                            'test_not_provided_then_results']
+                                            'test_empty_then_results',]
         if methods_names_to_implement:
             class_methods_names_to_implement += methods_names_to_implement
         super().setUp(allow_empty_value=allow_empty_value, methods_names_to_implement=class_methods_names_to_implement)
