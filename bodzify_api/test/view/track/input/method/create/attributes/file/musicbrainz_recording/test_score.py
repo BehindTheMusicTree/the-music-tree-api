85ecaf413d2a60ede46d89944a34af95d732a6f6
mignot <andreas.garcia@bodzify.com>
2025-02-28 01:36:48 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_score.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/musicbrainz_recording/test_score.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework import status
 
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
@@ -8,7 +9,8 @@ from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 class TestCase(LibTrackTestCase):
 
     def test_totaleclipse_with_three_scores_then_highest(self):
-        response = self._post_lib_track("total_eclipse_3_scores.flac")
+        response = self._post_lib_track(TestLibTrackFilename.RECORDING_TOTAL_ECLIPSE_3_SCORES_FLAC)
+
         assert response.status_code == status.HTTP_201_CREATED
         musicbrainz_recording = self.saved_object.track_file.musicbrainz_recording
         assert musicbrainz_recording
