6ec9b2f80e79373efb60788ca00f75b74a9ea552
mignot <andreas.garcia@bodzify.com>
2025-02-28 02:13:33 +0100

    Clean code

--- bodzify_api/test/view/track/input/save/test_extra_field.py
+++ bodzify_api/test/view/track/input/save/test_extra_field.py
@@ -18,4 +18,4 @@ class TestCase(LibTrackTestCase):
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
         assert error[ErrorResponseFields.FieldErrors.FIELD] == excta_field
-        assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.INVALID_FILTER
+        assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.UNKNOWN_FIELD
