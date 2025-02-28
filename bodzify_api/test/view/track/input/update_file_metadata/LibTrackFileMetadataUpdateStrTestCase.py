87bcea46a505e21a10c3361f3b8fbf47c136b027
mignot <andreas.garcia@bodzify.com>
2025-02-28 11:08:02 +0100

    Clean code

---
+++ bodzify_api/test/view/track/input/update_file_metadata/LibTrackFileMetadataUpdateStrTestCase.py
@@ -0,0 +1,61 @@
+
+
+from rest_framework import status
+
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
+from bodzify_api.test.view.track.input.update_file_metadata.LibTrackFileMetadataUpdateTestCase import LibTrackFileMetadataUpdateTestCase
+
+
+class LibTrackFileMetadataUpdateStrTestCase(LibTrackFileMetadataUpdateTestCase):
+
+    VALUE_EXPECTED_IN_METADATA_WHEN_NOT_PROVIDED = 'LJjksjsksjldkjlksjdlksjkdjskljdslkdjsldslnccsdvkjbvkvb'
+
+    length_max: int
+
+    def setUp(self):
+        super().setUp()
+        self.file_extension_metadata_none_test_lib_track_mapping = {
+            '.mp3': TestLibTrackFilename.METADATA_NONE_MP3,
+            '.flac': TestLibTrackFilename.METADATA_NONE_FLAC,
+            '.wav': TestLibTrackFilename.METADATA_NONE_WAV
+        }
+        self.file_extension_metadata_max_a_test_lib_track_mapping = {
+            '.mp3': TestLibTrackFilename.METADATA_MAX_A_ID3v2_MP3,
+            '.flac': TestLibTrackFilename.METADATA_MAX_A_ID3V2_FLAC,
+            '.wav': TestLibTrackFilename.METADATA_MAX_A_ID3V2_WAV
+        }
+
+    def _test_value(self,
+                    value: str | None,
+                    additional_data_dict,
+                    value_expected_in_metadata=VALUE_EXPECTED_IN_METADATA_WHEN_NOT_PROVIDED,
+                    file_has_tags=False):
+        data = {self.save_field: value}
+
+        if additional_data_dict:
+            data.update(additional_data_dict)
+
+        response = self._post_lib_track(file_has_metadata=file_has_tags, extension=self.file_extension, **data)
+
+        assert response.status_code == status.HTTP_201_CREATED
+
+        if value_expected_in_metadata == self.VALUE_EXPECTED_IN_METADATA_WHEN_NOT_PROVIDED:
+            value_expected_in_metadata = value
+
+        if value_expected_in_metadata is None:
+            if self.lib_track_app_metadata_key in self.saved_lib_track_metadata:
+                assert not self.saved_lib_track_metadata[self.lib_track_app_metadata_key]
+            else:
+                assert True
+        else:
+            assert self.lib_track_app_metadata_key in self.saved_lib_track_metadata
+            assert self.saved_lib_track_metadata[self.lib_track_app_metadata_key] == value_expected_in_metadata
+
+    def test_on_missing_tag_then_ok(self, additional_data_dict=None):
+        self._test_value("a", additional_data_dict=additional_data_dict, file_has_tags=False)
+
+    def test_on_present_tag_then_ok(self, additional_data_dict=None):
+        self._test_value("a", additional_data_dict=additional_data_dict, file_has_tags=True)
+
+    def test_longest_then_ok(self, additional_data_dict=None):
+        self._test_value('a' * self.length_max, additional_data_dict=additional_data_dict, file_has_tags=False)
