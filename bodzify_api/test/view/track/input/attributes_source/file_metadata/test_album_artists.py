6f089c7904bfb4b0a6be3e9e4b911a562bb151ef
mignot <andreas.garcia@bodzify.com>
2025-03-02 20:39:50 +0100

    Fix tests

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/test_album_artists.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/test_album_artists.py
@@ -2,35 +2,39 @@ from rest_framework import status
 
 from bodzify_api import settings
 from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
-from bodzify_api.test.view.track.input.attributes_source.file_metadata.StrMetadataFromFileTestCase import (
-    StrMetadataFromFileTestCase
-)
+from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
-class TestCase(StrMetadataFromFileTestCase):
+class TestCase(LibTrackTestCase):
     file_extension: str
 
     def test_none_then_none(self):
         response = \
-            self._post_lib_track(TestLibTrackFilename.ALBUM_KOKO_ID3V2_MP3, extension=self.file_extension)
+            self._post_lib_track(TestLibTrackFilename.ALBUM_KOKO_ID3V2_MP3)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.album
         assert self.saved_object.album.album_artists.count() == 0
 
-    def test_longest_id3v2_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_MP3, extension=self.file_extension)
+    def test_longest_id3v2_then_truncated(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_MP3)
         assert response.status_code == status.HTTP_201_CREATED
 
-        expected_name = 'a' * settings.ARTIST_NAME_LEN_MAX
-        album = self.saved_object.album
-        assert album
-        assert album.name == expected_name
+        assert self.saved_object.album
+        assert len(self.saved_object.album.name) == settings.ALBUM_NAME_LEN_MAX
+        assert self.saved_object.album.name == 'a' * settings.ALBUM_NAME_LEN_MAX
 
     def test_longest_riff_then_truncated(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_RIFF_WAV, extension=self.file_extension)
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_RIFF_WAV)
         assert response.status_code == status.HTTP_201_CREATED
 
-        expected_name = 'a' * settings.ARTIST_NAME_LEN_MAX
-        album = self.saved_object.album
-        assert album
-        assert album.name == expected_name
+        assert self.saved_object.album
+        assert len(self.saved_object.album.name) == settings.ALBUM_NAME_LEN_MAX
+        assert self.saved_object.album.name == 'a' * settings.ALBUM_NAME_LEN_MAX
+
+    def test_longest_vorbis_then_truncated(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_VORBIS_FLAC)
+        assert response.status_code == status.HTTP_201_CREATED
+
+        assert self.saved_object.album
+        assert len(self.saved_object.album.name) == settings.ALBUM_NAME_LEN_MAX
+        assert self.saved_object.album.name == 'a' * settings.ALBUM_NAME_LEN_MAX
