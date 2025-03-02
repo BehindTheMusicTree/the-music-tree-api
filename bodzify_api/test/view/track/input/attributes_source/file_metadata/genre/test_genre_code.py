85d0c92202c354c8e389a9c206a213440f741043
mignot <andreas.garcia@bodzify.com>
2025-03-02 17:13:37 +0100

    Clean code

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/genre/test_genre_code.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/genre/test_genre_code.py
@@ -1,9 +1,20 @@
-import pytest
 
+from rest_framework import status
+
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
 class TestCase(LibTrackTestCase):
 
     def test_genre_code_from_riff_id3v2_then_genre_name(self):
-        response = self._post_lib_track(
+        response = self._post_lib_track(TestLibTrackFilename.GENRE_CODE_ID3V1_ABSTRACT_MP3, title='genre code abstract')
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.genre == 'Abstract'
+
+    def test_genre_code_unknown_from_riff_id3v2_then_None(self):
+        response = self._post_lib_track(TestLibTrackFilename.GENRE_CODE_ID3V1_UNKNOWN_MP3, title='genre code unknown')
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.genre == None
