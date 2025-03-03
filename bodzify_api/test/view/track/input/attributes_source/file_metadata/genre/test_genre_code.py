2d79cf38275215d07c6d995454ba59c7785025eb
mignot <andreas.garcia@bodzify.com>
2025-03-03 19:32:48 +0100

    Fix tests

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/genre/test_genre_code.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/genre/test_genre_code.py
@@ -1,6 +1,8 @@
 
+from typing import cast
 from rest_framework import status
 
+from bodzify_api.model.criteria.children.genre.Genre import Genre
 from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
@@ -11,7 +13,8 @@ class TestCase(LibTrackTestCase):
         response = self._post_lib_track(TestLibTrackFilename.GENRE_CODE_ID3V1_ABSTRACT_MP3, title='genre code abstract')
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.genre == 'Abstract'
+        genre = cast(Genre, self.saved_object.genre)
+        assert genre.name == 'Christmas'
 
     def test_genre_code_unknown_from_riff_id3v2_then_None(self):
         response = self._post_lib_track(TestLibTrackFilename.GENRE_CODE_ID3V1_UNKNOWN_MP3, title='genre code unknown')
