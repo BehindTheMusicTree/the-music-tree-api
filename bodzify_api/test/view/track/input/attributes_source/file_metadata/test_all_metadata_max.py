040e10837debbc1e23fcd8e95a3fa530040a48e3
mignot <andreas.garcia@bodzify.com>
2025-03-03 17:48:51 +0100

    Improve tests

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/test_all_metadata_max.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/test_all_metadata_max.py
@@ -8,34 +8,66 @@ from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 @pytest.mark.django_db
 class TextCase(LibTrackTestCase):
 
-    def test_id3v1_mp3_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_MP3)
+    def test_id3v1_short_mp3_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_SHORT_MP3)
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_id3v1_flac_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_FLAC)
+    def test_id3v1_long_mp3_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_LONG_MP3)
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_id3v1_wav_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_WAV)
+    def test_id3v1_short_flac_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_SHORT_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_id3v2_mp3_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_MP3)
+    def test_id3v1_long_flac_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_LONG_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_id3v2_flac_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_FLAC)
+    def test_id3v1_short_wav_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_SHORT_WAV)
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_id3v2_wav_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_WAV)
+    def test_id3v1_long_wav_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_LONG_WAV)
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_riff_wav_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_RIFF_WAV)
+    def test_id3v2_short_mp3_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_SHORT_MP3)
         assert response.status_code == status.HTTP_201_CREATED
 
-    def test_vorbis_flac_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_VORBIS_FLAC)
+    def test_id3v2_long_mp3_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_LONG_MP3)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_id3v2_short_flac_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_SHORT_FLAC)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_id3v2_long_flac_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_LONG_FLAC)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_id3v2_short_wav_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_SHORT_WAV)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_id3v2_long_wav_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_LONG_WAV)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_riff_short_wav_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_RIFF_SHORT_WAV)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_riff_long_wav_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_RIFF_LONG_WAV)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_vorbis_short_flac_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_VORBIS_SHORT_FLAC)
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_vorbis_long_flac_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_VORBIS_SHORT_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
