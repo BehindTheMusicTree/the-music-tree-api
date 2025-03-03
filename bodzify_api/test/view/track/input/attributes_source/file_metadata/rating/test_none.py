38a1315782b8b28c43a4ae7ef35a38081909f677
mignot <andreas.garcia@bodzify.com>
2025-03-03 22:36:20 +0100

    Clean code

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/rating/test_none.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/rating/test_none.py
@@ -9,16 +9,16 @@ from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 class TestCase(LibTrackTestCase):
 
     def test_mp3_then_none(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3, extension="mp3")
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == None
 
     def test_wav_then_none(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_WAV, extension="wav")
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_WAV)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == None
 
     def test_flac_then_none(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_FLAC, extension="flac")
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == None
