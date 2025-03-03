d7b66bee05bb59fce84a9c1fde5672622481f5ae
mignot <andreas.garcia@bodzify.com>
2025-03-03 19:20:22 +0100

    Clean code

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/test_album_artists.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/test_album_artists.py
@@ -1,12 +1,12 @@
 from rest_framework import status
 
 from bodzify_api import settings
+from bodzify_api.model.artist.Artist import Artist
 from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
 class TestCase(LibTrackTestCase):
-    file_extension: str
 
     def test_none_then_none(self):
         response = self._post_lib_track(TestLibTrackFilename.ALBUM_KOKO_ID3V2_MP3)
@@ -19,21 +19,94 @@ class TestCase(LibTrackTestCase):
         assert response.status_code == status.HTTP_201_CREATED
 
         assert self.saved_object.album
-        assert len(self.saved_object.album.name) == settings.ALBUM_NAME_LEN_MAX
-        assert self.saved_object.album.name == 'a' * settings.ALBUM_NAME_LEN_MAX
+        assert self.saved_object.album.album_artists.count() == 1
+        artist: Artist | None = self.saved_object.album.album_artists.first()
+        assert artist
+        assert len(artist.name) == settings.ARTIST_NAME_LEN_MAX
+        assert artist.name == 'a' * settings.ARTIST_NAME_LEN_MAX
 
     def test_long_riff_then_truncated(self):
         response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_RIFF_SHORT_WAV)
         assert response.status_code == status.HTTP_201_CREATED
 
         assert self.saved_object.album
-        assert len(self.saved_object.album.name) == settings.ALBUM_NAME_LEN_MAX
-        assert self.saved_object.album.name == 'a' * settings.ALBUM_NAME_LEN_MAX
+        assert self.saved_object.album.album_artists.count() == 1
+        artist: Artist | None = self.saved_object.album.album_artists.first()
+        assert artist
+        assert len(artist.name) == settings.ARTIST_NAME_LEN_MAX
+        assert artist.name == 'a' * settings.ARTIST_NAME_LEN_MAX
 
     def test_long_vorbis_then_truncated(self):
         response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_VORBIS_SHORT_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
 
         assert self.saved_object.album
-        assert len(self.saved_object.album.name) == settings.ALBUM_NAME_LEN_MAX
-        assert self.saved_object.album.name == 'a' * settings.ALBUM_NAME_LEN_MAX
+        assert self.saved_object.album.album_artists.count() == 1
+        artist: Artist | None = self.saved_object.album.album_artists.first()
+        assert artist
+        assert len(artist.name) == settings.ARTIST_NAME_LEN_MAX
+        assert artist.name == 'a' * settings.ARTIST_NAME_LEN_MAX
+
+    def test_3_separated_by_antislash_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_ANTISLASH_ID3V2)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.artists.count() == 3
+        artists = self.saved_object.artists.all()
+        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+
+    def test_3_separated_by_comma_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_COMMA_ID3V2)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.artists.count() == 3
+        artists = self.saved_object.artists.all()
+        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+
+    def test_3_separated_by_double_antislash_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_DOUBLE_ANTISLASH_ID3V2)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.artists.count() == 3
+        artists = self.saved_object.artists.all()
+        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+
+    def test_3_separated_by_double_slash_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_DOUBLE_SLASH_ID3V2)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.artists.count() == 3
+        artists = self.saved_object.artists.all()
+        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+
+    def test_3_multi_tags_and_slash_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_MULTI_TAGS_AND_SLASH_VORBIS)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.artists.count() == 3
+        artists = self.saved_object.artists.all()
+        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+
+    def test_3_multi_tags_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_MULTI_TAGS_VORBIS)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.artists.count() == 3
+        artists = self.saved_object.artists.all()
+        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+
+    def test_3_separated_by_semicolon_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_SEMICOLON_ID3V2)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.artists.count() == 3
+        artists = self.saved_object.artists.all()
+        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+
+    def test_3_separated_by_slash_then_ok(self):
+        response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_SLASH_ID3V2)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.artists.count() == 3
+        artists = self.saved_object.artists.all()
+        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
