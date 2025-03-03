17b000e22e7c05fdafbe15d00d7421674935cdae
mignot <andreas.garcia@bodzify.com>
2025-03-03 21:58:52 +0100

    Clean code

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/rating/test_id3v2_base_255_wav.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/rating/test_id3v2_base_255_wav.py
@@ -11,26 +11,26 @@ from bodzify_api.test.view.track.input.attributes_source.file_metadata.rating.Ra
 class TestCase(RatingNotNoneTestCase):
 
     def test_1_then_2(self):
-        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_KID3_1_STAR_WAV)
+        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_BASE_255_KID3_1_STAR_WAV)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 2
 
     def test_2_then_4(self):
-        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_KID3_2_STAR_WAV)
+        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_BASE_255_KID3_2_STAR_WAV)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 4
 
     def test_3_then_6(self):
-        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_KID3_3_STAR_WAV)
+        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_BASE_255_KID3_3_STAR_WAV)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 6
 
     def test_4_then_8(self):
-        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_KID3_4_STAR_WAV)
+        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_BASE_255_KID3_4_STAR_WAV)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 8
 
     def test_5_then_10(self):
-        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_KID3_5_STAR_WAV)
+        response = self._post_lib_track(TestLibTrackFilename.RATING_ID3V2_BASE_255_KID3_5_STAR_WAV)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 10
