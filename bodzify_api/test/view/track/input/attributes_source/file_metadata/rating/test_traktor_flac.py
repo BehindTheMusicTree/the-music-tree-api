246b1d7c408456c706e1f52b465f919cd8f86d60
mignot <andreas.garcia@bodzify.com>
2025-03-03 22:36:04 +0100

    Clean code

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/rating/test_traktor_flac.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/rating/test_traktor_flac.py
@@ -1,35 +1,34 @@
 import pytest
 from rest_framework import status
 
-from bodzify_api.test.view.track.input.attributes_source.file_metadata.rating.RatingNotNoneTestCase import (
-    RatingNotNoneTestCase
-)
+from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
+from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
 @pytest.mark.django_db
-class TestCase(RatingNotNoneTestCase):
+class TestCase(LibTrackTestCase):
 
     def test_1_then_2(self):
-        response = self._post_lib_track("1 star.flac")
+        response = self._post_lib_track(TestLibTrackFilename.RATING_VORBIS_TRAKTOR_1_STAR_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 2
 
     def test_2_then_4(self):
-        response = self._post_lib_track("2 stars.flac")
+        response = self._post_lib_track(TestLibTrackFilename.RATING_VORBIS_TRAKTOR_2_STAR_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 4
 
     def test_3_then_6(self):
-        response = self._post_lib_track("3 stars.flac")
+        response = self._post_lib_track(TestLibTrackFilename.RATING_VORBIS_TRAKTOR_3_STAR_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 6
 
     def test_4_then_8(self):
-        response = self._post_lib_track("4 stars.flac")
+        response = self._post_lib_track(TestLibTrackFilename.RATING_VORBIS_TRAKTOR_4_STAR_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 8
 
     def test_5_then_10(self):
-        response = self._post_lib_track("5 stars.flac")
+        response = self._post_lib_track(TestLibTrackFilename.RATING_VORBIS_TRAKTOR_5_STAR_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
         assert self.saved_object.rating == 10
