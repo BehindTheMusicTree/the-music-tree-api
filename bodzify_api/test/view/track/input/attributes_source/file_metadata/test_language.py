86d8c8794c808cc656e50828b8a320fdf725579c
mignot <andreas.garcia@bodzify.com>
2025-03-03 18:37:32 +0100

    Fix tests

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/test_language.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/test_language.py
@@ -22,14 +22,6 @@ class TestCase(LibTrackTestCase):
         assert len(self.saved_object.language) == settings.LANGUAGE_LEN_MAX
         assert self.saved_object.language == 'a' * settings.LANGUAGE_LEN_MAX
 
-    def test_long_riff_then_truncated(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_RIFF_SHORT_WAV)
-
-        assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.language
-        assert len(self.saved_object.language) == settings.LANGUAGE_LEN_MAX
-        assert self.saved_object.language == 'a' * settings.LANGUAGE_LEN_MAX
-
     def test_long_vorbis_then_truncated(self):
         response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_VORBIS_SHORT_FLAC)
 
