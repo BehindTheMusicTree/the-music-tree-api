78ecd56c38963d3ceec1325a7d8cc9811679d684
mignot <andreas.garcia@bodzify.com>
2025-03-03 19:28:48 +0100

    Fix tests

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/test_album_artists.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/test_album_artists.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from django.db.models.query import QuerySet
 
 from bodzify_api import settings
 from bodzify_api.model.artist.Artist import Artist
@@ -25,17 +26,6 @@ class TestCase(LibTrackTestCase):
         assert len(artist.name) == settings.ARTIST_NAME_LEN_MAX
         assert artist.name == 'a' * settings.ARTIST_NAME_LEN_MAX
 
-    def test_long_riff_then_truncated(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_RIFF_SHORT_WAV)
-        assert response.status_code == status.HTTP_201_CREATED
-
-        assert self.saved_object.album
-        assert self.saved_object.album.album_artists.count() == 1
-        artist: Artist | None = self.saved_object.album.album_artists.first()
-        assert artist
-        assert len(artist.name) == settings.ARTIST_NAME_LEN_MAX
-        assert artist.name == 'a' * settings.ARTIST_NAME_LEN_MAX
-
     def test_long_vorbis_then_truncated(self):
         response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_VORBIS_SHORT_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
@@ -51,62 +41,70 @@ class TestCase(LibTrackTestCase):
         response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_ANTISLASH_ID3V2)
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.artists.count() == 3
-        artists = self.saved_object.artists.all()
-        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+        assert self.saved_object.album
+        album_artists: QuerySet[Artist] = self.saved_object.album.album_artists.all()
+        assert album_artists.count() == 3
+        assert [artist.name for artist in album_artists] == ['One', 'Two', 'Three']
 
     def test_3_separated_by_comma_then_ok(self):
         response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_COMMA_ID3V2)
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.artists.count() == 3
-        artists = self.saved_object.artists.all()
-        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+        assert self.saved_object.album
+        album_artists: QuerySet[Artist] = self.saved_object.album.album_artists.all()
+        assert album_artists.count() == 3
+        assert [artist.name for artist in album_artists] == ['One', 'Two', 'Three']
 
     def test_3_separated_by_double_antislash_then_ok(self):
         response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_DOUBLE_ANTISLASH_ID3V2)
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.artists.count() == 3
-        artists = self.saved_object.artists.all()
-        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+        assert self.saved_object.album
+        album_artists: QuerySet[Artist] = self.saved_object.album.album_artists.all()
+        assert album_artists.count() == 3
+        assert [artist.name for artist in album_artists] == ['One', 'Two', 'Three']
 
     def test_3_separated_by_double_slash_then_ok(self):
         response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_DOUBLE_SLASH_ID3V2)
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.artists.count() == 3
-        artists = self.saved_object.artists.all()
-        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+        assert self.saved_object.album
+        album_artists: QuerySet[Artist] = self.saved_object.album.album_artists.all()
+        assert album_artists.count() == 3
+        assert [artist.name for artist in album_artists] == ['One', 'Two', 'Three']
 
     def test_3_multi_tags_and_slash_then_ok(self):
         response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_MULTI_TAGS_AND_SLASH_VORBIS)
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.artists.count() == 3
-        artists = self.saved_object.artists.all()
-        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+        assert self.saved_object.album
+        album_artists: QuerySet[Artist] = self.saved_object.album.album_artists.all()
+        assert album_artists.count() == 3
+        assert [artist.name for artist in album_artists] == ['One', 'Two', 'Three']
 
     def test_3_multi_tags_then_ok(self):
         response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_MULTI_TAGS_VORBIS)
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.artists.count() == 3
-        artists = self.saved_object.artists.all()
-        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+        assert self.saved_object.album
+        album_artists: QuerySet[Artist] = self.saved_object.album.album_artists.all()
+        assert album_artists.count() == 3
+        assert [artist.name for artist in album_artists] == ['One', 'Two', 'Three']
 
     def test_3_separated_by_semicolon_then_ok(self):
         response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_SEMICOLON_ID3V2)
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.artists.count() == 3
-        artists = self.saved_object.artists.all()
-        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+        assert self.saved_object.album
+        album_artists: QuerySet[Artist] = self.saved_object.album.album_artists.all()
+        assert album_artists.count() == 3
+        assert [artist.name for artist in album_artists] == ['One', 'Two', 'Three']
 
     def test_3_separated_by_slash_then_ok(self):
         response = self._post_lib_track(TestLibTrackFilename.ALBUM_ARTISTS_ONE_TWO_THREE_SLASH_ID3V2)
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.artists.count() == 3
-        artists = self.saved_object.artists.all()
-        assert [artist.name for artist in artists] == ['One', 'Two', 'Three']
+        assert self.saved_object.album
+        album_artists: QuerySet[Artist] = self.saved_object.album.album_artists.all()
+        assert album_artists.count() == 3
+        assert [artist.name for artist in album_artists] == ['One', 'Two', 'Three']
