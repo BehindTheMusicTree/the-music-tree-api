129eb452224312111e67a2028a9f77384ab58a85
mignot <andreas.garcia@bodzify.com>
2025-03-04 16:20:42 +0100

    Fix tests

--- bodzify_api/test/view/track/input/method/create/attributes/file/test_wrong_extension.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/test_wrong_extension.py
@@ -18,7 +18,7 @@ class TestCase(LibTrackTestCase):
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
         assert error[ErrorResponseFields.FieldErrors.FIELD] == LibTrackPostFields.TRACK_FILE_PUBLIC
-        assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.INVALID_FILE_TYPE
+        assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.INVALID_EXTENSION
 
     def test_mp4_then_400(self):
         response = self._post_lib_track(TestLibTrackFilename.FORMAT_BAD_EXTENSION_MP4)
@@ -27,4 +27,4 @@ class TestCase(LibTrackTestCase):
         assert len(self.bad_request_result_field_errors) == 1
         error = self.bad_request_result_field_errors[0]
         assert error[ErrorResponseFields.FieldErrors.FIELD] == LibTrackPostFields.TRACK_FILE_PUBLIC
-        assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.INVALID_FILE_TYPE
+        assert error[ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.INVALID_EXTENSION
