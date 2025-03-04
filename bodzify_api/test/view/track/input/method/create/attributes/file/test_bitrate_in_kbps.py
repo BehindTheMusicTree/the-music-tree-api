4ed574ffc33b3739b71b7e4a9d2bce9823f3b3b3
mignot <andreas.garcia@bodzify.com>
2025-03-04 14:54:55 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/create/attributes/file/test_bitrate_in_kbps.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/test_bitrate_in_kbps.py
@@ -1,24 +1,45 @@
-import pytest
+from typing import cast
 from rest_framework import status
 
+from bodzify_api.model.track.file.TrackFile import TrackFile
 from bodzify_api.test.utils.lib_track.TestLibTrackFilename import TestLibTrackFilename
 from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 
-@pytest.mark.django_db
 class TestCase(LibTrackTestCase):
 
-    def test_wav(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_WAV)
+    def test_small_wav(self):
+        response = self._post_lib_track(TestLibTrackFilename.BITRATE_IN_KPBPS_SMALL_1152_WAV)
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.track_file.bitrate_in_kbps == 1152
+        track_file = cast(TrackFile, self.saved_object.track_file)
+        assert track_file.bitrate_in_kbps == 1152
 
-    def test_mp3(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3)
+    def test_small_mp3(self):
+        response = self._post_lib_track(TestLibTrackFilename.BITRATE_IN_KPBPS_SMALL_192_MP3)
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.track_file.bitrate_in_kbps == 192
+        track_file = cast(TrackFile, self.saved_object.track_file)
+        assert track_file.bitrate_in_kbps == 192
 
-    def test_flac(self):
-        response = self._post_lib_track(TestLibTrackFilename.BITRATE_1411_FLAC)
+    def test_small_flac(self):
+        response = self._post_lib_track(TestLibTrackFilename.BITRATE_IN_KPBPS_SMALL_1152_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
-        assert self.saved_object.track_file.bitrate_in_kbps == 1411
+        track_file = cast(TrackFile, self.saved_object.track_file)
+        assert track_file.bitrate_in_kbps == 1152
+
+    def test_big_wav(self):
+        response = self._post_lib_track(TestLibTrackFilename.BITRATE_IN_KPBPS_BIG_1411_WAV)
+        assert response.status_code == status.HTTP_201_CREATED
+        track_file = cast(TrackFile, self.saved_object.track_file)
+        assert track_file.bitrate_in_kbps == 1411
+
+    def test_big_mp3(self):
+        response = self._post_lib_track(TestLibTrackFilename.BITRATE_IN_KPBPS_BIG_320_MP3)
+        assert response.status_code == status.HTTP_201_CREATED
+        track_file = cast(TrackFile, self.saved_object.track_file)
+        assert track_file.bitrate_in_kbps == 320
+
+    def test_big_flac(self):
+        response = self._post_lib_track(TestLibTrackFilename.BITRATE_IN_KPBPS_BIG_705_FLAC)
+        assert response.status_code == status.HTTP_201_CREATED
+        track_file = cast(TrackFile, self.saved_object.track_file)
+        assert track_file.bitrate_in_kbps == 705
