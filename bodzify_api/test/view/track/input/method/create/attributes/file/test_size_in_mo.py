d7196d4e7bbeca219b5772278764f6d20667960f
mignot <andreas.garcia@bodzify.com>
2025-03-04 14:32:49 +0100

    Clean code

--- bodzify_api/test/view/track/input/method/create/attributes/file/test_size_in_mo.py
+++ bodzify_api/test/view/track/input/method/create/attributes/file/test_size_in_mo.py
@@ -9,20 +9,38 @@ from bodzify_api.test.view.track.LibTrackTestCase import LibTrackTestCase
 
 class TestCase(LibTrackTestCase):
 
-    def test_wav(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_WAV)
+    def test_small_wav(self):
+        response = self._post_lib_track(TestLibTrackFilename.SIZE_SMALL_0_08_MO_WAV)
         assert response.status_code == status.HTTP_201_CREATED
         track_file = cast(TrackFile, self.saved_object.track_file)
         assert Decimal(round(track_file.size_in_mo, 2) == round(81/1024, 2))
 
-    def test_mp3(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3)
+    def test_small_mp3(self):
+        response = self._post_lib_track(TestLibTrackFilename.SIZE_SMALL_0_01_MO_MP3)
         assert response.status_code == status.HTTP_201_CREATED
         track_file = cast(TrackFile, self.saved_object.track_file)
         assert Decimal(round(track_file.size_in_mo, 2) == round(14/1024, 2))
 
-    def test_flac(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_FLAC)
+    def test_small_flac(self):
+        response = self._post_lib_track(TestLibTrackFilename.SIZE_SMALL_0_05_MO_FLAC)
         assert response.status_code == status.HTTP_201_CREATED
         track_file = cast(TrackFile, self.saved_object.track_file)
         assert Decimal(round(track_file.size_in_mo, 2) == round(53/1024, 2))
+
+    def test_big_wav(self):
+        response = self._post_lib_track(TestLibTrackFilename.SIZE_BIG_79_55_MO_WAV)
+        assert response.status_code == status.HTTP_201_CREATED
+        track_file = cast(TrackFile, self.saved_object.track_file)
+        assert Decimal(round(track_file.size_in_mo, 2) == round(81/1024, 2))
+
+    def test_big_mp3(self):
+        response = self._post_lib_track(TestLibTrackFilename.SIZE_BIG_9_98_MO_MP3)
+        assert response.status_code == status.HTTP_201_CREATED
+        track_file = cast(TrackFile, self.saved_object.track_file)
+        assert Decimal(round(track_file.size_in_mo, 2) == round(14/1024, 2))
+
+    def test_big_flac(self):
+        response = self._post_lib_track(TestLibTrackFilename.SIZE_BIG_26_6_MO_FLAC)
+        assert response.status_code == status.HTTP_201_CREATED
+        track_file = cast(TrackFile, self.saved_object.track_file)
+        assert Decimal(round(track_file.size_in_mo, 2) == 26.6
