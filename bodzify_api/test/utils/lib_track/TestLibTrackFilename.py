7897fe2b3585f0fb8848abb87463dad8daf7354c
mignot <andreas.garcia@bodzify.com>
2025-03-04 14:55:06 +0100

    Clean code

--- bodzify_api/test/utils/lib_track/TestLibTrackFilename.py
+++ bodzify_api/test/utils/lib_track/TestLibTrackFilename.py
@@ -26,6 +26,14 @@ class TestLibTrackFilename(str, Enum):
     ARTISTS_ONE_TWO_THREE_SEMICOLON_ID3V2 = "artists=One Two Three_semicolon_id3v2.mp3"
     ARTISTS_ONE_TWO_THREE_SLASH_ID3V2 = "artists=One Two Three_slash_id3v2.mp3"
 
+    BITRATE_IN_KPBPS_BIG_320_MP3 = "bitrate in kbps_big=320.mp3"
+    BITRATE_IN_KPBPS_BIG_705_FLAC = "bitrate in kbps_big=705.flac"
+    BITRATE_IN_KPBPS_BIG_1411_WAV = "bitrate in kbps_big=1411.wav"
+
+    BITRATE_IN_KPBPS_SMALL_192_MP3 = "bitrate in kbps_small=192.mp3"
+    BITRATE_IN_KPBPS_SMALL_1152_FLAC = "bitrate in kbps_small=1152.flac"
+    BITRATE_IN_KPBPS_SMALL_1152_WAV = "bitrate in kbps_small=1152.wav"
+
     BITRATE_1411_FLAC = "bitrate=1411.flac"
 
     COPYRIGHT_DOMAINE_PUBLIC_RIFF_WAV = "copyright=domaine public_riff.wav"
