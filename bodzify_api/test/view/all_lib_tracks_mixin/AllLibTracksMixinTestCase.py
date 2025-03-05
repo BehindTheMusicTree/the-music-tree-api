96853b2766132637d4ed2f88c0cc9ccb7ae29bf2
mignot <andreas.garcia@bodzify.com>
2025-03-05 13:06:54 +0100

    Fix tests

--- bodzify_api/test/view/all_lib_tracks_mixin/AllLibTracksMixinTestCase.py
+++ bodzify_api/test/view/all_lib_tracks_mixin/AllLibTracksMixinTestCase.py
@@ -10,22 +10,21 @@ class AllLibTracksMixinTestCase(ApiTestCase[AllLibTracksMixin]):
     def _post_all_lib_tracks_mixin(self, **kwargs):
         return self.api_client.post(path=reverse('all-library-tracks-list'),
                                     data=kwargs,
-                                    content_type='application/x-www-form-urlencoded',
+                                    content_type='application/json',
                                     handle_response=self._set_results)
 
     def _get_all_lib_tracks_mixin(self, **kwargs):
-        return self.api_client.get(path=reverse('all-library-tracks-list'),
-                                   data=kwargs,
-                                   handle_response=self._set_results)
+        return self.api_client.get(
+            path=reverse('all-library-tracks-list'), data=kwargs, handle_response=self._set_results)
 
     def _retrieve_all_lib_tracks_mixin(self, uuid: UUID):
-        return self.api_client.get(path=reverse('all-library-tracks-detail', kwargs={'pk': uuid}),
-                                   handle_response=self._set_results)
+        return self.api_client.get(
+            path=reverse('all-library-tracks-detail', kwargs={'pk': uuid}), handle_response=self._set_results)
 
     def _put_all_lib_tracks_mixin(self, uuid: UUID, **kwargs):
         return self.api_client.put(path=reverse('all-library-tracks-detail', kwargs={'pk': uuid}),
                                    data=kwargs,
-                                   content_type='application/x-www-form-urlencoded',
+                                   content_type='application/json',
                                    handle_response=self._set_results)
 
     def _delete_all_lib_tracks_mixin(self, uuid: UUID):
