f11adf9c533a0487ebc58c948bbed671a7534095
mignot <andreas.garcia@bodzify.com>
2025-03-05 16:06:42 +0100

    Clean code

--- bodzify_api/serializer/field/AppFileField.py
+++ bodzify_api/serializer/field/AppFileField.py
@@ -6,9 +6,5 @@ from bodzify_api.serializer.field.AppField import AppField
 
 class AppFileField(AppField, serializers.FileField):
 
-    def __init__(self, *args, **kwargs):
-        self.field_name = kwargs.pop('field_name', None)
-        super().__init__(*args, **kwargs)
-
     def to_internal_value(self, data):
         return serializers.FileField.to_internal_value(self, data)
