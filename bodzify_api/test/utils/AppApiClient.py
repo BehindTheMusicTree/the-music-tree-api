080d926532960c98d709d9ea700b43126443a9f3
mignot <andreas.garcia@bodzify.com>
2025-03-06 00:09:45 +0100

    Fix tests

--- bodzify_api/test/utils/AppApiClient.py
+++ bodzify_api/test/utils/AppApiClient.py
@@ -1,9 +1,10 @@
+import json
 
 from django.http import HttpResponse
 from rest_framework.test import APIClient
 
 from bodzify_api.utils import data_transformer
-from bodzify_api.utils.json_utils import transform_uuids
+from bodzify_api.utils.json_utils import UUIDJSONEncoder
 
 
 class AppApiClient(APIClient):
@@ -52,7 +53,8 @@ class AppApiClient(APIClient):
                 data_url_encoded = data_transformer.replace_none_with_empty_string(**data)
 
                 if format != 'multipart':
-                    data_url_encoded = transform_uuids(data_url_encoded)
+                    if isinstance(data_url_encoded, dict):
+                        data_url_encoded = json.dumps(data_url_encoded, cls=UUIDJSONEncoder)
 
             if not content_type and not format:
                 content_type = 'application/json'
@@ -76,7 +78,7 @@ class AppApiClient(APIClient):
             else:
                 data_url_encoded = data_transformer.replace_none_with_empty_string(**data)
                 if format != 'multipart':
-                    data_url_encoded = transform_uuids(data_url_encoded)
+                    data_url_encoded = json.dumps(data_url_encoded, cls=UUIDJSONEncoder)
             if not content_type:
                 content_type = 'application/json'
 
@@ -95,7 +97,7 @@ class AppApiClient(APIClient):
         if data:
             data_url_encoded = data_transformer.replace_none_with_empty_string(**data)
             if format != 'multipart':
-                data_url_encoded = transform_uuids(data_url_encoded)
+                data_url_encoded = json.dumps(data_url_encoded, cls=UUIDJSONEncoder)
                 if not content_type:
                     content_type = 'application/json'
 
