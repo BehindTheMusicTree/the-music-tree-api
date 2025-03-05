21a342adba1fa4a5529ad749aceab54cf6f7354c
mignot <andreas.garcia@bodzify.com>
2025-03-06 00:53:53 +0100

    Fix tests

--- bodzify_api/test/view/playlist/children/criteria/tag/list/test_by_parent_uuid.py
+++ bodzify_api/test/view/playlist/children/criteria/tag/list/test_by_parent_uuid.py
@@ -32,7 +32,7 @@ class TestCase(TagPlaylistTestCase, PrivateForeignKeyFilterTestCase):
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert self.bad_request_result_field_errors[0][ErrorResponseFields.FieldErrors.FIELD] == RietrieveFields.PARENT
         assert self.bad_request_result_field_errors[0][
-            ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.BLANK
+            ErrorResponseFields.FieldErrors.CODE] == FieldValidationErrorCode.INVALID_FORMAT
 
     def test_empty_then_results(self):
         tag_fiesta = self.model_fixture_factory.create_tag(name="Fiesta")
