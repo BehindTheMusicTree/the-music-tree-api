769c69b221812c4c43078f56cc3c7636dc4c58b5
mignot <andreas.garcia@bodzify.com>
2025-03-07 01:21:47 +0100

    Clean code

--- bodzify_api/middleware/CamelToSnakeMiddleware.py
+++ bodzify_api/middleware/CamelToSnakeMiddleware.py
@@ -1,3 +1,5 @@
+from django.http import QueryDict
+from django.utils.datastructures import MultiValueDict
 
 from bodzify_api.utils import data_transformer
 
@@ -12,17 +14,22 @@ class CamelToSnakeMiddleware:
             request.data = data_transformer.form_data_to_snake_case(request.data)
 
         # Convert multipart form data
-        if request.POST:
-            request.POST = request.POST.copy()
-            request.POST = data_transformer.form_data_to_snake_case(request.POST)
+        content_type = request.headers.get('Content-Type', '')
+        if content_type.startswith('multipart/form-data'):
+            if not hasattr(request, '_post'):
+                request._load_post_and_files()
+            request.POST = QueryDict(mutable=True)
+            request.POST.update(data_transformer.form_data_to_snake_case(request._post))
 
-        # Convert file upload field names
-        if request.FILES:
-            files_copy = request.FILES.copy()
-            files_dict = data_transformer.dict_to_snake_case(dict(files_copy))
-            request._files.clear()
-            for key, value in files_dict.items():
-                request._files[key] = value
+            # Handle files by creating new MultiValueDict with converted keys
+            if request.FILES:
+                original_files = request.FILES
+                request._files = MultiValueDict()
+                for key, files in original_files.items():
+                    if not isinstance(files, (list, tuple)):
+                        files = [files]
+                    for file in files:
+                        request._files.appendlist(data_transformer.to_snake_case(key), file)
 
         # Convert GET parameters
         if request.GET:
