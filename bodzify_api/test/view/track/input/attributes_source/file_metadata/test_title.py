28c61291c8e44fb729bde40db66c04ba0acdffaa
mignot <andreas.garcia@bodzify.com>
2025-03-07 01:21:34 +0100

    Clean code

--- bodzify_api/test/view/track/input/attributes_source/file_metadata/test_title.py
+++ bodzify_api/test/view/track/input/attributes_source/file_metadata/test_title.py
@@ -14,28 +14,28 @@ class TestCase(LibTrackTestCase):
         assert self.saved_object.title
 
     def test_from_id3v1_big_file_then_ok(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V1_SMALL_MP3)
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_LONG_A_ID3V1_SMALL_MP3)
 
         assert response.status_code == status.HTTP_201_CREATED
         assert len(self.saved_object.title) == settings.LIB_TRACK_TITLE_LEN_MAX_ID3V1
         assert self.saved_object.title == 'a' * settings.LIB_TRACK_TITLE_LEN_MAX_ID3V1
 
     def test_long_from_id3v2_then_truncated(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_ID3V2_SMALL_MP3)
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_LONG_A_ID3V2_SMALL_MP3)
 
         assert response.status_code == status.HTTP_201_CREATED
         assert len(self.saved_object.title) == settings.LIB_TRACK_TITLE_LEN_MAX
         assert self.saved_object.title == 'a' * settings.LIB_TRACK_TITLE_LEN_MAX
 
     def test_long_from_riff_then_truncated(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_RIFF_SMALL_WAV)
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_LONG_A_RIFF_SMALL_WAV)
 
         assert response.status_code == status.HTTP_201_CREATED
         assert len(self.saved_object.title) == settings.LIB_TRACK_TITLE_LEN_MAX
         assert self.saved_object.title == 'a' * settings.LIB_TRACK_TITLE_LEN_MAX
 
     def test_long_from_vorbis_then_truncated(self):
-        response = self._post_lib_track(TestLibTrackFilename.METADATA_MAX_A_VORBIS_SMALL_FLAC)
+        response = self._post_lib_track(TestLibTrackFilename.METADATA_LONG_A_VORBIS_SMALL_FLAC)
 
         assert response.status_code == status.HTTP_201_CREATED
         assert len(self.saved_object.title) == settings.LIB_TRACK_TITLE_LEN_MAX
