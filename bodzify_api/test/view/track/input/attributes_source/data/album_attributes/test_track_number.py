c1add5a90047799cb987a769a25b6939bd5166d9
mignot <andreas.garcia@bodzify.com>
2025-03-10 22:40:46 +0100

    Fix tests

--- bodzify_api/test/view/track/input/attributes_source/data/album_attributes/test_track_number.py
+++ bodzify_api/test/view/track/input/attributes_source/data/album_attributes/test_track_number.py
@@ -9,7 +9,9 @@ class TestCase(LibTrackTestCase):
 
     def test_value_then_ok(self):
         value = 1
-        data = {PostFields.ALBUM_NAME: 'albumito', PostFields.TRACK_NUMBER: value}
+        data = {PostFields.ALBUM_NAME: 'albumito',
+                PostFields.ALBUM_ARTISTS_NAMES_ARRAY: [],
+                PostFields.TRACK_NUMBER: value}
         response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3, **data)
 
         assert response.status_code == status.HTTP_201_CREATED
@@ -17,7 +19,9 @@ class TestCase(LibTrackTestCase):
         assert self.saved_object.track_number == value
 
     def test_empty_then_none(self):
-        data = {PostFields.ALBUM_NAME: "albumito", PostFields.TRACK_NUMBER: None}
+        data = {PostFields.ALBUM_NAME: "albumito",
+                PostFields.ALBUM_ARTISTS_NAMES_ARRAY: [],
+                PostFields.TRACK_NUMBER: None}
         response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3, **data)
 
         assert response.status_code == status.HTTP_201_CREATED
@@ -25,7 +29,8 @@ class TestCase(LibTrackTestCase):
         assert self.saved_object.track_number == None
 
     def test_not_provided_then_none(self):
-        data = {PostFields.ALBUM_NAME: "albumito"}
+        data = {PostFields.ALBUM_NAME: "albumito",
+                PostFields.ALBUM_ARTISTS_NAMES_ARRAY: []}
         response = self._post_lib_track(TestLibTrackFilename.METADATA_NONE_MP3, **data)
 
         assert response.status_code == status.HTTP_201_CREATED
