7b050d07e964aa0919e0c09741f11bc1c678c684
mignot <andreas.garcia@bodzify.com>
2025-03-23 23:48:11 +0100

    Add destroy method to CriteriaViewSet for criteria deletion
    - Implemented a new destroy method that handles the deletion of criteria, including reassignment of children and management of associated playlists.
    - Ensured that tracks from root criteria are transferred to a criterialess playlist before deletion.
    - Added logic to manage children criteria based on their parent relationships during the deletion process.

--- bodzify_api/view/viewset/model/criteria/CriteriaViewSet.py
+++ bodzify_api/view/viewset/model/criteria/CriteriaViewSet.py
@@ -1,10 +1,12 @@
 from django.db import transaction
 from drf_spectacular.types import OpenApiTypes  # type: ignore
 from drf_spectacular.utils import OpenApiParameter, extend_schema  # type: ignore
+from rest_framework import status  # type: ignore
 from rest_framework.response import Response  # type: ignore
 
 from bodzify_api.filtering.set.criteria.Fields import Fields as FilterFields
 from bodzify_api.model.criteria.Criteria import Criteria
+from bodzify_api.model.playlist.children.criteria.CriteriaPlaylist import CriteriaPlaylist
 from bodzify_api.serializer.model.criteria.input.post import CriteriaPostSerializer
 from bodzify_api.serializer.model.criteria.input.put import CriteriaPutSerializer
 from bodzify_api.serializer.model.criteria.output.detailed import CriteriaDetailedSerializer
@@ -29,6 +31,65 @@ class CriteriaViewSet(AppModelViewSet[Criteria]):
     @extend_schema(request=CriteriaPostSerializer, responses=CriteriaDetailedSerializer)
     def create(self, request, *args, **kwargs):
         return self._handle_post(request)
+
+    @transaction.atomic
+    @extend_schema(responses={status.HTTP_204_NO_CONTENT: None})
+    def destroy(self, request, *args, **kwargs):
+        """
+        Delete a criteria.
+
+        When deleting a criteria:
+        - If it has children and a parent, children are reassigned to the parent
+        - If it has children but no parent, children become root criteria
+        - If it's a root criteria, tracks are moved to the criterialess playlist
+        - The criteria playlist is deleted along with the criteria
+        """
+        criteria = self.get_object()
+        is_root = criteria.is_root
+
+        # Handle tracks transfer for root criteria
+        if is_root:
+            # Get the criterialess playlist for this criteria type
+            criterialess_playlist = CriteriaPlaylist.objects.filter(
+                user=criteria.user,
+                criteria=None,
+                type=criteria.type
+            ).first()
+
+            if criterialess_playlist and hasattr(criteria, 'criteria_playlist'):
+                # Get tracks from criteria and its descendants
+                criteria_playlist = criteria.criteria_playlist
+
+                # Get all lib_track_playlist_rels from the playlist
+                lib_track_rels = list(criteria_playlist.lib_track_playlist_rels.all().order_by('position'))
+
+                # Add tracks to criterialess playlist in first positions
+                from bodzify_api.model.lib_track_playlist_rel.LibTrackPlaylistRel import LibTrackPlaylistRel
+
+                for rel in lib_track_rels:
+                    # Create new relationship for criterialess playlist
+                    LibTrackPlaylistRel(
+                        user=criteria.user,
+                        playlist=criterialess_playlist,
+                        lib_track=rel.lib_track
+                    ).save()
+
+        # Handle children reassignment before deletion
+        if criteria.children.exists():
+            children = list(criteria.children.all())
+            if criteria.parent:
+                # Reassign children to grandparent
+                for child in children:
+                    child.parent = criteria.parent
+                    child.save(update_fields=['parent_id'])
+            else:
+                # Make children root criteria
+                for child in children:
+                    child.parent = None
+                    child.save(update_fields=['parent_id'])
+
+        # The criteria_playlist will be deleted automatically via CASCADE
+        return self._handle_destroy()
 
     @extend_schema(parameters=[OpenApiParameter(name=FilterFields.NAME_PUBLIC,
                                                 type=OpenApiTypes.STR,
