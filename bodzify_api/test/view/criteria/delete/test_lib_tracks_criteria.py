6fd9cdd85c68138ea2bbe65073a944c1a6710df9
mignot <andreas.garcia@bodzify.com>
2025-03-24 03:19:14 +0100

    Update lib track creation in genre deletion tests for consistency
    - Changed parameter name from `name` to `title` in `create_lib_track_with_file` method calls to align with updated method signature.
    - Enhanced assertions to verify the correct genre metadata after genre deletion for both root and non-root genres.
    - Introduced specific test filenames for lib tracks to ensure accurate metadata handling during tests.

--- bodzify_api/test/view/criteria/delete/test_lib_tracks_criteria.py
+++ bodzify_api/test/view/criteria/delete/test_lib_tracks_criteria.py
@@ -8,8 +8,8 @@ class TestCase(GenreTestCase):
     def test_delete_not_root_then_lib_tracks_related_are_now_related_to_parent(self):
         rock = self.model_fixture_factory.create_genre(name='rock')
         punk = self.model_fixture_factory.create_genre(name='criteria', parent=rock)
-        lib_track_first = self.model_fixture_factory.create_lib_track_with_file(name='lib track first', genre=punk)
-        lib_track_second = self.model_fixture_factory.create_lib_track_with_file(name='lib track second', genre=punk)
+        lib_track_first = self.model_fixture_factory.create_lib_track_with_file(title='lib track first', genre=punk)
+        lib_track_second = self.model_fixture_factory.create_lib_track_with_file(title='lib track second', genre=punk)
 
         response = self._delete_genre(uuid=punk.uuid)
 
@@ -23,8 +23,8 @@ class TestCase(GenreTestCase):
 
     def test_delete_root_then_lib_tracks_related_have_no_genre(self):
         rock = self.model_fixture_factory.create_genre(name='rock')
-        lib_track_first = self.model_fixture_factory.create_lib_track_with_file(name='lib track first', genre=rock)
-        lib_track_second = self.model_fixture_factory.create_lib_track_with_file(name='lib track second', genre=rock)
+        lib_track_first = self.model_fixture_factory.create_lib_track_with_file(title='lib track first', genre=rock)
+        lib_track_second = self.model_fixture_factory.create_lib_track_with_file(title='lib track second', genre=rock)
 
         response = self._delete_genre(uuid=rock.uuid)
         assert response.status_code == status.HTTP_204_NO_CONTENT
--- bodzify_api/test/view/criteria/delete/test_lib_tracks_metadata_genre.py
+++ bodzify_api/test/view/criteria/delete/test_lib_tracks_metadata_genre.py
@@ -1,7 +1,9 @@
 
 from rest_framework import status
 
+from bodzify_api.test.utils.lib_track.LibTrackTestFilename import LibTrackTestFilename
 from bodzify_api.test.view.criteria.GenreTestCase import GenreTestCase
+from bodzify_api.utils import audio_metadata
 from bodzify_api.utils.audio_metadata.utils.AppMetadataKey import AppMetadataKey
 
 
@@ -10,18 +12,30 @@ class TestCase(GenreTestCase):
     def test_delete_not_root_then_lib_tracks_metadata_genre_updated_to_parent(self):
         rock = self.model_fixture_factory.create_genre(name='rock')
         punk = self.model_fixture_factory.create_genre(name='criteria', parent=rock)
-        self.model_fixture_factory.create_lib_track_with_file(name='lib track first', genre=punk)
+        lib_track = self.model_fixture_factory.create_lib_track_with_file(
+            title='lib track first', genre=punk,
+            test_lib_track_filename=LibTrackTestFilename.METADATA_LONG_A_ID3V1_SMALL_MP3)
 
         response = self._delete_genre(uuid=punk.uuid)
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
-        assert self.saved_lib_track_metadata_with_raw_rating[AppMetadataKey.GENRE_NAME] == rock.name
+        metadata = audio_metadata.get_merged_app_metadata(file=lib_track.track_file.file)
+
+        assert metadata[AppMetadataKey.GENRE_NAME] == rock.name
 
     def test_delete_root_then_lib_tracks_metadata_genre_updated_to_none(self):
         rock = self.model_fixture_factory.create_genre(name='rock')
-        self.model_fixture_factory.create_lib_track_with_file(name='lib track first', genre=rock)
+        lib_track = self.model_fixture_factory.create_lib_track_with_file(
+            title='lib track first',
+            genre=rock,
+            test_lib_track_filename=LibTrackTestFilename.METADATA_LONG_A_ID3V2_SMALL_MP3)
+
+        metadata = audio_metadata.get_merged_app_metadata(file=lib_track.track_file.file)
+        assert metadata.get(AppMetadataKey.GENRE_NAME) is not None
 
         response = self._delete_genre(uuid=rock.uuid)
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
-        assert self.saved_lib_track_metadata_with_raw_rating[AppMetadataKey.GENRE_NAME] is None
+        metadata = audio_metadata.get_merged_app_metadata(file=lib_track.track_file.file)
+
+        assert metadata.get(AppMetadataKey.GENRE_NAME) is None
