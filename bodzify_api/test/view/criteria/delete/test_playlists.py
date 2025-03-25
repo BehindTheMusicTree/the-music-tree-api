3c86a85efda47ca408a902e355d580ce5bfe0a82
mignot <andreas.garcia@bodzify.com>
2025-03-25 15:12:01 +0100

    Update genre creation and assertions in playlist deletion tests for accuracy
    - Modified the genre creation logic in the test for deleting playlists to establish a parent-child relationship between genres.
    - Renamed a test method for clarity regarding the handling of direct tracks in a criterialess playlist.
    - Enhanced assertions to verify the correct order of tracks in the genreless playlist after deletion, ensuring accurate test coverage for both archived and non-archived tracks.

--- bodzify_api/test/view/criteria/delete/test_playlists.py
+++ bodzify_api/test/view/criteria/delete/test_playlists.py
@@ -18,7 +18,7 @@ class TestCase(GenreTestCase):
 
     def test_delete_with_children_then_parent_playlist_not_changed(self):
         genre_rock = self.model_fixture_factory.create_genre(name='rock')
-        genre_punk = self.model_fixture_factory.create_genre(name='punk')
+        genre_punk = self.model_fixture_factory.create_genre(name='punk', parent=genre_rock)
         genre_punk_hardcore = self.model_fixture_factory.create_genre(name='punk hardcore', parent=genre_punk)
 
         lib_track_rock_added_first = self.model_fixture_factory.create_lib_track_with_file(
@@ -58,7 +58,7 @@ class TestCase(GenreTestCase):
         assert rock_tracks_dict_by_position[5].uuid == lib_track_punk_added_second.uuid
         assert rock_tracks_dict_by_position[6].uuid == lib_track_rock_added_first.uuid
 
-    def test_delete_root_criteria_with_children_then_tracks_in_criterialess_playlist_in_first_positions(self):
+    def test_delete_root_criteria_with_children_then_direct_tracks_in_criterialess_playlist_in_first_positions(self):
         genreless_lib_track_added_first = self.model_fixture_factory.create_lib_track_with_file(
             title='genreless first', use_manager_for_genre_playlist_adding=True)
         genreless_lib_track_added_second = self.model_fixture_factory.create_lib_track_with_file(
@@ -69,20 +69,22 @@ class TestCase(GenreTestCase):
         punk_criteria = self.model_fixture_factory.create_genre(name='punk', parent=rock_criteria)
         punk_hardcore_criteria = self.model_fixture_factory.create_genre(name='punk hardcore', parent=punk_criteria)
 
-        rock_lib_track_added_first = self.model_fixture_factory.create_lib_track_with_file(
-            title='rock first', genre=rock_criteria, use_manager_for_genre_playlist_adding=True)
-        punk_lib_track_added_second = self.model_fixture_factory.create_lib_track_with_file(
-            title='punk second', genre=punk_criteria, use_manager_for_genre_playlist_adding=True)
-        punk_hardcore_lib_track_added_third = self.model_fixture_factory.create_lib_track_with_file(
+        rock_lib_track_added_third = self.model_fixture_factory.create_lib_track_with_file(
             title='rock third', genre=rock_criteria, use_manager_for_genre_playlist_adding=True)
-        indie_lib_track_added_fourth = self.model_fixture_factory.create_lib_track_with_file(
-            title='indie fourth', genre=indie_criteria, use_manager_for_genre_playlist_adding=True)
-        punk_lib_track_added_fifth = self.model_fixture_factory.create_lib_track_with_file(
-            title='punk fifth', genre=punk_criteria, use_manager_for_genre_playlist_adding=True)
-        punk_hardcore_lib_track_added_sixth = self.model_fixture_factory.create_lib_track_with_file(
-            title='punk hardcore sixth', genre=punk_hardcore_criteria, use_manager_for_genre_playlist_adding=True)
-        indie_lib_track_added_seventh = self.model_fixture_factory.create_lib_track_with_file(
-            title='indie seventh', genre=indie_criteria, use_manager_for_genre_playlist_adding=True)
+        punk_lib_track_added_fourth = self.model_fixture_factory.create_lib_track_with_file(
+            title='punk fourth', genre=punk_criteria, use_manager_for_genre_playlist_adding=True)
+        punk_hardcore_lib_track_added_fifth = self.model_fixture_factory.create_lib_track_with_file(
+            title='punk hardcore fifth', genre=punk_hardcore_criteria, use_manager_for_genre_playlist_adding=True)
+        indie_lib_track_added_sixth = self.model_fixture_factory.create_lib_track_with_file(
+            title='indie sixth', genre=indie_criteria, use_manager_for_genre_playlist_adding=True)
+        punk_lib_track_added_seventh = self.model_fixture_factory.create_lib_track_with_file(
+            title='punk seventh', genre=punk_criteria, use_manager_for_genre_playlist_adding=True)
+        punk_hardcore_lib_track_added_eighth = self.model_fixture_factory.create_lib_track_with_file(
+            title='punk hardcore eighth', genre=punk_hardcore_criteria, use_manager_for_genre_playlist_adding=True)
+        indie_lib_track_added_ninth = self.model_fixture_factory.create_lib_track_with_file(
+            title='indie ninth', genre=indie_criteria, use_manager_for_genre_playlist_adding=True)
+        rock_lib_track_added_tenth = self.model_fixture_factory.create_lib_track_with_file(
+            title='rock tenth', genre=rock_criteria, use_manager_for_genre_playlist_adding=True)
 
         response = self._delete_genre(uuid=rock_criteria.uuid)
 
@@ -91,10 +93,37 @@ class TestCase(GenreTestCase):
         genreless_playlist: Playlist = GenrePlaylist.objects.get(user=self.test_user1, criteria=None)
         lib_tracks_dict_by_position = genreless_playlist.lib_tracks_not_archived_dict_by_position
         assert len(lib_tracks_dict_by_position) == 4
-        assert lib_tracks_dict_by_position[1].uuid == indie_lib_track_added_seventh.uuid
-        assert lib_tracks_dict_by_position[2].uuid == punk_hardcore_lib_track_added_sixth.uuid
-        assert lib_tracks_dict_by_position[3].uuid == punk_lib_track_added_fifth.uuid
-        assert lib_tracks_dict_by_position[4].uuid == indie_lib_track_added_fourth.uuid
-        assert lib_tracks_dict_by_position[5].uuid == punk_hardcore_lib_track_added_third.uuid
-        assert lib_tracks_dict_by_position[6].uuid == genreless_lib_track_added_second.uuid
-        assert lib_tracks_dict_by_position[7].uuid == genreless_lib_track_added_first.uuid
+        assert lib_tracks_dict_by_position[1].uuid == rock_lib_track_added_tenth.uuid
+        assert lib_tracks_dict_by_position[2].uuid == rock_lib_track_added_third.uuid
+        assert lib_tracks_dict_by_position[3].uuid == genreless_lib_track_added_second.uuid
+        assert lib_tracks_dict_by_position[4].uuid == genreless_lib_track_added_first.uuid
+
+    def test_delete_root_criteria_with_archived_lib_tracks_then_not_archived_tracks_in_criterialess_playlist_in_first_positions_and_archived_ones_last(self):
+        genreless_lib_track_added_first = self.model_fixture_factory.create_lib_track_with_file(
+            title='genreless first', use_manager_for_genre_playlist_adding=True)
+        genreless_lib_track_added_second = self.model_fixture_factory.create_lib_track_with_file(
+            title='genreless second', use_manager_for_genre_playlist_adding=True)
+
+        rock = self.model_fixture_factory.create_genre(name='rock')
+        rock_not_archived_first = self.model_fixture_factory.create_lib_track_with_file(
+            title='rock not archived first', genre=rock, use_manager_for_genre_playlist_adding=True)
+        rock_not_archived_second = self.model_fixture_factory.create_lib_track_with_file(
+            title='rock not archived second', genre=rock, use_manager_for_genre_playlist_adding=True)
+        rock_archived_third = self.model_fixture_factory.create_lib_track_with_file(
+            title='rock archived third', genre=rock, archived=True, use_manager_for_genre_playlist_adding=True)
+        rock_archived_fourth = self.model_fixture_factory.create_lib_track_with_file(
+            title='rock archived fourth', genre=rock, archived=True, use_manager_for_genre_playlist_adding=True)
+
+        response = self._delete_genre(uuid=rock.uuid)
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+        genreless_playlist: Playlist = GenrePlaylist.objects.get(user=self.test_user1, criteria=None)
+        lib_tracks_dict_by_position = genreless_playlist.lib_tracks_not_archived_dict_by_position
+        assert len(lib_tracks_dict_by_position) == 6
+        assert lib_tracks_dict_by_position[1].uuid == rock_not_archived_second.uuid
+        assert lib_tracks_dict_by_position[2].uuid == rock_not_archived_first.uuid
+        assert lib_tracks_dict_by_position[3].uuid == genreless_lib_track_added_second.uuid
+        assert lib_tracks_dict_by_position[4].uuid == genreless_lib_track_added_first.uuid
+        assert rock_archived_fourth.uuid in [lib_tracks_dict_by_position[5].uuid, lib_tracks_dict_by_position[6].uuid]
+        assert rock_archived_third.uuid in [lib_tracks_dict_by_position[5].uuid, lib_tracks_dict_by_position[6].uuid]
