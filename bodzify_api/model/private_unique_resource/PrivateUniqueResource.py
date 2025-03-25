d70d3f9d029829174e73b9d0b2fb802796081f3a
mignot <andreas.garcia@bodzify.com>
2025-03-25 16:50:52 +0100

    Remove UUID field from PrivateUniqueResource model to streamline inheritance structure

--- bodzify_api/model/private_unique_resource/PrivateUniqueResource.py
+++ bodzify_api/model/private_unique_resource/PrivateUniqueResource.py
@@ -1,6 +1,4 @@
-import uuid
 
-from django.db import models
 
 from bodzify_api.model.private.PrivateModel import PrivateModel
 from bodzify_api.model.public_standard_resource.PublicStandardResource import PublicStandardResource
@@ -8,7 +6,6 @@ from bodzify_api.model.uuid.UuidModel import UuidModel
 
 
 class PrivateUniqueResource(PrivateModel, UuidModel, PublicStandardResource):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     class Meta:
         abstract = True
