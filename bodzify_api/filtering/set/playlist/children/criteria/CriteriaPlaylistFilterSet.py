b906873e0faa440ea173c685db885929a113ff89
mignot <andreas.garcia@bodzify.com>
2025-03-27 17:54:17 +0100

    Update CriteriaPlaylistFilterSet to define model and fields
    - Changed the Meta class of CriteriaPlaylistFilterSet to specify CriteriaPlaylist as the model.
    - Updated fields to include NAME_PUBLIC, PARENT, and date fields from PrivateUniqueResourceFilterSet, enhancing filtering capabilities.

--- bodzify_api/filtering/set/playlist/children/criteria/CriteriaPlaylistFilterSet.py
+++ bodzify_api/filtering/set/playlist/children/criteria/CriteriaPlaylistFilterSet.py
@@ -2,6 +2,7 @@
 from bodzify_api.filtering.filter.char.CriteriaNameFilter import CriteriaNameFilter
 from bodzify_api.filtering.filter.foreign_key.ForeignKeyFilter import ForeignKeyFilter
 from bodzify_api.filtering.set.private_unique_resource.PrivateUniqueResourceFilterSet import PrivateUniqueResourceFilterSet
+from bodzify_api.model.playlist.children.criteria.CriteriaPlaylist import CriteriaPlaylist
 from bodzify_api.model.playlist.children.criteria.Fields import Fields as ModelFields
 
 from .Fields import Fields
@@ -14,4 +15,5 @@ class CriteriaPlaylistFilterSet(PrivateUniqueResourceFilterSet):
     parent = ForeignKeyFilter()
 
     class Meta:
-        abstract = True
+        model = CriteriaPlaylist
+        fields = [Fields.NAME_PUBLIC, Fields.PARENT, *PrivateUniqueResourceFilterSet.get_date_fields()]
