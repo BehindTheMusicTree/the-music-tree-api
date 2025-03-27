4b6a33ccfd4537905945e6a9cc9a1047172cba69
mignot <andreas.garcia@bodzify.com>
2025-03-27 16:38:12 +0100

    Add model reference to ManualPlaylistFilterSet
    - Included ManualPlaylist model in the ManualPlaylistFilterSet to enhance filtering capabilities.
    - Updated Meta class to specify the model, ensuring proper integration with the filtering logic.

--- bodzify_api/filtering/set/playlist/children/manual/ManualPlaylistFilterSet.py
+++ bodzify_api/filtering/set/playlist/children/manual/ManualPlaylistFilterSet.py
@@ -1,6 +1,7 @@
 
 from bodzify_api.filtering.filter.char.NonEmptiableCharFilter import NonEmptiableCharFilter
 from bodzify_api.filtering.set.private_unique_resource.PrivateUniqueResourceFilterSet import PrivateUniqueResourceFilterSet
+from bodzify_api.model.playlist.children.manual.ManualPlaylist import ManualPlaylist
 
 from .Fields import Fields
 
@@ -9,4 +10,5 @@ class ManualPlaylistFilterSet(PrivateUniqueResourceFilterSet):
     name = NonEmptiableCharFilter(lookup_expr='icontains')
 
     class Meta:
+        model = ManualPlaylist
         fields = [Fields.NAME_PUBLIC, *PrivateUniqueResourceFilterSet.get_date_fields()]
