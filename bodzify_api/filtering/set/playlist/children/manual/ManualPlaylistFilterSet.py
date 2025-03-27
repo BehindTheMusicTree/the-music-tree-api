98218fb7ded7ee5c8e78e9b4469372c49470ebf4
mignot <andreas.garcia@bodzify.com>
2025-03-27 16:30:58 +0100

    Refactor playlist filter sets to use PrivateUniqueResourceFilterSet
    - Updated CriteriaPlaylistFilterSet and ManualPlaylistFilterSet to inherit from PrivateUniqueResourceFilterSet instead of AppFilterSet.
    - Enhanced field definitions to include date fields from PrivateUniqueResourceFilterSet, improving filtering capabilities for playlists.

--- bodzify_api/filtering/set/playlist/children/manual/ManualPlaylistFilterSet.py
+++ bodzify_api/filtering/set/playlist/children/manual/ManualPlaylistFilterSet.py
@@ -1,12 +1,12 @@
 
 from bodzify_api.filtering.filter.char.NonEmptiableCharFilter import NonEmptiableCharFilter
-from bodzify_api.filtering.set.AppFilterSet import AppFilterSet
+from bodzify_api.filtering.set.private_unique_resource.PrivateUniqueResourceFilterSet import PrivateUniqueResourceFilterSet
 
 from .Fields import Fields
 
 
-class ManualPlaylistFilterSet(AppFilterSet):
+class ManualPlaylistFilterSet(PrivateUniqueResourceFilterSet):
     name = NonEmptiableCharFilter(lookup_expr='icontains')
 
     class Meta:
-        fields = [Fields.NAME_PUBLIC]
+        fields = [Fields.NAME_PUBLIC, *PrivateUniqueResourceFilterSet.get_date_fields()]
