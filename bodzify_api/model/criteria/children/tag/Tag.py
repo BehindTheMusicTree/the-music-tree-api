be2f4e15e87fc89dc6fc9756749b45409065f340
mignot <andreas.garcia@bodzify.com>
2025-03-28 13:50:45 +0100

    Add genre and tag save methods to set CriteriaType
    - Implemented save methods in Genre and Tag classes to automatically assign the appropriate CriteriaType based on their respective primary keys.
    - Imported necessary CriteriaType and CriteriaTypePks for functionality enhancement.
    - Ensured that the save process maintains existing behavior while integrating new type assignment logic.

--- bodzify_api/model/criteria/children/tag/Tag.py
+++ bodzify_api/model/criteria/children/tag/Tag.py
@@ -1,5 +1,6 @@
 from bodzify_api.model.criteria.Criteria import Criteria
-
+from bodzify_api.model.criteria.type.CriteriaType import CriteriaType
+from bodzify_api.model.criteria.type.CriteriaTypePks import CriteriaTypePks
 from .TagManager import TagManager
 
 
@@ -9,3 +10,7 @@ class Tag(Criteria):
 
     class Meta:
         proxy = True
+
+    def save(self, *args, **kwargs):
+        self.type = CriteriaType.objects.get(pk=CriteriaTypePks.TAG)
+        super().save(*args, **kwargs)
