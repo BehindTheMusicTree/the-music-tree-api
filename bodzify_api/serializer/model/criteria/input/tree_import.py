22e40704c319329454ece65fde29861d61a0c87d
mignot <andreas.garcia@bodzify.com>
2025-03-29 09:06:42 +0100

    Implement custom validation for data field in CriteriaTreeImportSerializer to raise AppValidationException for empty criteria

--- bodzify_api/serializer/model/criteria/input/tree_import.py
+++ bodzify_api/serializer/model/criteria/input/tree_import.py
@@ -1,11 +1,13 @@
 from rest_framework.serializers import ListField, DictField
 
+from bodzify_api.exception.validation.app.AppValidationException import AppValidationException
+from bodzify_api.exception.validation.FieldValidationErrorCode import FieldValidationErrorCode
 from bodzify_api.serializer.AppSerializer import AppSerializer
 from bodzify_api.serializer.model.criteria.input.tree_node import CriteriaTreeNodeSerializer
 
 
 class CriteriaTreeImportSerializer(AppSerializer):
-    data: ListField = ListField(child=DictField(), allow_empty=False)
+    data: ListField = ListField(child=DictField())
 
     def to_internal_value(self, data):
         if isinstance(data, list):
@@ -14,7 +16,9 @@ class CriteriaTreeImportSerializer(AppSerializer):
 
     def validate_data(self, value):
         if not value:
-            raise ValueError("At least one criteria must be provided")
+            raise AppValidationException(field_name="data",
+                                         message="At least one criteria must be provided",
+                                         field_validation_error_code=FieldValidationErrorCode.REQUIRED)
         for node in value:
             CriteriaTreeNodeSerializer(data=node).is_valid(raise_exception=True)
         return value
