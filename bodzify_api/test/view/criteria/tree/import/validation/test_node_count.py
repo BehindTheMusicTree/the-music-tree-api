809b03082ce65f198ee703263759879387d5e525
mignot <andreas.garcia@bodzify.com>
2025-04-02 17:14:54 +0200

    Refactor test_empty_then_400_bad_request to use structured data input for improved clarity in bad request validation.

--- bodzify_api/test/view/criteria/tree/import/validation/test_node_count.py
+++ bodzify_api/test/view/criteria/tree/import/validation/test_node_count.py
@@ -16,8 +16,7 @@ class TestNodeCount(GenreTestCase):
         assert self.bad_request_result_field_errors[0]["code"] == FieldValidationErrorCode.REQUIRED
 
     def test_empty_then_400_bad_request(self):
-        data = []
-        response = self._post_genres_tree_import(data)
+        response = self._post_genres_tree_import(data={Fields.TREE: []})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert self.bad_request_result_field_errors[0]["field"] == Fields.TREE
         assert self.bad_request_result_field_errors[0]["code"] == FieldValidationErrorCode.REQUIRED
