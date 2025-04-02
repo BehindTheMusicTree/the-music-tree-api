1c2007f4b991a3648fac7779b1656a7af6bcda62
mignot <andreas.garcia@bodzify.com>
2025-04-02 08:27:52 +0200

    Update genre tree import tests to use 'data' keyword argument for consistency and correct field assertion in validation tests.

--- bodzify_api/test/view/criteria/tree/import/validation/test_name.py
+++ bodzify_api/test/view/criteria/tree/import/validation/test_name.py
@@ -2,7 +2,6 @@ from rest_framework import status
 
 from bodzify_api.exception.validation.FieldValidationErrorCode import FieldValidationErrorCode
 from bodzify_api.serializer.model.criteria.input.tree_import.Fields import Fields
-from bodzify_api.test.utils.field.body_data.type.NotNullableListBodyDataTestCase import NotNullableListBodyDataTestCase
 from bodzify_api.test.view.criteria.GenreTestCase import GenreTestCase
 
 
@@ -20,7 +19,7 @@ class TestName(GenreTestCase):
         response = self._post_genres_tree_import(data={Fields.TREE_PUBLIC: tree_data})
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-        assert self.bad_request_result_field_errors[0]["field"] == Fields.NAME_PUBLIC
+        assert self.bad_request_result_field_errors[0]["field"] == Fields.TREE_PUBLIC
         assert self.bad_request_result_field_errors[0]["code"] == FieldValidationErrorCode.FORMAT_INVALID
 
     def test_duplicate_values_then_400_bad_request(self):
