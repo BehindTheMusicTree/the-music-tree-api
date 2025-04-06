f177dbc4956dcf57cc707533ee9a336f9e789304
mignot <andreas.garcia@bodzify.com>
2025-04-06 22:23:01 +0200

    Add SpotifyArtistDetailedSerializer for detailed artist output

---
+++ bodzify_api/serializer/model/spotify/children/artist/output/detailed.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from bodzify_api.model.spotify.children.artist.SpotifyArtist import SpotifyArtist
+from bodzify_api.serializer.model.spotify.children.artist.output.Fields import Fields
+
+
+class SpotifyArtistDetailedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SpotifyArtist
+        fields = [
+            Fields.SPOTIFY_ID,
+            Fields.NAME,
+            Fields.POPULARITY,
+            Fields.SPOTIFY_LINK,
+            Fields.GENRES,
+            Fields.IMAGES,
+            Fields.CREATED_ON,
+            Fields.UPDATED_ON
+        ]
