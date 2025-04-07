cf1ad0234a21e3b011f85f64b50266d88c81d3a7
mignot <andreas.garcia@bodzify.com>
2025-04-07 16:38:06 +0200

    Refactor Spotify authentication logic in spotify_auth.py for improved readability and structure

--- bodzify_api/view/spotify_auth.py
+++ bodzify_api/view/spotify_auth.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from django.contrib.auth import login
 
 from bodzify_api.utils.spotify.oauth import SpotifyOAuthService
-from bodzify_api.model.user.SpotifyUser import SpotifyUser
+from bodzify_api.model.user.spotify.SpotifyUser import SpotifyUser
 from bodzify_api.utils.jwt import create_jwt_token
 from bodzify_api.model.user.Fields import Fields
 from bodzify_api.model.spotify.Fields import Fields as SpotifyFields
@@ -23,70 +23,69 @@ def spotify_auth(request):
     """
     Handle Spotify authentication code from frontend
     """
-    try:
-        code = request.data.get('code')
-        if not code:
-            raise AppValidationException(
-                field_name='code',
-                message='No code provided',
-                field_validation_error_code=FieldValidationErrorCode.REQUIRED
-            )
-
-        oauth_service = SpotifyOAuthService()
-
-        # Get access token
-        token_info = oauth_service.get_access_token(code)
-        access_token = token_info['access_token']
-        refresh_token = token_info['refresh_token']
-
-        # Get user info
-        user_info = oauth_service.get_user_info(access_token)
-
-        spotify_id = user_info['id']
-        email = user_info.get('email')
-        display_name = user_info.get('display_name', spotify_id)
-
-        # Create or update user
-        user, created = SpotifyUser.objects.get_or_create(
-            spotify_id=spotify_id,
-            defaults={
-                'email': email,
-                'username': display_name,
-                'spotify_access_token': access_token,
-                'spotify_refresh_token': refresh_token,
-                'spotify_token_expires_at': timezone.now() + timezone.timedelta(seconds=token_info['expires_in']),
-                'spotify_profile': user_info
-            }
+    code = request.data.get('code')
+    if not code:
+        raise AppValidationException(
+            field_name='code',
+            message='No code provided',
+            field_validation_error_code=FieldValidationErrorCode.REQUIRED
         )
 
-        if not created:
-            # Update tokens and profile for existing user
-            user.spotify_access_token = access_token
-            user.spotify_refresh_token = refresh_token
-            user.spotify_profile = user_info
-            user.spotify_token_expires_at = timezone.now() + timezone.timedelta(seconds=token_info['expires_in'])
-            user.save()
+    oauth_service = SpotifyOAuthService()
 
-        # Create JWT token
-        jwt_token = create_jwt_token(user)
+    # Get access token
+    token_info = oauth_service.get_access_token(code)
+    access_token = token_info['access_token']
+    refresh_token = token_info['refresh_token']
 
-        login(request, user)
-        return Response({
-            'accessToken': jwt_token,
-            'user': {
-                'spotify_profile': user.spotify_profile,
-                Fields.ID: user.id,
-                Fields.EMAIL: user.email,
-                SpotifyFields.SPOTIFY_ID: user.spotify_id,
-                'display_name': user.spotify_profile.get('display_name'),
-                'external_urls': user.spotify_profile.get('external_urls'),
-                'followers': user.spotify_profile.get('followers'),
-                'href': user.spotify_profile.get('href'),
-                'images': user.spotify_profile.get('images'),
-                'type': user.spotify_profile.get('type'),
-                'uri': user.spotify_profile.get('uri')
-            }
-        })
+    # Get user info
+    user_info = oauth_service.get_user_info(access_token)
+
+    spotify_id = user_info['id']
+    email = user_info.get('email')
+    display_name = user_info.get('display_name', spotify_id)
+
+    # Create or update user
+    user, created = SpotifyUser.objects.get_or_create(
+        spotify_id=spotify_id,
+        defaults={
+            'email': email,
+            'username': display_name,
+            'spotify_access_token': access_token,
+            'spotify_refresh_token': refresh_token,
+            'spotify_token_expires_at': timezone.now() + timezone.timedelta(seconds=token_info['expires_in']),
+            'spotify_profile': user_info
+        }
+    )
+
+    if not created:
+        # Update tokens and profile for existing user
+        user.spotify_access_token = access_token
+        user.spotify_refresh_token = refresh_token
+        user.spotify_profile = user_info
+        user.spotify_token_expires_at = timezone.now() + timezone.timedelta(seconds=token_info['expires_in'])
+        user.save()
+
+    # Create JWT token
+    jwt_token = create_jwt_token(user)
+
+    login(request, user)
+    return Response({
+        'accessToken': jwt_token,
+        'user': {
+            'spotify_profile': user.spotify_profile,
+            Fields.ID: user.id,
+            Fields.EMAIL: user.email,
+            SpotifyFields.SPOTIFY_ID: user.spotify_id,
+            'display_name': user.spotify_profile.get('display_name'),
+            'external_urls': user.spotify_profile.get('external_urls'),
+            'followers': user.spotify_profile.get('followers'),
+            'href': user.spotify_profile.get('href'),
+            'images': user.spotify_profile.get('images'),
+            'type': user.spotify_profile.get('type'),
+            'uri': user.spotify_profile.get('uri')
+        }
+    })
 
 
 @api_view(['GET'])
