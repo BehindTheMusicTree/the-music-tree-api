f69e573f378b9730a76f86d1db452a53b6385c42
mignot <andreas.garcia@bodzify.com>
2025-04-07 17:42:11 +0200

    Add unit tests for SpotifyArtist API methods to verify listing and retrieval functionality

---
+++ bodzify_api/test/view/spotify_artist/SpotifyArtistTestCase.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+
+from bodzify_api.model.spotify.children.artist.SpotifyArtist import SpotifyArtist
+from bodzify_api.test.utils.AppTestCase import AppTestCase
+
+
+class SpotifyArtistTestCase(AppTestCase[SpotifyArtist]):
+    model_class = SpotifyArtist
+    saved_object: SpotifyArtist
+
+    def _list_spotify_artists(self, **params):
+        return self.api_client.get(
+            path=reverse('spotify-artist-list'),
+            data=params,
+            handle_response=self._set_results
+        )
+
+    def _retrieve_spotify_artist(self, spotify_id: str):
+        return self.api_client.get(
+            path=reverse('spotify-artist-detail', kwargs={'spotify_id': spotify_id}),
+            handle_response=self._set_results
+        )
