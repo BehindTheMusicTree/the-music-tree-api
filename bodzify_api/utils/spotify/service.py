db100e9db5b08cf99e5c602435e675622b522a6f
mignot <andreas.garcia@bodzify.com>
2025-04-07 16:38:15 +0200

    Update import path for SpotifyUser model to reflect new directory structure

--- bodzify_api/utils/spotify/service.py
+++ bodzify_api/utils/spotify/service.py
@@ -11,7 +11,7 @@ from spotipy.exceptions import SpotifyException as SpotipyException
 
 from bodzify_api.exception import spotify as spotify_exception
 from bodzify_api.model.spotify.children.track.SpotifyLibTrack import SpotifyLibTrack
-from bodzify_api.model.user.SpotifyUser import SpotifyUser
+from bodzify_api.model.user.spotify.SpotifyUser import SpotifyUser
 from . import utils
 from .ApiFields import ApiFields
 
