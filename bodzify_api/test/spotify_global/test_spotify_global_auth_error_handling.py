c91939167a132ddeb17fab47e9533b1a0d8c62cb
mignot <andreas.garcia@bodzify.com>
2025-04-07 14:36:19 +0200

    Remove obsolete test file for Spotify global error handling

--- bodzify_api/test/spotify_global/test_spotify_global_auth_error_handling.py
+++ bodzify_api/test/spotify_global/test_spotify_global_auth_error_handling.py
@@ -11,9 +11,9 @@ class TestSpotifyGlobalErrorHandling(TestCase):
     def test_spotify_authentication_exception_then_401_unauthorized(self):
         exception = SpotifyAuthenticationException("Invalid Spotify credentials")
         response = ErrorResponse.handle_exception(exception)
-        response_data = json.loads(response.content)
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        response_data = json.loads(response.content)
         assert response_data['code'] == ApiErrorCodeNumeric.AUTH_INVALID_CREDENTIALS
         assert response_data['details']['message'] == "Invalid Spotify credentials"
         assert response_data['details']['code'] == 'spotify_authentication_error'
@@ -22,9 +22,9 @@ class TestSpotifyGlobalErrorHandling(TestCase):
     def test_spotify_authentication_exception_with_empty_message_then_401_unauthorized(self):
         exception = SpotifyAuthenticationException("")
         response = ErrorResponse.handle_exception(exception)
-        response_data = json.loads(response.content)
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        response_data = json.loads(response.content)
         assert response_data['code'] == ApiErrorCodeNumeric.AUTH_INVALID_CREDENTIALS
         assert response_data['details']['message'] == ""
         assert response_data['details']['code'] == 'spotify_authentication_error'
@@ -33,9 +33,9 @@ class TestSpotifyGlobalErrorHandling(TestCase):
     def test_spotify_authentication_exception_with_none_message_then_401_unauthorized(self):
         exception = SpotifyAuthenticationException(None)
         response = ErrorResponse.handle_exception(exception)
-        response_data = json.loads(response.content)
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        response_data = json.loads(response.content)
         assert response_data['code'] == ApiErrorCodeNumeric.AUTH_INVALID_CREDENTIALS
         assert response_data['details']['message'] == "None"
         assert response_data['details']['code'] == 'spotify_authentication_error'
