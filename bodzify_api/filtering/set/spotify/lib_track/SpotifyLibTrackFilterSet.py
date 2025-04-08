15d0039b2ff62d6ef3ac8a4b8445576141c9e93b
mignot <andreas.garcia@bodzify.com>
2025-04-08 10:46:11 +0200

    Add Fields and SpotifyLibTrackFilterSet for Spotify library track filtering

---
+++ bodzify_api/filtering/set/spotify/lib_track/SpotifyLibTrackFilterSet.py
@@ -0,0 +1,57 @@
+from django_filters import NumberFilter, BooleanFilter, DateTimeFilter
+from bodzify_api.filtering.filter.char.NonEmptiableCharFilter import NonEmptiableCharFilter
+from bodzify_api.filtering.filter.char.RelatedObjectCharFilter import RelatedObjectCharFilter
+from bodzify_api.filtering.set.private_unique_resource.PrivateUniqueResourceFilterSet import (
+    PrivateUniqueResourceFilterSet
+)
+from bodzify_api.model.spotify.children.track.SpotifyLibTrack import SpotifyLibTrack
+from bodzify_api.model.spotify.children.track.Fields import Fields as ModelFields
+from bodzify_api.model.spotify.children.artist.Fields import Fields as ArtistModelFields
+
+from .Fields import Fields
+
+
+class SpotifyLibTrackFilterSet(PrivateUniqueResourceFilterSet):
+    name = NonEmptiableCharFilter(field_name=ModelFields.NAME,
+                                  field_name_public=Fields.NAME_PUBLIC,
+                                  lookup_expr='icontains')
+    album_artist_name = RelatedObjectCharFilter(primary_field=ArtistModelFields.NAME,
+                                                field_name=ModelFields.SPOTIFY_ARTISTS,
+                                                field_name_public=Fields.ALBUM_ARTIST_NAME,
+                                                lookup_expr='icontains')
+    duration_sec_min = NumberFilter(method='filter_duration_sec_min')
+    duration_sec_max = NumberFilter(method='filter_duration_sec_max')
+    popularity_min = NumberFilter(field_name=ModelFields.POPULARITY, lookup_expr='gte')
+    popularity_max = NumberFilter(field_name=ModelFields.POPULARITY, lookup_expr='lte')
+    explicit = BooleanFilter(field_name=ModelFields.EXPLICIT)
+    last_synced_at = DateTimeFilter(field_name=ModelFields.LAST_SYNCED_AT)
+    last_synced_at_gt = DateTimeFilter(field_name=ModelFields.LAST_SYNCED_AT, lookup_expr='gt')
+    last_synced_at_lt = DateTimeFilter(field_name=ModelFields.LAST_SYNCED_AT, lookup_expr='lt')
+    last_synced_at_gte = DateTimeFilter(field_name=ModelFields.LAST_SYNCED_AT, lookup_expr='gte')
+    last_synced_at_lte = DateTimeFilter(field_name=ModelFields.LAST_SYNCED_AT, lookup_expr='lte')
+    is_removed = BooleanFilter(field_name=ModelFields.IS_REMOVED)
+
+    def filter_duration_sec_min(self, queryset, name, value):
+        return queryset.filter(**{f"{ModelFields.DURATION_MS}__gte": value * 1000})
+
+    def filter_duration_sec_max(self, queryset, name, value):
+        return queryset.filter(**{f"{ModelFields.DURATION_MS}__lte": value * 1000})
+
+    class Meta:
+        model = SpotifyLibTrack
+        fields = [
+            Fields.NAME_PUBLIC,
+            Fields.ALBUM_ARTIST_NAME,
+            Fields.DURATION_SEC_MIN,
+            Fields.DURATION_SEC_MAX,
+            Fields.POPULARITY_MIN,
+            Fields.POPULARITY_MAX,
+            Fields.EXPLICIT,
+            Fields.LAST_SYNCED_AT,
+            Fields.LAST_SYNCED_AT_GT,
+            Fields.LAST_SYNCED_AT_LT,
+            Fields.LAST_SYNCED_AT_GTE,
+            Fields.LAST_SYNCED_AT_LTE,
+            Fields.IS_REMOVED,
+            *PrivateUniqueResourceFilterSet.get_date_fields()
+        ]
