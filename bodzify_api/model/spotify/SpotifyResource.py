788ee6c4d8ae9cc59adbce036553443b1afb5c6e
mignot <andreas.garcia@bodzify.com>
2025-04-08 11:59:48 +0200

    Update migration timestamp and enforce non-nullable spotify_id field in SpotifyResource model.

--- bodzify_api/model/spotify/SpotifyResource.py
+++ bodzify_api/model/spotify/SpotifyResource.py
@@ -1,10 +1,9 @@
-from bodzify_api import settings
 from bodzify_api.model.field.AppCharField import AppCharField
 from bodzify_api.model.public_standard_resource.PublicStandardResource import PublicStandardResource
 
 
 class SpotifyResource(PublicStandardResource):
-    spotify_id = AppCharField(max_length=50, unique=True)
+    spotify_id = AppCharField(max_length=50, unique=True, null=False, blank=False)
 
     class Meta:
         abstract = True
