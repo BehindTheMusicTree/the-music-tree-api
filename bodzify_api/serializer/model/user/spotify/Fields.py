0080c2b24784486d076d9d421b770dcbf00f7904
mignot <andreas.garcia@bodzify.com>
2025-04-23 10:01:16 +0200

    Add Fields class to map Spotify model fields for user serialization

---
+++ bodzify_api/serializer/model/user/spotify/Fields.py
@@ -0,0 +1,13 @@
+from bodzify_api.model.user.spotify.Fields import Fields as ModelFields
+
+
+class Fields:
+    SPOTIFY_PROFILE = ModelFields.SPOTIFY_PROFILE
+    SPOTIFY_ID = ModelFields.SPOTIFY_ID
+    EMAIL = ModelFields.EMAIL
+    DISPLAY_NAME = ModelFields.DISPLAY_NAME
+    FOLLOWERS = ModelFields.FOLLOWERS
+    HREF = ModelFields.HREF
+    IMAGES = ModelFields.IMAGES
+    TYPE = ModelFields.TYPE
+    URI = ModelFields.URI
