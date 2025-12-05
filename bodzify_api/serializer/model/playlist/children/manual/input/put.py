239d5a6f19a14e2d1c8554a20eb93ea4542677ce
mignot <garcia.andreas.1991@gmail.com>
2025-12-05 17:57:19 +0100

    fix(playlist): set name field required=False in PUT serializer
    - Override name field in ManualPlaylistPutSerializer with required=False
    - Allows PutSerializer.validate() to check for NO_UPDATES before required field validation
    - Fixes test_no_field_specified_then_400_bad_request to expect NO_UPDATES instead of REQUIRED

--- bodzify_api/serializer/model/playlist/children/manual/input/put.py
+++ bodzify_api/serializer/model/playlist/children/manual/input/put.py
@@ -1,12 +1,20 @@
 
+from bodzify_api import settings
 from bodzify_api.model.playlist.children.manual.Fields import Fields as ModelFields
 from bodzify_api.model.playlist.children.manual.ManualPlaylist import ManualPlaylist
 from bodzify_api.serializer.PutSerializer import PutSerializer
+from bodzify_api.serializer.field.UniquePerUserNameField import UniquePerUserNameField
 
 from .input import ManualPlaylistInputSerializer
 
 
 class ManualPlaylistPutSerializer(ManualPlaylistInputSerializer, PutSerializer):
+    name = UniquePerUserNameField(
+        model=ManualPlaylist,
+        max_length=settings.MANUAL_PLAYLIST_NAME_LEN_MAX,
+        allow_blank=False,
+        required=False
+    )
 
     class Meta:
         model = ManualPlaylist
