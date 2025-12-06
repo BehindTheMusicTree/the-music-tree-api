327fb67d94f288f1a0cd1fb29b124bfeee100c64
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:04:34 +0100

    chore: rename app dir to api

--- bodzify_api/model/uploaded_track/file/fingerprinting/missing_cause/Fields.py
+++
@@ -1,6 +0,0 @@
-from bodzify_api.model.private_unique_resource.Fields import Fields as PrivateStandardResourceFields
-
-
-class Fields(PrivateStandardResourceFields):
-    CODE = "code"
-    MESSAGE = "message"
