069a8ee409687a9ad59ccda4fdd022ceb70a4b61
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:17:40 +0100

    chore: rename app dir to api

--- bodzify_api/serializer/model/fingerprint_missing_cause/Fields.py
+++
@@ -1,6 +0,0 @@
-from bodzify_api.model.uploaded_track.file.fingerprinting.missing_cause.Fields import Fields as ModelFields
-
-
-class Fields:
-    CODE = ModelFields.CODE
-    MESSAGE = ModelFields.MESSAGE
