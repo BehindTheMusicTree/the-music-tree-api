069a9699ae004d42c2066199ac307159f31de260
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 00:41:56 +0100

    Rename filesystem according to project structure

--- bodzify_api/model/uploaded_track_playlist_rel/Fields.py
+++
@@ -1,8 +0,0 @@
-from bodzify_api.model.private_standard_resource.Fields import Fields as PrivateStandardResourceFields
-
-
-class Fields(PrivateStandardResourceFields):
-    PLAYLIST = 'playlist'
-    UPLOADED_TRACK_INTERNAL = 'uploaded_track'
-    UPLOADED_TRACK_PUBLIC = 'uploaded_track'
-    POSITION = 'position'
