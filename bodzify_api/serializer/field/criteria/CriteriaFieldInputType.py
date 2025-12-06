0e42901ecc356c14764841c853ed37b0fbbf36ce
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:00:39 +0100

    chore: rename app dir to api

--- bodzify_api/serializer/field/criteria/CriteriaFieldInputType.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-from bodzify_api.model.criteria.Fields import Fields as CriteriaFields
-
-
-class CriteriaFieldInputType(Enum):
-    UUID = CriteriaFields.UUID
-    NAME = CriteriaFields.NAME_PUBLIC
