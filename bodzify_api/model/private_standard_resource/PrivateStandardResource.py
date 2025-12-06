d205114d832271fb00183d381fb84553f75d8ffb
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:04:38 +0100

    chore: rename app dir to api

--- bodzify_api/model/private_standard_resource/PrivateStandardResource.py
+++
@@ -1,8 +0,0 @@
-from bodzify_api.model.private.PrivateModel import PrivateModel
-from bodzify_api.model.public_standard_resource.PublicStandardResource import PublicStandardResource
-
-
-class PrivateStandardResource(PrivateModel, PublicStandardResource):
-
-    class Meta:
-        abstract = True
