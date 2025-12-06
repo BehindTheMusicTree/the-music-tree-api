3ff2868293b9f9c00972cdfe76923d6d6a45fd8e
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:03:38 +0100

    chore: rename app dir to api

--- bodzify_api/model/publique_unique_resource/PublicUniqueResource.py
+++
@@ -1,8 +0,0 @@
-from bodzify_api.model.public_standard_resource.PublicStandardResource import PublicStandardResource
-from bodzify_api.model.uuid.UuidModel import UuidModel
-
-
-class PublicUniqueResource(PublicStandardResource, UuidModel):
-
-    class Meta:
-        abstract = True
