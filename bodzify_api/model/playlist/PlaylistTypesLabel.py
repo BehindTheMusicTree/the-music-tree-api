0772d5f2b7ae3bb647a0e87dd8800d707338ad02
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:08:28 +0100

    chore: rename app dir to api

--- bodzify_api/model/playlist/PlaylistTypesLabel.py
+++
@@ -1,8 +0,0 @@
-from bodzify_api.model.criteria.type.CriteriaTypesLabel import CriteriaTypesLabel
-from bodzify_api.model.playlist.children.manual import ManualPlaylistTypeLabel
-
-
-class PlaylistTypesLabel:
-    GENRE = CriteriaTypesLabel.GENRE
-    TAG = CriteriaTypesLabel.TAG
-    MANUAL = ManualPlaylistTypeLabel.VALUE
