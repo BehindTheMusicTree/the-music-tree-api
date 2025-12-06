55f34bfdd8503aba6a3cf7ed6134758501db918a
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:13:11 +0100

    chore: rename app dir to api

--- bodzify_api/view/pagination/PaginatedResponseFields.py
+++
@@ -1,8 +0,0 @@
-class PaginatedResponseFields:
-    OVERALL_TOTAL = 'overallTotal'
-    NEXT = 'next'
-    PREVIOUS = 'previous'
-    RESULTS = 'results'
-    PAGE = 'page'
-    PAGE_SIZE = 'pageSize'
-    TOTAL_PAGES = 'totalPages'
