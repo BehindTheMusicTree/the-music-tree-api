974cad16b73df92a3e1a2baac2dc419f287628e5
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:12:47 +0100

    chore: rename app dir to api

--- bodzify_api/model/criteria/lineage_rel/Fields.py
+++
@@ -1,7 +0,0 @@
-from bodzify_api.model.private_standard_resource.Fields import Fields as PrivateRelationFields
-
-
-class Fields(PrivateRelationFields):
-    DESCENDANT = 'descendant'
-    ASCENDANT = 'ascendant'
-    DEGREE = "degree"
