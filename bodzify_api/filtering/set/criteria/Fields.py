69293bc914cb72fa3b961e7f59b9141f8658e3ed
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:01:59 +0100

    chore: rename app dir to api

--- bodzify_api/filtering/set/criteria/Fields.py
+++
@@ -1,7 +0,0 @@
-from bodzify_api.model.criteria.Criteria import Fields as ModelFields
-
-
-class Fields:
-    NAME_PUBLIC = ModelFields.NAME_PUBLIC
-    NAME_INTERNAL = ModelFields.NAME_INTERNAL
-    PARENT = ModelFields.PARENT
