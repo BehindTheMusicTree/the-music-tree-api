e9a57a1f5a7f9108dde1b160583fb5c1d8332c10
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:03:00 +0100

    chore: rename app dir to api

--- bodzify_api/model/play/Fields.py
+++
@@ -1,6 +0,0 @@
-from bodzify_api.model.ContentObjectFields import ContentObjectFields
-from bodzify_api.model.private_unique_resource.Fields import Fields as PrivateUniqueResourceFields
-
-
-class Fields(PrivateUniqueResourceFields, ContentObjectFields):
-    pass
