80727cc63bdc9946f22f9daf7347bc26be7e3f28
mignot <garcia.andreas.1991@gmail.com>
2025-12-06 21:01:50 +0100

    chore: rename app dir to api

--- bodzify_api/model/spotify_resource/SpotifyResource.py
+++
@@ -1,9 +0,0 @@
-from bodzify_api.model.field.AppCharField import AppCharField
-from bodzify_api.model.public_standard_resource.PublicStandardResource import PublicStandardResource
-
-
-class SpotifyResource(PublicStandardResource):
-    spotify_id = AppCharField(max_length=50, unique=True, null=False, blank=False, primary_key=True)
-
-    class Meta:
-        abstract = True
