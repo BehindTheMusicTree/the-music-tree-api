fab8459bf00290fa2ae6e33839019ca3a2cd305e
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 02:07:22 +0100

    chore: adapt filesystem to new project name

---
+++ api/test/integration/view/playlist/children/criteria/tag/list/test_by_parent_uuid.py
@@ -0,0 +1,61 @@
+from rest_framework import status
+
+from api.exception.validation.FieldValidationErrorCode import FieldValidationErrorCode
+from api.serializer.model.playlist.children.criteria.output.detailed import Fields as RietrieveFields
+from api.test.utils.field.filter.foreign_key.PrivateForeignKeyFilterTestCase import (
+    PrivateForeignKeyFilterTestCase
+)
+from api.test.integration.view.playlist.children.criteria.tag.TagPlaylistTestCase import TagPlaylistTestCase
+
+
+class TestCase(TagPlaylistTestCase, PrivateForeignKeyFilterTestCase):
+
+    def setUp(self, methods_names_to_implement=None):
+        return super().setUp(allow_empty_value=True, methods_names_to_implement=methods_names_to_implement)
+
+    def test_not_provided_then_results(self):
+        tag_fiesta = self.model_fixture_factory.create_tag(name="Fiesta")
+        self.model_fixture_factory.create_tag(name="Fiestaabilly")
+        self.model_fixture_factory.create_tag(name="Koko", parent=tag_fiesta)
+
+        response = self._list_tag_playlists()
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.results_overall_total == 4
+
+    def test_invalid_uuid_then_400_bad_request(self):
+        self.model_fixture_factory.create_tag(name="Fiesta")
+
+        response = self._list_tag_playlists(**{RietrieveFields.PARENT: 'invalid-uuid'})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert self.bad_request_result_field_errors[0]['field'] == RietrieveFields.PARENT
+        assert self.bad_request_result_field_errors[0][
+            'code'] == FieldValidationErrorCode.FORMAT_INVALID
+
+    def test_empty_then_results(self):
+        tag_fiesta = self.model_fixture_factory.create_tag(name="Fiesta")
+        tag_fiestaabilly = self.model_fixture_factory.create_tag(name="Fiestaabilly")
+        tag_koko = self.model_fixture_factory.create_tag(name="Koko", parent=tag_fiesta)
+
+        response = self._list_tag_playlists(**{RietrieveFields.PARENT: ''})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.results_overall_total == 3
+        result_names = [result[RietrieveFields.NAME] for result in self.results]
+        assert tag_fiesta.name in result_names
+        assert tag_fiestaabilly.name in result_names
+        assert tag_koko.name not in result_names
+
+    def test_tags_playlist_parent_corresponds_to_filter_then_return_instances(self):
+        tag_fiesta = self.model_fixture_factory.create_tag(name="Fiesta")
+        tag_fiestaabilly = self.model_fixture_factory.create_tag(name="Fiestaabilly", parent=tag_fiesta)
+        tag_punk = self.model_fixture_factory.create_tag(name="Punk", parent=tag_fiesta)
+
+        response = self._list_tag_playlists(**{RietrieveFields.PARENT: tag_fiesta.criteria_playlist.uuid})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.results_overall_total == 2
+        result_names = [result[RietrieveFields.NAME] for result in self.results]
+        assert tag_fiestaabilly.name in result_names
+        assert tag_punk.name in result_names
---
+++ api/test/integration/view/playlist/children/manual/save/fields/test_extra_field.py
@@ -0,0 +1,18 @@
+from rest_framework import status
+
+from api.exception.validation.FieldValidationErrorCode import FieldValidationErrorCode
+from api.serializer.model.criteria.input.post import Fields as PostFields
+from api.test.integration.view.playlist.children.manual.ManualPlaylistTestCase import ManualPlaylistTestCase
+
+
+class TestCase(ManualPlaylistTestCase):
+
+    def test_extra_field_then_400_bad_request(self):
+        extra_field = "extraField"
+        response = self._post_manual_playlist(**{PostFields.NAME_PUBLIC: "Rock", extra_field: "extra_value"})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert len(self.bad_request_result_field_errors) == 1
+        error = self.bad_request_result_field_errors[0]
+        assert error['field'] == extra_field
+        assert error['code'] == FieldValidationErrorCode.UNKNOWN
