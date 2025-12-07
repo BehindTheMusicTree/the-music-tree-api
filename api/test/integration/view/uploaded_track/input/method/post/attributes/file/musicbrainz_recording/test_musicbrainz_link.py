e392e9db4e2d3fc9cb8bc6fc3bef6dccd7bb34ed
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 02:09:24 +0100

    chore: adapt filesystem to new project name

---
+++ api/test/integration/view/uploaded_track/input/method/post/attributes/file/musicbrainz_recording/test_musicbrainz_link.py
@@ -0,0 +1,17 @@
+import pytest
+from rest_framework import status
+
+from api.test.utils.uploaded_track.UploadedTrackTestFilename import UploadedTrackTestFilename
+from api.test.integration.view.uploaded_track.UploadedTrackTestCase import UploadedTrackTestCase
+
+
+@pytest.mark.usefixtures("enable_audio_metadata_analysis")
+class TestCase(UploadedTrackTestCase):
+
+    def test_musicbrainz_link(self):
+        response = self._post_uploaded_track(UploadedTrackTestFilename.RECORDING_DANS_LA_LEGENDE_FLAC)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert self.saved_object.track_file.musicbrainz_recording
+        assert self.saved_object.track_file.musicbrainz_recording.musicbrainz_link == (
+            "https://musicbrainz.org/recording/2f880dca-3a46-42c4-a0a0-ecdba619d2d1"
+        )
---
+++ api/test/integration/view/uploaded_track/input/method/post/attributes/file/musicbrainz_recording/test_score.py
@@ -0,0 +1,17 @@
+import pytest
+from rest_framework import status
+
+from api.test.utils.uploaded_track.UploadedTrackTestFilename import UploadedTrackTestFilename
+from api.test.integration.view.uploaded_track.UploadedTrackTestCase import UploadedTrackTestCase
+
+
+@pytest.mark.usefixtures("enable_audio_metadata_analysis")
+class TestCase(UploadedTrackTestCase):
+
+    def test_totaleclipse_with_three_scores_then_highest(self):
+        response = self._post_uploaded_track(UploadedTrackTestFilename.RECORDING_TOTAL_ECLIPSE_3_SCORES_FLAC)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        musicbrainz_recording = self.saved_object.track_file.musicbrainz_recording
+        assert musicbrainz_recording
+        assert float(musicbrainz_recording.score) > 0.98
