73f1cd301c9fce189edd2bd3d988f546c2a54c97
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 02:09:55 +0100

    chore: adapt filesystem to new project name

---
+++ api/test/integration/view/uploaded_track/input/method/put/fields/archived/test_lib_track_mixin.py
@@ -0,0 +1,18 @@
+from rest_framework import status
+
+from api.serializer.model.uploaded_track.input.put.Fields import Fields
+from api.test.integration.view.uploaded_track.UploadedTrackTestCase import UploadedTrackTestCase
+
+
+class TestCase(UploadedTrackTestCase):
+
+    def test_archived_uploaded_track_then_all_uploaded_tracks_mixin_has_plus_1_archived_uploaded_tracks(self):
+        self.model_fixture_factory.create_uploaded_track_with_file(title="not archived 1")
+        self.model_fixture_factory.create_uploaded_track_with_file(title="archived 1", archived=True)
+        track_love = self.model_fixture_factory.create_uploaded_track_with_file(title="Love")
+
+        response = self._put_uploaded_track(uuid=track_love.uuid, **{Fields.ARCHIVED: "true"})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.test_user1.all_uploaded_tracks_mixin.uploaded_tracks_archived_count == 2
+        assert self.test_user1.all_uploaded_tracks_mixin.uploaded_tracks_not_archived_count == 1
