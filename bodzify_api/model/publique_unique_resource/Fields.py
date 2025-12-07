5025f419d508b7c7f190547e44c25d639ed8c5cb
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 01:02:05 +0100

    Add bodzify_api/model/publique_unique_resource/Fields.py

--- bodzify_api/model/publique_unique_resource/Fields.py
+++
@@ -1,6 +0,0 @@
-from bodzify_api.model.public_standard_resource.Fields import Fields as PublicRelationFields
-from bodzify_api.model.uuid.Fields import Fields as UuidFields
-
-
-class Fields(PublicRelationFields, UuidFields):
-    pass
