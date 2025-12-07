510d58fdab6132b41f222bcc178c26a46599aa50
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 02:05:11 +0100

    chore: update filesystem

---
+++ api/test/integration/view/album/delete/test_rollback.py
@@ -0,0 +1,24 @@
+from unittest.mock import patch
+
+from rest_framework import status
+
+from api.model.uploaded_track.UploadedTrack import UploadedTrack
+from api.test.integration.view.album.AlbumTestCase import AlbumTestCase
+
+
+class TestCase(AlbumTestCase):
+
+    def test_exception_then_rollback(self):
+        uploaded_track_title = "Assassin"
+        muse_artist = self.model_fixture_factory.create_artist(name="Muse")
+        black_holes_album = self.model_fixture_factory.create_album(name="Black Holes And Revelations")
+        self.model_fixture_factory.create_uploaded_track_with_file(
+            title=uploaded_track_title, artists=[muse_artist], album=black_holes_album)
+
+        with patch('api.model.artist.Artist.Artist.delete') as mock_delete:
+            exception_message = "Delete failed!"
+            mock_delete.side_effect = Exception(exception_message)
+
+            response = self._delete_album(uuid=black_holes_album.uuid)
+            assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+            assert UploadedTrack.objects.filter(user=self.test_user1, title=uploaded_track_title).exists()
---
+++ api/test/integration/view/artist/retrieve/test_lib_tracks.py
@@ -0,0 +1,43 @@
+from rest_framework import status
+
+from api.serializer.model.artist.detailed import Fields as ArtistFields
+from api.test.utils.uploaded_track.UploadedTrackTestFilename import UploadedTrackTestFilename
+from api.test.integration.view.artist.ArtistTestCase import ArtistTestCase
+from api.utils import data_transformer
+
+
+class TestCase(ArtistTestCase):
+
+    def test_duration(self):
+        artist = self.model_fixture_factory.create_artist(name="Sum 41")
+        self.model_fixture_factory.create_uploaded_track_with_file(
+            title="celine", test_uploaded_track_filename=UploadedTrackTestFilename.DURATION_277S_MP3, artists=[artist])
+        self.model_fixture_factory.create_uploaded_track_with_file(
+            title="tokyo", test_uploaded_track_filename=UploadedTrackTestFilename.DURATION_472S_WAV, artists=[artist])
+
+        response = self._retrieve_artist(artist.uuid)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.result[data_transformer.to_camel_case(ArtistFields.DURATION_IN_SEC)] == 277 + 472
+
+    def test_count(self):
+        artist = self.model_fixture_factory.create_artist(name="Sum 41")
+        self.model_fixture_factory.create_uploaded_track_with_file(title="In Too Deep", artists=[artist])
+        self.model_fixture_factory.create_uploaded_track_with_file(title="Summer", artists=[artist])
+
+        response = self._retrieve_artist(artist.uuid)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.result[data_transformer.to_camel_case(ArtistFields.UPLOADED_TRACKS_NOT_ARCHIVED_COUNT_PUBLIC)] == 2
+
+    def test_archived_count(self):
+        artist = self.model_fixture_factory.create_artist(name="Sum 41")
+        self.model_fixture_factory.create_uploaded_track_with_file(title="In Too Deep", artists=[artist])
+        self.model_fixture_factory.create_uploaded_track_with_file(title="Summer", artists=[artist], archived=True)
+        self.model_fixture_factory.create_uploaded_track_with_file(title="Summer2", artists=[artist], archived=True)
+        self.model_fixture_factory.create_uploaded_track_with_file(title="Summer3", artists=[artist], archived=True)
+
+        response = self._retrieve_artist(artist.uuid)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.result[data_transformer.to_camel_case(ArtistFields.UPLOADED_TRACKS_ARCHIVED_COUNT_PUBLIC)] == 3
