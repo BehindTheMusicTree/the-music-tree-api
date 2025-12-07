7a7a9091aa1225a50571af1e6a9d4828d50a90d0
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 02:10:28 +0100

    chore: adapt filesystem to new project name

---
+++ api/test/integration/view/uploaded_track/input/update_file_metadata/title/test_title.py
@@ -0,0 +1,9 @@
+import pytest
+
+from api.test import conftest
+from api.test.integration.view.uploaded_track.input.update_file_metadata.title.TestCase import FlacTestCase, Mp3TestCase, WavTestCase
+
+
+@pytest.fixture(params=[Mp3TestCase, WavTestCase, FlacTestCase])
+def childinstance(request, db):
+    yield from conftest.base_childinstance(request, db)
