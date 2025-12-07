d89b04694d51ad645f6e02ced3902ab6937b016d
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 01:02:40 +0100

    Add bodzify_api/serializer/model/criteria/input/tree_import/Fields.py

--- bodzify_api/serializer/model/criteria/input/tree_import/Fields.py
+++
@@ -1,7 +0,0 @@
-from ..Fields import Fields as InputFields
-
-
-class Fields:
-    TREE: str = InputFields.TREE
-    NAME_PUBLIC: str = InputFields.NAME_PUBLIC
-    CHILDREN: str = InputFields.CHILDREN
