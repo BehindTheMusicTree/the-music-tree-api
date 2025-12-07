df82582322b040f38162feeadb43944cb1292140
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 02:09:40 +0100

    chore: adapt filesystem to new project name

---
+++ api/test/integration/view/uploaded_track/input/method/post/attributes/file/source/url/test_invalid_url.py
@@ -0,0 +1,18 @@
+from rest_framework import status
+
+from api.exception.validation.FieldValidationErrorCode import FieldValidationErrorCode
+from api.test.utils.uploaded_track.UploadedTrackDownloadTestUrl import UploadedTrackDownloadTestUrl
+from api.test.integration.view.uploaded_track.UploadedTrackTestCase import UploadedTrackTestCase
+from api.serializer.model.uploaded_track.input.post.Fields import Fields
+
+
+class TestCase(UploadedTrackTestCase):
+
+    def test_invalid_url_then_400_bad_request(self):
+        response = self._post_uploaded_track_from_url(UploadedTrackDownloadTestUrl.INVALID)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert len(self.bad_request_result_field_errors) == 1
+        error = self.bad_request_result_field_errors[0]
+        assert error['field'] == Fields.TRACK_FILE_PUBLIC
+        assert error['code'] == FieldValidationErrorCode.URL_NOT_FOUND
---
+++ api/test/integration/view/uploaded_track/input/method/put/fields/test_artists.py
@@ -0,0 +1,88 @@
+from rest_framework import status
+
+from api.model.artist.Artist import Artist
+from api.serializer.model.uploaded_track.input.put.Fields import Fields as PutFields
+from api.test.utils.field.body_data.method.PutBodyDataTestCase import PutBodyDataTestCase
+from api.test.integration.view.uploaded_track.UploadedTrackTestCase import UploadedTrackTestCase
+
+
+class TestCase(UploadedTrackTestCase, PutBodyDataTestCase):
+
+    def test_not_provided_then_unchanged(self):
+        artist = self.model_fixture_factory.create_artist(name="a-ha")
+        uploaded_track = self.model_fixture_factory.create_uploaded_track_with_file(title="Love", artists=[artist])
+
+        response = self._put_uploaded_track(uploaded_track.uuid, **{PutFields.TITLE: "Lova"})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.saved_object.artists.count() == 1
+        assert self.saved_object.artists.first() == artist
+
+    def test_empty_then_none(self):
+        artist_old = self.model_fixture_factory.create_artist(name="a-ha")
+        uploaded_track = self.model_fixture_factory.create_uploaded_track_with_file(title="koko", artists=[artist_old])
+
+        response = self._put_uploaded_track(uuid=uploaded_track.uuid, **{PutFields.ARTISTS_NAMES_MULTIPART: []})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.saved_object.artists.count() == 0
+
+    def test_provided_then_update(self):
+        artist_old = self.model_fixture_factory.create_artist(name="a-ha")
+        uploaded_track = self.model_fixture_factory.create_uploaded_track_with_file(title="koko", artists=[artist_old])
+        artist_new = self.model_fixture_factory.create_artist(name="Koko")
+
+        data = {PutFields.ARTISTS_NAMES_MULTIPART: [artist_new.name]}
+        response = self._put_uploaded_track(uuid=uploaded_track.uuid, **data)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.saved_object.artists.count() == 1
+        assert self.saved_object.artists.first() == artist_new
+
+    def test_two_artists_then_update(self):
+        artist_old = self.model_fixture_factory.create_artist(name="a-ha")
+        uploaded_track = self.model_fixture_factory.create_uploaded_track_with_file(title="koko", artists=[artist_old])
+        artist_new_1 = self.model_fixture_factory.create_artist(name="Chopin")
+        artist_new_2 = self.model_fixture_factory.create_artist(name="Lopato")
+
+        data = {PutFields.ARTISTS_NAMES_MULTIPART: [artist_new_1.name, artist_new_2.name]}
+        response = self._put_uploaded_track(uuid=uploaded_track.uuid, **data)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert self.saved_object.artists.count() == 2
+        assert self.saved_object.artists.filter(name=artist_new_1.name).exists()
+        assert self.saved_object.artists.filter(name=artist_new_2.name).exists()
+        assert not self.saved_object.artists.filter(name=artist_old.name).exists()
+
+    def test_delete_old_one_because_nothing_linked_to_it(self):
+        artist_name = "a-ha"
+        artist = self.model_fixture_factory.create_artist(name=artist_name)
+        track = self.model_fixture_factory.create_uploaded_track_with_file(title="Foire", artists=[artist])
+
+        data = {PutFields.ARTISTS_NAMES_MULTIPART: ["Other artist"]}
+        response = self._put_uploaded_track(uuid=track.uuid, **data)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert not Artist.objects.filter(user=self.test_user1, name=artist_name).exists()
+
+    def test_not_delete_old_one_because_a_track_linked_to_it(self):
+        artist_name = "a-ha"
+        artist = self.model_fixture_factory.create_artist(name=artist_name)
+        track = self.model_fixture_factory.create_uploaded_track_with_file(title="Foire", artists=[artist])
+        self.model_fixture_factory.create_uploaded_track_with_file(title="Josie", artists=[artist])
+
+        response = self._put_uploaded_track(uuid=track.uuid, **{PutFields.ARTISTS_NAMES_MULTIPART: [artist_name]})
+        assert response.status_code == status.HTTP_200_OK
+        assert Artist.objects.filter(user=self.test_user1, name=artist_name).exists()
+
+    def test_not_delete_old_one_because_an_album_with_a_track_linked_to_it(self):
+        artist_name = "a-ha"
+        artist = self.model_fixture_factory.create_artist(name=artist_name)
+        track = self.model_fixture_factory.create_uploaded_track_with_file(title="Foire", artists=[artist])
+        album = self.model_fixture_factory.create_album(name="Hunting High and Low", album_artists=[artist])
+        self.model_fixture_factory.create_uploaded_track_with_file(title="Josie", album=album)
+
+        response = self._put_uploaded_track(uuid=track.uuid, **{PutFields.ARTISTS_NAMES_MULTIPART: [artist_name]})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert Artist.objects.filter(user=self.test_user1, name=artist_name).exists()
