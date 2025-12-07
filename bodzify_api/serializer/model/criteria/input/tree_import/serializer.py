be3d5ceed92fa77043f245fbd7871516af4985bb
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 01:02:41 +0100

    Add bodzify_api/serializer/model/criteria/input/tree_import/serializer.py

--- bodzify_api/serializer/model/criteria/input/tree_import/serializer.py
+++
@@ -1,9 +0,0 @@
-
-
-from bodzify_api import settings
-from bodzify_api.serializer.AppInputSerializer import AppInputSerializer
-from bodzify_api.serializer.field.TreeField import TreeField
-
-
-class CriteriaTreeImportSerializer(AppInputSerializer):
-    tree: TreeField = TreeField(allow_empty=False, max_nodes_count=settings.CRITERIA_TREE_IMPORT_MAX_TOTAL_COUNT)
