7d8fd36651355d5fafe19613439565cad97da95a
mignot <garcia.andreas.1991@gmail.com>
2025-12-07 01:01:45 +0100

    Add bodzify_api/model/field/foreign_key/PrivateManyToManyField.py

--- bodzify_api/model/field/foreign_key/PrivateManyToManyField.py
+++
@@ -1,8 +0,0 @@
-from bodzify_api.model.field.foreign_key.AppManyToManyField import AppManyToManyField
-from bodzify_api.serializer.field.foreign_key.PrivateUuidField import PrivateUuidField
-
-
-class PrivateManyToManyField(AppManyToManyField):
-    def __init__(self, to, **kwargs):
-        super().__init__(to, **kwargs)
-        self.serializer_field_class = PrivateUuidField
