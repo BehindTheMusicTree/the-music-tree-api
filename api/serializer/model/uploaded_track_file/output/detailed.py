d179a1528c4fa67f18e901a58fbb5a55bb112684
mignot <garcia.andreas.1991@gmail.com>
2025-12-09 13:13:59 +0100

    refactor: remove fingerprint fields from track file output serializer

--- api/serializer/model/uploaded_track_file/output/detailed.py
+++ api/serializer/model/uploaded_track_file/output/detailed.py
@@ -1,14 +1,13 @@
 from rest_framework import serializers
 
 from api.model.uploaded_track.file.TrackFile import TrackFile
-from api.serializer.model.fingerprint_missing_cause.detailed import FingerprintMissingCauseDetailedSerializer
 from api.serializer.model.musicbrainz.recording.detailed import MusicbrainzRecordingDetailedSerializer
+from api.serializer.model.musicbrainz.recording_missing_cause.detailed import MusicbrainzRecordingMissingCauseDetailedSerializer
 
 from .Fields import Fields
 
 
 class FileDetailedSerializer(serializers.ModelSerializer):
-    fingerprint_missing_cause = serializers.SerializerMethodField()
     musicbrainz_recording = MusicbrainzRecordingDetailedSerializer()
     musicbrainz_recording_missing_cause = serializers.SerializerMethodField()
 
@@ -19,7 +18,6 @@ class FileDetailedSerializer(serializers.ModelSerializer):
                   Fields.MD5_HAS_BEEN_CORRECTED,
                   Fields.DURATION_IN_SEC,
                   Fields.DURATION_STR_IN_HOUR_MIN_SEC,
-                  Fields.FINGERPRINT_MISSING_CAUSE,
                   Fields.SIZE_IN_BYTES,
                   Fields.SIZE_IN_KO,
                   Fields.SIZE_IN_MO,
@@ -29,12 +27,7 @@ class FileDetailedSerializer(serializers.ModelSerializer):
                   Fields.CREATED_ON,
                   Fields.UPDATED_ON]
 
-    def get_fingerprint_missing_cause(self, obj: TrackFile):
-        if obj.fingerprint_memory is None:
-            return FingerprintMissingCauseDetailedSerializer(obj.fingerprint_missing_cause).data
-        return None
-
     def get_musicbrainz_recording_missing_cause(self, obj: TrackFile):
         if obj.musicbrainz_recording is None:
-            return FingerprintMissingCauseDetailedSerializer(obj.musicbrainz_recording_missing_cause).data
+            return MusicbrainzRecordingMissingCauseDetailedSerializer(obj.musicbrainz_recording_missing_cause).data
         return None
