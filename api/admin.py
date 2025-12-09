d88079402c959a821e3dde9b5d3a55133cceb621
mignot <garcia.andreas.1991@gmail.com>
2025-12-09 13:02:25 +0100

    refactor: remove fingerprint model registrations from admin

--- api/admin.py
+++ api/admin.py
@@ -15,8 +15,6 @@ from .model.musicbrainz_resource.children.recording.missing_cause.MbRecordingMis
 from .model.playlist.children.criteria.CriteriaPlaylist import CriteriaPlaylist
 from .model.playlist.children.manual.ManualPlaylist import ManualPlaylist
 from .model.playlist.Playlist import Playlist
-from .model.uploaded_track.file.fingerprinting.missing_cause.code.FingerprintMissingCauseCode import FingerprintMissingCauseCode
-from .model.uploaded_track.file.fingerprinting.missing_cause.FingerprintMissingCause import FingerprintMissingCause
 from .model.uploaded_track.file.TrackFile import TrackFile
 from .model.uploaded_track.UploadedTrack import UploadedTrack
 from .model.user.admin.UserAdmin import UserAdmin
@@ -28,8 +26,6 @@ from .model.spotify_resource.children.artist.SpotifyArtist import SpotifyArtist
 admin.site.register(User, UserAdmin)
 admin.site.register(UploadedTrack)
 admin.site.register(TrackFile)
-admin.site.register(FingerprintMissingCause)
-admin.site.register(FingerprintMissingCauseCode)
 admin.site.register(AllUploadedTracksMixin)
 admin.site.register(Artist)
 admin.site.register(Album)
